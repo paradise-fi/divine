133b5dbb8dddcb0ebb3d2523e98b707c00f24266
me <me@mornfall.net>
2006-03-22 21:21:00 +0000

    wibble: private copy of tut in wibble::tut

--- wibble/tests/tut-main.cpp
+++ wibble/tests/tut-main.cpp
@@ -1,15 +1,19 @@
-#include <tut.h>
-#include <tut_reporter.h>
+#include <wibble/tests/tut.h>
+#include <wibble/tests/tut_reporter.h>
 #include <signal.h>
 
+namespace wibble {
 namespace tut
 {
   test_runner_singleton runner;
 }
+}
+
+using namespace wibble;
 
 void signal_to_exception(int)          
 {
-	  throw std::runtime_error("killing signal catched");
+    throw std::runtime_error("killing signal catched");
 }
 
 int main(int argc,const char* argv[])
@@ -31,18 +35,18 @@ int main(int argc,const char* argv[])
 
   // std::cout << "\nFAILURE and EXCEPTION in these tests are FAKE ;)\n\n";
 
-  tut::runner.get().set_callback(&visi);
+  wibble::tut::runner.get().set_callback(&visi);
 
   try
   {
     if( argc == 1 || (argc == 2 && std::string(argv[1]) == "regression") )
     {
-      tut::runner.get().run_tests();
+      wibble::tut::runner.get().run_tests();
     }
     else if( argc == 2 && std::string(argv[1]) == "list" )
     {
       std::cout << "registered test groups:" << std::endl;
-      tut::groupnames gl = tut::runner.get().list_groups();
+      tut::groupnames gl = wibble::tut::runner.get().list_groups();
       tut::groupnames::const_iterator i = gl.begin();
       tut::groupnames::const_iterator e = gl.end();
       while( i != e )
@@ -53,11 +57,11 @@ int main(int argc,const char* argv[])
     }
     else if( argc == 2 && std::string(argv[1]) != "regression" )
     {
-      tut::runner.get().run_tests(argv[1]);
+      wibble::tut::runner.get().run_tests(argv[1]);
     }
     else if( argc == 3 )
     {
-      tut::runner.get().run_test(argv[1],::atoi(argv[2]));
+      wibble::tut::runner.get().run_test(argv[1],::atoi(argv[2]));
     }
   }
   catch( const std::exception& ex )
