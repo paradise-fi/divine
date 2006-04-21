24f0c92556457c543f01a8bc9bc76297c9e2fdca
me <me@mornfall.net>
2006-04-21 10:57:23 +0000

    wibble: some documentation for range

--- wibble/range.h
+++ wibble/range.h
@@ -22,6 +22,8 @@ namespace wibble {
 template< typename > struct Range;
 template< typename > struct Consumer;
 
+// auxilliary class, used as Range< T >::iterator
+// basically same as Range but with post/pre-increment public
 template< typename R >
 struct RangeIterator : R {
     RangeIterator() {}
@@ -34,12 +36,18 @@ public:
     }
 };
 
+// interface to be implemented by all range implementations
+// refinement of IteratorInterface (see iterator.h)
+// see also amorph.h on how the Morph/Amorph classes are designed
 template< typename T >
 struct RangeInterface : IteratorInterface< T > {
     virtual void setToEnd() = 0;
     virtual ~RangeInterface() {}
 };
 
+// base class for range implementations... mixes into a range
+// implementation and provides all the common parts
+// if you didn't check amorph.h yet, do it *now*
 template< typename T, typename Self, typename Interface = RangeInterface< T > >
 struct RangeImpl: IteratorImpl< T, Self, Interface >
 {
@@ -65,6 +73,48 @@ protected:
     Self operator++(int) { return Base::operator++(0); }
 };
 
+// the Amorph of Ranges... if you still didn't check amorph.h, go and
+// do it... unless you don't care in which case i am not sure why you
+// are reading this anyway
+
+/*
+  Range< T > (and all its Morphs (implementations) alike) work as an
+  iterable, immutable range of items -- in a way like STL
+  const_begin(), const_end() pair of iterators. However, Range packs
+  these two in a single object, which you can then pass as a single
+  argument or return as a value. There are many Range implementations,
+  some of them use STL containers (or just a pair of iterators) as
+  backing (IteratorRange, BackedRange), some use other ranges.
+
+  The latter are slightly more interesting, since they provide an
+  "adapted" view on the underlying range(s). See FilteredRange,
+  TransformedRange, UniqueRange, CastedRange , IntersectionRange.
+
+  GeneratedRange has no "real" backing at all, but use a pair of
+  functors and "generates" (by calling those functors) the complete
+  range as it is iterated.
+
+  Example usage:
+
+  // create a range from a pair of STL iterators
+  Range< int > i = range( myIntVector.begin(), myIntVector.end() );
+  // create a range that is filtered view of another range
+  Range< int > j = filteredRange( i, predicate );
+  std::vector< int > vec;
+  // copy out items in j into vec; see also consumer.h
+  j.output( consumer( vec ) );
+  // vec now contains all items from i (and thus myIntVector) that
+  // match 'predicate'
+
+  You don't have to use Range< int > all the time, since it's fairly
+  inefficient (adding level of indirection). However in common cases
+  it is ok. In the uncommon cases you can use the specific
+  implementation type and there you won't suffer the indirection
+  penalty. You can also write generic functions that have type of
+  range as their template parameter and these will work more
+  efficiently if Morph is used directly and less efficiently when the
+  Amorph Range is used instead.
+ */
 template< typename T >
 struct Range : Amorph< Range< T >, RangeInterface< T >, 0 >,
                RangeImpl< T, Range< T > >
@@ -95,7 +145,12 @@ Range< typename R::ElementType > range( R r ) {
 
 namespace wibble {
 
-// sfinae: substitution failure is not an error
+// a simple pair of iterators -- suffers the same invalidation
+// semantics as normal STL iterators and also same problems when the
+// backing storage goes out of scope
+
+// this is what you get when using range( iterator1, iterator2 )
+// see also range()
 template< typename It >
 struct IteratorRange : public RangeImpl<
     typename std::iterator_traits< It >::value_type,
@@ -121,6 +176,12 @@ protected:
     It m_current, m_end;
 };
 
+// a slightly less simple pair of iterators, this one also holds a
+// reference to the container used, so if the container is
+// heap-allocated with GC enabled, it won't disappear
+
+// this is what you get when using range( container, ... )
+
 template< typename C >
 struct BackedRange : public RangeImpl< typename C::value_type, BackedRange< C > >
 {
@@ -145,10 +206,12 @@ protected:
     It m_begin, m_end;
 };
 
-template< typename C >
-Range< typename C::value_type > backedRange( const C &b ) {
-    return BackedRange< C >( b );
-}
+// first in the series of ranges that use another range as backing
+// this one just does static_cast to specified type on all the
+// returned elements
+
+// this is what you get when casting Range< S > to Range< T > and S is
+// static_cast-able to T
 
 template< typename T, typename Casted >
 struct CastedRange : public RangeImpl< T, CastedRange< T, Casted > >
@@ -171,22 +234,26 @@ protected:
     Range< Casted > m_casted;
 };
 
+// explicit range-cast... probably not very useful explicitly, just
+// use the casting operator of Range
 template< typename T, typename C >
 Range< T > castedRange( C r ) {
     return CastedRange< T, typename C::ElementType >( r );
 }
 
-// compat
+// old-code-compat for castedRange... slightly silly
 template< typename T, typename C >
 Range< T > upcastRange( C r ) {
     return CastedRange< T, typename C::ElementType >( r );
 }
 
+// the range-cast operator, see castedRange and CastedRange
 template< typename T> template< typename C >
 Range< T >::operator Range< C >() {
     return castedRange< C >( *this );
 }
 
+// range( iterator1, iterator2 ) -- see IteratorRange
 template< typename In >
 Range< typename In::value_type > range( In b, In e ) {
     return IteratorRange< In >( b, e );
