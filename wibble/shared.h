97579130da90595fa370632e2caec6b92b4f3284
me <me@mornfall.net>
2006-04-22 23:30:03 +0000

    wibble: add SharedVector that's by default GCable and uses GCable STL allocation

--- wibble/shared.h
+++ wibble/shared.h
@@ -2,11 +2,16 @@
 */
 
 #include <gc/gc_cpp.h>
+#include <gc/gc_allocator.h>
 
 #ifndef WIBBLE_SHARED_H
 #define WIBBLE_SHARED_H
 
 namespace wibble {
+
+template< typename T >
+struct SharedVector : gc, std::vector< T, gc_allocator< T > >
+{};
 
 template< typename T >
 T &shared() {
