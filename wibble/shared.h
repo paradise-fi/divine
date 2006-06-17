f230b3357e596a64874df5c0ab931e9d26c0e1bc
me <me@mornfall.net>
2006-06-17 12:43:31 +0000

    wibble: disable gc for now (leak mem instead) -- interferes with threads and valgrind

--- wibble/shared.h
+++ wibble/shared.h
@@ -1,8 +1,9 @@
 /** -*- C++ -*-
 */
 
-#include <gc/gc_cpp.h>
-#include <gc/gc_allocator.h>
+/* #include <gc/gc_cpp.h>
+   #include <gc/gc_allocator.h> */
+#include <vector>
 
 #ifndef WIBBLE_SHARED_H
 #define WIBBLE_SHARED_H
@@ -10,13 +11,13 @@
 namespace wibble {
 
 template< typename T >
-struct SharedVector : gc, std::vector< T, gc_allocator< T > >
-{};
+struct SharedVector : /* gc, */ std::vector< T/*, gc_allocator< T >*/ > {
+};
 
-template< typename T >
+/* template< typename T >
 T &shared() {
     return *(new (GC) T());
-}
+    } */
 
 }
 
