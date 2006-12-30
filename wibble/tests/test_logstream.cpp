e3ac965e65818c8d3a2ca17dc5356dc8d8c990d5
enrico <enrico@enricozini.org>
2006-12-30 08:19:32 +0000

    wibble: Implement the sender as a separate class, to allow having different senders.

--- wibble/tests/test_logstream.cpp
+++ wibble/tests/test_logstream.cpp
@@ -10,46 +10,83 @@
 #include <iostream>
 
 namespace wibble {
+namespace log {
 
-class LogStream : public std::streambuf
+/// Urgency of a log message
+enum Level
+{
+	DEBUG,
+	INFO,
+	UNUSUAL,
+	WARN,
+	ERR,
+	CRIT
+};
+
+struct Sender
+{
+	virtual ~Sender() {}
+	virtual void send(Level level, const std::string& msg) = 0;
+};
+
+class Streambuf : public std::streambuf
 {
 protected:
+	/// Level to use for messages whose level has not been specified
+	static const Level defaultLevel = INFO;
+	/// Line buffer with the log message we are building
 	std::string line;
-	std::string level;
+	/// Level of the next log message
+	Level level;
 
+	/// Sender used to send log messages
+	/* Note: we have to use composition instead of overloading because the
+	 * sender needs to be called in the destructor, and destructors cannot call
+	 * overridden methods */
+	Sender* sender;
+
+	/// Send the message in line with the level in level
 	void send();
 
 public:
-	LogStream();
-	virtual ~LogStream();
+	/**
+	 * @param s
+	 *   The sender to use to send log messages.  Streambuf will just use the
+	 *   pointer, but will  not take over memory maintenance
+	 */
+	Streambuf(Sender* s);
+	virtual ~Streambuf();
 
-	void setLevel(const std::string& level);
+	/// Set the level for the next message, and the next message only
+	void setLevel(const Level& level);
 
+	/// override to get data as a std::streambuf
 	int overflow(int c);
 };
 
-LogStream::LogStream() : level("warning") {}
+Streambuf::Streambuf(Sender* s) : level(defaultLevel), sender(s) {}
 
-LogStream::~LogStream()
+Streambuf::~Streambuf()
 {
 	if (!line.empty())
 		send();
 }
 
-void LogStream::send()
+void Streambuf::send()
 {
-	std::cerr << level << " -> " << line << " <-" << std::endl;
-	line.clear();
+	// Send the message
+	sender->send(level, line);
 	// Reset the level
-	level = "warning";
+	level = defaultLevel;
+	line.clear();
 }
 
-void LogStream::setLevel(const std::string& level)
+void Streambuf::setLevel(const Level& level)
 {
 	this->level = level;
 }
 
-int LogStream::overflow(int c)
+int Streambuf::overflow(int c)
 {
 	if (c == '\n')
 		send();
@@ -60,11 +97,22 @@ int LogStream::overflow(int c)
 
 std::ostream& levWarning(std::ostream &s)
 {
-	if (LogStream* ls = dynamic_cast<LogStream*>(s.rdbuf()))
-		ls->setLevel("WARNING");
+	if (Streambuf* ls = dynamic_cast<Streambuf*>(s.rdbuf()))
+		ls->setLevel(WARN);
 	return s;
 }
 
+struct TestSender : public Sender
+{
+	virtual ~TestSender() {}
+
+	virtual void send(Level level, const std::string& msg)
+	{
+		std::cerr << level << " -> " << msg << " <-" << std::endl;
+	}
+};
+
+}
 }
 
 #endif
@@ -78,14 +126,17 @@ namespace tut {
 struct logstream_shar {};
 TESTGRP( logstream );
 
-// Instantiate a LogStream and write something in it
+// Instantiate a Streambuf and write something in it
 template<> template<>
 void to::test< 1 >() {
-	wibble::LogStream ls;
+	using namespace wibble;
+
+	log::TestSender s;
+	log::Streambuf ls(&s);
 	ostream o(&ls);
 
 	o << "test" << endl;
-	o << wibble::levWarning << "test" << endl;
+	o << log::levWarning << "test" << endl;
 	o << "should eventually appear";
 }
 
