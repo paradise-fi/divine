0eacfb81f3fd57bdeca02205c33248f66784b932
enrico <enrico@enricozini.org>
2006-12-30 12:18:18 +0000

    wibble: Split the logstream prototype into proper files

---
+++ wibble/log/stream.cpp
@@ -0,0 +1,51 @@
+#include <wibble/log/stream.h>
+#include <ostream>
+
+namespace wibble {
+namespace log {
+
+Streambuf::Streambuf(Sender* s) : level(defaultLevel), sender(s) {}
+
+Streambuf::~Streambuf()
+{
+	if (!line.empty())
+		send();
+}
+
+void Streambuf::send()
+{
+	// Send the message
+	sender->send(level, line);
+	// Reset the level
+	level = defaultLevel;
+	line.clear();
+}
+
+void Streambuf::setLevel(const Level& level)
+{
+	this->level = level;
+}
+
+int Streambuf::overflow(int c)
+{
+	if (c == '\n')
+		send();
+	else
+		line += c;
+	return c;	// or EOF
+}
+
+std::ostream& operator<<(std::ostream& s, Level lev)
+{
+	if (Streambuf* ls = dynamic_cast<Streambuf*>(s.rdbuf()))
+	{
+		ls->setLevel(lev);
+		return s;
+	} else
+		return s << lev;
+}
+
+}
+}
+
+// vim:set ts=4 sw=4:
--- wibble/tests/test_logstream.cpp
+++ wibble/tests/test_logstream.cpp
@@ -1,124 +1,26 @@
+#include <wibble/log/stream.h>
 #include <wibble/config.h>
 #include <vector>
-
-#ifndef WIBBLE_LOGSTREAM_H
-#define WIBBLE_LOGSTREAM_H
-
-#include <streambuf>
-#include <string>
-
-// Dummy impl / debug headers
 #include <iostream>
 
 namespace wibble {
 namespace log {
 
-/// Urgency of a log message
-enum Level
-{
-	DEBUG,
-	INFO,
-	UNUSUAL,
-	WARN,
-	ERR,
-	CRIT
-};
-
-struct Sender
-{
-	virtual ~Sender() {}
-	/**
-	 * Log one line of text with the given level.
-	 *
-	 * Do not add a trailing newline
-	 */
-	virtual void send(Level level, const std::string& msg) = 0;
-};
-
-class Streambuf : public std::streambuf
-{
-protected:
-	/// Level to use for messages whose level has not been specified
-	static const Level defaultLevel = INFO;
-	/// Line buffer with the log message we are building
-	std::string line;
-	/// Level of the next log message
-	Level level;
-
-	/// Sender used to send log messages
-	/* Note: we have to use composition instead of overloading because the
-	 * sender needs to be called in the destructor, and destructors cannot call
-	 * overridden methods */
-	Sender* sender;
-
-	/// Send the message "line" with the level "level"
-	void send();
-
-public:
-	/**
-	 * @param s
-	 *   The sender to use to send log messages.  Streambuf will just use the
-	 *   pointer, but will  not take over memory maintenance
-	 */
-	Streambuf(Sender* s);
-	virtual ~Streambuf();
-
-	/// Set the level for the next message, and the next message only
-	void setLevel(const Level& level);
-
-	/// override to get data as a std::streambuf
-	int overflow(int c);
-};
-
-Streambuf::Streambuf(Sender* s) : level(defaultLevel), sender(s) {}
-
-Streambuf::~Streambuf()
-{
-	if (!line.empty())
-		send();
-}
-
-void Streambuf::send()
-{
-	// Send the message
-	sender->send(level, line);
-	// Reset the level
-	level = defaultLevel;
-	line.clear();
-}
-
-void Streambuf::setLevel(const Level& level)
-{
-	this->level = level;
-}
-
-int Streambuf::overflow(int c)
-{
-	if (c == '\n')
-		send();
-	else
-		line += c;
-	return c;	// or EOF
-}
-
-std::ostream& levWarning(std::ostream &s)
-{
-	if (Streambuf* ls = dynamic_cast<Streambuf*>(s.rdbuf()))
-		ls->setLevel(WARN);
-	return s;
-}
-
+// Test sender for log::Streambuf
 struct TestSender : public Sender
 {
+	// Here go the log messages
 	std::vector< std::pair<Level, std::string> > log;
 
 	virtual ~TestSender() {}
 
+	// Interface for the streambuf to send messages
 	virtual void send(Level level, const std::string& msg)
 	{
 		log.push_back(make_pair(level, msg));
 	}
 
+	// Dump all the logged messages to cerr
 	void dump()
 	{
 		for (size_t i = 0; i < log.size(); ++i)
@@ -128,8 +30,6 @@ struct TestSender : public Sender
 
 }
 }
-
-#endif
 
 using namespace std;
 
@@ -157,14 +57,17 @@ void to::test< 1 >() {
 		ensure_equals(s.log[0].second, "test");
 
 		// Send a log message with a different priority
-		o << log::levWarning << "test" << endl;
+		//o << log::lev(log::WARN) << "test" << endl;
+		o << log::WARN << "test" << endl;
 		ensure_equals(s.log.size(), 2u);
 		ensure_equals(s.log[1].first, log::WARN);
 		ensure_equals(s.log[1].second, "test");
 
+		// Ensure that log messages are only sent after a newline
 		o << "should eventually appear";
 		ensure_equals(s.log.size(), 2u);
 	}
+	// Or at streambuf destruction
 	ensure_equals(s.log.size(), 3u);
 	ensure_equals(s.log[2].first, log::INFO);
 	ensure_equals(s.log[2].second, "should eventually appear");
