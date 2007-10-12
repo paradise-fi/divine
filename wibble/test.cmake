e805d4734ba00c9999b63ce1532d19f91a1e31ac
me <me@mornfall.net>
2007-10-12 19:10:01 +0000

    wibble: cmake support for the new testing framework

---
+++ wibble/test.cmake
@@ -0,0 +1,16 @@
+macro( wibble_add_test name  )
+  add_custom_command(
+    OUTPUT "${name}-generated.cpp"
+    DEPENDS ${wibble_SOURCE_DIR}/test-genrunner.pl ${ARGN}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMAND perl ${wibble_SOURCE_DIR}/test-genrunner.pl ${ARGN} > ${CMAKE_CURRENT_BINARY_DIR}/${name}-generated.cpp
+  )
+  set_source_files_properties( ${CMAKE_CURRENT_BINARY_DIR}/${name}-generated.cpp
+    PROPERTIES GENERATED ON )
+  add_executable( ${name} ${CMAKE_CURRENT_BINARY_DIR}/${name}-generated.cpp ${wibble_SOURCE_DIR}/test.cpp )
+endmacro( wibble_add_test )
+
+macro( wibble_check_target tgt )
+  add_custom_target( check COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${tgt}
+    DEPENDS ${ARGV} )
+endmacro( wibble_check_target )
