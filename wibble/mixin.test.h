fc0cd17ad229e9fc622a63e9b7fbfa428b256fd5
me <me@mornfall.net>
2007-10-14 05:28:46 +0000

    wibble: _TestMixin -> TestMixin

--- wibble/mixin.test.h
+++ wibble/mixin.test.h
@@ -24,7 +24,7 @@ public:
 	}
 };
 
-struct _TestMixin {
+struct TestMixin {
 
 // Test Comparable mixin
     Test comparable() {
@@ -71,5 +71,3 @@ struct _TestMixin {
 };
 
 }
-
-typedef _TestMixin TestMixin;
