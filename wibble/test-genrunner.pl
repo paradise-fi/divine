b815b4e8bd92f90fc8fe94f5583cb212fa72c179
me <me@mornfall.net>
2007-10-14 05:26:39 +0000

    wibble: test-genrunner.pl - improve parser, drop support for _TestX -> TestX typedefs

--- wibble/test-genrunner.pl
+++ wibble/test-genrunner.pl
@@ -5,16 +5,34 @@ my $in, $set, %tests, %prefix;
 for $file (@ARGV) {
     $in = `cat $file`;
 
-# TODO we should push/pop on { and }
-    for (split /[;{}]/, $in) {
-        #print "parsing: $_\n";
-        if (/struct _?([tT]est_?)([A-Za-z]+)/) {
-            #push @sets, $1;
-            $set = $2;
-            $prefix{$set} = $1;
-        } elsif (/Test[ \t\n]+([a-zA-Z]+)[ \t\n]*\(/) {
-            $test{$set} = () unless ($test{$set});
-            push @{$tests{$set}}, $1;
+    $set = "not defined";
+    my $nest = 0, $depth = 0;
+    # TODO we should push/pop on { and }
+    for (split /[{]/, $in) {
+        $nest+=2; # for each item in split /}/, we decrement by 1
+                  # meaning decrement by number of }, plus one 1
+        for (split /[}]/, $_) {
+            $nest--;
+            if ($nest < $depth) {
+                $set = "not defined";
+                $depth = 0;
+            }
+            for (split /[;]/, $_) {
+                #print "parsing: $_\n";
+                if (/struct ([tT]est_?)([A-Za-z]+)/) {
+                    #push @sets, $1;
+                    $set = $2;
+                    $prefix{$set} = $1;
+                    $depth = $nest;
+                } elsif (/Test[ \t\n]+([a-zA-Z]+)[ \t\n]*\(/) {
+                    if ($set eq "not defined") {
+                        print STDERR "W: test found out of scope of a Test structure, ignoring\n";
+                    } else {
+                        $test{$set} = () unless ($test{$set});
+                        push @{$tests{$set}}, $1;
+                    }
+                }
+            }
         }
     }
 }
@@ -28,19 +46,19 @@ print "#include <wibble/test-runner.h>\n";
 for (keys %tests) {
     my $set = $_;
     for (@{$tests{$set}}) {
-        print "void run$set$_() {";
+        print "void run_${set}_$_() {";
         print " RUN( $prefix{$set}$set, $_ ); }\n";
     }
 
-    print "RunTest run${set}[] = {\n";
-    print "\t{ \"$_\", run$set$_ },\n" for (@{$tests{$set}});
+    print "RunTest run_${set}[] = {\n";
+    print "\t{ \"$_\", run_${set}_$_ },\n" for (@{$tests{$set}});
     print "};\n";
 }
 
 print "RunSuite suites[] = {\n";
 for (keys %tests) {
     my $count = scalar @{$tests{$_}};
-    print "{ \"$_\", run$_, $count },\n";
+    print "{ \"$_\", run_$_, $count },\n";
 }
 print "};\n";
 print "#include <wibble/test-main.h>\n";
