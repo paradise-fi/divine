86f682bcb1371d77771c55bcc6a57b7e12e73bc7
me <me@mornfall.net>
2007-10-14 05:27:24 +0000

    wibble: _TestAmorph -> TestAmorph, Test* -> T* to avoid name clashes

--- wibble/amorph.test.h
+++ wibble/amorph.test.h
@@ -6,69 +6,69 @@ namespace {
 
 using namespace wibble;
 
-struct TestInterface {
+struct TInterface {
     virtual int value() = 0;
 };
 
 template< typename W >
-struct TestMorph : Morph< TestMorph< W >, W, TestInterface >
+struct TMorph : Morph< TMorph< W >, W, TInterface >
 {
-    TestMorph() {}
-    TestMorph( const W &w ) : Morph< TestMorph, W, TestInterface >( w ) {}
+    TMorph() {}
+    TMorph( const W &w ) : Morph< TMorph, W, TInterface >( w ) {}
 
     virtual int value() { return this->wrapped().value(); }
 };
 
-struct Test : Amorph< Test, TestInterface >
+struct T : Amorph< T, TInterface >
 {
-    Test( const MorphInterface< TestInterface > &i )
-        : Amorph< Test, TestInterface >( i ) {}
-    Test() {}
+    T( const MorphInterface< TInterface > &i )
+        : Amorph< T, TInterface >( i ) {}
+    T() {}
 
     int value() {
         return this->implementation()->value();
     }
 };
 
-struct Test1 : VirtualBase {
+struct T1 : VirtualBase {
     virtual int value() const { return 1; }
-    bool operator<=( const Test1 &o ) const {
+    bool operator<=( const T1 &o ) const {
         return value() <= o.value();
     }
 
 };
 
-struct Test3 : Test1 {
+struct T3 : T1 {
     virtual int value() const { return 3; }
 };
 
-struct Test2 {
+struct T2 {
     int value() const { return 2; }
-    bool operator<=( const Test2 &o ) const {
+    bool operator<=( const T2 &o ) const {
         return value() <= o.value();
     }
 };
 
 struct ExtractT1Value {
     typedef int result_type;
-    typedef Test1 argument_type;
-    int operator()( const Test1 &t ) {
+    typedef T1 argument_type;
+    int operator()( const T1 &t ) {
         return t.value();
     }
 };
 
 template< typename T >
-TestMorph< T > testMorph( T t ) {
-    return TestMorph< T >( t );
+TMorph< T > testMorph( T t ) {
+    return TMorph< T >( t );
 }
 
-struct _TestAmorph {
+struct TestAmorph {
     Test basic()
     {
-        Test1 t1;
-        Test2 t2;
-        Test3 t3;
-        Test t = testMorph( t1 );
+        T1 t1;
+        T2 t2;
+        T3 t3;
+        T t = testMorph( t1 );
         assert_eq( t.value(), 1 );
         assert_eq( t.ifType( ExtractT1Value() ), Maybe< int >::Just( 1 ) );
         t = testMorph( t2 );
@@ -81,5 +81,3 @@ struct _TestAmorph {
 };
 
 }
-
-typedef _TestAmorph TestAmorph;
