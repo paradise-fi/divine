7c96653513e6a6ac83be79a57a087ee83a190c04
me <me@mornfall.net>
2008-03-10 08:33:52 +0000

    wibble: Avoid stringstream for the trivial (char *) case in fmt.

--- wibble/string.h
+++ wibble/string.h
@@ -37,8 +37,6 @@ inline std::string fmt(const T& val)
     str << val;
     return str.str();
 }
-template<> inline std::string fmt<std::string>(const std::string& val) { return val; }
-template<> inline std::string fmt<char*>(char * const & val) { return val; }
 
 /// Given a pathname, return the file name without its path
 inline std::string basename(const std::string& pathname)
