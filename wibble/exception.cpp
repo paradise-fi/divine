2e1e0771d0a41e727d41b895bf0c64d07ea60c4f
enrico <enrico@enricozini.org>
2008-04-08 16:43:21 +0000

    wibble: try to use the right strerror_r according to what's available

--- wibble/exception.cpp
+++ wibble/exception.cpp
@@ -83,6 +83,7 @@ string System::desc() const throw ()
 {
 	const int buf_size = 500;
 	char buf[buf_size];
+#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
 	if (strerror_r(m_errno, buf, buf_size))
 	{
 		buf[buf_size - 1] = 0;
@@ -90,6 +91,9 @@ string System::desc() const throw ()
 	} else {
 		return "Unable to get a description for errno value";
 	}
+#else
+	return string(strerror_r(m_errno, buf, buf_size));
+#endif
 }
 
 }
