a4eb018b4c1b29c4635717ecde23440ad974a524
Petr Rockai <me@mornfall.net>
2008-10-07 12:44:51 +0000

    Update auxilliary files from branch-1.3.

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 set -e
 rm -rf _test
 mkdir _test
-cd _test
-cmake .. -DHAVE_TUT=ON
-make
-make check
-cd ..
+(cd _test && cmake .. -DHAVE_TUT=ON || exit 1)
+(cd _test && make || exit 1)
+(cd _test && make check || exit 1)
 rm -rf _test
