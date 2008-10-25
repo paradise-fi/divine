5eb699924930e247343a6c9b60c87ffe4e6e4e2f
Petr Rockai <me@mornfall.net>
2008-10-25 06:33:56 +0000

    Add a multi-threaded stress-test for Pool.

--- divine/pool.test.h
+++ divine/pool.test.h
@@ -31,4 +31,50 @@ struct TestPool {
         p.steal( c, 32 );
         assert_eq( p.alloc( 32 ), c );
     }
+
+    struct Checker : wibble::sys::Thread
+    {
+        char padding[128];
+        divine::Pool pool;
+        std::deque< char * > ptrs;
+        int limit;
+        unsigned seedp;
+        int terminate;
+        char padding2[128];
+        
+        bool decide( int i ) {
+            int j = rand() % limit;
+            if ( i + j > limit )
+                return false;
+            return true;
+        }
+
+        void *main()
+        {
+            limit = 1024*1024;
+            for ( int i = 0; i < limit; ++i ) {
+                if ( decide( i ) || ptrs.empty() ) {
+                    ptrs.push_back( pool.alloc( 32 ) );
+                } else {
+                    pool.free( ptrs.front(), 32 );
+                    ptrs.pop_front();
+                }
+            }
+            return 0;
+        }
+
+        Checker() : terminate( 0 ) {}
+    };
+
+    Test stress() {
+        std::vector< Checker > c;
+        c.resize( 3 );
+        int j = 0;
+        for ( int j = 0; j < 5; ++j ) {
+            for ( int i = 0; i < c.size(); ++i )
+                c[ i ].start();
+            for ( int i = 0; i < c.size(); ++i )
+                c[ i ].join();
+        }
+    }
 };
