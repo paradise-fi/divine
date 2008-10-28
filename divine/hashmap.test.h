382e67a6f368189707add6576ac052ad7a5caae5
Petr Rockai <me@mornfall.net>
2008-10-28 12:14:04 +0000

    Add a sort-of stress test for hashmap.

--- divine/hashmap.test.h
+++ divine/hashmap.test.h
@@ -22,6 +22,19 @@ struct TestHashmap {
         assert( map.has( 1 ) );
         assert_eq( map.value( map.get( 1 ) ), 2 );
     }
+
+    Test stress() {
+        HashMap< int, int > map( 32, 2 );
+        for ( int i = 1; i < 32*1024; ++i ) {
+            map.insert( std::make_pair( i, 2*i ) );
+            assert( map.has( i ) );
+            assert_eq( map.value( map.get( i ) ), 2*i );
+        }
+        for ( int i = 1; i < 32*1024; ++i ) {
+            assert( map.has( i ) );
+            assert_eq( map.value( map.get( i ) ), 2*i );
+        }
+    }
 };
 
 }
