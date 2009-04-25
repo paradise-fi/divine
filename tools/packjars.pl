5bab4d878ab32db158cecaa06af2a41823c3a74b
Petr Rockai <me@mornfall.net>
2009-04-25 08:58:53 +0000

    Add option to configure the promela compiler with external JAR files.
    This enables distributions to package upstream promela compiler separately and
    only provide path to the jar files when configuring divine, using
    -DPROMELA_JARS=/path/to/jar/files. The special value "BUNDLE" will cause the
    system to produce a standalone promela compiler that does not need any external
    JAR files. Please note that unless "BUNDLE" is used, the directory
    "nips-compiler-jars" might be safely removed from source distribution as well.

--- tools/packjars.pl
+++ tools/packjars.pl
@@ -1,26 +1,39 @@
+my $path = $ARGV[0];
+shift @ARGV;
+
 my $head = `cat "$ARGV[0]"`;
 shift @ARGV;
+
+my $bundle = 0;
+$bundle = 1 if ( $path eq "BUNDLE" );
+$path = "/tmp" if ( $bundle );
 
 print $head;
 print "sub unpack_jars() {\n";
 print "    my \$out;";
 for (@ARGV) {
     my $jar = $_;
-    open JAR, "$jar";
-    $in .= $_ while (<JAR>);
+
+    if ( $bundle ) {
+        open JAR, "$jar";
+        $in .= $_ while (<JAR>);
+    }
 
     $jar =~ s,.*/(.*?),$1,;
-    push @jars, "/tmp/$jar";
-    $packed = pack("u", $in);
-    print <<_EOP;
-    open JAR, ">/tmp/$jar" or die "Failed to write '/tmp/$jar'";
-    \$out = <<'_EOF';
-${packed}_EOF
-    print JAR unpack("u", \$out);
-    close JAR;
+    push @jars, "$path/$jar";
 
+    if ( $bundle ) {
+        $packed = pack("u", $in);
+        print <<_EOP;
+        open JAR, ">/tmp/$jar" or die "Failed to write '/tmp/$jar'";
+        \$out = <<'_EOF';
+${packed}_EOF
+        print JAR unpack("u", \$out);
+        close JAR;
 _EOP
-    close JAR;
+    }
+
+    close JAR if ( $bundle );
 }
 
 print "    return join ':', (";
