f7701c38114ac21b54afc87fb7810dfea6c5661d
Petr Rockai <me@mornfall.net>
2009-09-24 10:55:42 +0000

    Make update-context.sh more resilient.

--- divine/update-context.sh
+++ divine/update-context.sh
@@ -2,6 +2,7 @@
 sha1sum="$1"
 from="$2"
 where="$3"
+empty="0000000000000000000000000000000000000000";
 
 cd $from
 
@@ -9,16 +10,21 @@ if test -e manifest; then
     manifest=`cat manifest`;
 else
     if ! test -d _darcs || ! darcs --version > /dev/null; then
-        old="0000000000000000000000000000000000000000";
-        new=$old;
+        old="na"
+        new=$empty
     else
         manifest=`darcs query manifest | grep -v _attic`;
     fi
 fi
 
 test -z "$old" && old=`cat $where`
-test -z "$new" && new=`echo "$manifest" | grep -v examples | xargs $sha1sum \
-    | cut -d' ' -f1 | $sha1sum | cut -d' ' -f1`
+if which $sha1sum; then
+    test -z "$new" && new=`echo "$manifest" | grep -v examples | xargs $sha1sum \
+        | cut -d' ' -f1 | $sha1sum | cut -d' ' -f1`
+else
+    old="na"
+    new=$empty
+fi
 
 echo $new > "$where"
 
