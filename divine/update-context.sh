8647314fbf423428fcc9fa402d93f17060567779
Petr Rockai <me@mornfall.net>
2009-10-02 14:46:16 +0000

    Fix update-context to only take "interesting" files into account.

--- divine/update-context.sh
+++ divine/update-context.sh
@@ -6,19 +6,20 @@ empty="0000000000000000000000000000000000000000";
 
 cd $from
 
+interesting='\.c$|\.cpp$|\.h$|\.cc$|\.hh$';
 if test -e manifest; then
-    manifest=`cat manifest`;
+    manifest=`cat manifest | egrep "$interesting"`;
 else
     if ! test -d _darcs || ! darcs --version > /dev/null; then
         old="na"
         new=$empty
     else
-        manifest=`darcs query manifest | grep -v _attic`;
+        manifest=`darcs query manifest | grep -v _attic | egrep "$interesting"`
     fi
 fi
 
 test -z "$old" && old=`cat $where`
-if which $sha1sum; then
+if which $sha1sum > /dev/null; then
     test -z "$new" && new=`echo "$manifest" | grep -v examples | xargs $sha1sum \
         | cut -d' ' -f1 | $sha1sum | cut -d' ' -f1`
 else
