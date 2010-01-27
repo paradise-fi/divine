43e1ffb0e67872a699327e118f6969d314c6c2d1
Petr Rockai <me@mornfall.net>
2010-01-27 21:20:41 +0000

    More elaborate test for the DVE compiler.

--- test/dvecompile.sh
+++ test/dvecompile.sh
@@ -1,6 +1,9 @@
 set -vex
 not () { "$@" && exit 1 || return 0; }
 
+rm -f peterson-naive.dve.so
+rm -f leader_election.dve.so
+
 divine compile peterson-naive.dve
 divine metrics --report peterson-naive.dve.so > report
 grep "^Finished: Yes" report
@@ -14,3 +17,10 @@ grep "^Finished: Yes" report
 grep "^Error-State-Count: 0" report
 grep "^States-Visited:" report
 grep "^States-Visited: 94062" report
+
+divine compile leader_election.dve
+divine reachability --report leader_election.dve.so > report
+grep "^Finished: Yes" report
+grep "^Error-State-Count: 0" report
+grep "^States-Visited:" report
+grep "^States-Visited: 2152" report
