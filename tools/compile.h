6fda9be2f9725d92d10e30303dfa9102e58c0dbb
Petr Rockai <me@mornfall.net>
2010-02-17 12:23:10 +0000

    Add a rudimentary support for compiling murphi models.

--- tools/compile.h
+++ tools/compile.h
@@ -33,13 +33,62 @@ struct Compile {
 #endif
     }
 
+    void compileMurphi( std::string in ) {
+        std::stringstream cmd;
+        cmd << "mu " + in;
+        std::string outfile = std::string( in, 0, in.length() - 2 ) + ".C"; // ick
+        int status = system( cmd.str().c_str() ); // ought to leave basename( in ).C around
+        if ( status != -1 && WEXITSTATUS( status ) != 0 )
+            die( "Error running the mu compiler." );
+        cmd.str( "" );
+        ofstream c( outfile.c_str(), ios_base::app );
+        c << "StartStateGenerator startgen; // FIXME" << std::endl;
+        c << "NextStateGenerator nextgen; // FIXME" << std::endl;
+        c << "extern \"C\" int get_state_size() { \n\
+                   args = new argclass( 0, NULL ); // FIXME \n\
+                   theworld.to_state(NULL); // trick : marks variables in world \n\
+                   std::cerr << \"symmetry: \" << args->symmetry_reduction.value << \", \" \n\
+                             << args->multiset_reduction.value << \", \" \n     \
+                             << args->sym_alg.mode << std::endl; \n\
+                   return sizeof( state ); \n\
+              }" << std::endl;
+        c << "extern \"C\" void get_initial_state( char *to ) {\n\
+                   startgen.Code( 0 ); \n\
+                   StateCopy( (state *)to, workingstate ); \n\
+              }" << std::endl;
+        c << "extern \"C\" int get_successor( int h, char *from, char *to ) { \n\
+                   unsigned rule = h - 1; \n\
+                   StateCopy( workingstate, (state *) from ); \n\
+                 another: \n\
+                   nextgen.SetNextEnabledRule( rule ); \n\
+                   if ( rule >= numrules ) \n\
+                          return 0; \n\
+                   nextgen.Code( rule ); \n\
+                   if ( StateCmp( workingstate, (state *) from) == 0 ) { \n\
+                          ++ rule; \n\
+                          goto another; \n\
+                   } \n\
+                   workingstate->Normalize(); \n\
+                   StateCopy( (state *)to, workingstate ); \n\
+                   return rule + 2; \n\
+        }" << std::endl;
+        c.close();
+        cmd << "g++ -Wno-write-strings -I" << getenv( "MU_INCLUDE_PATH" )
+            << " -O2 -shared -fPIC -o " << str::basename( in ) + ".so" << " " << outfile;
+        status = system( cmd.str().c_str() );
+        if ( status != -1 && WEXITSTATUS( status ) != 0 )
+            die( "Error compiling intermediate C++ code." );
+    }
+
     void main() {
         std::string input = opts.next();
         if ( access( input.c_str(), R_OK ) )
             die( "FATAL: cannot open input file " + input + " for reading" );
-        if ( str::endsWith( input, ".dve" ) ) {
+        if ( str::endsWith( input, ".dve" ) )
             compileDve( input );
-        } else {
+        else if ( str::endsWith( input, ".m" ) )
+            compileMurphi( input );
+        else {
             std::cerr << "Do not know how to compile this file type." << std::endl;
         }
     }
