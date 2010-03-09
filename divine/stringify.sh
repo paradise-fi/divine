fc8afb4a270cb0e1a423ff65098053c3e5b26cad
Petr Rockai <me@mornfall.net>
2010-03-09 10:00:05 +0000

    Fix up divine/stringify.sh.

--- divine/stringify.sh
+++ divine/stringify.sh
@@ -3,8 +3,9 @@
 set -e
 sed -e '1i \
 namespace divine { const char *'"${1}"'_str = "\\' \
-    -e 's,\\,\\\\,' \
+    -e 's,\\,\\\\,g' \
     -e 's,$,\\n\\,' \
+    -e 's,",\\",g' \
     -e 's,wibble/test.h,cassert,' \
     -e '$a \
 \"\; }' \
