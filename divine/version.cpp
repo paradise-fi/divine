bf5368862a77dc0e835c329d2569f69e473ba350
Petr Rockai <me@mornfall.net>
2010-08-08 14:45:24 +0000

    Bump version to 2.3.

--- divine/version.cpp
+++ divine/version.cpp
@@ -5,8 +5,8 @@ extern const char *DIVINE_SOURCE_SHA;
 extern const char *DIVINE_BUILD_DATE;
 
 // You need to update the version number *and* the SHA to current release one.
-#define DIVINE_VERSION "2.2"
-#define DIVINE_RELEASE_SHA "ffbb797b7a459ce22d55f5b1692bc631f48c37a0"
+#define DIVINE_VERSION "2.3"
+#define DIVINE_RELEASE_SHA "7d8e54f6f2729fc8118671ee7784686dce8b1ae9"
 
 namespace divine {
 
