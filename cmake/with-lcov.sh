8f398e9294afdf916c711e55301acd0aacc4cd41
Petr Rockai <me@mornfall.net>
2010-11-21 02:31:19 +0000

    Fix #73: rework and fix the LCOV machinery.

---
+++ cmake/with-lcov.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+LCOV() {
+       "${LCOV_BIN}" -d "${LCOV_IN}" "$@"
+}
+
+set -ex
+
+mkdir -p "${LCOV_OUT}"
+
+test -e "${LCOV_OUT}/base.info" || {
+    LCOV --zerocounters
+    LCOV --capture --initial -o "${LCOV_OUT}/base.info"
+}
+
+eval "$@"
+
+for i in `seq 1 32`; do
+    test -f "${LCOV_OUT}/collect-$i.info" && continue
+    LCOV --quiet -b "${LCOV_IN}" -c -o "${LCOV_OUT}/collect-$i.info"
+    break
+done
+LCOV --zerocounters
