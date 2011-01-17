6b947f49c9c3f15ba3ae967f18f42e2a4f2dce62
Petr Rockai <me@mornfall.net>
2011-01-17 10:56:55 +0000

    Add an example of a simulink-like model implemented in C++.

---
+++ examples/circuit.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <cstdlib>
+#include "circuit.h"
+
+#include "../divine/generator/custom-api.h"
+
+// An example circuit.
+
+struct Example1 {
+    Input< int > input1, input2, input3, input4;
+    Gain< int > gain;
+    Sum< int > sum;
+    Product< int > product;
+    MinMax< int > min;
+
+    Output< int > output1, output2;
+
+    Example1() : gain( 2 ), min( MinMax< int > ::Min ) {
+        connect( input1, sum.in( 0 ) );
+        connect( input2, sum.in( 1 ) );
+        connect( sum, output1 );
+
+        connect( input3, gain.in );
+
+        connect( sum, min.a );
+        connect( gain, min.b );
+
+        connect( min, product.a );
+        connect( input4, product.b );
+
+        connect( product, output2 );
+    }
+
+};
+
+template< typename T >
+struct Flip : Value< T > {
+    In< T > a, b;
+
+    T get() {
+        return ( a->get() < b->get() ) ? a->get() : 0;
+    }
+};
+
+struct Example2 {
+    Input< int > a, limit;
+    Delay< int > d1, d2;
+    Sum< int > add;
+    Flip< int > min;
+
+    Example2() {
+        connect( a, d1.in );
+        connect( d1, add.in( 0 ) );
+        connect( d2, add.in( 1 ) );
+        connect( add, min.a );
+        connect( limit, min.b );
+        connect( min, d2.in );
+    }
+
+};
+
+void example1()
+{
+    Example1 ex;
+    Input< int > in( 2 );
+    connect( in, ex.input1 ); // = 2;
+    ex.input2 = 5;
+    ex.input3 = 8;
+    ex.input4 = 2;
+
+    std::cout << ex.output1.get() << std::endl;
+    std::cout << ex.output2.get() << std::endl;
+}
+
+/* int main() {
+    example1();
+    example2();
+} */
+
+static inline char *make( CustomSetup *setup, char **to )
+{
+    int total = setup->state_size + setup->slack;
+    *to = (char *) pool_allocate_blob( setup->cpool, total );
+    return ((*to) + setup->slack + 4); // FIXME; 4 = BlobHeader size
+}
+
+System &system( CustomSetup *setup )
+{
+    return *(System *)setup->custom;
+}
+
+extern "C" void get_initial( CustomSetup *setup, char **to )
+{
+    char *v = make( setup, to );
+    system( setup ).write( v );
+}
+
+extern "C" int get_successor( CustomSetup *setup, int handle, char *from, char **to )
+{
+    if ( handle > 1 )
+        return 0;
+
+    char *in = from + setup->slack + 4;
+    system( setup ).read( in );
+    char *v = make( setup, to );
+    system( setup ).tick();
+    system( setup ).write( v );
+    return 2;
+}
+
+extern "C" char *show_node( CustomSetup *setup, char *from, int )
+{
+    char *in = from + setup->slack + 4;
+    system( setup ).read( in );
+    std::string s = system( setup ).print();
+    char *ret = (char *) malloc( s.length() + 1 );
+    std::copy( s.begin(), s.end(), ret );
+    ret[ s.length() ] = 0;
+    return ret;
+}
+
+extern "C" void setup( CustomSetup *setup )
+{
+    Example2 *ex = new Example2();
+
+    ex->a = 5;
+    ex->limit = 30;
+
+    System *sys = new System();
+    sys->add( ex->d1 ).add( ex->d2 );
+
+    setup->state_size = sys->size();
+    setup->custom = sys;
+}
---
+++ examples/circuit.h
@@ -0,0 +1,224 @@
+// -*- C++ -*-
+#include <cstdarg>
+#include <vector>
+#include <sstream>
+
+struct Serialise {
+    virtual int size() = 0;
+    virtual void write( char *where ) = 0;
+    virtual std::string print() { return std::string(); }
+};
+
+// A building element of a circuit that provides an output value.
+template< typename T >
+struct Value : virtual Serialise {
+    virtual T get() = 0;
+
+    virtual int size() { return sizeof( T ); }
+    virtual void write( char *where ) {
+        *(T *)where = get();
+    }
+
+    std::string print() {
+        std::stringstream s;
+        s << get();
+        return s.str();
+    }
+};
+
+// An input *port*, for use within building blocks.
+template< typename T >
+struct In {
+    Value< T > *p;
+    Value< T > *operator->() { return p; }
+    In() { p = 0; }
+};
+
+template< typename T >
+struct InSet {
+    typedef std::vector< In< T > > Set;
+    typedef typename Set::iterator iterator;
+    Set set;
+
+    In< T > &operator()( int idx ) {
+        if ( set.size() <= idx )
+            set.resize( idx + 1 );
+        return set[ idx ];
+    }
+
+    iterator begin() { return set.begin(); }
+    iterator end() { return set.end(); }
+};
+
+// A building element of a circuit that needs to act upon the clock signal.
+struct Clock {
+    virtual void tick() = 0;
+};
+
+// A stateful building element: maintains (serialisable) state and reacts to
+// clock ticks.
+struct Stateful : virtual Clock, virtual Serialise {
+    virtual void read( char *from ) = 0;
+};
+
+// Implementation of the delay block. This is an example of a stateful element.
+template< typename T >
+struct Delay : virtual Value< T >, virtual Clock, virtual Stateful {
+    In< T > in;
+    T last;
+    virtual T get() { return last; }
+    void tick() { last = in->get(); }
+    void read( char *from ) {
+        last = *(T *)from;
+    }
+    Delay() : last() {}
+};
+
+// An input *block* (not a port).
+template< typename T >
+struct Input : virtual Value< T >, virtual In< T > {
+    T value;
+    virtual T get() { return value; }
+    Input() : value() {}
+    Input( T t ) : value( t ) {}
+    Input< T > &operator=( T t ) { value = t; return *this; }
+};
+
+// A simple output *block*.
+template< typename T >
+struct Output : Value< T > {
+    Value< T > *from;
+    virtual T get() { return from->get(); }
+};
+
+// A type-cast block.
+template< typename From, typename To >
+struct Cast : Value< To > {
+    In< From > in;
+
+    virtual To get() {
+        return static_cast< To >( in->get() );
+    }
+};
+
+// Connect a value-providing block to an input port.
+template< typename T >
+void connect( Value< T > &from, In< T > &to )
+{
+    to.p = &from;
+}
+
+template< typename T >
+void connect( Value< T > &from, Output< T > &to )
+{
+    to.from = &from;
+}
+
+// This class wraps the complete system and provides a single synchronised
+// clock for all the stateful elements.
+struct System : Clock
+{
+    typedef std::vector< Stateful * > Elements;
+    Elements elements;
+    virtual void tick() {
+        for ( Elements::iterator i = elements.begin(); i != elements.end(); ++i )
+            (*i)->tick();
+    }
+
+    System &add( Stateful &e ) {
+        elements.push_back( &e );
+        return *this;
+    }
+
+    int size() {
+        int s = 0;
+        for ( Elements::iterator i = elements.begin(); i != elements.end(); ++i )
+            s += (*i)->size();
+        return s;
+    }
+
+    void write( char *where ) {
+        for ( Elements::iterator i = elements.begin(); i != elements.end(); ++i ) {
+            (*i)->write( where );
+            where += (*i)->size();
+        }
+    }
+
+    void read( char *from ) {
+        for ( Elements::iterator i = elements.begin(); i != elements.end(); ++i ) {
+            (*i)->read( from );
+            from += (*i)->size();
+        }
+    }
+
+    std::string print() {
+        std::string s = "[";
+        for ( Elements::iterator i = elements.begin(); i != elements.end(); ++i ) {
+            s += (*i)->print();
+            if ( i + 1 != elements.end() )
+                s += ", ";
+        }
+        s += "]";
+        return s;
+    }
+};
+
+// ------------ example block implementations -------------
+
+template< typename T >
+struct Sum : Value< T > {
+    InSet< T > in;
+
+    virtual T get() {
+        T acc = 0;
+        for ( typename InSet< T >::iterator i = in.begin(); i != in.end(); ++i )
+            acc += (*i)->get();
+        return acc;
+    }
+};
+
+template< typename T >
+struct Product : Value< T > {
+    In< T > a, b;
+
+    virtual T get() {
+        return a->get() * b->get();
+    }
+};
+
+template< typename T >
+struct Min : Value< T > {
+    In< T > a, b;
+
+    virtual T get() {
+        return std::min( a->get(), b->get() );
+    }
+};
+
+template< typename T >
+struct Gain : Value< T > {
+    In< T > in;
+    T value;
+
+    virtual T get() {
+        return value * in->get();
+    }
+
+    Gain( T v ) : value( v ) {}
+};
+
+template< typename T >
+struct MinMax : Value< T > {
+    In< T > a, b;
+    enum Function { Min, Max };
+    Function f;
+
+    MinMax( Function _f ) : f( _f ) {}
+    T get() {
+        switch (f) {
+            case Min: return std::min( a->get(), b->get() );
+            case Max: return std::max( a->get(), b->get() );
+        }
+    }
+};
+
