00ee7dd8f2ccb02a8ce19390f94cf5f203340fec
Petr Rockai <me@mornfall.net>
2011-11-12 15:04:55 +0000

    LLVM: Add a convenience header for C/C++ verification, divine-llvm.h.
    This provides correct prototypes for the builtins provided by our LLVM
    interpreter and a partial implementation of the pthreads API in terms of these
    builtins.

---
+++ examples/llvm/divine-llvm.h
@@ -0,0 +1,89 @@
+#ifdef DIVINE
+
+#ifndef __cplusplus__
+#define NULL 0L
+typedef enum { true, false } bool; // well...
+#endif
+
+#ifdef NO_MALLOC_FAILURE
+#define malloc malloc_guaranteed
+#endif
+
+#ifdef __cplusplus__
+extern "C" {
+#endif
+
+/* Prototypes for DiVinE-provided builtins. */
+void *malloc( unsigned );
+void *malloc_guaranteed( unsigned );
+void free( void * );
+int thread_create();
+void thread_stop() __attribute__((noreturn));
+void assert( int );
+void trace( const char *, ... );
+
+#ifdef __cplusplus__
+}
+
+// TODO: global new & delete
+#endif
+
+typedef int pthread_t;
+typedef void pthread_attr_t;
+
+typedef struct {
+    void *result;
+    volatile bool done;
+} __Tid;
+
+static __Tid __tid[32]; // TODO: externalize this so it is shared by all modules
+static int __tid_last = 0;
+
+static int pthread_create(pthread_t *ptid,
+                          const pthread_attr_t *attr, /* TODO? */
+                          void *(*entry)(void *), void *arg)
+{
+    trace("Creating thread %d...", __tid_last);
+    if (__tid_last == 32) {
+        trace("FATAL: Thread capacity exceeded.");
+        return 1;
+    }
+
+    int id = thread_create();
+    if ( id == 0 ) { // this will be the parent
+        *ptid = __tid_last++;
+        return 0;
+    } else {
+        trace("Thread %d created.", __tid_last);
+        __tid[__tid_last].done = false;
+        __tid[__tid_last].result = entry(arg);
+        __tid[__tid_last].done = true;
+        thread_stop(); /* die */
+    }
+}
+
+int pthread_join(pthread_t *ptid, void **x) {
+    while ( !__tid[*ptid].done ) ;
+    *x = __tid[*ptid].result;
+    return 0;
+}
+
+#else // (NOT DIVINE)
+#include <pthread.h>
+#include <stdarg.h>
+#include "assert.h"
+
+/* compatibility for native execution */
+
+void trace(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+}
+
+void *malloc_guaranteed( unsigned x ) {
+    return malloc( x );
+}
+
+#endif
