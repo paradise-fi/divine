a70ab305f67f54298dfa07e734eaa36bcffdf701
Petr Rockai <me@mornfall.net>
2011-11-26 11:43:17 +0000

    Move some of the LTL -> Buchi code from tools/ to divine/.

---
+++ divine/buchi.h
@@ -0,0 +1,22 @@
+// -*- C++ -*- (c) 2011 Petr Rockai <me@mornfall.net>
+
+#include <divine/ltl2ba/ltl.hh>
+#include <divine/ltl2ba/alt_graph.hh>
+#include <divine/ltl2ba/formul.hh>
+#include <divine/ltl2ba/support_dve.hh>
+#include <divine/ltl2ba/DBA_graph.hh>
+
+#include <wibble/test.h> // for assert
+
+#ifndef DIVINE_BUCHI_H
+#define DIVINE_BUCHI_H
+
+namespace divine {
+
+void copy_graph(const ALT_graph_t& aG, BA_opt_graph_t& oG);
+int ltl2buchi( int argc, char **argv );
+BA_opt_graph_t buchi( std::string ltl, bool probabilistic = false );
+
+}
+
+#endif
--- tools/combine.h
+++ tools/combine.h
@@ -2,18 +2,13 @@
 #include <iostream>
 #include <sstream>
 #include <divine/version.h>
+#include <divine/buchi.h>
 #include <wibble/commandline/parser.h>
 #include <wibble/string.h>
 #include <wibble/sys/fs.h>
 #include <wibble/regexp.h>
 #include <wibble/sys/pipe.h>
 #include <wibble/sys/exec.h>
-
-#include <divine/ltl2ba/ltl.hh>
-#include <divine/ltl2ba/alt_graph.hh>
-#include <divine/ltl2ba/formul.hh>
-#include <divine/ltl2ba/support_dve.hh>
-#include <divine/ltl2ba/DBA_graph.hh>
 
 #include "compile.h"
 
@@ -22,7 +17,6 @@
 
 extern const char *combine_m4;
 
-int ltl2buchi( int argc, char **argv );
 #ifdef LTL2DSTAR
 int main_ltl2dstar(int argc, const char **argv, std::istream& in, std::ostream& out);
 #endif
@@ -31,41 +25,6 @@ using namespace wibble;
 using namespace commandline;
 using namespace sys;
 using namespace divine;
-
-// paste from ltl2ba.cc
-static void copy_graph(const ALT_graph_t& aG, BA_opt_graph_t& oG)
-{
-	std::list<ALT_ba_node_t*> node_list, init_nodes, accept_nodes;
-	std::list<ALT_ba_node_t*>::const_iterator n_b, n_e, n_i;
-	std::list<ALT_ba_trans_t>::const_iterator t_b, t_e, t_i;
-	KS_BA_node_t *p_N;
-
-	node_list = aG.get_all_BA_nodes();
-	init_nodes = aG.get_init_BA_nodes();
-	accept_nodes = aG.get_accept_BA_nodes();
-
-	oG.clear();
-
-	n_b = node_list.begin(); n_e = node_list.end();
-	for (n_i = n_b; n_i != n_e; n_i++) {
-		p_N = oG.add_node((*n_i)->name);
-		t_b = (*n_i)->adj.begin(); t_e = (*n_i)->adj.end();
-		for (t_i = t_b; t_i != t_e; t_i++) {
-			oG.add_trans(p_N, t_i->target->name,
-				t_i->t_label);
-		}
-	}
-
-	n_b = init_nodes.begin(); n_e = init_nodes.end();
-	for (n_i = n_b; n_i != n_e; n_i++) {
-		oG.set_initial((*n_i)->name);
-	}
-
-	n_b = accept_nodes.begin(); n_e = accept_nodes.end();
-	for (n_i = n_b; n_i != n_e; n_i++) {
-		oG.set_accept((*n_i)->name);
-	}
-}
 
 std::string graph_to_cpp(const BA_opt_graph_t &g);
 
@@ -160,42 +119,6 @@ struct Combine {
         return str.str();
     }
 
-    BA_opt_graph_t buchi( std::string ltl )
-    {
-        std::ostringstream str;
-        LTL_parse_t L;
-        LTL_formul_t F;
-        ALT_graph_t G;
-        BA_opt_graph_t oG, oG1;
-        L.nacti( ltl );
-        if ( !L.syntax_check( F ) )
-            std::cerr << "Error: Syntax error in LTL formula." << std::endl;
-        F = F.negace();
-        G.create_graph( F );
-        G.transform_vwaa();
-        G.simplify_GBA();
-        G.degeneralize();
-        G.simplify_BA();
-        copy_graph(G, oG);
-        oG.to_one_initial();
-        oG.optimize(oG1, 6);
-
-        if ( oG1.is_empty() )
-        {
-            // XXX warning?
-            oG1.clear();
-            oG1.set_initial( 1 );
-        }
-
-        if ( probabilistic && !oG1.is_semideterministic() ) {
-            DBA_graph_t DG;
-            DG.semideterminization( oG1 );
-            oG1 = DG.transform();
-        }
-
-        return oG1;
-    }
-
 #ifdef LTL2DSTAR
     /// Translates ltl formula to automaton specified by acceptanceConditionType using external translator
     std::string ltl2dstarTranslation( const std::string& acceptanceConditionType, std::string ltl ) {
@@ -262,7 +185,7 @@ struct Combine {
         if ( !o_condition->boolValue() || o_condition->stringValue() == "NBA" )
 #endif
         {
-            divine::output( buchi( ltl ), automaton_str );
+            divine::output( buchi( ltl, probabilistic ), automaton_str );
             automaton = automaton_str.str();
         }
 #ifdef LTL2DSTAR
@@ -291,7 +214,7 @@ struct Combine {
         f << divine::generator_custom_api_h_str << std::endl;
         f << divine::blob_h_str << std::endl;
         f << "using namespace divine;" << std::endl;
-        f << graph_to_cpp( buchi( ltl ) );
+        f << graph_to_cpp( buchi( ltl, probabilistic ) );
         f.close();
         Compile::gplusplus( filename + " " + input, outFile( id ) );
     }
