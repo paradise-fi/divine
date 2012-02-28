0f8d730ea5a53354c97103ae1664b9c4aa0ffbb3
Petr Rockai <me@mornfall.net>
2012-02-28 10:01:38 +0000

    Tweak FindLLVM to use a real (non-symlinked) path to llvm-config.

--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -1,10 +1,14 @@
 find_program(LLVM_CONFIG_EXECUTABLE llvm-config DOC "path to the llvm-config executable")
+
+# llvm-config does not like to be called through a symlink
+get_filename_component(LLVM_CONFIG_REAL ${LLVM_CONFIG_EXECUTABLE} REALPATH)
+
 if(LLVM_CONFIG_EXECUTABLE)
-  exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --version OUTPUT_VARIABLE LLVM_STRING_VERSION )
-  exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --libdir OUTPUT_VARIABLE LLVM_LIBRARY_DIRS_ )
-  exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --includedir OUTPUT_VARIABLE LLVM_INCLUDE_DIRS_ )
-  exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --cppflags  OUTPUT_VARIABLE LLVM_COMPILE_FLAGS )
-  exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --ldflags   OUTPUT_VARIABLE LLVM_LDFLAGS )
+  exec_program(${LLVM_CONFIG_REAL} ARGS --version OUTPUT_VARIABLE LLVM_STRING_VERSION )
+  exec_program(${LLVM_CONFIG_REAL} ARGS --libdir OUTPUT_VARIABLE LLVM_LIBRARY_DIRS_ )
+  exec_program(${LLVM_CONFIG_REAL} ARGS --includedir OUTPUT_VARIABLE LLVM_INCLUDE_DIRS_ )
+  exec_program(${LLVM_CONFIG_REAL} ARGS --cppflags  OUTPUT_VARIABLE LLVM_COMPILE_FLAGS )
+  exec_program(${LLVM_CONFIG_REAL} ARGS --ldflags   OUTPUT_VARIABLE LLVM_LDFLAGS )
 endif()
 
 set(LLVM_INCLUDE_DIRS ${LLVM_INCLUDE_DIRS_} CACHE STRING "LLVM include path")
