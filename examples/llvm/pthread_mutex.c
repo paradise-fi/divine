52d3ce617e1a080d268a5432c7dd6915a8fee7a7
Petr Rockai <me@mornfall.net>
2012-03-30 12:24:13 +0000

    LLVM: Add an example for (recursive) pthread mutexes.

---
+++ examples/llvm/pthread_mutex.c
@@ -0,0 +1,45 @@
+#include "divine-llvm.h"
+
+int _critical = 0;
+
+void critical() {
+    assert( !_critical );
+    _critical = 1;
+    assert( _critical );
+    _critical = 0;
+}
+
+void *other(void *x) {
+    pthread_mutex_t *mutex = x;
+    pthread_mutex_lock( mutex );
+    critical();
+    pthread_mutex_unlock( mutex );
+    return NULL;
+}
+
+int main() {
+    pthread_t id;
+    pthread_mutex_t mutex;
+    pthread_mutexattr_t attr;
+
+    pthread_mutexattr_init( &attr );
+    pthread_mutexattr_settype( &attr, PTHREAD_MUTEX_RECURSIVE );
+
+    pthread_mutex_init( &mutex, &attr );
+
+    void *result;
+    pthread_create( &id, NULL, other, &mutex );
+
+    pthread_mutex_lock( &mutex );
+
+    pthread_mutex_lock( &mutex );
+    pthread_mutex_unlock( &mutex );
+
+    critical();
+
+    pthread_mutex_unlock( &mutex );
+
+    trace( "waiting for %d", id );
+    pthread_join( id, &result );
+    return 0;
+}
