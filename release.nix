45eeb918ec3f1bf264309bb63abb4b78a20d68a9
Petr Rockai <me@mornfall.net>
2012-04-18 10:19:41 +0000

    Add a release.nix recipe for hydra.

---
+++ release.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? <nixpkgs> }:
+
+let
+  pkgs = import nixpkgs {}; 
+
+  src = pkgs.fetchurl {
+    url = "http://divine.fi.muni.cz/divine-2.5.2.tar.gz";
+    sha256 = "0sxnpqrv9wbfw1m1pm9jzd7hs02yvvnvkm8qdbafhdl2qmll7c0l";
+  };
+
+  jobs = rec { 
+
+    tarball = { divineSrc ? src }: 
+      pkgs.releaseTools.sourceTarball { 
+        name = "divine-tarball";
+        src = divineSrc;
+        buildInputs = (with pkgs; [ cmake ]);
+      };
+
+    build = 
+      { tarball ? jobs.tarball {}        
+      , system ? builtins.currentSystem
+      }:
+
+      let pkgs = import nixpkgs { inherit system; }; in
+      pkgs.releaseTools.nixBuild { 
+        name = "divine" ;
+        src = tarball;
+        configureFlags = [ "--disable-silent-rules" ];
+      };
+  };
+in
+  jobs
