085526087ebbdc51276b297f539e413218f948b5
Petr Rockai <me@mornfall.net>
2012-07-11 08:29:22 +0000

    Only run mdve (m4-based) tests in combine.sh when m4 is available.

--- test/tools/combine.sh
+++ test/tools/combine.sh
@@ -50,21 +50,22 @@ for i in 1 2 3; do
     check_peterson$i peterson.prop$i.dve
 done
 
-divine combine data/peterson.mdve -o N=2 > peterson.combined.dve
-not grep "process LTL_property" peterson.combined.dve
-not grep "system async property LTL_property" peterson.combined.dve
-grep "process P_0" peterson.combined.dve
-grep "process P_1" peterson.combined.dve
-not grep "process P_2" peterson.combined.dve
+if which m4; then
+    divine combine data/peterson.mdve -o N=2 > peterson.combined.dve
+    not grep "process LTL_property" peterson.combined.dve
+    not grep "system async property LTL_property" peterson.combined.dve
+    grep "process P_0" peterson.combined.dve
+    grep "process P_1" peterson.combined.dve
+    not grep "process P_2" peterson.combined.dve
 
-divine combine data/peterson.mdve N=2 -f data/peterson.ltl
-for i in 1 2 3; do
-    test -f peterson.prop$i.dve
-    grep "process LTL_property" peterson.prop$i.dve
-    grep "system async property LTL_property" peterson.prop$i.dve
-    grep "process P_0" peterson.prop$i.dve
-    grep "process P_1" peterson.prop$i.dve
-    not grep "process P_2" peterson.prop$i.dve
-    check_peterson$i peterson.prop$i.dve
-done
-
+    divine combine data/peterson.mdve N=2 -f data/peterson.ltl
+    for i in 1 2 3; do
+        test -f peterson.prop$i.dve
+        grep "process LTL_property" peterson.prop$i.dve
+        grep "system async property LTL_property" peterson.prop$i.dve
+        grep "process P_0" peterson.prop$i.dve
+        grep "process P_1" peterson.prop$i.dve
+        not grep "process P_2" peterson.prop$i.dve
+        check_peterson$i peterson.prop$i.dve
+    done
+fi
