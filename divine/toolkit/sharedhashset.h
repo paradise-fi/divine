1cf0c8363a6aa74105e8d3109265df7b888a05ec
Petr Rockai <me@mornfall.net>
2012-10-29 18:53:55 +0000

    toolkit: Add initial SharedHashSet (adapted from T. Janousek and A. Laarman).

---
+++ divine/toolkit/sharedhashset.h
@@ -0,0 +1,129 @@
+// -*- C++ -*- (c) 2010 Petr Rockai <me@mornfall.net>
+
+#include <numeric>
+#include <atomic>
+#include <stdint.h>
+#include <wibble/sys/mutex.h>
+#include <divine/toolkit/hashset.h>
+
+#ifndef DIVINE_SHAREDHASHSET_H
+#define DIVINE_SHAREDHASHSET_H
+
+namespace divine {
+
+/**
+ * High-performance concurrent hash-table without support for resizing
+ * (neither concurrent nor sequential). It's an open-hashing implementation
+ * with a combination of linear and quadratic probing. It also uses a
+ * hash-compacted prefilter to avoid fetches when looking up an item and the
+ * item stored at the current lookup position is distinct (a collision).
+ *
+ * An initial size _must_ be provided.
+ *
+ * TODO: Merge SharedHashSet and HashSet, if feasible.
+ */
+template < typename T, typename Hasher = default_hasher< T > >
+struct SharedHashSet {
+    enum { maxCollisions = 65536 };
+
+    struct Cell {
+	std::atomic< hash_t > hashLock;
+	T value;
+
+	/* TODO: parallel table initialization */
+	Cell() : hashLock( 0 ) {}
+	Cell( const Cell & ) : hashLock( 0 ) {}
+    };
+
+    unsigned mask;
+    std::vector< Cell > table;
+    Hasher hasher;
+
+    explicit SharedHashSet( unsigned size, Hasher h = Hasher() )
+        : mask( 0 ), hasher( h )
+    {
+        while ((size = size >> 1))
+            mask |= size;
+        table.resize( mask + 1 );
+    }
+
+    /* TODO: cache-line walking and double hashing */
+    unsigned index( hash_t h, unsigned i ) const {
+        const unsigned Q = 1, R = 1, thresh = 16;
+        if ( i <= thresh )
+            return (h + i) & mask;
+        else {
+            i = i - thresh;
+            return (h + thresh + (2 * Q + 1) * i + 2 * R * (i * i)) & mask;
+        }
+    }
+
+    /**
+     * Insert an element into the set. Returns true if the element was added,
+     * false if it was already in the set.
+     */
+    bool insert( const T &x ) {
+	hash_t h = hasher.hash( x ) << 1;
+
+	for ( unsigned i = 0; i < maxCollisions; ++i ) {
+	    Cell &cell = table[ index( h, i ) ];
+	    hash_t chl = cell.hashLock;
+
+	    if ( chl == 0 ) { /* empty → insert */
+		/* Strong CAS since it won't be called twice. */
+		if ( cell.hashLock.compare_exchange_strong( chl, h | 1 ) ) {
+		    cell.value = x;
+		    cell.hashLock.exchange( h & ~1 ); /* We need a barrier here. */
+		    return true;
+		}
+	    }
+
+	    if ( (chl | 1) == (h | 1) ) { /* hashes match → compare */
+		/* Wait until value write finishes. */
+		if ( chl & 1 )
+		    while ( cell.hashLock & 1 );
+
+		/* The wait here is not really necessary because x86 supports
+		 * 8-byte CAS and x86_64 supports 16-byte CAS. We don't use it
+		 * (yet) as the code would be less readable and, more
+		 * importantly, gcc doesn't provide an API for 16-byte CAS.
+		 * TODO: Remove the wait. */
+
+		if ( hasher.equal( cell.value, x ) )
+		    return false;
+	    }
+
+	    /* not empty and not equal → reprobe */
+	}
+
+	std::cerr << "too many collisions" << std::endl;
+	abort();
+	return false;
+    }
+
+    bool has( const T &x ) {
+	hash_t h = hasher.hash( x ) << 1;
+
+	for ( unsigned i = 0; i < maxCollisions; ++i ) {
+            Cell &cell = table[ index( h, i ) ];
+            hash_t chl = cell.hashLock;
+
+            if ( chl == 0 ) {
+                // std::cerr << "index = " << index( h, i ) << ", h = " << h << ", i = " << i << ", chl = " << chl << std::endl;
+                return false;
+            }
+            if ( (chl | 1) == (h | 1) && ( hasher.equal( cell.value, x ) ) )
+                return true;
+        }
+
+	std::cerr << "too many collisions" << std::endl;
+	abort(); return false;
+    }
+
+    SharedHashSet( const SharedHashSet & ) = delete;
+    SharedHashSet &operator=( const SharedHashSet & )= delete;
+};
+
+}
+
+#endif
---
+++ divine/toolkit/sharedhashset.test.h
@@ -0,0 +1,90 @@
+// -*- C++ -*- (c) 2010 Petr Rockai <me@mornfall.net>
+
+#include <wibble/sys/thread.h>
+#include <divine/toolkit/sharedhashset.h>
+
+using namespace divine;
+
+struct TestSharedHashset
+{
+    Test basic() {
+        SharedHashSet< int > set( 8 );
+        assert( !set.has( 1 ) );
+        assert( set.insert( 1 ) );
+        assert( set.has( 1 ) );
+    }
+
+    template< typename T >
+    struct Insert : wibble::sys::Thread {
+        SharedHashSet< T > *set;
+        int from, to;
+        bool overlap;
+
+        void *main() {
+            for ( int i = from; i < to; ++i ) {
+                set->insert( i );
+                assert( set->has( i ) );
+                if (!overlap)
+                    assert( !set->has( i + 1 ) );
+            }
+            return 0;
+        }
+    };
+
+    Test stress() {
+        SharedHashSet< int > set( 65536 );
+        Insert< int > a;
+        a.set = &set;
+        a.from = 1;
+        a.to = 32 * 1024;
+        a.overlap = false;
+        a.main();
+        for ( int i = 1; i < 32*1024; ++i ) {
+            assert( set.has( i ) );
+        }
+    }
+
+    void par( SharedHashSet< int > *set, int f1, int t1, int f2, int t2 )
+    {
+        Insert< int > a, b;
+
+        a.from = f1;
+        a.to = t1;
+        b.from = f2;
+        b.to = t2;
+        a.set = b.set = set;
+        a.overlap = b.overlap = (t1 > f2);
+
+        a.start();
+        b.start();
+        a.join();
+        b.join();
+    }
+
+    Test parstress() {
+        SharedHashSet< int > set( 64*1024 );
+        par( &set, 1, 16*1024, 8*1024, 32*1024 );
+
+        for ( int i = 1; i < 32*1024; ++i ) {
+            assert( set.has( i ) );
+        }
+    }
+
+    Test set() {
+        SharedHashSet< int > set( 64*1024 );
+
+        for ( int i = 1; i < 32*1024; ++i ) {
+            assert( !set.has( i ) );
+        }
+
+        par( &set, 1, 16*1024, 16*1024, 32*1024 );
+
+        for ( int i = 1; i < 32*1024; ++i ) {
+            assert_eq( i, i * set.has( i ) );
+        }
+
+        for ( int i = 32*1024; i < 64 * 1024; ++i ) {
+            assert( !set.has( i ) );
+        }
+    }
+};
