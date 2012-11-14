14525efde5a050e246f301501ccf17d1dfe44a74
Petr Rockai <me@mornfall.net>
2012-11-14 17:29:24 +0000

    LLVM: Streamline tests, check that addition works.

--- divine/llvm-new/interpreter.test.h
+++ divine/llvm-new/interpreter.test.h
@@ -13,7 +13,7 @@ struct TestLLVM {
 
     Function *_main() {
         Module *m = new Module( "code1", ctx );
-        FunctionType *mainT = FunctionType::get(Type::getInt64Ty(ctx), std::vector<Type *>(), false);
+        FunctionType *mainT = FunctionType::get(Type::getInt32Ty(ctx), std::vector<Type *>(), false);
         Function *main = Function::Create(mainT, Function::ExternalLinkage, "test_main", m);
         BasicBlock *BB = BasicBlock::Create(ctx, "entry", main);
         builder.SetInsertPoint( BB );
@@ -22,7 +22,7 @@ struct TestLLVM {
 
     Function *code1() {
         Function *main = _main();
-        Value *result = ConstantInt::get(ctx, APInt(64, 1));
+        Value *result = ConstantInt::get(ctx, APInt(32, 1));
         builder.CreateRet( result );
         return main;
     }
@@ -31,6 +31,35 @@ struct TestLLVM {
         Function *f = _main();
         builder.CreateBr( &*(f->begin()) );
         return f;
+    }
+
+    Function *code3() {
+        Function *f = _main();
+        Value *a = ConstantInt::get(ctx, APInt(32, 1)),
+              *b = ConstantInt::get(ctx, APInt(32, 2));
+        Value *result = builder.Insert( BinaryOperator::Create( Instruction::Add, a, b ), "meh" );
+        builder.CreateBr( &*(f->begin()) );
+        builder.CreateRet( result );
+        return f;
+    }
+
+    divine::Blob _ith( Function *f, int step ) {
+        divine::Allocator a;
+        dlvm::Interpreter interpreter( a, f->getParent() );
+        divine::Blob ini = interpreter.initial( f ), fin;
+        interpreter.rewind( ini );
+        fin = ini;
+
+        for ( int i = 0; i < step; ++i ) {
+            fin = divine::Blob();
+            interpreter.run( 0, [&]( divine::Blob b ) {
+                    assert( !fin.valid() ); // only one allowed
+                    fin = b;
+                });
+            assert( fin.valid() );
+        }
+
+        return fin;
     }
 
     Test initial()
@@ -43,33 +72,21 @@ struct TestLLVM {
 
     Test successor1()
     {
-        divine::Allocator a;
-        Function *main = code1();
-        dlvm::Interpreter i( a, main->getParent() );
-        divine::Blob ini = i.initial( main ), fin;
-        i.rewind( ini );
-        i.run( 0, [&]( divine::Blob b ) {
-                assert( !fin.valid() ); // only one allowed
-                fin = b;
-            });
-        assert( fin.valid() );
-        // 2x flags, thread count, thread 1( stack, pagetable, memory size )
-        assert_eq( wibble::str::fmt( fin ), "[ 0, 0, 1, 0, 0, 0 ]" );
+        assert_eq( wibble::str::fmt( _ith( code1(), 1 ) ),
+                   "[ 0, 0, 1, 0, 0, 0 ]" );
     }
 
     Test successor2()
     {
-        divine::Allocator a;
-        Function *main = code2();
-        dlvm::Interpreter i( a, main->getParent() );
-        divine::Blob ini = i.initial( main ), fin;
-        i.rewind( ini );
-        i.run( 0, [&]( divine::Blob b ) {
-                assert( !fin.valid() ); // only one allowed
-                fin = b;
-            });
-        assert( fin.valid() );
-        // 2x flags, thread count, thread 1( stack size ( pc ), pagetable, memory size )
-        assert_eq( wibble::str::fmt( fin ), "[ 0, 0, 1, 1, 1, 0, 0, 0 ]" );
+        assert_eq( wibble::str::fmt( _ith( code2(), 1 ) ),
+                   "[ 0, 0, 1, 1, 1, 0, 0, 0 ]" );
+    }
+
+    Test successor3()
+    {
+        assert_eq( wibble::str::fmt( _ith( code3(), 1 ) ),
+                   "[ 0, 0, 1, 1, 1, 3, 0, 0, 0 ]" );
+        assert_eq( wibble::str::fmt( _ith( code3(), 2 ) ),
+                   "[ 0, 0, 1, 1, 1, 3, 0, 0, 0 ]" );
     }
 };
