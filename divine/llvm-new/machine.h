3ff8c44a59731fc1c29b196c2defd1f7e83c3c95
Petr Rockai <me@mornfall.net>
2012-11-21 22:05:52 +0000

    LLVM: Split off program.h and machine.h from interpreter.h.

---
+++ divine/llvm-new/machine.h
@@ -0,0 +1,529 @@
+// -*- C++ -*- (c) 2012 Petr Rockai
+
+#include <divine/llvm-new/program.h>
+#include <divine/toolkit/lens.h>
+#include <divine/graph/allocator.h>
+
+#ifndef DIVINE_LLVM_MACHINE_H
+#define DIVINE_LLVM_MACHINE_H
+
+namespace divine {
+namespace llvm2 {
+
+struct MachineState
+{
+    struct StateAddress : lens::LinearAddress
+    {
+        ProgramInfo *_info;
+
+        StateAddress( StateAddress base, int index, int offset )
+            : LinearAddress( base, index, offset ), _info( base._info )
+        {}
+
+        StateAddress( ProgramInfo *i, Blob b, int offset )
+            : LinearAddress( b, offset ), _info( i )
+        {}
+
+        StateAddress copy( StateAddress to, int size ) {
+            std::copy( dereference(), dereference() + size, to.dereference() );
+            return StateAddress( _info, to.b, to.offset + size );
+        }
+    };
+
+    struct Frame {
+        PC pc;
+        char memory[0];
+
+        StateAddress advance( StateAddress a, int ) {
+            return StateAddress( a, 0, sizeof( Frame ) + a._info->function(pc).framesize );
+        }
+        int end() { return 0; }
+    };
+
+    struct Globals {
+        char memory[0];
+        StateAddress advance( StateAddress a, int ) {
+            return StateAddress( a, 0, a._info->globalsize );
+        }
+        int end() { return 0; }
+    };
+
+    static int size_jumptable( int segcount ) {
+        /* 4-align, extra item for "end of memory" */
+        return 2 * (2 + segcount - segcount % 2);
+    }
+
+    static int size_bitmap( int bytecount ) {
+        return bytecount / 32 + ((bytecount % 32) ? 4 : 0);
+    }
+
+    static int size_heap( int segcount, int bytecount ) {
+        return sizeof( Heap ) +
+            bytecount +
+            size_jumptable( segcount ) +
+            size_bitmap( bytecount );
+    }
+
+    struct Heap {
+        int segcount;
+        char _memory[0];
+
+        int size() {
+            return jumptable( segcount ) * 4;
+        }
+
+        uint16_t &jumptable( int segment ) {
+            return reinterpret_cast< uint16_t * >( _memory )[ segment ];
+        }
+
+        uint16_t &jumptable( Pointer p ) {
+            assert( owns( p ) );
+            return jumptable( p.segment );
+        }
+
+        uint16_t &bitmap( Pointer p ) {
+            assert( owns( p ) );
+            return reinterpret_cast< uint16_t * >(
+                _memory + size_jumptable( segcount ) )[ p.segment / 64 ];
+        }
+
+        int offset( Pointer p ) {
+            assert( owns( p ) );
+            return int( jumptable( p ) * 4 ) + p.offset;
+        }
+
+        int size( Pointer p ) {
+            assert( owns( p ) );
+            return 4 * (jumptable( p.segment + 1 ) - jumptable( p ));
+        }
+
+        uint16_t mask( Pointer p ) {
+            assert_eq( offset( p ) % 4, 0 );
+            return 1 << ((offset( p ) % 64) / 4);
+        }
+
+        StateAddress advance( StateAddress a, int ) {
+            return StateAddress( a, 0, size_heap( segcount, size() ) );
+        }
+        int end() { return 0; }
+
+        void setPointer( Pointer p, bool ptr ) {
+            if ( ptr )
+                bitmap( p ) |= mask( p );
+            else
+                bitmap( p ) &= ~mask( p );
+        }
+
+        bool isPointer( Pointer p ) {
+            return bitmap( p ) & mask( p );
+        }
+
+        bool owns( Pointer p ) {
+            return p.valid && p.segment < segcount;
+        }
+
+        char *dereference( Pointer p ) {
+            assert( owns( p ) );
+            return _memory + size_bitmap( size() ) + size_jumptable( segcount ) + offset( p );
+        }
+    };
+
+    struct Nursery {
+        std::vector< int > offsets;
+        std::vector< char > memory;
+        std::vector< bool > pointer;
+        int segshift;
+
+        Pointer malloc( int size ) {
+            int segment = offsets.size() - 1;
+            int start = offsets[ segment ];
+            int end = start + size;
+            align( end, 4 );
+            offsets.push_back( end );
+            memory.resize( end );
+            pointer.resize( end / 4 );
+            std::fill( memory.begin() + start, memory.end(), 0 );
+            std::fill( pointer.begin() + start / 4, pointer.end(), 0 );
+            return Pointer( segment + segshift, 0 );
+        }
+
+        bool owns( Pointer p ) {
+            return p.valid && (p.segment - segshift < offsets.size() - 1);
+        }
+
+        int offset( Pointer p ) {
+            assert( owns( p ) );
+            return offsets[ p.segment - segshift ] + p.offset;
+        }
+
+        int size( Pointer p ) {
+            assert( owns( p ) );
+            return offsets[ p.segment - segshift + 1] - offsets[ p.segment - segshift ];
+        }
+
+        bool isPointer( Pointer p ) {
+            assert_eq( p.offset % 4, 0 );
+            return pointer[ offset( p ) / 4 ];
+        }
+
+        void setPointer( Pointer p, bool is ) {
+            pointer[ offset( p ) / 4 ] = is;
+        }
+
+        char *dereference( Pointer p ) {
+            assert( owns( p ) );
+            return &memory[ offset( p ) ];
+        }
+
+        void reset( int shift ) {
+            segshift = shift;
+            memory.clear();
+            offsets.clear();
+            offsets.push_back( 0 );
+            pointer.clear();
+        }
+    };
+
+    Blob _blob, _stack;
+    Nursery nursery;
+    ProgramInfo &_info;
+    Allocator &_alloc;
+    int _thread; /* the currently scheduled thread */
+    int _thread_count;
+    Frame *_frame; /* pointer to the currently active frame */
+
+    template< typename T >
+    using Lens = lens::Lens< StateAddress, T >;
+
+    struct Flags {
+        uint64_t assert:1;
+        uint64_t null_dereference:1;
+        uint64_t invalid_dereference:1;
+        uint64_t invalid_argument:1;
+        uint64_t ap:48;
+        uint64_t buchi:12;
+    };
+
+    typedef lens::Array< Frame > Stack;
+    typedef lens::Array< Stack > Threads;
+    typedef lens::Tuple< Flags, Globals, Heap, Threads > State;
+
+    char *dereference( Pointer p ) {
+        assert( validate( p ) );
+        if ( heap().owns( p ) )
+            return heap().dereference( p );
+        else
+            return nursery.dereference( p );
+    }
+
+    bool validate( Pointer p ) {
+        return p.valid && ( heap().owns( p ) || nursery.owns( p ) );
+    }
+
+    Pointer followPointer( Pointer p ) {
+        if ( !validate( p ) )
+            return Pointer();
+
+        Pointer next = *reinterpret_cast< Pointer * >( dereference( p ) );
+        if ( heap().owns( p ) ) {
+            if ( heap().isPointer( p ) )
+                return next;
+        } else if ( nursery.isPointer( p ) )
+            return next;
+        return Pointer();
+    }
+
+    int pointerSize( Pointer p ) {
+        if ( !validate( p ) )
+            return 0;
+
+        if ( heap().owns( p ) )
+            return heap().size( p );
+        else
+            return nursery.size( p );
+    }
+
+    Lens< State > state() {
+        return Lens< State >( StateAddress( &_info, _blob, _alloc._slack ) );
+    }
+
+    /*
+     * Get a pointer to the value storage corresponding to "v". If "v" is a
+     * local variable, look into thread "thread" (default, i.e. -1, for the
+     * currently executing one) and in frame "frame" (default, i.e. 0 is the
+     * topmost frame, 1 is the frame just below that, etc...).
+     */
+    char *dereference( ProgramInfo::Value v, int tid = -1, int frame = 0 )
+    {
+        char *block = _frame->memory;
+
+        if ( tid < 0 )
+            tid = _thread;
+
+        if ( !v.global && !v.constant && ( frame || tid != _thread ) )
+            block = stack( tid ).get( stack( tid ).get().length() - frame - 1 ).memory;
+
+        if ( v.global )
+            block = state().get( Globals() ).memory;
+
+        if ( v.constant )
+            block = &_info.constdata[0];
+
+        return block + v.offset;
+    }
+
+    void rewind( Blob to, int thread = 0 )
+    {
+        _blob = to;
+        _thread = -1; // special
+
+        _thread_count = threads().get().length();
+        nursery.reset( heap().segcount );
+
+        if ( thread >= 0 && thread < threads().get().length() )
+            switch_thread( thread );
+        // else everything becomes rather unsafe...
+    }
+
+    void switch_thread( int thread )
+    {
+        assert_leq( thread, threads().get().length() - 1 );
+        _thread = thread;
+
+        StateAddress stackaddr( &_info, _stack, 0 );
+        _blob_stack( _thread ).copy( stackaddr );
+        assert( stack().get().length() );
+        _frame = &stack().get( stack().get().length() - 1 );
+   }
+
+    int new_thread()
+    {
+        Blob old = _blob;
+        _blob = snapshot();
+        old.free( _alloc.pool() );
+        _thread = _thread_count ++;
+
+        /* Set up an empty thread. */
+        stack().get().length() = 0;
+        return _thread;
+    }
+
+    Lens< Threads > threads() {
+        return state().sub( Threads() );
+    }
+
+    Lens< Stack > _blob_stack( int i ) {
+        assert_leq( 0, i );
+        return state().sub( Threads(), i );
+    }
+
+    Lens< Stack > stack( int thread = -1 ) {
+        if ( thread == _thread || thread < 0 ) {
+            assert_leq( 0, _thread );
+            return Lens< Stack >( StateAddress( &_info, _stack, 0 ) );
+        } else
+            return _blob_stack( thread );
+    }
+
+    Heap &heap() {
+        return state().get( Heap() );
+    }
+
+    Frame &frame( int thread = -1, int idx = 0 ) {
+         if ( !idx )
+             return *_frame;
+
+        auto s = stack( thread );
+        return s.get( s.get().length() - idx - 1 );
+    }
+
+    Flags &flags() {
+        return state().get( Flags() );
+    }
+
+    void enter( int function ) {
+        int framesize = _info.functions[ function ].framesize;
+        int depth = stack().get().length();
+        bool masked = depth ? frame().pc.masked : false;
+        stack().get().length() ++;
+
+        _frame = &stack().get( depth );
+        _frame->pc = PC( function, 0, 0 );
+        _frame->pc.masked = masked; /* inherit */
+        std::fill( _frame->memory, _frame->memory + framesize, 0 );
+    }
+
+    void leave() {
+        int fun = frame().pc.function;
+        auto &s = stack().get();
+        s.length() --;
+        if ( stack().get().length() )
+            _frame = &stack().get( stack().get().length() - 1 );
+        else
+            _frame = nullptr;
+    }
+
+    struct Canonic {
+        MachineState &ms;
+        std::map< int, int > segmap;
+        int allocated, segcount;
+        int stack;
+        int boundary, segdone;
+
+        Canonic( MachineState &ms )
+            : ms( ms ), allocated( 0 ), segcount( 0 ), stack( 0 ), boundary( 0 ), segdone( 0 )
+        {}
+
+        Pointer operator[]( Pointer idx ) {
+            if ( !idx.valid )
+                return idx;
+            if ( !segmap.count( idx.segment ) ) {
+                segmap.insert( std::make_pair( int( idx.segment ), segcount ) );
+                ++ segcount;
+                allocated += ms.pointerSize( idx );
+            }
+            return Pointer( segmap[ int( idx.segment ) ], idx.offset );
+        }
+    };
+
+    void trace( Pointer p, Canonic &canonic ) {
+        if ( p.valid ) {
+            canonic[ p ];
+            trace( followPointer( p ), canonic );
+        }
+    }
+
+    void trace( Frame &f, Canonic &canonic ) {
+        auto vals = _info.function( f.pc ).values;
+        for ( auto val = vals.begin(); val != vals.end(); ++val ) {
+            canonic.stack += val->width;
+            if ( val->pointer )
+                trace( *reinterpret_cast< Pointer * >( &f.memory[val->offset] ), canonic );
+        }
+    }
+
+    template< typename F >
+    void eachframe( Lens< Stack > s, F f ) {
+        int idx = 0;
+        auto address = s.sub( 0 ).address();
+        while ( idx < s.get().length() ) {
+            Frame &fr = address.as< Frame >();
+            f( fr );
+            address = fr.advance( address, 0 );
+            ++ idx;
+        }
+    }
+
+    int size( int stack, int heapbytes, int heapsegs ) {
+        return sizeof( Flags ) +
+               sizeof( int ) + /* thread count */
+               stack + size_heap( heapsegs, heapbytes ) + _info.globalsize;
+    }
+
+    void snapshot( Pointer from, Pointer to, Canonic &canonic, Heap &heap ) {
+        if ( !validate( from ) || to.segment < canonic.segdone )
+            return; /* invalid or done */
+        assert_eq( to.segment, canonic.segdone );
+        canonic.segdone ++;
+        int size = pointerSize( from );
+        heap.jumptable( to ) = canonic.boundary / 4;
+        canonic.boundary += size;
+
+        /* Work in 4 byte steps, since pointers are 4 bytes and 4-byte aligned. */
+        from.offset = to.offset = 0;
+        for ( ; from.offset < size; from.offset += 4, to.offset += 4 ) {
+            Pointer p = followPointer( from ), q = canonic[ p ];
+            heap.setPointer( to, p.valid );
+            if ( p.valid ) { /* recurse */
+                *reinterpret_cast< Pointer * >( heap.dereference( to ) ) = q;
+                snapshot( p, q, canonic, heap );
+            } else /* not a pointer, make a straight copy */
+                *reinterpret_cast< uint32_t * >( heap.dereference( to ) ) =
+                    *reinterpret_cast< uint32_t * >( dereference( from ) );
+        }
+    }
+
+    void snapshot( Frame &f, Canonic &canonic, Heap &heap, StateAddress &address ) {
+        auto vals = _info.function( f.pc ).values;
+        address.as< PC >() = f.pc;
+        address.advance( sizeof( PC ) );
+        for ( auto val = vals.begin(); val != vals.end(); ++val ) {
+            char *from_addr = &f.memory[val->offset];
+            if ( val->pointer ) {
+                Pointer from = *reinterpret_cast< Pointer * >( from_addr );
+                Pointer to = canonic[ from ];
+                address.as< Pointer >() = to;
+                snapshot( from, to, canonic, heap );
+            } else
+                std::copy( from_addr, from_addr + val->width, address.dereference() );
+            address.advance( val->width );
+        }
+    }
+
+    Blob snapshot() {
+        Canonic canonic( *this );
+        int live_threads = _thread_count;
+
+        for ( int tid = 0; tid < _thread_count; ++tid ) {
+            if ( !stack( tid ).get().length() ) { /* leave out dead threads */
+                -- live_threads;
+                continue;
+            }
+
+            canonic.stack += sizeof( Stack );
+            eachframe( stack( tid ), [&]( Frame &fr ) {
+                    canonic.stack += sizeof( Frame );
+                    trace( fr, canonic );
+                } );
+        }
+
+        Blob b = _alloc.new_blob(
+            size( canonic.stack, canonic.allocated, canonic.segcount ) );
+
+        StateAddress address( &_info, b, _alloc._slack );
+        address = state().sub( Flags() ).copy( address );
+        assert_eq( address.offset, _alloc._slack + sizeof( Flags ) );
+        address = state().sub( Globals() ).copy( address );
+
+        /* skip the heap */
+        Heap *_heap = &address.as< Heap >();
+        _heap->segcount = canonic.segcount;
+        /* heap needs to know its size in order to correctly dereference! */
+        _heap->jumptable( canonic.segcount ) = canonic.allocated / 4;
+        address.advance( size_heap( canonic.segcount, canonic.allocated ) );
+
+        address.as< int >() = live_threads;
+        address.advance( sizeof( int ) ); // ick. length of the threads array
+
+        for ( int tid = 0; tid < _thread_count; ++tid ) {
+            if ( !stack( tid ).get().length() )
+                continue;
+
+            address.as< int >() = stack( tid ).get().length();
+            address.advance( sizeof( int ) );
+            eachframe( stack( tid ), [&]( Frame &fr ) {
+                    snapshot( fr, canonic, *_heap, address );
+                });
+        }
+
+        assert_eq( canonic.segdone, canonic.segcount );
+        assert_eq( canonic.boundary, canonic.allocated );
+        assert_eq( address.offset, b.size() );
+
+        return b;
+    }
+
+    MachineState( ProgramInfo &i, Allocator &alloc )
+        : _stack( 4096 ), _info( i ), _alloc( alloc )
+    {
+        _thread_count = 0;
+        _frame = nullptr;
+        nursery.reset( 0 ); /* nothing in the heap */
+    }
+
+};
+
+}
+}
+
+#endif
---
+++ divine/llvm-new/program.h
@@ -0,0 +1,226 @@
+// -*- C++ -*- (c) 2012 Petr Rockai
+
+#include <wibble/mixin.h>
+#include <wibble/test.h>
+
+#include <llvm/Function.h>
+#include <llvm/Target/TargetData.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <map>
+
+#ifndef DIVINE_LLVM_PROGRAM_H
+#define DIVINE_LLVM_PROGRAM_H
+
+namespace divine {
+namespace llvm2 {
+
+struct Interpreter;
+
+void static align( int &v, int a ) {
+    if ( v % a )
+        v += a - (v % a);
+}
+
+struct PC : wibble::mixin::Comparable< PC >
+{
+    uint32_t function:10;
+    uint32_t block:12;
+    uint32_t instruction:9;
+    bool masked:1;
+    explicit PC( int f = 0, int b = 0, int i = 0 )
+        : function( f ), block( b ), instruction( i ), masked( false )
+    {}
+
+    bool operator<= ( PC o ) const {
+        /* masked is irrelevant for equality! */
+        return std::make_tuple( int( function ), int( block ), int( instruction ) )
+            <= std::make_tuple( int( o.function ), int( o.block ), int( o.instruction ) );
+    }
+};
+
+/*
+ * We use a *non-overlapping* segmented memory scheme, with *variable-sized
+ * segments*. Each allocation creates a new segment. Pointers cannot cross
+ * segment boundaries through pointer arithmetic or any other manipulation.
+ */
+struct Pointer : wibble::mixin::Comparable< Pointer > {
+    bool valid:1; /* make a (0, 0) pointer different from NULL */
+    uint32_t segment:16; /* at most 64k objects */
+    uint32_t offset:15; /* each at most 32kB */
+    Pointer operator+( int relative ) {
+        return Pointer( segment, offset + relative );
+    }
+    Pointer( int segment, int offset )
+        : valid( true ), segment( segment ), offset( offset )
+    {}
+    Pointer() : valid( false ), segment( 0 ), offset( 0 ) {}
+    bool null() { return !valid; } // all size = 0 pointers represent NULL
+
+    bool operator<=( Pointer o ) const {
+        if ( segment < o.segment )
+            return true;
+        return segment == o.segment && offset <= o.offset;
+    }
+};
+
+static std::ostream &operator<<( std::ostream &o, Pointer p ) {
+    return o << "<" << p.segment << ":" << p.offset << ">";
+}
+
+enum Builtin {
+    NotBuiltin = 0,
+    BuiltinChoice = 1,
+    BuiltinMask = 2,
+    BuiltinUnmask = 3,
+    BuiltinGetTID = 4,
+    BuiltinNewThread = 5
+};
+
+struct ProgramInfo {
+    ::llvm::TargetData &target;
+    /* required for getConstantValue/StoreValueToMemory */
+    Interpreter &interpreter;
+
+    struct Value {
+        bool global:1;
+        bool constant:1;
+        bool pointer:1;
+        uint32_t offset:20;
+        uint32_t width:9;
+
+        bool operator<( Value v ) const {
+            return *reinterpret_cast< const uint32_t * >( this ) < *reinterpret_cast< const uint32_t * >( &v );
+        }
+
+        Value()
+            : global( false ), constant( false ), pointer( false ),
+              offset( 0 ), width( 0 )
+        {}
+    };
+
+    struct Instruction {
+        Value result;
+        std::vector< Value > operands;
+
+        int builtin; /* non-zero if this is a call to a builtin */
+        ::llvm::Instruction *op; /* the actual operation */
+        Instruction() : op( nullptr ), builtin( NotBuiltin ) {}
+        /* next instruction is in the same BB unless op == NULL */
+    };
+
+    struct BB {
+        std::vector< Instruction > instructions;
+        ::llvm::BasicBlock *bb;
+        Instruction &instruction( PC pc ) {
+            assert_leq( int( pc.instruction ), int( instructions.size() ) - 1 );
+            return instructions[ pc.instruction ];
+        }
+        BB() : bb( nullptr ) {}
+    };
+
+    struct Function {
+        int framesize;
+        std::vector< Value > values;
+        std::vector< BB > blocks;
+        BB &block( PC pc ) {
+            assert_leq( int( pc.block ), int( blocks.size() ) - 1 );
+            return blocks[ pc.block ];
+        }
+    };
+
+    std::vector< Function > functions;
+    std::vector< Value > globals;
+    std::vector< char > constdata;
+    int globalsize, constdatasize;
+
+    std::map< ::llvm::Value *, Value > valuemap;
+    std::map< Value, ::llvm::Value * > llvmvaluemap;
+    std::map< ::llvm::Instruction *, PC > pcmap;
+
+    std::map< ::llvm::BasicBlock *, PC > blockmap;
+    std::map< ::llvm::Function *, int > functionmap;
+
+    template< typename Container >
+    static void makeFit( Container &c, int index ) {
+        c.resize( std::max( index + 1, int( c.size() ) ) );
+    }
+
+    Instruction &instruction( PC pc ) {
+        assert_leq( int( pc.block ), int( function( pc ).blocks.size() ) - 1 );
+        return block( pc ).instruction( pc );
+    }
+
+    BB &block( PC pc ) {
+        assert_leq( int( pc.block ), int( function( pc ).blocks.size() ) - 1 );
+        return function( pc ).block( pc );
+    }
+
+    Function &function( PC pc ) {
+        assert_leq( int( pc.function ), int( functions.size() ) - 1 );
+        return functions[ pc.function ];
+    }
+
+    template< typename T >
+    T &constant( Value v ) {
+        return *reinterpret_cast< T * >( &constdata[ v.offset ] );
+    }
+
+    char *allocateConstant( Value &result ) {
+        result.constant = true;
+        result.offset = constdatasize;
+        constdatasize += result.width;
+        constdata.resize( constdata.size() + result.width );
+        return &constdata[ result.offset ];
+    }
+
+    void allocateValue( int fun, Value &result ) {
+        result.constant = false;
+        if ( fun ) {
+            result.global = false;
+            result.offset = functions[ fun ].framesize;
+            functions[ fun ].framesize += result.width;
+        } else {
+            result.global = true;
+            result.offset = globalsize;
+            globalsize += result.width;
+        }
+    }
+
+    void storeGV( char *where, ::llvm::GenericValue GV, ::llvm::Type *ty, int width );
+
+    void storeConstant( Value &result, int v ) {
+        result.width = sizeof( int );
+        allocateConstant( result );
+        constant< int >( result ) = v;
+    }
+
+    void storeConstant( Value &result, PC pc ) {
+        result.width = sizeof( PC );
+        allocateConstant( result );
+        constant< PC >( result ) = pc;
+    }
+
+    void storeConstant( Value &result, ::llvm::GenericValue GV, ::llvm::Type *ty );
+
+    struct Position {
+        PC pc;
+        ::llvm::BasicBlock::iterator I;
+        Position( PC pc, ::llvm::BasicBlock::iterator I ) : pc( pc ), I( I ) {}
+    };
+
+    Position insert( Position );
+    Position lower( Position ); // convert intrinsic into normal insns
+    void builtin( Position );
+    Value insert( int function, ::llvm::Value *val );
+
+    ProgramInfo( ::llvm::TargetData &td, Interpreter &i ) : target( td ), interpreter( i )
+    {
+        constdatasize = 0;
+        globalsize = 0;
+    }
+};
+
+}
+}
+
+#endif
