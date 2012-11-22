bd2079f98246e28882faa16a949dfd2d06aa7c58
Petr Rockai <me@mornfall.net>
2012-11-22 20:59:39 +0000

    toolkit: Add a couple of shared memory utility classes.

---
+++ divine/toolkit/shmem.h
@@ -0,0 +1,103 @@
+// -*- C++ -*- (c) 2012 Petr Rockai <me@mornfall.net>,
+//                 2011 Tomáš Janoušek <tomi@nomi.cz>
+
+
+#include <mutex>
+#include <atomic>
+
+#ifndef DIVINE_SHMEM_H
+#define DIVINE_SHMEM_H
+
+namespace divine {
+
+/**
+ * A spinlock implementation.
+ *
+ * One has to wonder why this is missing from the C++0x STL.
+ */
+struct SpinLock {
+    std::atomic< bool > b;
+
+    SpinLock() : b( false ) {}
+
+    void lock() {
+	while ( true ) {
+	    while ( b );
+	    if ( !b.exchange( true ) )
+		break;
+	}
+    }
+
+    void unlock() {
+	/* Exchange instead of assignment forces memory barrier and prevents
+	 * gcc from reordering. */
+	b.exchange( false );
+    }
+
+    SpinLock( const SpinLock & ) = delete;
+    SpinLock &operator=( const SpinLock & ) = delete;
+};
+
+/**
+ * Termination detection implemented as a shared counter of open (not yet
+ * processed) states. This appears to be fast because the shared counter is
+ * modified very rarely -- its incremented in large steps for thousands of
+ * states in advance and then adjusted down to its actual value only if the
+ * queue gets empty.
+ *
+ * Shared counter - Σ local of all threads = actual open count.
+ * local ≥ 0, hence shared is an overapproximation of the actual open count.
+ * This implies partial correctness.
+ *
+ * Termination follows from proper calls to sync().
+ */
+struct ApproximateCounter {
+    enum { step = 100000 };
+
+    struct Shared {
+	std::atomic< unsigned > counter;
+	Shared() : counter( 0 ) {}
+    };
+
+    Shared &shared;
+    unsigned local;
+
+    ApproximateCounter( Shared &s ) : shared( s ), local( 0 ) {}
+    ~ApproximateCounter() { sync(); }
+
+    void sync() {
+	if ( local > 0 ) {
+	    shared.counter -= local;
+	    local = 0;
+	}
+    }
+
+    ApproximateCounter& operator++() {
+	if ( local == 0 ) {
+	    shared.counter += step;
+	    local = step;
+	}
+
+	--local;
+
+	return *this;
+    }
+
+    ApproximateCounter &operator--() {
+	++local;
+	return *this;
+    }
+
+    bool isZero() {
+	/* user is responsible for calling sync(), this method is called way
+	 * too often */
+	return shared.counter == 0;
+    }
+
+    ApproximateCounter( const ApproximateCounter & ) = delete;
+    ApproximateCounter operator=( const ApproximateCounter & ) = delete;
+};
+
+}
+
+#endif
