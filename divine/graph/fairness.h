9ee108bae23a5cf815cc3e1cb673acafaf13b42a
Petr Rockai <me@mornfall.net>
2012-12-01 12:22:25 +0000

    graph: Update FairGraph to the lambda-based successor interface.

--- divine/graph/fairness.h
+++ divine/graph/fairness.h
@@ -13,7 +13,7 @@ template< typename G >
 struct FairGraph : NonPORGraph< G > {
 
     typedef typename G::Node Node;
-    typedef typename G::Successors GenSuccs;
+    typedef typename G::Label Label;
 
     int m_algslack;
 
@@ -32,70 +32,43 @@ struct FairGraph : NonPORGraph< G > {
         return n.template get< Extension >( m_algslack );
     }
 
-    // Successor container consisting of multiple successor containers returned by generator
-    struct Successors {
-        std::vector< std::pair< GenSuccs, int > > parts;
-        int current;
-        FairGraph *parent;
+    template< typename Yield >
+    void withCopy( Node n, Label l, int copy, Yield yield ) {
+        this->extension( n ).copy = copy;
+        yield( n, l );
+    }
 
-        Node from() {
-            assert( !parts.empty() );
-            return parts[ 0 ].first.from();
-        }
-
-        bool empty() const {
-            return size_t( current ) >= parts.size();
-        }
-
-        Node head() const {
-            Node n = parts[ current ].first.head();
-            parent->extension( n ).copy = parts[ current ].second;
-            return n;
-        }
-
-        Successors tail() const {
-            Successors s = *this;
-            if ( empty() ) return s;
-            s.parts[ s.current ].first = s.parts[ s.current ].first.tail();
-            while ( !s.empty() && s.parts[ s.current ].first.empty() ) s.current++;
-            return s;
-        }
-
-        // Adds succesor container and directs all contained states to given copy of the statespace
-        void add( GenSuccs s, int copy ) {
-            if ( size_t( current ) == parts.size() && s.empty() )
-                current++;
-            parts.push_back( std::make_pair( s, copy ) );
-        }
-
-        Successors() : current( 0 ) {}
-    };
-
-    Successors successors( Node st ) {
+    template< typename Yield >
+    void successors( Node st, Yield yield ) {
         int procs = this->base().processCount();
-        Successors succs;
-        succs.parent = this;
 
         int copy = extension( st ).copy;
-        assert( copy >= 0 && copy <= procs );
+        bool accepting = this->base().isAccepting( st );
+        assert_leq( 0, copy );
+        assert_leq( copy, procs );
+
         // 0-th copy: transitions from accepting states are redirected to copy 1, other remain unchanged
         if ( copy == 0 ) {
-            succs.add( this->base().successors( st ), this->base().isAccepting( st ) ? 1 : 0 );
-        }
-        // i-th copy: redirect all transitions made by (i-1)-th process to copy (i+1)
-        else {
-            GenSuccs proc_succs;
+            this->base().successors( st, [&] ( Node n, Label l ) {
+                    this->withCopy( n, l, accepting ? 1 : 0, yield ); } );
+        } else { // i-th copy: redirect all transitions made by (i-1)-th process to copy (i+1)
+            bool enabled = false;
             do {
-                succs.add( this->base().processSuccessors( st , copy - 1, false ), copy );  // stay in curent copy
-                proc_succs = this->base().processSuccessors( st , copy - 1, true );
+                this->base().processSuccessors( st, [&] ( Node n, Label l ) {
+                        this->withCopy( n, l, copy, yield ); }, copy - 1, false );
+                this->base().processSuccessors( st, [&] ( Node n, Label l ) {
+                        enabled = true;
+                        this->withCopy( n, l, ( copy + 1 ) % ( procs + 1 ), yield ); },
+                    copy - 1, true );
                 copy = ( copy + 1 ) % ( procs + 1 );
                 // if process is disabled, epsilon-step to the next copy can be made
-            } while ( proc_succs.empty() && copy > 0 );
-            if ( proc_succs.empty() ) // break the epsilon-chain to avoid skipping the copy 0
-                proc_succs = this->base().successors( st );
-            succs.add( proc_succs, copy ); // go to next copy
+            } while ( !enabled && copy > 0 );
+
+            if ( !enabled ) { // break the epsilon-chain to avoid skipping the copy 0
+                this->base().successors( st, [&] ( Node n, Label l ) {
+                        this->withCopy( n, l, copy, yield ); } );
+            }
         }
-        return succs;
     }
 
     // only states in copy 0 can be accepting
