c78d5c2630c8bacd957612054234afd276424f72
Nikola Benes <xbenes3@fi.muni.cz>
2012-12-03 12:33:34 +0000

    Modify CoIn lexer so that it handles CRLF line endings.

--- divine/coin/parse/lex.coin.cpp
+++ divine/coin/parse/lex.coin.cpp
@@ -1,6 +1,6 @@
-#line 2 "lex.coin.cc"
+#line 2 "lex.coin.cpp"
 
-#line 4 "lex.coin.cc"
+#line 4 "lex.coin.cpp"
 
 #define  YY_INT_ALIGNED short int
 
@@ -73,6 +73,7 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
+#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -102,8 +103,6 @@ typedef unsigned int flex_uint32_t;
 #ifndef UINT32_MAX
 #define UINT32_MAX             (4294967295U)
 #endif
-
-#endif /* ! C99 */
 
 #endif /* ! FLEXINT_H */
 
@@ -161,15 +160,7 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k.
- * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
- * Ditto for the __ia64__ case accordingly.
- */
-#define YY_BUF_SIZE 32768
-#else
 #define YY_BUF_SIZE 16384
-#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -416,7 +407,7 @@ static yyconst flex_int16_t yy_accept[104] =
 static yyconst flex_int32_t yy_ec[256] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    2,    4,    1,    1,    1,    1,    1,    5,    6,
         6,    7,    1,    6,    8,    1,    9,    5,    5,    5,
@@ -567,7 +558,7 @@ void yylloc_line(int num)
 #define YY_USER_ACTION yylloc.last_column += coin_yyleng;
 
 
-#line 571 "lex.coin.cc"
+#line 562 "lex.coin.cpp"
 
 #define INITIAL 0
 #define COMMENT 1
@@ -649,12 +640,7 @@ static int input (void );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k */
-#define YY_READ_BUF_SIZE 16384
-#else
 #define YY_READ_BUF_SIZE 8192
-#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -662,7 +648,7 @@ static int input (void );
 /* This used to be an fputs(), but since the string might contain NUL's,
  * we now use fwrite().
  */
-#define ECHO do { if (fwrite( coin_yytext, coin_yyleng, 1, coin_yyout )) {} } while (0)
+#define ECHO fwrite( coin_yytext, coin_yyleng, 1, coin_yyout )
 #endif
 
 /* Gets input and stuffs it into "buf".  number of characters read, or YY_NULL,
@@ -673,7 +659,7 @@ static int input (void );
 	if ( YY_CURRENT_BUFFER_LVALUE->yy_is_interactive ) \
 		{ \
 		int c = '*'; \
-		size_t n; \
+		int n; \
 		for ( n = 0; n < max_size && \
 			     (c = getc( coin_yyin )) != EOF && c != '\n'; ++n ) \
 			buf[n] = (char) c; \
@@ -762,7 +748,7 @@ YY_DECL
   yylloc_step();
 
 
-#line 766 "lex.coin.cc"
+#line 752 "lex.coin.cpp"
 
 	if ( !(yy_init) )
 		{
@@ -981,7 +967,7 @@ YY_RULE_SETUP
 #line 91 "coin.l"
 ECHO;
 	YY_BREAK
-#line 985 "lex.coin.cc"
+#line 971 "lex.coin.cpp"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1739,8 +1725,8 @@ YY_BUFFER_STATE coin_yy_scan_string (yyconst char * yystr )
 
 /** Setup the input buffer state to scan the given bytes. The next call to coin_yylex() will
  * scan from a @e copy of @a bytes.
- * @param yybytes the byte buffer to scan
- * @param _yybytes_len the number of bytes in the buffer pointed to by @a bytes.
+ * @param bytes the byte buffer to scan
+ * @param len the number of bytes in the buffer pointed to by @a bytes.
  * 
  * @return the newly allocated buffer state object.
  */
