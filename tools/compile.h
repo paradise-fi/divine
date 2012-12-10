56e908940a1d40dc5219a6eb74e47162b78e02de
xlenco <xlenco@fi.muni.cz>
2012-12-10 17:01:57 +0000

    LLVM: Support in divine compile.

--- tools/compile.h
+++ tools/compile.h
@@ -23,6 +23,9 @@ extern const char *generator_cesmi_client_h_str;
 extern const char *toolkit_pool_h_str;
 extern const char *toolkit_blob_h_str;
 extern const char *compile_defines_str;
+extern const char *llvm_usr_pthread_h_str;
+extern const char *llvm_usr_pthread_cpp_str;
+extern const char *llvm_usr_h_str;
 
 using namespace wibble;
 
@@ -30,7 +33,8 @@ struct Compile {
     commandline::Engine *cmd_compile;
     commandline::StandardParserWithMandatoryCommand &opts;
 
-    BoolOption *o_cesmi;
+    BoolOption *o_cesmi, *o_llvm;
+    StringOption *o_cflags;
 
     void die_help( std::string bla )
     {
@@ -112,6 +116,29 @@ struct Compile {
 	run ( "rm -f " + cesmi_aux );
     }
 
+    void compileLLVM( std::string in, std::string cflags ) {
+        std::string name( str::basename(in), 0, str::basename(in).rfind( '.' ) );
+        char tmp_dir_template[] = "__tmpXXXXXX";
+        std::string tmp_dir = mkdtemp(tmp_dir_template);
+        std::string usr_h = tmp_dir + "/usr.h";
+        std::string pthread_h = tmp_dir + "/pthread.h";
+        std::string pthread_cpp = tmp_dir + "/pthread.cpp";
+        std::string pthread_bc = tmp_dir + "/pthread.bc";
+        std::string unlinked = tmp_dir + "/_" + name + ".bc";
+        std::string linked = name + ".bc";
+
+        fs::writeFile(usr_h, llvm_usr_h_str);
+        fs::writeFile(pthread_h, llvm_usr_pthread_h_str);
+        fs::writeFile(pthread_cpp, llvm_usr_pthread_cpp_str);
+
+        std::string flags = "-c -emit-llvm -g -I'" + tmp_dir + "' " + cflags;
+        runCompiler("clang", pthread_cpp, pthread_bc, flags);
+        runCompiler("clang", in, unlinked, flags);
+        run("llvm-link " + unlinked + " " + pthread_bc + " -o " + linked);
+
+        run ( "rm -rf " + tmp_dir );
+    }
+
     void main() {
         std::string input = opts.next();
         if ( access( input.c_str(), R_OK ) )
@@ -120,9 +147,17 @@ struct Compile {
             compileDve( input );
         else if ( str::endsWith( input, ".m" ) )
             compileMurphi( input );
-        else if ( ( str::endsWith( input, ".c" ) || str::endsWith( input, ".cpp" ) || str::endsWith( input, ".cc" ) || str::endsWith( input, ".C" ))
-		  && o_cesmi->boolValue() ) 
-            compileCESMI( input );
+        else if ( ( str::endsWith( input, ".c" ) || str::endsWith( input, ".cpp" ) ||
+                    str::endsWith( input, ".cc" ) || str::endsWith( input, ".C" )) )
+            if (o_cesmi->boolValue())
+                compileCESMI( input );
+            else if (o_llvm->boolValue())
+                compileLLVM( input, o_cflags->stringValue() );
+            else {
+                std::cerr << "Do not know wheter to process input file as of CESMI type ";
+                std::cerr <<  "(use --cesmi or -c) or generate output file in LLVM bytecode format ";
+                std::cerr << "(use --llvm or -l)." << std::endl;
+            }
         else {
             std::cerr << "Do not know how to compile this file type." << std::endl;
         }
@@ -138,6 +173,15 @@ struct Compile {
         o_cesmi = cmd_compile->add< BoolOption >(
             "cesmi", 'c', "cesmi", "",
             "process input .cpp or .cc files as of CESMI type" );
+
+        o_llvm = cmd_compile->add< BoolOption >(
+            "llvm", 'l', "llvm", "",
+            "generate output file in LLVM bytecode format" );
+
+        o_cflags = cmd_compile->add< StringOption >(
+            "cflags", 'f', "cflags", "",
+            "set flags for C/C++ compiler" );
+
     }
 
 };
