ead0cec400d87f2ca0e1809eb04705b8e2867b70
Petr Rockai <me@mornfall.net>
2012-12-28 16:34:46 +0000

    Disable DVE compiler tests for now.

--- test/algorithm/metrics.sh
+++ test/algorithm/metrics.sh
@@ -2,5 +2,5 @@
 all_small metrics -w 1
 all_small metrics -w 2
 
-dve_compiled_small metrics -w 1
-dve_compiled_small metrics -w 2
+#dve_compiled_small metrics -w 1
+#dve_compiled_small metrics -w 2
