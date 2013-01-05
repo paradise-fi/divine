6583d8e9a049f6e3568472d54792bd076d301026
Petr Rockai <me@mornfall.net>
2013-01-05 11:55:02 +0000

    wibble: Simplify POSIX guards in signal.h.

--- wibble/sys/signal.h
+++ wibble/sys/signal.h
@@ -10,58 +10,45 @@ namespace wibble {
 namespace sys {
 namespace sig {
 
+#ifdef POSIX
+
 /**
  * RAII-style sigprocmask wrapper
  */
 struct ProcMask
 {
-#ifdef POSIX
 	sigset_t oldset;
-#else
-#define SIG_BLOCK 0 // FIXME, is this reasonable?
-#endif
 	ProcMask(const sigset_t& newset, int how = SIG_BLOCK)
 	{
-#ifdef POSIX
 		if (sigprocmask(how, &newset, &oldset) < 0)
 			throw wibble::exception::System("setting signal mask");
-#else
-                assert_die();
-#endif
 	}
+
 	~ProcMask()
 	{
-#ifdef POSIX
 		if (sigprocmask(SIG_SETMASK, &oldset, NULL) < 0)
 			throw wibble::exception::System("restoring signal mask");
-#endif
 	}
 };
 
 struct Action
 {
 	int signum;
-#ifdef POSIX
 	struct sigaction oldact;
-#endif
 
 	Action(int signum, const struct sigaction& act) : signum(signum)
 	{
-#ifdef POSIX
 		if (sigaction(signum, &act, &oldact) < 0)
 			throw wibble::exception::System("setting signal action");
-#else
-                assert_die();
-#endif
 	}
 	~Action()
 	{
-#ifdef POSIX
 		if (sigaction(signum, &oldact, NULL) < 0)
 			throw wibble::exception::System("restoring signal action");
-#endif
 	}
 };
+
+#endif
 
 }
 }
