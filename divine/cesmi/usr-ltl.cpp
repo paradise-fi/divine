e7423ad0326656a6990dc35e6c8ba04ad9f34a24
Petr Rockai <me@mornfall.net>
2013-01-13 22:06:01 +0000

    CESMI: A bunch of fixes for LTL support code.

--- divine/cesmi/usr-ltl.cpp
+++ divine/cesmi/usr-ltl.cpp
@@ -10,7 +10,7 @@ typedef struct {
     unsigned system:24;
 } handle;
 
-extern const int buchi_property_count;
+extern int buchi_property_count;
 
 int *buchi_state( cesmi_node state ) { return (int *)state.memory; }
 cesmi_node system_state( cesmi_node state ) {
@@ -22,6 +22,10 @@ int buchi_handle( int handle ) { return handle >> 24; }
 int system_handle( int handle ) { return handle & 0xffffff; }
 int combine_handles( int buchi, int system ) {
     return (system & 0xffffff) | (buchi << 24);
+}
+int buchi_property( cesmi_setup *cs ) {
+    struct buchi_setup *s = (struct buchi_setup *) cs->instance;
+    return cs->property - s->property_count;
 }
 
 cesmi_node buchi_make_node( void *meh, int size )
@@ -37,12 +41,15 @@ cesmi_setup override_setup( cesmi_setup *setup )
 {
     cesmi_setup r = *setup;
     r.make_node = &buchi_make_node;
-    r.allocation_handle = &setup;
+    r.allocation_handle = setup;
     return r;
 }
 
 int buchi_get_initial( cesmi_setup *setup, int handle, cesmi_node *to, get_initial_t next )
 {
+    if ( buchi_property( setup ) < 0 )
+        return next( setup, handle, to );
+
     cesmi_setup over = override_setup( setup );
     return combine_handles( 0, next( &over, handle, to ) );
 }
@@ -50,9 +57,12 @@ int buchi_get_initial( cesmi_setup *setup, int handle, cesmi_node *to, get_initi
 int buchi_get_successor( cesmi_setup *setup, int handle,
                          cesmi_node from, cesmi_node *to, get_successor_t next )
 {
+    if ( buchi_property( setup ) < 0 )
+        return next( setup, handle, from, to );
+
     cesmi_setup over = override_setup( setup );
-    int bs = buchi_next( setup->property, *buchi_state( from ),
-                         buchi_handle( handle ), system_state( from ) );
+    int bs = buchi_next( buchi_property( setup ), *buchi_state( from ),
+                         buchi_handle( handle ), setup, system_state( from ) );
 
     if ( bs ) { /* the buchi automaton can move */
         int system = next( &over, system_handle( handle ), system_state( from ), to );
@@ -66,29 +76,45 @@ int buchi_get_successor( cesmi_setup *setup, int handle,
                 *to = setup->clone_node( setup->allocation_handle, from );
                 *buchi_state( *to ) = bs;
             }
-            return combine_handles( buchi_handle( handle ) + 1, 1 );
+            return buchi_get_successor( setup, combine_handles( buchi_handle( handle ) + 1, 1 ), from, to, next );
         }
     }
 
     return 0; /* stuck */
 }
 
+uint64_t buchi_flags( cesmi_setup *setup, cesmi_node n ) {
+    if ( buchi_accepting( buchi_property( setup ), *buchi_state( n ) ) )
+        return cesmi_accepting;
+    return 0;
+}
+
 char *buchi_show_node( cesmi_setup *setup, cesmi_node from, show_node_t next )
 {
+    if ( buchi_property( setup ) < 0 )
+        return next( setup, from );
+
     char *system = next( setup, system_state( from ) );
     char *res = 0;
-    asprintf( &res, "%s [LTL: %d]", buchi_state( from ) );
+    asprintf( &res, "%s [LTL: %d]", system, *buchi_state( from ) );
     free( system );
     return res;
 }
 
 char *buchi_show_transition( cesmi_setup *setup, cesmi_node from, int handle, show_transition_t next )
 {
+    if ( buchi_property( setup ) < 0 )
+        return next( setup, from, handle );
+
     /* TODO LTL labels? */
     return next( setup, system_state( from ), system_handle( handle ) );
 }
 
-char *buchi_setup( cesmi_setup *setup )
+void buchi_setup( cesmi_setup *setup )
 {
-    setup->property_count = buchi_property_count;
+    struct buchi_setup *s = (struct buchi_setup *) malloc( sizeof( struct buchi_setup ) );
+    s->instance = setup->instance;
+    s->property_count = setup->property_count;
+    setup->instance = s;
+    setup->property_count += buchi_property_count;
 }
--- divine/cesmi/usr-ltl.h
+++ divine/cesmi/usr-ltl.h
@@ -1,5 +1,14 @@
 #include "cesmi.h"
 
+struct buchi_setup {
+    int property_count;
+    void *instance;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Code for buchi_next is generated from the LTL formula by divine compile. */
 int buchi_next( int property, int from, int transition, cesmi_setup *setup, cesmi_node evalstate );
 int buchi_accepting( int property, int id );
@@ -9,6 +18,7 @@ typedef int (*get_successor_t)( cesmi_setup *setup, int handle, cesmi_node from,
 typedef char *(*show_node_t)( cesmi_setup *setup, cesmi_node from );
 typedef char *(*show_transition_t)( cesmi_setup *setup, cesmi_node from, int handle );
 
+uint64_t buchi_flags( cesmi_setup *setup, cesmi_node n );
 int buchi_get_initial( cesmi_setup *setup, int handle, cesmi_node *to, get_initial_t next );
 int buchi_get_successor( cesmi_setup *setup, int handle, cesmi_node from,
                          cesmi_node *to, get_successor_t next );
@@ -16,4 +26,8 @@ char *buchi_show_node( cesmi_setup *setup, cesmi_node from, show_node_t next );
 char *buchi_show_transition( cesmi_setup *setup, cesmi_node from, int handle, show_transition_t next );
 
 char *buchi_show_property( cesmi_setup *setup, int property );
-char *buchi_setup( cesmi_setup *setup );
+void buchi_setup( cesmi_setup *setup );
+
+#ifdef __cplusplus
+}
+#endif
