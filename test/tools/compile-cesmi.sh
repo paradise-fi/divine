1a2ddc918d19c5e5c2ac709c0570e6aa15339571
Petr Rockai <me@mornfall.net>
2013-01-21 03:27:10 +0000

    test: Update compile-cesmi to match reality.

--- test/tools/compile-cesmi.sh
+++ test/tools/compile-cesmi.sh
@@ -2,10 +2,19 @@
 
 divine compile --cesmi data/withltl.c data/withltl.ltl
 divine info withltl.so | grep neverclaim
+
 run metrics withltl.so --property=p_1
 check statespace 24 32 0 8
+
 run metrics withltl.so --property=p_3
-check statespace 33 74 8 14
+check statespace 38 66 24 0
+run verify withltl.so --property=p_3
+check ltl_invalid
+
+run metrics withltl.so --property=p_4
+check statespace 14 18 14 4
+run verify withltl.so --property=p_4
+check ltl_invalid
 
 divine compile --cesmi data/simple.c
 divine info simple.so
