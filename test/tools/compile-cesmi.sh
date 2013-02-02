6c65d7c63e3596480b10740003c5c102009666ae
Petr Rockai <me@mornfall.net>
2013-02-02 15:54:35 +0000

    test: The DVE compiler produces .dll (CESMI) files on WIN32.

--- test/tools/compile-cesmi.sh
+++ test/tools/compile-cesmi.sh
@@ -1,28 +1,22 @@
 . lib
 
 divine compile --cesmi data/withltl.c data/withltl.ltl
-test -f withltl.so && out=withltl.so
-test -f withltl.dll && out=withltl.dll
+divine info withltl$cesmiext | grep neverclaim
 
-divine info $out | grep neverclaim
-
-run metrics $out --property=p_1
+run metrics withltl$cesmiext --property=p_1
 check statespace 24 32 0 8
 
-run metrics $out --property=p_3
+run metrics withltl$cesmiext --property=p_3
 check statespace 38 66 24 0
-run verify $out --property=p_3
+run verify withltl$cesmiext --property=p_3
 check ltl_invalid
 
-run metrics $out --property=p_4
+run metrics withltl$cesmiext --property=p_4
 check statespace 14 18 14 4
-run verify $out --property=p_4
+run verify withltl$cesmiext --property=p_4
 check ltl_invalid
 
 divine compile --cesmi data/simple.c
-test -f simple.so && out=simple.so
-test -f simple.dll && out=simple.dll
-
-divine info $out
-run metrics $out --property=p_1
+divine info simple$cesmiext
+run metrics simple$cesmiext --property=p_1
 check statespace 24 32 0 8
