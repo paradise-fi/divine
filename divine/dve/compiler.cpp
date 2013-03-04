d5014d4c147a3617c92dd4eea3f1f89430a5004d
Petr Rockai <me@mornfall.net>
2013-03-04 10:43:27 +0000

    DVE: Drop an unused variable.

--- divine/dve/compiler.cpp
+++ divine/dve/compiler.cpp
@@ -464,8 +464,6 @@ void DveCompiler::insertTransition( parse::Process & proc, parse::Transition & t
 
 void DveCompiler::analyse()
 {
-    parse::Transition * transition;
-    (void) transition;
     have_property = ast->property.valid();
     property_transitions.resize( propCount() );
 
