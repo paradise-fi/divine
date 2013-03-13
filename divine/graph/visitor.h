078d2e8c95caede98a909a19f777abd11ecd8786
Jiri Weiser <xweiser1@fi.muni.cz>
2013-03-13 23:05:00 +0000

    graph: Change structure of Partition Visitor.

--- divine/graph/visitor.h
+++ divine/graph/visitor.h
@@ -218,132 +218,138 @@ struct DFV : Common< Stack, S > {
         : Super( n, g, s, typename Super::Queue( g ) ) {}
 };
 
-template< typename S, typename Worker >
 struct Partitioned {
-    typedef typename S::Node Node;
-    typedef typename S::Label Label;
-    typedef typename S::Graph Graph;
-
-    Worker &worker;
-    typename S::Listener &notify;
-    typename S::Graph &graph;
-    typedef typename S::Store Store;
-    typedef typename Store::Hasher Hasher;
-    typedef typename S::Statistics Statistics;
-    typedef Partitioned< S, Worker > This;
-
-    Store &_store;
-
-    int owner( Node n, hash_t hint = 0 ) const {
-        return graph.owner( _store.hasher(), worker, n, hint );
-    }
-
-    inline void queue( Node from, Node to, Label label, hash_t hint = 0 ) {
-        if ( owner( to, hint ) != worker.id() )
-            return;
-        queueAny( from, to, label, hint );
-    }
-
-    inline void queueAny( Node from, Node to, Label label, hash_t hint = 0 ) {
-        int _to = owner( to, hint ), _from = worker.id();
-        Statistics::global().sent( _from, _to, sizeof(from) + memSize(to) );
-        worker.submit( _from, _to, std::make_tuple( unblob< Node >( from ),
-                                                    unblob< Node >( to ), label ) );
-    }
-
-    visitor::TransitionAction transition( Node f, Node t ) {
-        visitor::TransitionAction tact = S::transition( notify, f, t );
-        if ( tact == TerminateOnTransition )
-            worker.interrupt();
-        return tact;
-    }
-
-    visitor::ExpansionAction expansion( Node n ) {
-        assert_eq( owner( n ), worker.id() );
-        ExpansionAction eact = S::expansion( notify, n );
-        if ( eact == TerminateOnState )
-            worker.interrupt();
-        return eact;
-    }
-
-    template< typename BFV >
-    void run( BFV &bfv ) {
-        worker.restart();
-        while ( true ) {
-            if ( worker.workWaiting() ) {
-
-                int to = worker.id();
-
-                if ( worker.interrupted() ) {
-                    while ( !worker.idle() )
-                        while ( worker.comms().pending( to ) )
-                            worker.comms().take( to );
-                    return;
-                }
-
-                for ( int from = 0; from < worker.peers(); ++from ) {
-                    while ( worker.comms().pending( from, to ) ) {
-                        auto p = worker.comms().take( from, to );
-                        Statistics::global().received(
-                            from, to, sizeof( Node ) + memSize( std::get< 1 >( p ) ) );
-                        bfv.edge( unblob< Node >( std::get< 0 >( p ) ),
-                                  unblob< Node >( std::get< 1 >( p ) ),
-                                  std::get< 2 >( p ) );
-                    }
-                }
-
-                bfv.processQueue();
-
-            } else {
-                if ( worker.idle() )
-                    return;
-            }
-        }
-    }
-
-    typedef Partitioned< S, Worker > P;
-    struct Ours : SetupOverride< S, This >
-    {
-        typedef typename SetupOverride< S, This >::Listener Listener;
-        static inline TransitionAction transitionHint(
-            Listener l, Node f, Node t, Label label, hash_t hint )
-        {
-            This &n = *l.second;
-            if ( n.owner( t, hint ) != n.worker.id() ) {
-                if (n._store.valid( f ) )
-                    assert_eq( n.owner( f ), n.worker.id() );
-                n.queueAny( f, t, label, hint );
-                return visitor::IgnoreTransition;
-            }
-            return visitor::FollowTransition;
-        }
+    template< typename S >
+    struct Data {
     };
 
-    template< typename T >
-    void setIds( T &bfv ) {
-        bfv.store.id = &worker;
-        bfv._queue.id = worker.id();
-    }
+    template< typename S, typename Worker >
+    struct Implementation {
+        typedef typename S::Node Node;
+        typedef typename S::Label Label;
+        typedef typename S::Graph Graph;
 
-    void exploreFrom( Node initial ) {
-        BFV< Ours > bfv( *this, graph, _store );
-        setIds( bfv );
-        if ( owner( initial ) == worker.id() ) {
-            bfv.exploreFrom( unblob< Node >( initial ) );
+        Worker &worker;
+        typename S::Listener &notify;
+        typename S::Graph &graph;
+        typedef typename S::Store Store;
+        typedef typename Store::Hasher Hasher;
+        typedef typename S::Statistics Statistics;
+        typedef Implementation< S, Worker > This;
+
+        Store &_store;
+
+        int owner( Node n, hash_t hint = 0 ) const {
+            return graph.owner( _store.hasher(), worker, n, hint );
         }
-        run( bfv );
-    }
 
-    void processQueue() {
-        auto l = std::make_pair( &notify, this );
-        BFV< Ours > bfv( l, graph, _store );
-        setIds( bfv );
-        run( bfv );
-    }
+        inline void queue( Node from, Node to, Label label, hash_t hint = 0 ) {
+            if ( owner( to, hint ) != worker.id() )
+                return;
+            queueAny( from, to, label, hint );
+        }
 
-    Partitioned( typename S::Listener &n, Worker &w, Graph &g, Store &s )
-        : worker( w ), notify( n ), graph( g ), _store( s )
-    {}
+        inline void queueAny( Node from, Node to, Label label, hash_t hint = 0 ) {
+            int _to = owner( to, hint ), _from = worker.id();
+            Statistics::global().sent( _from, _to, sizeof(from) + memSize(to) );
+            worker.submit( _from, _to, std::make_tuple( unblob< Node >( from ),
+                                                        unblob< Node >( to ), label ) );
+        }
+
+        visitor::TransitionAction transition( Node f, Node t ) {
+            visitor::TransitionAction tact = S::transition( notify, f, t );
+            if ( tact == TerminateOnTransition )
+                worker.interrupt();
+            return tact;
+        }
+
+        visitor::ExpansionAction expansion( Node n ) {
+            assert_eq( owner( n ), worker.id() );
+            ExpansionAction eact = S::expansion( notify, n );
+            if ( eact == TerminateOnState )
+                worker.interrupt();
+            return eact;
+        }
+
+        template< typename BFV >
+        void run( BFV &bfv ) {
+            worker.restart();
+            while ( true ) {
+                if ( worker.workWaiting() ) {
+
+                    int to = worker.id();
+
+                    if ( worker.interrupted() ) {
+                        while ( !worker.idle() )
+                            while ( worker.comms().pending( to ) )
+                                worker.comms().take( to );
+                        return;
+                    }
+
+                    for ( int from = 0; from < worker.peers(); ++from ) {
+                        while ( worker.comms().pending( from, to ) ) {
+                            auto p = worker.comms().take( from, to );
+                            Statistics::global().received(
+                                from, to, sizeof( Node ) + memSize( std::get< 1 >( p ) ) );
+                            bfv.edge( unblob< Node >( std::get< 0 >( p ) ),
+                                    unblob< Node >( std::get< 1 >( p ) ),
+                                    std::get< 2 >( p ) );
+                        }
+                    }
+
+                    bfv.processQueue();
+
+                } else {
+                    if ( worker.idle() )
+                        return;
+                }
+            }
+        }
+
+        typedef Implementation< S, Worker > P;
+        struct Ours : SetupOverride< S, This >
+        {
+            typedef typename SetupOverride< S, This >::Listener Listener;
+            static inline TransitionAction transitionHint(
+                Listener l, Node f, Node t, Label label, hash_t hint )
+            {
+                This &n = *l.second;
+                if ( n.owner( t, hint ) != n.worker.id() ) {
+                    if (n._store.valid( f ) )
+                        assert_eq( n.owner( f ), n.worker.id() );
+                    n.queueAny( f, t, label, hint );
+                    return visitor::IgnoreTransition;
+                }
+                return visitor::FollowTransition;
+            }
+        };
+
+        template< typename T >
+        void setIds( T &bfv ) {
+            bfv.store.id = &worker;
+            bfv._queue.id = worker.id();
+        }
+
+        void exploreFrom( Node initial ) {
+            BFV< Ours > bfv( *this, graph, _store );
+            setIds( bfv );
+            if ( owner( initial ) == worker.id() ) {
+                bfv.exploreFrom( unblob< Node >( initial ) );
+            }
+            run( bfv );
+        }
+
+        void processQueue() {
+            auto l = std::make_pair( &notify, this );
+            BFV< Ours > bfv( l, graph, _store );
+            setIds( bfv );
+            run( bfv );
+        }
+
+        Implementation( typename S::Listener &n, Worker &w, Graph &g, Store &s, Data< typename S::AlgorithmSetup > )
+            : worker( w ), notify( n ), graph( g ), _store( s )
+        {}
+    };
 };
 
 /*
