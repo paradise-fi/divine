940256b3e29f976f63ee40776bfbb9e62dc4ea0b
Petr Rockai <me@mornfall.net>
2013-04-14 09:42:33 +0000

    wibble: Fix printing of unconditional assertion failures.

--- wibble/test.cpp
+++ wibble/test.cpp
@@ -5,7 +5,9 @@ int assertFailure = 0;
 
 void assert_die_fn( Location l )
 {
-    AssertFailed f( l );
+    {
+        AssertFailed f( l );
+    }
     abort(); // in the NDEBUG case, the above is a no-op
 }
 
