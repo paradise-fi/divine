20443c3b86992a43c1bcf887ed1a382f8b1b2e58
Petr Rockai <me@mornfall.net>
2013-04-14 20:59:07 +0000

    NIX: Build our own Qt on Windows.

--- nix/windows_qt.nix
+++ nix/windows_qt.nix
@@ -1,19 +1,43 @@
-{stdenv, fetchurl, writeScript}:
+{stdenv, fetchurl, windows_img, windows_mingw, writeScript, pkgs}:
 
 stdenv.mkDerivation rec {
   name = "windows-qt";
   version = "4.8.4";
   pkg = "qt-${version}-x86";
 
-  qt = fetchurl {
-    url = "http://releases.qt-project.org/qt4/source/qt-win-opensource-${version}-mingw.exe";
-    sha256 = "12gnrxan9vdg951126y3balrbzk89awkscn5pa2k6z9il5pdvab0";
+  source = fetchurl {
+    url = "http://releases.qt-project.org/qt4/source/qt-everywhere-opensource-src-4.8.4.tar.gz";
+    sha256 = "0w1j16q6glniv4hppdgcvw52w72gb2jab35ylkw0qjn5lj5y7c1k";
   };
 
-  builder = writeScript "${name}-builder" "source $stdenv/setup && ensureDir $out";
-  unpack = "cp ${qt} qt-setup.exe";
+  build = pkgs.callPackage ./windows_build.nix {
+    inherit windows_mingw;
+    img = windows_img;
+    src = source;
+    name = "qt";
+    mem = "2048M";
+    buildScript = ''
+      set -ex
+      cd source
+      echo y | ./configure.exe -opensource -fast -release
+      for i in `seq 1 20`; do # make seems to die often due to a bug in MSYS
+          make && break
+      done
+      make install INSTALL_ROOT=/install
+      cp -R C:/install/WORK/source E:/qt
+    '';
+  };
+
+  builder = writeScript "${name}-builder" ''
+      source $stdenv/setup
+      ensureDir $out
+      ln -s ${build}/qt $out/qt
+  '';
+
+  unpack = "cp -R ${build}/qt qt";
   install = ''
-      D:\qt-setup.exe /S
-      set PATH=%PATH%;C:\Qt\${version}\bin
+      set PATH=%PATH%;D:\qt\bin
+      set QTDIR=D:\qt
+      setx QTDIR D:\qt
   '';
 }
