6d9418dc130c3e4664a151953989b3ccef68e27b
Petr Rockai <me@mornfall.net>
2013-04-20 16:04:36 +0000

    build: Generalize stringify a little, move to cmake/.

---
+++ cmake/stringify.cmake
@@ -0,0 +1,24 @@
+set( STRINGIFY ${CMAKE_SOURCE_DIR}/cmake/stringify.sh )
+
+macro( stringify file )
+  string( REPLACE "." "_" fileu "${file}" )
+  string( REPLACE "/" "_" fileu "${fileu}" )
+  string( REPLACE "-" "_" fileu "${fileu}" )
+  add_custom_command(
+    OUTPUT ${fileu}_str.cpp
+    DEPENDS ${file} ${STRINGIFY}
+    COMMAND sh ${STRINGIFY} "${CMAKE_CURRENT_SOURCE_DIR}" "${file}"
+    VERBATIM
+  )
+  list( APPEND STRINGIFIED_FILES "${file}" )
+  list( APPEND STRINGIFIED "${fileu}_str.cpp" )
+endmacro( stringify )
+
+macro( stringlist out )
+  add_custom_command(
+    OUTPUT ${out}_list.cpp
+    DEPENDS ${STRINGIFY}
+    COMMAND sh ${STRINGIFY} "-l" "${out}" ${STRINGIFIED_FILES}
+    VERBATIM
+  )
+endmacro( stringlist )
---
+++ cmake/stringify.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+flatten() {
+    sed -e "s|\\.|_|" -e "s|/|_|" -e "s|-|_|"
+}
+
+if test "$1" = "-l"; then
+    var="$2_list"
+    out="$var.cpp"
+    rm -f $out
+    shift 2
+    echo "namespace divine {" >> $out
+
+    for n in "$@"; do
+        echo "extern const char *$(echo $n | flatten)_str;" >> $out
+    done
+
+    echo "struct stringtable { const char *n, *c; };" >> $out
+    echo "stringtable ${var}[] = { " >> $out
+    for n in "$@"; do
+        var=$(echo "$n" | flatten)
+        echo "{ \"$n\", ${var}_str }," >> $out
+    done
+    echo "{ nullptr, nullptr }" >> $out
+    echo "};" >> $out
+    echo "}" >> $out
+else
+    flat=$(echo $2 | flatten)
+    sed -e '1i \
+namespace divine { const char *'"${flat}"'_str = "\\' \
+    -e 's,\\,\\\\,g' \
+    -e 's,$,\\n\\,' \
+    -e 's,",\\",g' \
+    -e 's,wibble/test.h,cassert,' \
+    -e '$a \
+\"\; }' \
+    < "$1/$2" > "${flat}_str.cpp"
+fi
