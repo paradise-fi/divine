fb476686435e508c71f9323f6bff1b236e3e6a95
Petr Rockai <me@mornfall.net>
2013-04-28 16:29:29 +0000

    toolkit: Re-enable last NTreeHashSet unit test.

--- divine/toolkit/ntreehashset.test.h
+++ divine/toolkit/ntreehashset.test.h
@@ -61,24 +61,6 @@ struct TestNTreeHashSet {
         return static_cast< unsigned >( static_cast< unsigned char >( c ) );
     }
 
-/*
-    template< typename Table >
-    void free( Table& t, Pool& pool ) {
-        for ( int i = 0; i < t._roots.size(); ++i ) {
-            if ( t._roots[ i ] != nullptr )
-                t._roots[ i ]->free( pool );
-        }
-        for ( int i = 0; i < t._forks.size(); ++i ) {
-            if ( t._forks[ i ] != nullptr )
-                t._forks[ i ]->free( pool );
-        }
-        for ( int i = 0; i < t._leafs.size(); ++i ) {
-            if ( t._leafs[ i ] != nullptr )
-                t._leafs[ i ]->free( pool );
-        }
-    }
-*/
-
     Test binary2() {
         FakeGeneratorBinary fg;
         BlobSet set( Hasher( fg.pool() ) );
@@ -125,11 +107,9 @@ struct TestNTreeHashSet {
 
         for ( auto x : { b, b2 } )
             fg.pool().free( x );
-        // free( set, fg.pool() );
     }
 
-/*
-    void binary4() {
+    Test binary4() {
         FakeGeneratorBinary fg;
         BlobSet set( Hasher( fg.pool() ) );
 
@@ -137,62 +117,59 @@ struct TestNTreeHashSet {
         for ( unsigned i = 0; i < 67; ++i )
             fg.pool().dereference( b )[ i ] = char(i & 0xff);
 
-        BlobSet::Root* root;
+        BlobSet::Root root;
         bool inserted;
         std::tie( root, inserted ) = set.insertHinted( b, set.hasher.hash( b ), fg );
         assert( inserted );
-        assert( !root->leaf );
-        assert_eq( root->selfSize, sizeof( BlobSet::Root ) + 2 * sizeof( BlobSet::LeafOrFork ) );
-        assert_eq( root->forksSize(), 2 * sizeof( BlobSet::LeafOrFork ) );
-        assert_eq( uintptr_t( root->slack() ), uintptr_t( root ) + root->selfSize );
-        assert_eq( root + root->selfSize, root + sizeof( BlobSet::Root ) + root->forksSize() );
-        auto childs = root->childs();
+        assert( !root.leaf( fg.pool() ) );
+        assert_eq( root.forkcount( fg.pool() ), 2 );
+        auto children = root.childvector( fg.pool() );
 
-        assert( childs[ 0 ].isFork() );
-        assert( childs[ 1 ].isFork() );
+        assert( children[ 0 ].isFork() );
+        assert( children[ 1 ].isFork() );
 
-        assert( !childs[ 0 ].end() );
-        assert( childs[ 1 ].end() );
+        assert_eq( children.size(), 2 );
 
-        assert_eq( childs[ 0 ].fork()->size(), 2 * sizeof( BlobSet::LeafOrFork ) );
-        assert_eq( childs[ 1 ].fork()->size(), 2 * sizeof( BlobSet::LeafOrFork ) );
+        assert_eq( children[ 0 ].fork().forkcount( fg.pool() ), 2 );
+        assert_eq( children[ 1 ].fork().forkcount( fg.pool() ), 2 );
 
-        assert( childs[ 0 ].fork()->childs[ 0 ].isLeaf() );
-        assert( childs[ 0 ].fork()->childs[ 1 ].isLeaf() );
-        assert( childs[ 1 ].fork()->childs[ 0 ].isLeaf() );
-        assert( childs[ 1 ].fork()->childs[ 1 ].isLeaf() );
+        auto left = children[ 0 ].fork().childvector( fg.pool() );
+        auto right = children[ 1 ].fork().childvector( fg.pool() );
 
-        assert( !childs[ 0 ].fork()->childs[ 0 ].end() );
-        assert( childs[ 0 ].fork()->childs[ 1 ].end() );
-        assert( !childs[ 1 ].fork()->childs[ 0 ].end() );
-        assert( childs[ 1 ].fork()->childs[ 1 ].end() );
+        assert( left[ 0 ].isLeaf() );
+        assert( left[ 1 ].isLeaf() );
+        assert( right[ 0 ].isLeaf() );
+        assert( right[ 1 ].isLeaf() );
 
-        assert_eq( childs[ 0 ].fork()->childs[ 0 ].leaf()->size, 17 );
-        assert_eq( childs[ 0 ].fork()->childs[ 1 ].leaf()->size, 17 );
-        assert_eq( childs[ 1 ].fork()->childs[ 0 ].leaf()->size, 17 );
-        assert_eq( childs[ 1 ].fork()->childs[ 1 ].leaf()->size, 16 );
+        assert_eq( left.size(), 2 );
+        assert_eq( right.size(), 2 );
+
+        assert_eq( left[ 0 ].leaf().size( fg.pool() ), 17 );
+        assert_eq( left[ 1 ].leaf().size( fg.pool() ), 17 );
+        assert_eq( right[ 0 ].leaf().size( fg.pool() ), 17 );
+        assert_eq( right[ 1 ].leaf().size( fg.pool() ), 16 );
 
         for ( unsigned i = 0; i < 67; ++i )
-            assert_eq( c2u( childs[ i / 34 ].fork()->childs[ (i / 17) % 2 ]
-                        .leaf()->data[ i % 17 ] ), i & 0xff );
+            assert_eq( c2u( ( i < 34 ? left : right )[ (i / 17) % 2 ]
+                            .leaf().data( fg.pool() )[ i % 17 ] ),
+                       i & 0xff );
 
-        Blob b2 = root->reassemble( fg.pool() );
+        Blob b2 = root.reassemble( fg.pool() );
         assert( fg.pool().equal( b, b2 ) );
 
         std::tie( std::ignore, inserted ) = set.insertHinted( b, set.hasher.hash( b ), fg );
         assert( !inserted );
 
-        BlobSet::Root* root2;
+        BlobSet::Root root2;
         bool had;
         std::tie( root2, had ) = set.get( b );
         assert( had );
-        assert_eq( root, root2 );
+        assert_eq( root.b.raw(), root2.b.raw() );
 
         for ( auto x : { b, b2 } )
             fg.pool().free( x );
-        free( set, fg.pool() );
     }
-*/
+
     Test basicFlat() {
         return basic< FakeGeneratorFlat, true >();
     }
@@ -239,7 +216,6 @@ struct TestNTreeHashSet {
 
         for ( auto x : { b, b2 } )
             fg.pool().free( x );
-        // free( set, fg.pool() );
     }
 
     template< typename Generator, bool leaf >
