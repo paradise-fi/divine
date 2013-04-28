9a0df70642306fb91696e8bd4e65a103ed6b82e7
Petr Rockai <me@mornfall.net>
2013-04-28 09:37:32 +0000

    test: Make the reachability POR test a little more thorough.

--- test/reduction/reachability-por.sh
+++ test/reduction/reachability-por.sh
@@ -5,4 +5,5 @@ for COMP in $COMPRESSIONS
 do
     run reachability --reduce=por data/por.dve --compression=$COMP
     check reachability_deadlock
+    dve_small reachability --reduce=por --compression=$COMP
 done
