e9b9d226a12f6ecb35aed2f2d835bf70838eb755
Petr Rockai <me@mornfall.net>
2013-04-30 14:05:40 +0000

    LLVM: Fix warnings in usr-cstdlib.cpp stubs.

--- divine/llvm/usr-cstdlib.cpp
+++ divine/llvm/usr-cstdlib.cpp
@@ -63,7 +63,7 @@ extern "C" void fputs() { __divine_assert( 0 ); }
 extern "C" void abort() { __divine_assert( 0 ); }
 extern "C" void abort_message( const char * ) { __divine_assert( 0 ); }
 
-extern "C" void *dlsym(void *, void *) { __divine_assert( 0 ); } // oh golly...
+extern "C" void *dlsym(void *, void *) { __divine_assert( 0 ); return 0; } // oh golly...
 
 /******** stdio.h stubs */
 
@@ -97,21 +97,21 @@ extern "C" int isxdigit(int c) { __divine_assert( 0 ); return 0; }
 
 #include "libsupc++/unwind.h"
 
-_Unwind_Reason_Code _Unwind_RaiseException (struct _Unwind_Exception *) { __divine_assert( 0 ); }
-_Unwind_Reason_Code _Unwind_Resume_or_Rethrow (struct _Unwind_Exception *) { __divine_assert( 0 ); }
-_Unwind_Reason_Code _Unwind_ForcedUnwind (struct _Unwind_Exception *, _Unwind_Stop_Fn, void *) { __divine_assert( 0 ); }
+_Unwind_Reason_Code _Unwind_RaiseException (struct _Unwind_Exception *) { __divine_assert( 0 ); return _URC_NO_REASON; }
+_Unwind_Reason_Code _Unwind_Resume_or_Rethrow (struct _Unwind_Exception *) { __divine_assert( 0 ); return _URC_NO_REASON; }
+_Unwind_Reason_Code _Unwind_ForcedUnwind (struct _Unwind_Exception *, _Unwind_Stop_Fn, void *) { __divine_assert( 0 ); return _URC_NO_REASON; }
 void _Unwind_DeleteException (struct _Unwind_Exception *) { __divine_assert( 0 ); }
-_Unwind_Word _Unwind_GetGR (struct _Unwind_Context *, int) { __divine_assert( 0 ); }
+_Unwind_Word _Unwind_GetGR (struct _Unwind_Context *, int) { __divine_assert( 0 ); return 0; }
 void _Unwind_SetGR (struct _Unwind_Context *, int, _Unwind_Word) { __divine_assert( 0 ); }
-_Unwind_Ptr _Unwind_GetIP (struct _Unwind_Context *) { __divine_assert( 0 ); }
-_Unwind_Ptr _Unwind_GetIPInfo (struct _Unwind_Context *, int *) { __divine_assert( 0 ); }
+_Unwind_Ptr _Unwind_GetIP (struct _Unwind_Context *) { __divine_assert( 0 ); return 0; }
+_Unwind_Ptr _Unwind_GetIPInfo (struct _Unwind_Context *, int *) { __divine_assert( 0 ); return 0; }
 void _Unwind_SetIP (struct _Unwind_Context *, _Unwind_Ptr) { __divine_assert( 0 ); }
-_Unwind_Word _Unwind_GetCFA (struct _Unwind_Context *) { __divine_assert( 0 ); }
-void *_Unwind_GetLanguageSpecificData (struct _Unwind_Context *) { __divine_assert( 0 ); }
-_Unwind_Ptr _Unwind_GetRegionStart (struct _Unwind_Context *) { __divine_assert( 0 ); }
+_Unwind_Word _Unwind_GetCFA (struct _Unwind_Context *) { __divine_assert( 0 ); return 0; }
+void *_Unwind_GetLanguageSpecificData (struct _Unwind_Context *) { __divine_assert( 0 ); return 0; }
+_Unwind_Ptr _Unwind_GetRegionStart (struct _Unwind_Context *) { __divine_assert( 0 ); return 0; }
 void _Unwind_SjLj_Register (struct SjLj_Function_Context *) { __divine_assert( 0 ); }
 void _Unwind_SjLj_Unregister (struct SjLj_Function_Context *) { __divine_assert( 0 ); }
-_Unwind_Word _Unwind_GetBSP (struct _Unwind_Context *) { __divine_assert( 0 ); }
-_Unwind_Ptr _Unwind_GetDataRelBase (struct _Unwind_Context *) { __divine_assert( 0 ); }
-_Unwind_Ptr _Unwind_GetTextRelBase (struct _Unwind_Context *) { __divine_assert( 0 ); }
-void * _Unwind_FindEnclosingFunction (void *pc) { __divine_assert( 0 ); }
+_Unwind_Word _Unwind_GetBSP (struct _Unwind_Context *) { __divine_assert( 0 ); return 0; }
+_Unwind_Ptr _Unwind_GetDataRelBase (struct _Unwind_Context *) { __divine_assert( 0 ); return 0; }
+_Unwind_Ptr _Unwind_GetTextRelBase (struct _Unwind_Context *) { __divine_assert( 0 ); return 0; }
+void * _Unwind_FindEnclosingFunction (void *pc) { __divine_assert( 0 ); return 0; }
