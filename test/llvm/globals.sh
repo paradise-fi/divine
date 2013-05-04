9ca04fd6536e097cdc7975986f18587ddd60a7ab
Petr Rockai <me@mornfall.net>
2013-05-04 13:19:30 +0000

    test: Add a few tests for handling globals in LLVM.

---
+++ test/llvm/globals.sh
@@ -0,0 +1,30 @@
+. lib
+
+llvm_verify valid <<EOF
+int array[2];
+void main() {
+    array[1] = 3;
+    __divine_assert( array[1] == 3 );
+}
+EOF
+
+llvm_verify invalid "bad dereference" testcase.c:3 <<EOF
+int array[2];
+void main() {
+    array[2] = 3;
+}
+EOF
+
+llvm_verify valid <<EOF
+int array[2];
+void main() {
+    memset( array, 1, 2 * sizeof( int ) );
+}
+EOF
+
+llvm_verify invalid "bad dereference" memset.c <<EOF
+int array[2];
+void main() {
+    memset( array, 1, 3 * sizeof( int ) );
+}
+EOF
