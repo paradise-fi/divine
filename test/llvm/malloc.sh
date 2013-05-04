8cf5122243e5a6c763cf345ed65c9ea86d959f7f
Petr Rockai <me@mornfall.net>
2013-05-04 13:08:23 +0000

    test: Add a couple cases for invalid heap dereferences in LLVM.

---
+++ test/llvm/malloc.sh
@@ -0,0 +1,35 @@
+. lib
+
+llvm_verify invalid "bad dereference" testcase.c:6 <<EOF
+#include <stdlib.h>
+
+void main() {
+    int *mem = malloc(4);
+    if ( mem ) {
+        mem[2] = 3;
+    }
+}
+EOF
+
+llvm_verify valid <<EOF
+#include <stdlib.h>
+
+void main() {
+    int *mem = malloc(4);
+    if ( mem ) {
+        mem[0] = 3;
+    }
+}
+EOF
+
+llvm_verify invalid "bad dereference" testcase.c:7 <<EOF
+#include <stdlib.h>
+
+void main() {
+    int *mem = malloc(4);
+    if ( mem ) {
+        free( mem );
+        mem[0] = 3;
+    }
+}
+EOF
