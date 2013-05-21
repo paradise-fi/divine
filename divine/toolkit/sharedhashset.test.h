3d80379b565c84fabc8d5967b5abbde0b5f6c2c9
xweiser1 <xweiser1@fi.muni.cz>
2013-05-21 22:39:44 +0000

    toolkit: Update SharedHashSet tests.

--- divine/toolkit/sharedhashset.test.h
+++ divine/toolkit/sharedhashset.test.h
@@ -1,15 +1,61 @@
 // -*- C++ -*- (c) 2010 Petr Rockai <me@mornfall.net>
 
+#include <set>
 #include <wibble/sys/thread.h>
 #include <divine/toolkit/sharedhashset.h>
 
 using namespace divine;
 
-struct TestSharedHashset
+template< typename _Item, typename _Hasher = divine::default_hasher< _Item > >
+struct SharedHashSetWrapper {
+
+    using Set = SharedHashSet< _Item, _Hasher >;
+    using Hasher = _Hasher;
+    using Item = _Item;
+
+    Set &table;
+    Hasher &hasher;
+    typename Set::ThreadData tld;
+
+    SharedHashSetWrapper( Set &t ) : table( t ), hasher( table.hasher ), tld()
+    {}
+
+    void setSize( size_t s ) {
+        table.setSize( s );
+    }
+
+    size_t size() {
+        return table.size();
+    }
+
+    Item operator[]( size_t index ) {
+        return table[ index ];
+    }
+
+    std::tuple< Item, bool > insertHinted( Item x, hash_t h ) {
+        return table.insertHinted( x, h, tld );
+    }
+
+    std::tuple< Item, bool > getHinted( Item x, hash_t h ) {
+        return table.getHinted( x, h, tld );
+    }
+
+    std::tuple< Item, bool > insert( Item x ) {
+        return insertHinted( x, hasher.hash( x ) );
+    }
+
+    bool has( Item x ) {
+        return std::get< 1 >( getHinted( x, hasher.hash( x ) ) );
+    }
+};
+
+struct TestSharedHashSet
 {
 
     Test basic() {
-        SharedHashSet< int > set;
+        SharedHashSet< int > _set;
+        SharedHashSetWrapper< int > set( _set );
+
         set.setSize( 8 );
 
         assert( !set.has( 1 ) );
@@ -23,15 +69,15 @@ struct TestSharedHashset
     }
 
     struct Insert : wibble::sys::Thread {
-        SharedHashSet< int > *set;
+        SharedHashSetWrapper< int > *set;
         int from, to;
         bool overlap;
 
         void *main() {
             for ( int i = from; i < to; ++i ) {
                 set->insert( i );
-                assert( set->has( i ) );
-                if (!overlap && i < to - 1)
+                assert( !std::get< 1 >( set->insert( i ) ) );
+                if ( !overlap && i < to - 1 )
                     assert( !set->has( i + 1 ) );
             }
             return 0;
@@ -39,7 +85,8 @@ struct TestSharedHashset
     };
 
     Test stress() {
-        SharedHashSet< int > set;
+        SharedHashSet< int > _set;
+        SharedHashSetWrapper< int > set( _set );
         set.setSize( 4 * 1024 );
         Insert a;
         a.set = &set;
@@ -60,13 +107,17 @@ struct TestSharedHashset
         a.to = t1;
         b.from = f2;
         b.to = t2;
-        a.set = b.set = set;
+        a.set = new SharedHashSetWrapper< int >( *set );
+        b.set = new SharedHashSetWrapper< int >( *set );
         a.overlap = b.overlap = (t1 > f2);
 
         a.start();
         b.start();
         a.join();
         b.join();
+
+        delete a.set;
+        delete b.set;
     }
 
     void multi( SharedHashSet< int > *set, std::size_t count, int from, int to ) {
@@ -75,38 +126,49 @@ struct TestSharedHashset
         for ( std::size_t i = 0; i < count; ++i ) {
             field[ i ].from = from;
             field[ i ].to = to;
-            field[ i ].set = set;
+            field[ i ].set = new SharedHashSetWrapper< int >( *set );
             field[ i ].overlap = true;
         }
 
         for ( std::size_t i = 0; i < count; ++i )
             field[ i ].start();
 
-        for ( std::size_t i = 0; i < count; ++i )
+        for ( std::size_t i = 0; i < count; ++i ) {
             field[ i ].join();
+            delete field[ i ].set;
+        }
         delete[] field;
     }
 
     Test multistress() {
-        SharedHashSet< int > set;
+        SharedHashSet< int > _set;
+        SharedHashSetWrapper< int > set( _set );
         set.setSize( 4 * 1024 );
-        multi( &set, 10, 1, 32 * 1024 );
+        multi( &_set, 10, 1, 32 * 1024 );
 
         for  ( int i = 1; i < 32 * 1024; ++i ) {
             assert( set.has( i ) );
         }
         int count = 0;
+        std::set< int > s;
         for ( size_t i = 0; i != set.size(); ++i ) {
-            if ( set[ i ] )
+            if ( set[ i ] ) {
+                if ( s.find( set[ i ] ) != s.end() )
+                    std::cout << set[ i ] << std::endl;
+                else
+                    s.insert( set[ i ] );
                 ++count;
+            }
         }
         assert_eq( count, 32 * 1024 - 1 );
     }
 
     Test parstress() {
-        SharedHashSet< int > set;
+        SharedHashSet< int > _set;
+        SharedHashSetWrapper< int > set( _set );
+
         set.setSize( 4 * 1024 );
-        par( &set, 1, 16*1024, 8*1024, 32*1024 );
+        par( &_set, 1, 16*1024, 8*1024, 32*1024 );
 
         for ( int i = 1; i < 32*1024; ++i ) {
             assert( set.has( i ) );
@@ -114,13 +176,14 @@ struct TestSharedHashset
     }
 
     Test set() {
-        SharedHashSet< int > set;
+        SharedHashSet< int > _set;
+        SharedHashSetWrapper< int > set( _set );
         set.setSize( 4 * 1024 );
         for ( int i = 1; i < 32*1024; ++i ) {
             assert( !set.has( i ) );
         }
 
-        par( &set, 1, 16*1024, 16*1024, 32*1024 );
+        par( &_set, 1, 16*1024, 16*1024, 32*1024 );
 
         for ( int i = 1; i < 32*1024; ++i ) {
             assert_eq( i, i * set.has( i ) );
