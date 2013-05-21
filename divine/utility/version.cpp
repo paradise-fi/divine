a4c5ae82fad96dfd430457bf40b0b80e0a73f02e
Petr Rockai <me@mornfall.net>
2013-05-21 16:23:30 +0000

    Bump version to 2.97 (3.0 RC 2).

--- divine/utility/version.cpp
+++ divine/utility/version.cpp
@@ -12,9 +12,9 @@ extern const char *DIVINE_SOURCE_SHA;
 extern const char *DIVINE_BUILD_DATE;
 
 // You need to update the version number *and* the SHA to current release one.
-#define DIVINE_VERSION "2.96"
-#define DIVINE_RELSTATE " (3.0 RC 1)"
-#define DIVINE_RELEASE_SHA "6d9241dffcbc3cbf5b5934427a7fb53e472934a7"
+#define DIVINE_VERSION "2.97"
+#define DIVINE_RELSTATE " (3.0 RC 2)"
+#define DIVINE_RELEASE_SHA "4e5b6ba11ee07b6cb7a2b830dd8c4c93115fc115"
 
 namespace divine {
 
