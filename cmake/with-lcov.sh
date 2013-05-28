0d62d3fdac25b0588604f73263eed50c7ae3468e
Petr Rockai <me@mornfall.net>
2013-05-28 09:33:32 +0000

    cmake: Make lcov-related errors non-fatal.

--- cmake/with-lcov.sh
+++ cmake/with-lcov.sh
@@ -8,12 +8,14 @@ set -ex
 
 mkdir -p "${LCOV_OUT}"
 
-test -e "${LCOV_OUT}/base.info" || {
+if ! test -e "${LCOV_OUT}/failed" && ! test -e "${LCOV_OUT}/base.info"; then
     LCOV --zerocounters
-    LCOV --capture --initial -o "${LCOV_OUT}/base.info"
-}
+    LCOV --capture --initial -o "${LCOV_OUT}/base.info" || touch "${LCOV_OUT}/failed"
+fi
 
 eval "$@"
+
+test -e "${LCOV_OUT}/failed" && exit 0
 
 for i in `seq 1 32`; do
     test -f "${LCOV_OUT}/collect-$i.info" && continue
