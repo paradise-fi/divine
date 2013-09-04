0f0de3c15908e9cb62d86ac8f4cc5df00460a8dc
Vladimir Still <xstill@fi.muni.cz>
2013-09-04 20:26:54 +0000

    LLVM: Add test of loading atomic propositions from AP enum.

---
+++ test/llvm/ap_enum.sh
@@ -0,0 +1,14 @@
+. lib
+
+# this test tests that atomic proposition names get demangled
+# if they dont't divine will fail
+
+llvm_verify ltl_valid p0 <<EOF
+#include <divine.h>
+enum AP { _test_ap_bla_bla_123_ = 0x42 };
+LTL(p0, GF _test_ap_bla_bla_123_ );
+
+int main() {
+    while (1) { ap( _test_ap_bla_bla_123_ ); }
+}
+EOF
