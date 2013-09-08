718d6db12879a2650ded86ca0830b51f87c0ccf6
Petr Rockai <me@mornfall.net>
2013-09-08 16:52:46 +0000

    LLVM: Add a test for C++ exceptions.

---
+++ test/llvm/exceptions.sh
@@ -0,0 +1,54 @@
+. lib
+
+llvm_verify_cpp invalid ASSERTION testcase.cpp:5 <<EOF
+#include <cassert>
+int main() {
+    try { throw 5; }
+    catch( ... ) {}
+    assert( 0 );
+}
+EOF
+
+llvm_verify_cpp valid <<EOF
+#include <cassert>
+#include <exception>
+
+int a = 0;
+int b = 0;
+
+struct destructible {
+    ~destructible() { a = 1; }
+};
+
+int throws() {
+    std::exception blergh;
+    throw blergh;
+}
+
+int dt() {
+    destructible x;
+    try {
+        throws();
+    } catch (...) {
+        a = b = 3;
+        throw;
+    }
+}
+
+int main() {
+    int x = 0;
+    try {
+        x = 3;
+        dt();
+    } catch (std::exception &e) {
+        x = 8;
+    } catch (...) {
+        x = 2;
+    }
+    assert( x == 8 );
+    assert( a == 1 );
+    assert( b == 3 );
+    return 0;
+}
+EOF
+
