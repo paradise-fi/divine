1038143ec5621fe9fce70c6e6c24f2f05e37bf57
Petr Rockai <me@mornfall.net>
2013-09-13 20:20:32 +0000

    test: Fix llvm/memcpy to cope with failing malloc().

--- test/llvm/memcpy.sh
+++ test/llvm/memcpy.sh
@@ -4,7 +4,8 @@ llvm_verify invalid assert memcpy <<EOF
 #include <string.h>
 void main() {
     char *mem = malloc(4);
-    memcpy( mem + 1, mem, 2 );
+    if ( mem )
+        memcpy( mem + 1, mem, 2 );
 }
 EOF
 
@@ -12,7 +13,8 @@ llvm_verify invalid assert memcpy <<EOF
 #include <string.h>
 void main() {
     char *mem = malloc(4);
-    memcpy( mem, mem + 1, 2 );
+    if ( mem )
+        memcpy( mem, mem + 1, 2 );
 }
 EOF
 
@@ -21,9 +23,11 @@ llvm_verify valid <<EOF
 #include <assert.h>
 void main() {
     char *mem = malloc(4);
-    mem[0] = 4;
-    memmove( mem + 1, mem, 2 );
-    assert( mem[0] == 4 );
-    assert( mem[1] == 4 );
+    if ( mem ) {
+        mem[0] = 4;
+        memmove( mem + 1, mem, 2 );
+        assert( mem[0] == 4 );
+        assert( mem[1] == 4 );
+    }
 }
 EOF
