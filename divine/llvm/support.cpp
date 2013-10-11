4d8fb5426efd28ae82f5f0b83efca49612a0ddb0
Vladimir Still <xstill@fi.muni.cz>
2013-10-11 15:12:41 +0000

    llvm: Put multithreaded initialization to separate file in llvm directory.
    It was previously in instances/definition.h.

---
+++ divine/llvm/support.cpp
@@ -0,0 +1,18 @@
+#ifdef O_LLVM
+#include <llvm/Support/Threading.h>
+#endif
+
+namespace divine {
+namespace llvm {
+
+bool initMultithreaded() {
+#ifdef O_LLVM
+    return ::llvm::llvm_is_multithreaded() || ::llvm::llvm_start_multithreaded();
+#else
+    assert_unreachable( "LLVM not available" );
+#endif
+}
+
+}
+}
+
---
+++ divine/llvm/support.h
@@ -0,0 +1,10 @@
+#ifndef DIVINE_LLVM_SUPPORT
+#define DIVINE_LLVM_SUPPORT
+
+namespace divine {
+namespace llvm {
+bool initMultithreaded();
+}
+}
+
+#endif
