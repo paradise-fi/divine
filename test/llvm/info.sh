07b17f9a1fd6cccdbab8e0cfc40f141680b783b7
Vladimir Still <xstill@fi.muni.cz>
2013-10-19 19:23:03 +0000

    test: Add functional test running info on all LLVM examples.

---
+++ test/llvm/info.sh
@@ -0,0 +1,22 @@
+. lib
+
+test "$O_LLVM" = "ON" || skip
+llvm_precompile
+
+for I in llvm_examples/*.c llvm_examples/*.cpp; do
+
+    if echo "$I" | grep '\.c\(pp\|c\)$'; then
+        COMPILATION=$(cat $I | grep -A3 '\(Verification\|Solution\)' | fgrep 'divine compile' | head -n1 | fgrep -- '--cflags' | sed 's/^.*\*//')
+        if echo "$COMPILATION" | fgrep -- '-std='; then
+            STD=$(echo $COMPILATION | sed 's/^.*-std=\([^ ]*\).*$/\1/')
+        fi
+    fi
+    if ! [ -z $STD ]; then
+        CFLAGS="--cflags='-std=$STD'"
+    fi
+    echo "CFLAGS: $CFLAGS"
+    divine compile --llvm $I $CFLAGS --precompiled=. >& progress
+    BC=$(echo $I | sed 's,^.*/,,' | sed 's/\.c\(pp\|c\)\{0,1\}$/.bc/')
+    echo "BC: $BC"
+    divine info $BC 2>> progress | capture
+done
