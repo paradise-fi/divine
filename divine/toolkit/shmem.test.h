2f54a0ba9ce6124e6ff4013e3c530dd02aca29dc
xweiser1 <xweiser1@fi.muni.cz>
2013-10-27 15:02:24 +0000

    toolkit: Add tests for ApproximateCounter.

--- divine/toolkit/shmem.test.h
+++ divine/toolkit/shmem.test.h
@@ -35,5 +35,105 @@ struct TestSharedMemoryUtilities {
             assert_eq( sh.counter.load(), 0 );
         }
     }
+
+    struct CounterWorker : wibble::sys::Thread {
+        divine::StartDetector detector;
+        divine::ApproximateCounter counter;
+        std::atomic< int > &queue;
+        std::atomic< bool > &interrupted;
+        int produce;
+        int consume;
+        bool terminateEarly;
+
+        template< typename D, typename C >
+        CounterWorker( D &d, C &c, std::atomic< int > &q, std::atomic< bool > &i, bool t ) :
+            detector( d ),
+            counter( c ),
+            queue( q ),
+            interrupted( i ),
+            produce( 0 ),
+            consume( 0 ),
+            terminateEarly( t )
+        {}
+
+        void *main() {
+            detector.waitForAll( peers );
+            while ( !counter.isZero() ) {
+                assert_leq( 0, queue.load() );
+                if ( queue == 0 ) {
+                    counter.sync();
+                    continue;
+                }
+                if ( produce ) {
+                    --produce;
+                    ++queue;
+                    ++counter;
+                }
+                if ( consume ) {
+                    int v = queue;
+                    if ( v == 0 || !queue.compare_exchange_strong( v, v - 1 ) )
+                        continue;
+                    --consume;
+                    --counter;
+                }
+                // last worker will always fulfill this condition
+                // after exactly one consuming operation
+                if ( terminateEarly && consume == peers ) {
+                    queue = 0;
+                    interrupted = true;
+                    counter.reset();
+                }
+                if ( interrupted )
+                    break;
+            }
+            return nullptr;
+        }
+    };
+
+    void process( bool terminateEarly ) {
+        divine::StartDetector::Shared detectorShared;
+        divine::ApproximateCounter::Shared counterShared;
+        std::atomic< bool > interrupted( false );
+
+        // queueInitials
+        std::atomic< int > queue{ 1 };
+        divine::ApproximateCounter c( counterShared );
+        ++c;
+        c.sync();
+
+        std::vector< CounterWorker > threads{ peers,
+            CounterWorker{ detectorShared, counterShared, queue, interrupted, terminateEarly } };
+
+        // set consume and produce limits to each worker
+        int i = 1;
+        for ( auto &w : threads ) {
+            w.produce = i;
+            // let last worker consume the rest of produced values
+            w.consume = peers - i;
+            if ( w.consume == 0 )
+                w.consume = peers + 1;// also initials
+            ++i;
+        }
+
+        alarm( 1 );
+
+        for ( auto &w : threads )
+            w.start();
+        for ( auto &w : threads )
+            w.join();
+
+        if ( !terminateEarly ) {// do not check it on early termination
+            assert_eq( queue.load(), 0 );
+            assert_eq( counterShared.counter.load(), 0 );
+        }
+    }
+
+    Test approximateCounterProcessAll() {
+        process( false );
+    };
+
+    Test approximateCounterTerminateEarly() {// this test must finish
+        process( true );
+    }
 };
 
