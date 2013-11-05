80f71a1e2e19bad36d0fff1ab53f6918bb734aff
Petr Rockai <me@mornfall.net>
2013-11-05 12:23:58 +0000

    LLVM: Add a test for invalid stack dereference.

---
+++ test/llvm/locals.sh
@@ -0,0 +1,16 @@
+. lib
+
+llvm_verify invalid "bad dereference" testcase.c:10 <<EOF
+#include <assert.h>
+
+int *test() {
+    int x = 3;
+    return &x;
+}
+
+int main() {
+    int *x = test();
+    assert( *x == 3 );
+    return 0;
+}
+EOF
