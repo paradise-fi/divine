b8947f90b21edb2e94cc02607e18ff78b0da88d4
Vladimir Still <xstill@fi.muni.cz>
2014-06-16 21:43:13 +0000

    test: Add rudimentary test of algorithm instantiation.

---
+++ test/algorithm/instance.sh
@@ -0,0 +1,34 @@
+. lib
+
+checkInstance() {
+    key=$1
+    msg=$2
+    shift
+    shift
+
+    inst=`divine verify --dummy "$@" -r | grep 'Instance:'`
+    if echo $inst | egrep -i "$key"; then
+        echo "#### PASSED"
+    else
+        echo "ERROR: Missing $key in instance $msg (found $inst)"
+        exit 1
+    fi
+}
+
+# partitioned
+checkInstance '\<Reachability.*Partitioned' "for reachability" --reachability
+checkInstance 'WeakReachability.*Partitioned' "for weak reachability" --weak
+checkInstance 'Owcty.*Partitioned' "for OWCTY" --owcty
+checkInstance 'Map.*Partitioned' "for MAP" --map
+checkInstance 'NestedDfs.*Partitioned' "for NestedDFS" --nested-dfs
+
+#shared
+checkInstance '\<Reachability.*Shared' "for shared reachability" --reachability --shared
+checkInstance 'WeakReachability.*Shared' "for shared weak reachability" --weak --shared
+checkInstance 'Owcty.*Shared' "for shared OWCTY" --owcty --shared
+checkInstance 'Map.*Shared' "for shared MAP" --map --shared
+checkInstance 'NestedDFS.*Shared' "for double threaded NestedDFS" --nested-dfs -w 2
+
+if test "$O_COMPRESSION" = "ON"; then
+    checkInstance 'NTree' "with enabled compression" --compression=tree
+fi
