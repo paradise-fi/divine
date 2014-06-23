bdb1e58d51f8a23ece3ea168e3f16d384c6ffeda
Vladimir Still <xstill@fi.muni.cz>
2014-06-23 17:58:23 +0000

    wibble: Get rid of some duplicate code in commandline/core.cpp.

--- wibble/commandline/core.cpp
+++ wibble/commandline/core.cpp
@@ -16,23 +16,6 @@ bool ArgList::isSwitch(const iterator& iter)
 }
 
 
-bool ArgList::isSwitch(const std::string& str)
-{
-	// No empty strings
-	if (str[0] == 0)
-		return false;
-	// Must start with a dash
-	if (str[0] != '-')
-		return false;
-	// Must not be "-" (usually it means 'stdin' file argument)
-	if (str[1] == 0)
-		return false;
-	// Must not be "--" (end of switches)
-	if (str == "--")
-		return false;
-	return true;
-}
-
 bool ArgList::isSwitch(const char* str)
 {
 	// No empty strings
@@ -48,6 +31,10 @@ bool ArgList::isSwitch(const char* str)
 	if (strcmp(str, "--") == 0)
 		return false;
 	return true;
+}
+
+bool ArgList::isSwitch(const std::string& str) {
+    return ArgList::isSwitch( str.c_str() );
 }
 
 }
