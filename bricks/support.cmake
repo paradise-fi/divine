0ca381ef26b47cdb0bd795ae89d0ec2220d0933b
Petr Rockai <me@mornfall.net>
2014-07-03 11:52:26 +0000

    bricks: Extend & document support.cmake.

--- bricks/support.cmake
+++ bricks/support.cmake
@@ -1,4 +1,15 @@
-macro( bricks_unittest name )
+## This file contains various support functions and macros for making use of
+## bricks easier with cmake-based projects. Call include(bricks/support.cmake)
+## in your toplevel CMakeLists.txt to access those functions.
+
+# Create a unit test driver for a bunch of header files. Syntax:
+#
+#    bricks_unittest( driver_name header1 header2 ... )
+#
+# This will get you an executable target driver_name that you can run
+# to run the testsuite.
+
+function( bricks_unittest name )
   set( main "${CMAKE_CURRENT_BINARY_DIR}/${name}-main.cpp" )
   if( EXISTS ${main} )
     file( READ ${main} old )
@@ -21,4 +32,39 @@ macro( bricks_unittest name )
 
   add_executable( ${name} ${main} )
   set_source_files_properties( ${main} PROPERTIES COMPILE_DEFINITIONS UNITTEST )
+endfunction()
+
+# Register a target "test-bricks" that builds and runs all the unit tests
+# included with bricks. Use test_bricks( directory_with_bricks ). Also note
+# that if you write your own unit tests using brick-unittesh.h, the tests of
+# any bricks that you use in the tested units will be automatically included in
+# your testsuite.
+
+function( test_bricks dir )
+  include_directories( ${dir} )
+  file( GLOB SRC "${dir}/brick-*.h" )
+  bricks_unittest( test-bricks ${SRC} )
+endfunction()
+
+# Run feature checks and define -DBRICKS_* feature macros. You can use bricks
+# without feature checks, but you may be missing some of the features that
+# way. Calling this macro from your toplevel CMakeLists.txt is therefore a good
+# idea.
+
+macro( bricks_check_features )
+
+  include( CheckCXXSourceCompiles )
+
+  check_cxx_source_compiles(
+   "#include <dirent.h>
+    int main() {
+        struct dirent *d;
+        (void)d->d_type;
+        return 0;
+    }" BRICKS_HAVE_DIRENT_D_TYPE )
+
+  if ( BRICKS_HAVE_DIRENT_D_TYPE )
+    add_definitions( -DBRICKS_HAVE_DIRENT_D_TYPE )
+  endif()
+
 endmacro()
