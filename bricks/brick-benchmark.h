d71373fea4c28ee5c58cb061474387dbcf0f06a4
Petr Rockai <me@mornfall.net>
2014-07-06 16:55:55 +0000

    bricks: Implement gnuplot output for brick-benchmark.

--- bricks/brick-benchmark.h
+++ bricks/brick-benchmark.h
@@ -51,6 +51,124 @@ namespace benchmark {
 
 using Sample = std::vector< double >;
 
+struct Matrix {
+    std::vector< double > m;
+    int width;
+
+    int index( int r, int c ) {
+        ASSERT_LEQ( r * width + c, int( m.size() ) - 1 );
+        return r * width + c;
+    }
+
+    int height() { return m.size() / width; }
+
+    struct Proxy {
+        Matrix &A;
+        int x;
+        double &operator[]( double y ) {
+            return A.m[ A.index( x, y ) ];
+        }
+        Proxy( Matrix &A, int x ) : A( A ), x( x ) {}
+    };
+
+    Proxy operator[]( double x ) {
+        return Proxy( *this, x );
+    }
+
+    Matrix( int height, int width )
+        : m( width * height, 0.0 ), width( width )
+    {}
+
+    std::vector< double > solve() {
+        Matrix &A = *this;
+        int m = A.height();
+
+        for( int k = 0; k < m; ++ k )
+        {
+            int i_max = 0; // pivot for column
+            double vali = -std::numeric_limits< double >::infinity();
+
+            for ( int i = k; i < m; ++ i )
+                if( A[i][k] > vali ) { i_max = i; vali = A[i][k]; }
+
+            A.swapRows( k, i_max );
+
+            for( int i = k + 1; i < m; ++ i ) // for all rows below pivot
+            {
+                for(int j = k + 1; j < m + 1; ++ j )
+                    A[i][j] = A[i][j] - A[k][j] * (A[i][k] / A[k][k]);
+                A[i][k] = 0;
+            }
+        }
+
+        std::vector< double > x( m, 0.0 );
+
+        for( int i = m - 1; i >= 0; -- i) // rows = columns
+        {
+            double v = A[i][m] / A[i][i];
+            x[i] = v;
+            for( int j = i - 1; j >= 0; -- j) // rows
+            {
+                A[j][m] -= A[j][i] * v;
+                A[j][i] = 0;
+            }
+        }
+
+        return x;
+    }
+
+    void swapRows( int k, int l ) {
+        Matrix &A = *this;
+        for ( int i = 0; i < width; ++i )
+            std::swap( A[k][i], A[l][i] );
+    }
+};
+
+struct Spline {
+    std::vector< double > xs, ys, ks;
+
+    void push( double x, double y ) {
+        xs.push_back( x );
+        ys.push_back( y );
+    }
+
+    void interpolateNaturalKs() {
+        int n = xs.size() - 1;
+        Matrix A( n + 1, n + 2 );
+
+        for( int i = 1; i < n; ++ i ) // rows
+        {
+            A[i][i-1] = 1 / (xs[i] - xs[i-1]);
+            A[i][i  ] = 2 * (1/(xs[i] - xs[i-1]) + 1/(xs[i+1] - xs[i])) ;
+            A[i][i+1] = 1 / (xs[i+1] - xs[i]);
+            A[i][n+1] = 3 * ( (ys[i]-ys[i-1]) / ((xs[i] - xs[i-1]) * (xs[i] - xs[i-1]))  +
+                              (ys[i+1]-ys[i]) / ((xs[i+1] - xs[i]) * (xs[i+1] - xs[i])) );
+        }
+
+        A[0][  0] = 2/(xs[1] - xs[0]);
+        A[0][  1] = 1/(xs[1] - xs[0]);
+        A[0][n+1] = 3 * (ys[1] - ys[0]) / ((xs[1]-xs[0])*(xs[1]-xs[0]));
+
+        A[n][n-1] = 1 / (xs[n] - xs[n-1]);
+        A[n][  n] = 2 / (xs[n] - xs[n-1]);
+        A[n][n+1] = 3 * (ys[n] - ys[n-1]) / ((xs[n]-xs[n-1])*(xs[n]-xs[n-1]));
+
+        ks = A.solve();
+    }
+
+    double eval( double x ) {
+        int i = 1;
+        while(xs[i]<x) i++;
+
+        double t = (x - xs[i-1]) / (xs[i] - xs[i-1]);
+        double a =  ks[i-1]*(xs[i]-xs[i-1]) - (ys[i]-ys[i-1]);
+        double b = -ks[i  ]*(xs[i]-xs[i-1]) + (ys[i]-ys[i-1]);
+        double q = (1-t)*ys[i-1] + t*ys[i] + t*(1-t)*(a*(1-t)+b*t);
+
+        return q;
+    }
+};
+
 struct Estimate {
     double low, high, mean;
 };
@@ -109,19 +227,12 @@ Sample bootstrap( Sample s, E estimator, int iterations = 20000 )
     return result;
 }
 
-struct BenchmarkBase : unittest::TestCaseBase {
-    struct timespec start, end;
-    int fds[2];
-    virtual std::pair< int, int > dimensions() = 0;
-    virtual std::pair< int, int > amplitude() = 0;
-    virtual void setup( int p_seq, int q_seq ) = 0;
-};
-
 struct Axis {
     bool active; /* only evaluate the axis if it is active */
     bool log; /* if true, step is multiplicative, in percent */
     bool normalize; /* scale time per unit on this axis? */
-    int64_t min, max, step;
+    int64_t min, max;
+    double step; // useful for log-scaled benchmarks
     int64_t unit_mul, unit_div;
     std::string name, unit;
     Axis() : active( false ), log( false ), normalize( false ),
@@ -129,8 +240,12 @@ struct Axis {
              unit_mul( 1 ), unit_div( 1 ),
              name( "n" ), unit( "unit" ) {}
 
-    int scaled( int p ) {
-        return (p * unit_mul) / unit_div;
+    double scaled( int p ) {
+        return double(p * unit_mul) / unit_div;
+    }
+
+    int amplitude() {
+        return floor( 1 + log10( scaled( max ) ) );
     }
 
     int count() {
@@ -141,13 +256,24 @@ struct Axis {
             int r = 1, n = min;
             while ( n < max ) {
                 ++ r;
-                n = (n * step) / 100;
+                n = n * step;
             }
             return r;
         }
         return 1 + (max - min) / step;
     }
 };
+
+struct BenchmarkBase : unittest::TestCaseBase {
+    struct timespec start, end;
+    int fds[2];
+
+    virtual int parameter( Axis, int ) = 0;
+    virtual std::pair< Axis, Axis > axes() = 0;
+
+    virtual void setup( int p_seq, int q_seq ) = 0;
+};
+
 
 struct BenchmarkGroup {
     Axis x, y; // z is time
@@ -157,40 +283,65 @@ struct BenchmarkGroup {
         if ( !a.log )
             return a.min + seq * a.step;
         for ( int i = 0; i < seq; ++i )
-            a.min = (a.min * a.step) / 100;
+            a.min = a.min * a.step;
         return a.min;
     }
-    virtual int p_scaled() { return x.scaled( p ); }
-    virtual int q_scaled() { return y.scaled( q ); }
+    virtual std::string name() { return ""; }
 };
 
 namespace {
 
 std::vector< BenchmarkBase * > *benchmarks = nullptr;
 
-std::string render_ci( double point, double low_err, double high_err )
+std::vector< std::string > time_units = { "s ", "ms", "μs", "ns", "ps", "fs", "as", "zs", "ys" };
+
+std::string render_ci( double point, double low_err, double high_err, double factor = 1 )
 {
     int scale = 0;
-    double mult = 1;
+    double mult = factor;
 
     std::stringstream str;
-    std::vector< std::string > names = { "s ", "ms", "μs", "ns" };
 
     while ( point * mult < 1 &&
             low_err * mult < 0.01 &&
-            high_err * mult < 0.01 ) {
+            high_err * mult < 0.01 &&
+            scale < 8 ) {
         ++ scale;
-        mult = pow( 1000, scale );
+        mult = factor * pow( 1000, scale );
     }
 
     str << std::fixed << std::setprecision( 2 ) << "(∓"
         << std::setw( 4 ) << low_err * mult << " "
-        << std::setw( 6 ) << point * mult << " " << names[ scale ] << " ±"
+        << std::setw( 6 ) << point * mult << " " << time_units[ scale ] << " ±"
         << std::setw( 4 ) << high_err * mult << ")";
     return str.str();
 }
 
-void repeat( BenchmarkBase *tc ) {
+struct ResultSet {
+    Spline mean, mean_low, mean_high;
+
+    void push( int x, double m, double l, double h ) {
+        mean.push( x, m );
+        mean_low.push( x, l );
+        mean_high.push( x, h );
+    }
+
+    void interpolate() {
+        mean.interpolateNaturalKs();
+        mean_low.interpolateNaturalKs();
+        mean_high.interpolateNaturalKs();
+    }
+
+    std::string sample( int x, double mult ) {
+        std::stringstream str;
+        str << mean.eval( x ) * mult << " "
+            << mean_low.eval( x ) * mult << " "
+            << mean_high.eval( x )* mult ;
+        return str.str();
+    }
+};
+
+void repeat( BenchmarkBase *tc, ResultSet &res ) {
 #ifdef __unix
     char buf[1024];
     ::socketpair( AF_UNIX, SOCK_STREAM, PF_UNIX, tc->fds );
@@ -204,7 +355,7 @@ void repeat( BenchmarkBase *tc ) {
     double m_sample, m_mean, m_median;
     double sd_sample;
 
-    std::pair< int, int > amplitude = tc->amplitude();
+    Axis x = tc->axes().first, y = tc->axes().second;
 
     int iterations = 0;
 
@@ -222,6 +373,7 @@ void repeat( BenchmarkBase *tc ) {
             parse >> p_name >> p >> p_unit
                   >> q_name >> q >> q_unit
                   >> time;
+
             sample.push_back( time );
 #endif
         }
@@ -255,17 +407,21 @@ void repeat( BenchmarkBase *tc ) {
         }
     }
 
-    std::cerr << "  " << p_name << ": " << std::setw( amplitude.first ) << p << " " << p_unit
-              << " " << q_name << ": " << std::setw( amplitude.second ) << q << " " << q_unit
-              << " μ: " << render_ci( m_mean, m_mean - b_mean.low, b_mean.high - m_mean )
+    double factor = (x.normalize ? 1.0 / p : 1) * (y.normalize ? 1.0 / q : 1);
+
+    std::cerr << "  " << p_name << ": " << std::setw( x.amplitude() ) << round( x.scaled( p ) ) << " " << p_unit
+              << " "  << q_name << ": " << std::setw( y.amplitude() ) << round( y.scaled( q ) ) << " " << q_unit
+              << " μ: " << render_ci( m_mean, m_mean - b_mean.low, b_mean.high - m_mean, factor )
               << " m: " << render_ci( b_sample.median,
-                                       b_sample.median - b_median.low,
-                                       b_median.high - b_sample.median )
+                                      b_sample.median - b_median.low,
+                                      b_median.high - b_sample.median, factor )
               << " σ: " << render_ci( sd_sample,
-                                       sd_sample - b_stddev.low,
-                                       b_stddev.high - sd_sample )
+                                      sd_sample - b_stddev.low,
+                                      b_stddev.high - sd_sample, factor )
               << " | n = " << std::setw( 3 ) << sample.size()
               << ", bad = " << std::setw( 3 ) << iterations - sample.size() << std::endl;
+
+    res.push( p, m_mean * factor, b_mean.low * factor, b_mean.high * factor );
 
     ::close( tc->fds[0] );
     ::close( tc->fds[1] );
@@ -275,6 +431,28 @@ void run( int argc, const char **argv ) {
     ASSERT( benchmarks );
     std::set< std::string > done;
 
+
+    std::cout << "set terminal pdfcairo enhanced font 'Liberation Sans,10'" << std::endl;
+
+    /* set up line styles */
+    std::cout << "set style line 1 lc rgb '#ff4500' lt 1 lw 2" << std::endl
+              << "set style line 2 lc rgb '#ffa500' lt 1 lw 2" << std::endl
+              << "set style line 3 lc rgb '#006400' lt 1 lw 2" << std::endl
+              << "set style line 4 lc rgb '#0000ff' lt 1 lw 2" << std::endl
+              << "set style line 5 lc rgb '#9400d3' lt 1 lw 2" << std::endl
+              << "set style line 6 lc rgb '#800000' lt 1 lw 2" << std::endl
+              << "set style line 7 lc rgb '#ff0000' lt 1 lw 2" << std::endl
+              << "set style fill transparent solid 0.3" << std::endl;
+
+    /* set up axis styles */
+    std::cout << "set style line 11 lc rgb '#808080' lt 1" << std::endl
+              << "set border 3 back ls 11" << std::endl
+              << "set tics nomirror out scale 0.75" << std::endl
+              << "set style line 12 lc rgb'#808080' lt 0 lw 1" << std::endl
+              << "set grid back ls 12" << std::endl
+              << "set arrow from graph 1,0 to graph 1.05,0 size screen 0.025,10,60 filled ls 11" << std::endl
+              << "set arrow from graph 0,1 to graph 0,1.05 size screen 0.025,10,60 filled ls 11" << std::endl;
+
     for ( auto group : *benchmarks ) {
         if ( done.count( group->group() ) )
             continue;
@@ -282,12 +460,100 @@ void run( int argc, const char **argv ) {
             if ( group->group() != tc->group() )
                 continue;
             std::cerr << tc->group() << " " << tc->name << ":" << std::endl;
-            auto dim = tc->dimensions();
-            for ( int p_seq = 0; p_seq < dim.first; ++ p_seq )
-                for ( int q_seq = 0; q_seq < dim.second; ++ q_seq ) {
+            auto axes = tc->axes();
+            Axis x = axes.first, y = axes.second;
+
+            std::vector< ResultSet > results;
+
+            for ( int q_seq = 0; q_seq < y.count(); ++ q_seq ) {
+                ResultSet res;
+                for ( int p_seq = 0; p_seq < x.count(); ++ p_seq ) {
                     tc->setup( p_seq, q_seq );
-                    repeat( tc );
+                    repeat( tc, res );
                 }
+                results.push_back( res );
+            }
+
+            double t_max = 0, t_mult = 1;
+            int t_scale = 0;
+
+            std::stringstream header;
+
+            header << "plot \\" << std::endl;
+            for ( int q_seq = 0; q_seq < y.count(); ++ q_seq ) {
+                for ( int p_seq = 0; p_seq < x.count(); ++ p_seq )
+                    t_max = std::max( t_max, results[ q_seq ].mean_high.ys[ p_seq ] );
+
+                header << " '-' using 1:3:4 title \"" << tc->parameter( y, q_seq )
+                       << "\" with filledcurves ls " << q_seq + 1;
+                header << ", '-' using 1:2 notitle with lines ls " << q_seq + 1;
+                header << ", '-' using 1:3 notitle with lines ls " << q_seq + 1 << " lw 0.5";
+                header << ", '-' using 1:4 notitle with lines ls " << q_seq + 1 << " lw 0.5";
+                header << ", '-' using 1:2 notitle with points ls " << q_seq + 1 << " pt 7 ps 0.1 lc rgb '#000000'";
+                if ( q_seq + 1 < y.count() )
+                    header << ", \\";
+                header << std::endl;
+            }
+
+            while ( t_mult * t_max < 1 ) {
+                ++ t_scale;
+                t_mult = pow( 1000, t_scale );
+            }
+
+            std::cout << "unset logscale" << std::endl;
+
+            if ( x.log )
+                std::cout << "set logscale x" << std::endl;
+
+            double x_range = x.scaled( x.max ) - x.scaled( x.min );
+            int k = 1;
+
+            while ( x.log && log(x_range) / log(pow(x.step, k)) > 20 )
+                ++ k;
+
+            while ( !x.log && x_range / x.step * k > 10 )
+                ++ k;
+
+            /* measurement-specific */
+            std::cout << "set xrange [" << x.scaled( x.min ) << ":" << x.scaled( x.max ) << "]" << std::endl
+                      << "set xtics " << (x.log ? pow(x.step, k) : x.step * k) << std::endl
+                      << "unset mxtics" << std::endl
+                      << "set xlabel \"" << x.name << " [" << x.unit << "]\"" << std::endl
+                      << "set ylabel \"time [" << time_units[ t_scale ] << "]\"" << std::endl
+                      << "set title \"{/LiberationMono " << tc->group() << "::" << tc->name << "}\"" << std::endl
+                      << "set key outside title '"  << y.name << " [" << y.unit << "]' Left" << std::endl
+                      << "set format x '%.0f'" << std::endl;
+
+            std::cout << header.str();
+
+            for ( int q_seq = 0; q_seq < y.count(); ++ q_seq ) {
+                ResultSet res = results[ q_seq ];
+                res.interpolate();
+
+                std::stringstream raw, interp;
+
+                for ( int p_seq = 0; p_seq < x.count(); ++ p_seq ) {
+                    int p = tc->parameter( x, p_seq );
+                    raw << " " << x.scaled( p ) << " " << res.sample( p, t_mult ) << std::endl;
+                    interp << " " << x.scaled( p ) << " " << res.sample( p, t_mult ) << std::endl;
+
+                    if ( p_seq < x.count() - 1 ) {
+                        int p_next = tc->parameter( x, p_seq + 1 );
+                        for ( int i = 1; i < 20; ++i ) {
+                            int p_ = p + i * (p_next - p) / 20;
+                            interp << " " << x.scaled( p_ )
+                                   << " " << res.sample( p_, t_mult ) << std::endl;
+                        }
+                    }
+                }
+
+                /* each overlay needs the data to be repeated :-( */
+                std::cout << interp.str() << "end" << std::endl
+                          << interp.str() << "end" << std::endl
+                          << interp.str() << "end" << std::endl
+                          << interp.str() << "end" << std::endl
+                          << raw.str()    << "end" << std::endl;
+            }
         }
         done.insert( group->group() );
     }
@@ -300,15 +566,14 @@ struct Benchmark : BenchmarkBase
 {
     int64_t p, q;
 
-    std::pair< int, int > dimensions() {
+    std::pair< Axis, Axis > axes() {
         BenchGroup bg;
-        return std::make_pair( bg.x.count(), bg.y.count() );
+        return std::make_pair( bg.x, bg.y );
     }
 
-    std::pair< int, int > amplitude() {
+    int parameter( Axis a, int p ) {
         BenchGroup bg;
-        return std::make_pair( floor( 1 + log10( bg.x.scaled( bg.x.max ) ) ),
-                               floor( 1 + log10( bg.y.scaled( bg.y.max ) ) ) );
+        return bg.parameter( a, p );
     }
 
     void setup( int p_seq, int q_seq ) {
@@ -331,15 +596,18 @@ struct Benchmark : BenchmarkBase
             s -= 1;
             ns += 1000000000;
         }
-        std::cout << bg.x.name << " " << bg.p_scaled() << " " << bg.x.unit << " "
-                  << bg.y.name << " " << bg.q_scaled() << " " << bg.y.unit << " "
+        std::cout << bg.x.name << " " << bg.p << " " << bg.x.unit << " "
+                  << bg.y.name << " " << bg.q << " " << bg.y.unit << " "
                   << s << "." << std::setfill( '0' ) << std::setw( 9 ) << ns;
 #endif
     }
 
     std::string group() {
+        BenchGroup bg;
         int stat;
-        return abi::__cxa_demangle( typeid( BenchGroup ).name(), nullptr, nullptr, &stat );
+        if ( bg.name().empty() )
+            return abi::__cxa_demangle( typeid( BenchGroup ).name(), nullptr, nullptr, &stat );
+        return bg.name();
     }
 
     Benchmark() {
@@ -387,7 +655,7 @@ struct SelfTest : BenchmarkGroup {
         y.max =     6400000;
         y.unit_div =   1000;
         y.log = true;
-        y.step = 200; // percent
+        y.step = 2;
         y.unit = "k";
     }
 
