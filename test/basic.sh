25f40c91a6acaa436be2f4815f89baab54af0669
Petr Rockai <me@mornfall.net>
2014-07-07 13:43:47 +0000

    test: Remove obsolete unused test scripts.

--- test/basic.sh
+++
@@ -1,8 +0,0 @@
-set -vex
-not () { "$@" && exit 1 || return 0; }
-
-divine --version
-divine --version | grep '2\.[0-9]'
-not divine foobar
-
-type -p divine
--- test/custom.sh
+++
@@ -1,9 +0,0 @@
-set -vex
-not () { "$@" && exit 1 || return 0; }
-
-test -f ../examples/BenchmarkC.so || exit 200
-
-divine metrics --report ../examples/BenchmarkC.so > report
-grep "^Finished: Yes" report
-grep "^States-Visited:" report
-grep "^States-Visited: 1050624" report
