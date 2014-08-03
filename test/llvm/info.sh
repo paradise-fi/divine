f039acafc582129719e2db81900ca141a3b07fbf
Petr Rockai <me@mornfall.net>
2014-08-03 11:47:01 +0000

    test: Fix llvm/info.sh (drop extra $CFLAGS).

--- test/llvm/info.sh
+++ test/llvm/info.sh
@@ -5,7 +5,7 @@ llvm_precompile
 
 for I in llvm_examples/*.c llvm_examples/*.cpp; do
 
-    divine compile --llvm $I $CFLAGS --precompiled=. >& progress
+    divine compile --llvm $I --precompiled=. >& progress
     BC=$(echo $I | sed 's,^.*/,,' | sed 's/\.c\(pp\|c\)\{0,1\}$/.bc/')
     echo "BC: $BC"
     divine info $BC 2>> progress | capture
