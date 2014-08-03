d03d8a08fd852ea073facc72b1b9064928f20e72
Vladimir Still <xstill@fi.muni.cz>
2014-08-03 08:54:00 +0000

    generator: Make dummy smaller inside divine.

--- divine/generator/dummy.h
+++ divine/generator/dummy.h
@@ -8,6 +8,13 @@
 namespace divine {
 namespace generator {
 
+#ifndef __divine__
+// size is (dummy_size_param + 1) ^ 2
+const int dummy_size = 512;
+#else
+const int dummy_size = 2;
+#endif
+
 struct Dummy : Common< Blob > {
     typedef std::pair< short, short > Content;
     typedef Blob Node;
@@ -41,14 +48,14 @@ struct Dummy : Common< Blob > {
         if ( !pool().valid( st ) )
             return;
 
-        if ( content( st ).first < 512 ) {
+        if ( content( st ).first < dummy_size ) {
             r = this->makeBlob( sizeof( Content ) );
             content( r ) = content( st );
             content( r ).first ++;
             yield( r, Label( 7 ) );
         }
 
-        if ( content( st ).second < 512 ) {
+        if ( content( st ).second < dummy_size ) {
             r = this->makeBlob( sizeof( Content ) );
             content( r ) = content( st );
             content( r ).second ++;
@@ -62,7 +69,7 @@ struct Dummy : Common< Blob > {
 
     bool isGoal( Node s ) {
         Content f = pool().get< Content >( s, this->slack() );
-        return f.first == 512;
+        return f.first == dummy_size;
     }
 
     bool isAccepting( Node ) { return false; }
