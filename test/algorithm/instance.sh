668a1f01a7b6ca7d2465ad978401a7973d0a8492
Petr Rockai <me@mornfall.net>
2014-09-13 10:24:11 +0000

    test: Fix algorithm-dependent checks in instance.sh.

--- test/algorithm/instance.sh
+++ test/algorithm/instance.sh
@@ -25,17 +25,17 @@ checkInstance() {
 
 # partitioned
 checkInstance '\<Reachability.*Partitioned' "for reachability" --reachability
-checkInstance 'WeakReachability.*Partitioned' "for weak reachability" --weak
+test "$ALG_WEAKREACHABILITY" = "ON" && checkInstance 'WeakReachability.*Partitioned' "for weak reachability" --weak
 checkInstance 'Owcty.*Partitioned' "for OWCTY" --owcty
-checkInstance 'Map.*Partitioned' "for MAP" --map
-checkInstance 'NestedDfs.*Partitioned' "for NestedDFS" --nested-dfs
+test "$ALG_MAP" = "ON" && checkInstance 'Map.*Partitioned' "for MAP" --map
+test "$ALG_NDFS" = "ON" && checkInstance 'NestedDfs.*Partitioned' "for NestedDFS" --nested-dfs
 
 #shared
 checkInstance '\<Reachability.*Shared' "for shared reachability" --reachability --shared
-checkInstance 'WeakReachability.*Shared' "for shared weak reachability" --weak --shared
+test "$ALG_WEAKREACHABILITY" = "ON" && checkInstance 'WeakReachability.*Shared' "for shared weak reachability" --weak --shared
 checkInstance 'Owcty.*Shared' "for shared OWCTY" --owcty --shared
-checkInstance 'Map.*Shared' "for shared MAP" --map --shared
-checkInstance 'NestedDFS.*Shared' "for double threaded NestedDFS" --nested-dfs -w 2
+test "$ALG_MAP" = "ON" && checkInstance 'Map.*Shared' "for shared MAP" --map --shared
+test "$ALG_NDFS" = "ON" && checkInstance 'NestedDFS.*Shared' "for double threaded NestedDFS" --nested-dfs -w 2
 
 if test "$STORE_COMPRESS" = "ON"; then
     checkInstance 'NTree' "with enabled compression" --compression=tree
