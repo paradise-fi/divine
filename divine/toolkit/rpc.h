c8b5aa473d99bf1fcd58f9ab175d69b2b331fdbb
Petr Rockai <me@mornfall.net>
2014-09-13 14:55:53 +0000

    toolkit: Move bitstream operators for Blobs to rpc.h.

--- divine/toolkit/rpc.h
+++ divine/toolkit/rpc.h
@@ -33,6 +33,52 @@ bitstream_base< B > &operator>>( bitstream_base< B > &bs, wibble::Unit ) {
     return bs;
 }
 
+/* NB. The previous contents of the blob that's read is *not released*. */
+template< typename B >
+bitstream_base< B > &operator>>( bitstream_base< B > &bs, Blob &b )
+{
+    int size = 0;
+    bs >> size;
+
+    if ( !size ) {
+        b = Blob();
+        return bs;
+    }
+
+    b = bs.pool->allocate( size );
+
+    ASSERT_LEQ( bs.pool->size( b ), bs.size() * 4 );
+
+    char *begin = bs.pool->dereference( b );
+    const char *end = begin + bs.pool->size( b );
+    uint32_t *ptr = reinterpret_cast< uint32_t * >( begin );
+    while ( ptr < reinterpret_cast< const uint32_t * >( end ) ) {
+        bs >> *ptr;
+        ++ptr;
+    }
+
+    return bs;
+}
+
+template< typename B >
+bitstream_base< B > &operator<<( bitstream_base< B > &bs, Blob b )
+{
+    ASSERT( bs.pool );
+    if ( !bs.pool->valid( b ) )
+        return bs << 0;
+
+    const int size = bs.pool->size( b );
+    bs << size;
+    const char *begin = bs.pool->dereference( b );
+    const char *end = begin + size;
+    const uint32_t *ptr = reinterpret_cast< const uint32_t * >( begin );
+    while ( ptr < reinterpret_cast< const uint32_t * >( end ) ) {
+        bs << *ptr;
+        ++ptr;
+    }
+    return bs;
+}
+
 }
 
 #endif
