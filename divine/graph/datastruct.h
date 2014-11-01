864e7551f30c0cdd6c78ee040e2e5ad57e939963
Petr Rockai <me@mornfall.net>
2014-11-01 19:18:02 +0000

    graph: Port unit tests to brick-unittest.

--- divine/graph/datastruct.h
+++ divine/graph/datastruct.h
@@ -23,6 +23,7 @@ struct QueueFrontend {
     typedef typename Setup::Graph Graph;
     typedef typename Graph::Node Node;
     typedef typename Graph::Label Label;
+    typedef typename Setup::Vertex Vertex;
 
     template< typename Next >
     void processOpen( Next next ) {
@@ -30,7 +31,7 @@ struct QueueFrontend {
         axed = false;
 
         auto from = self().store().vertex( self().front() );
-        self().g.successors( from, [&]( Node n, Label label ) {
+        self().g.successors( from.node(), [&]( Node n, Label label ) {
                 this->deadlocked = false;
                 if ( !this->axed )
                     next( from, n, label );
@@ -152,7 +153,7 @@ struct Stack {
         _from = s.vertex( h );
         _stack.push_back( StackItem( h, Label() ) );
         deadlocked = true;
-        g.successors( _from, [&]( Node n, Label l ) {
+        g.successors( _from.node(), [&]( Node n, Label l ) {
                 ++ this->_pushes;
                 this->deadlocked = false;
                 this->_stack.push_back( StackItem( n, l ) );
@@ -314,6 +315,301 @@ void safe_delete_array( T* &ptr ) {
         ptr = NULL;
     }
 }
+
+}
+
+#include <divine/generator/dummy.h>
+#include <divine/utility/statistics.h>
+#include <divine/graph/visitor.h>
+#include <divine/graph/store.h>
+
+namespace divine_test {
+
+struct TestDatastruct {
+    typedef Blob Node;
+    typedef generator::Dummy::Label Label;
+
+    struct SeqSetup {
+        typedef generator::Dummy Graph;
+        typedef NoStatistics Statistics;
+        using Store = StoreFor< Graph, PartitionedProvider >;
+        typedef typename Store::Vertex Vertex;
+        typedef typename Store::Handle Handle;
+    };
+
+    Node first, second, third;
+
+    void init( generator::Dummy &g ) {
+        int count = 0;
+        g.initials( [&]( Node, Node n, Label ) { first = n; } );
+        g.successors( first, [&]( Node n, Label ) {
+                if ( count == 0 )
+                    second = n;
+                if (count == 1 )
+                    third = n;
+                ++ count;
+            } );
+        assert_eq( count, 2 );
+    }
+
+    template< typename Q >
+    void _queue(generator::Dummy& d, Q& q) {
+        typedef typename Q::Vertex Vertex;
+        auto getShort = [&d]( Node n, int p ) { return d.pool().template get< short >( n, p ); };
+        int count = 0;
+
+        init( d );
+
+        assert( q.empty() );
+        q.push( TrivialHandle( first, 0 ) );
+        assert( !q.empty() );
+
+        count = 0;
+        q.processOpen( [&]( Vertex, Node, Label ) { ++count; } );
+        q.processClosed( [&]( Vertex ) {} );
+        assert_eq( count, 2 );
+        assert( q.empty() );
+
+        count = 0;
+        q.push( TrivialHandle( first, 0 ) );
+        q.push( TrivialHandle( second, 0 ) );
+        assert( !q.empty() );
+
+        q.processOpen( [&]( Vertex, Node n, Label ) {
+                if ( count == 0 ) {
+                    assert_eq( getShort( n, 0 ), 1 );
+                    assert_eq( getShort( n, 2 ), 0 );
+                    assert( d.pool().equal( n, second ) );
+                }
+                if ( count == 1 ) {
+                    assert_eq( getShort( n, 0 ), 0 );
+                    assert_eq( getShort( n, 2 ), 1 );
+                    assert( d.pool().equal( n, third ) );
+                }
+                ++ count;
+            } );
+        q.processClosed( [&]( Vertex ) {} );
+
+        assert_eq( count, 2 );
+        assert( !q.empty() );
+
+        count = 0;
+        q.processOpen( [&]( Vertex, Node n, Label ) {
+                if ( count == 0 ) {
+                    assert_eq( getShort( n, 0 ), 2 );
+                    assert_eq( getShort( n, 2 ), 0 );
+                }
+                if ( count == 1 ) {
+                    assert_eq( getShort( n, 0 ), 1 );
+                    assert_eq( getShort( n, 2 ), 1 );
+                }
+                ++ count;
+            } );
+        q.processClosed( [&]( Vertex ) {} );
+        assert_eq( count, 2 );
+        assert( q.empty() );
+    }
+
+    TEST(queue) {
+        generator::Dummy d;
+        d.setPool( Pool() );
+
+        SeqSetup::Store s( d, 0 );
+        Queue< SeqSetup > q( d, s );
+        _queue( d, q );
+    }
+
+    template < typename G >
+    struct SharedSetup {
+        typedef G Graph;
+        typedef NoStatistics Statistics;
+        using Store = StoreFor< Graph, SharedProvider >;
+        typedef typename Store::Vertex Vertex;
+        typedef typename Store::Handle Handle;
+    };
+
+    TEST(sharedQueue) {
+        generator::Dummy d;
+        typedef SharedSetup< generator::Dummy > Setup;
+        typedef SharedQueue< Setup > Queue;
+        Queue::TerminatorPtr t = std::make_shared< Queue::Terminator >();
+        Queue::ChunkQPtr ch = std::make_shared< Queue::ChunkQ >();
+
+        d.setPool( Pool() );
+
+        Setup::Store s( d, 0 );
+        Queue q( d, s, ch, t );
+        q.maxChunkSize = 1;
+        q.chunkSize = 1;
+        _queue( d, q );
+    }
+
+    template< typename Queue >
+    struct Worker : brick::shmem::Thread
+    {
+        std::shared_ptr< Queue > queue;
+        int add;
+        int interleaveAdd;
+        int count;
+        int i;
+        void main() {
+            bool stopPushing = false;
+            for ( int i = 0; i < add; ++i ) {
+                queue->push( i );
+            }
+            queue->termination.sync();
+            while ( !queue->termination.isZero() ) {
+
+                if ( queue->empty() ) {
+                    queue->flush();
+                    queue->termination.sync();
+                    continue;
+                }
+
+                if ( i < interleaveAdd ) {
+                    queue->push( i );
+                    ++i;
+                }
+
+                if ( !stopPushing && i == interleaveAdd ) {
+                    stopPushing = true;
+                }
+
+                queue->pop_front();
+                ++count;
+            }
+        }
+
+        Worker() : queue(), add( 0 ), count( 0 ), i( 0 )
+        {}
+
+    };
+
+    template< typename N >
+    struct DummyGraph {
+        typedef N Node;
+
+        struct Label {
+            short probability;
+            Label( short p = 0 ) : probability( p ) {}
+        };
+    };
+
+/*  TEST(sharedQueueMultiStress) {
+        const int threads = 4;
+        const int amount = 32 * 1024;
+
+        typedef SharedQueue< SharedSetup< DummyGraph< Int > > > Queue;
+        Queue::TerminatorPtr t = std::make_shared< Queue::Terminator >();
+        Queue::ChunkQPtr ch = std::make_shared< Queue::ChunkQ >();
+        DummyGraph< Int > g;
+
+        Worker< Queue >* workers = new Worker< Queue >[ threads ];
+
+        std::size_t shouldBe = 0;
+
+        for ( int i = 0;  i < threads; ++i ) {
+            workers[ i ].queue = std::make_shared< Queue >( ch, g, t );
+            shouldBe += workers[ i ].add = amount * (1 + i);
+            workers[ i ].interleaveAdd = amount;
+            workers[ i ].start();
+        }
+
+        std::size_t sum = 0;
+        for ( int i = 0; i < threads; ++i) {
+            workers[ i ].join();
+            sum += workers[ i ].count;
+            shouldBe+= workers[ i ].i;
+        }
+
+        assert_eq( sum, shouldBe );
+        delete[] workers;
+    } */
+
+    TEST(stack) {
+        typedef typename SeqSetup::Vertex Vertex;
+
+        generator::Dummy d;
+        d.setPool( Pool() );
+
+        SeqSetup::Store s( d, 0 );
+        Stack< SeqSetup > q( d, s );
+
+        bool die = true;
+        auto getShort = [&d]( Node f, int p ) { return d.pool().get< short >( f, p ); };
+
+        d.setPool( Pool() );
+        init( d );
+
+        assert( q.empty() );
+        q.push( TrivialHandle( first, 0 ) );
+        q.processClosed( []( Vertex ) { assert_die(); } );
+        assert( !q.empty() );
+
+        q.processOpen( [&]( Vertex, Node, Label ) { die = false; } );
+        q.processClosed( []( Vertex ) { assert_die(); } );
+        assert( !q.empty() );
+        assert( !die );
+
+        die = true;
+        q.processOpen( [&]( Vertex, Node, Label ) { die = false; } );
+        assert( !die );
+
+        die = true;
+        q.processClosed( [&]( Vertex ) { die = false; } );
+        assert( !die );
+        assert( q.empty() );
+
+        q.push( TrivialHandle( first, 0 ) );
+        q.processClosed( []( Vertex ) { assert_die(); } );
+        q.push( TrivialHandle( second, 0 ) );
+
+        // 1, 1, from 1, 0
+        q.processOpen( [&]( Vertex f, Node t, Label ) {
+                assert_eq( getShort( f.node(), 0 ), 1 );
+                assert_eq( getShort( f.node(), 2 ), 0 );
+                assert_eq( getShort( t, 0 ), 1 );
+                assert_eq( getShort( t, 2 ), 1 );
+            } );
+        q.processClosed( []( Vertex ) { assert_die(); } );
+        assert( !q.empty() );
+
+        // 2, 0, from 1, 0
+        q.processOpen( [&]( Vertex f, Node t, Label ) {
+                assert_eq( getShort( f.node(), 0 ), 1 );
+                assert_eq( getShort( f.node(), 2 ), 0 );
+                assert_eq( getShort( t, 0 ), 2 );
+                assert_eq( getShort( t, 2 ), 0 );
+            } );
+        die = true;
+        q.processClosed( [&]( Vertex ) { die = false; } );
+        assert( !die );
+        assert( !q.empty() );
+
+        // 0, 1, from 0, 0
+        q.processOpen( [&]( Vertex f, Node t, Label ) {
+                assert_eq( getShort( f.node(), 0 ), 0 );
+                assert_eq( getShort( f.node(), 2 ), 0 );
+                assert_eq( getShort( t, 0 ), 0 );
+                assert_eq( getShort( t, 2 ), 1 );
+            } );
+        assert( !q.empty() );
+
+        // 1, 0, from 0, 0
+        q.processOpen( [&]( Vertex f, Node t, Label ) {
+                assert_eq( getShort( f.node(), 0 ), 0 );
+                assert_eq( getShort( f.node(),  2 ), 0 );
+                assert_eq( getShort( t, 0 ), 1 );
+                assert_eq( getShort( t, 2 ), 0 );
+            } );
+
+        die = true;
+        q.processClosed( [&]( Vertex ) { die = false; } );
+        assert( !die );
+
+        assert( q.empty() );
+    }
+};
 
 }
 
