d57780c45b033f4fb20eb8d7ce82f6d891c55310
Petr Rockai <me@mornfall.net>
2014-11-01 18:15:15 +0000

    toolkit: Port Blob unit tests to bricks.

--- divine/toolkit/blob.h
+++ divine/toolkit/blob.h
@@ -2,8 +2,8 @@
 
 #include <cstdint>
 
-#include <wibble/test.h> // for assert*
-#include <wibble/string.h>
+#include <brick-unittest.h>
+#include <brick-string.h>
 
 #include <divine/toolkit/pool.h>
 #include <divine/toolkit/rpc.h>
@@ -11,25 +11,75 @@
 #ifndef DIVINE_BLOB_H
 #define DIVINE_BLOB_H
 
-namespace wibble {
-namespace str {
-
-template<>
-inline std::string fmt( const divine::UnBlob &b ) {
-    divine::bitstream bs;
-    bs.pool = &b.p;
-    bs << b.b;
-    bs.bits.pop_front(); /* remove size */
-    return fmt( bs.bits );
-}
-
-}
-}
-
 namespace divine {
+
 inline std::string fmtblob( Pool &p, Blob b ) {
-    return wibble::str::fmt( UnBlob( p, b ) );
-}
+    divine::bitstream bs;
+    bs.pool = &p;
+    bs << b;
+    bs.bits.pop_front(); /* remove size */
+    return brick::string::fmt( bs.bits );
 }
 
+}
+
+namespace divine_test {
+
+using namespace divine;
+
+struct TestBlob {
+
+    TEST(basic) {
+        Pool p;
+        Blob b = p.allocate( 32 );
+        p.get< int >( b ) = 42;
+        ASSERT_EQ( p.get< int >( b ), 42 );
+
+        Blob c = p.allocate( sizeof( int ) );
+        p.get< int >( c ) = 42;
+        ASSERT_EQ( p.get< int >( c ), 42 );
+    }
+
+    template< typename T >
+    void comparison() {
+        Pool p;
+        Blob b1 = p.allocate( sizeof( T ) ),
+             b2 = p.allocate( sizeof( T ) ),
+             b3 = p.allocate( sizeof( T ) );
+        BlobComparerEQ ecomp( p );
+        BlobComparerLT lecomp( p );
+        p.get< T >( b1 ) = 32;
+        p.get< T >( b2 ) = 32;
+        p.get< T >( b3 ) = 42;
+        ASSERT_EQ( p.get< T >( b1 ), 32 );
+        ASSERT_EQ( p.get< T >( b2 ), 32 );
+        ASSERT_EQ( p.get< T >( b3 ), 42 );
+        ASSERT( ecomp( b1, b2 ) );
+        ASSERT( !ecomp(b1, b3) );
+        ASSERT( !ecomp(b2, b3) );
+        ASSERT( !lecomp(b1, b2) );
+        ASSERT( !lecomp(b2, b1) );
+        ASSERT( lecomp( b1, b3 ) );
+    }
+
+    TEST(comparisonShort) {
+        comparison< short >();
+    }
+
+    TEST(comparisonInt) {
+        comparison< int >();
+    }
+
+    TEST(comparisonChar) {
+        comparison< char >();
+    }
+
+    TEST(align) {
+        for ( int i = 0; i < 100; ++i )
+            ASSERT_LEQ( i, divine::align( i, sizeof( void * ) ) );
+    }
+
+};
+
+}
 #endif
