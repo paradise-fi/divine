c732d5104b0c8c3c1c2822fcf4500eba729ab7d0
Petr Rockai <me@mornfall.net>
2014-11-14 12:22:32 +0000

    tools: Fix ambiguous references to access().

--- tools/divine.cpp
+++ tools/divine.cpp
@@ -685,7 +685,7 @@ struct Main {
         meta.output.saveStates = !o_noSaveStates->boolValue();
 
 
-        if ( !meta.input.dummygen && access( input.c_str(), R_OK ) )
+        if ( !meta.input.dummygen && brick::fs::access( input, R_OK ) )
             die( "FATAL: cannot open input file " + input + " for reading" );
 
         {
