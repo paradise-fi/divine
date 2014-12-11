f5ebae4a66674de448d169350c87890574026500
Vladimir Still <xstill@fi.muni.cz>
2014-12-11 18:21:19 +0000

    test: Add test that verify can run without -p option.

---
+++ test/defproperty.sh
@@ -0,0 +1,18 @@
+. lib
+
+# test that divine has default property for each major generator
+
+if [ "$GEN_LLVM" = "ON" ]; then
+    run llvm_assemble data/global-ok.ll verify
+    check report Finished Yes
+fi
+
+if [ "$GEN_DVE" = "ON" ]; then
+    run verify data/empty.dve
+    check report Finished Yes
+fi
+
+if [ "$GEN_TIMED" = "ON" ]; then
+    run verify data/zeno.xml
+    check report Finished Yes
+fi
