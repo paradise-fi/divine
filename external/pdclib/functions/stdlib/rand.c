f9cbeb5e68aea0e3dbab79f3dc093641841acdf2
xlenco <xlenco@fi.muni.cz>
2015-01-05 21:31:49 +0000

    pdclib: Fix rand().

--- external/pdclib/functions/stdlib/rand.c
+++ external/pdclib/functions/stdlib/rand.c
@@ -12,7 +12,8 @@
 
 int rand( void )
 {
-    _PDCLIB_seed = _PDCLIB_seed * 1103515245 + 12345;
+    _PDCLIB_seed = (unsigned)(_PDCLIB_seed * 1103515245) + 12345;
+
     return (int)( _PDCLIB_seed / 65536 ) % 32768;
 }
 
