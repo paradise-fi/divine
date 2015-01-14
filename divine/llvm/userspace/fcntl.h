2e56eea15d5c1345b533db558bd22ac630def4ff
xweiser1 <xweiser1@fi.muni.cz>
2015-01-14 20:25:46 +0000

    llvm: Add draft of fcntl.h.

---
+++ divine/llvm/userspace/fcntl.h
@@ -0,0 +1,11 @@
+#ifndef _FCNTL_H_
+#define _FCNTL_H_
+
+#define AT_FDCWD            -100
+
+#define AT_SYMLINK_NOFOLLOW 0x100
+#define AT_REMOVEDIR        0x200
+#define AT_SYMLINK_FOLLOW   0x400
+#define AT_EACCESS          0x200
+
+#endif
