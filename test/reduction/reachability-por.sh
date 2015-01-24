430958e374d8b4f73d703d90bb8c591875522a83
Petr Rockai <me@mornfall.net>
2015-01-24 09:51:00 +0000

    test: Skip POR tests if the given algorithm is compiled out.

--- test/reduction/reachability-por.sh
+++ test/reduction/reachability-por.sh
@@ -1,6 +1,8 @@
 . lib
 
+test "$ALG_REACHABILITY" = ON || skip
 test "$GEN_DVE" = "OFF" && skip
+
 for COMP in $COMPRESSIONS
 do
     run reachability --property=deadlock --reduce=por data/por.dve --compression=$COMP
