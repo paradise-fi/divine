0af64beaa03e196cda8b81ca1f2a0ab801d8e502
xweiser1 <xweiser1@fi.muni.cz>
2015-02-13 13:55:44 +0000

    llvm: Add prototypes and macros into fcntl.h in VFS.

--- divine/llvm/userspace/fcntl.h
+++ divine/llvm/userspace/fcntl.h
@@ -8,4 +8,32 @@
 #define AT_SYMLINK_FOLLOW   0x400
 #define AT_EACCESS          0x200
 
+#define O_RDONLY            0
+#define O_WRONLY            1
+#define O_RDWR              2
+#define O_CREAT            64
+#define O_EXCL            128
+#define O_APPEND         1024
+#define O_TRUNC           512
+#define O_NOFOLLOW     131072
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FS_NOINLINE __attribute__((noinline))
+
+FS_NOINLINE int creat( const char *path, int mode );
+FS_NOINLINE int open( const char *path, int flags, ... );
+FS_NOINLINE int openat( int dirfd, const char *path, int flags, ... );
+
+#undef FS_NOINLINE
+
+#ifdef __cplusplus
+}
+#endif
+
+
 #endif
