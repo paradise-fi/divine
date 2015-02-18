70e3c147c0b4f6a6d41e0bc7140aaa10c5346684
Vladimir Still <xstill@fi.muni.cz>
2015-02-18 08:24:58 +0000

    llvm: Avoid need to define useconds_t for now.

--- divine/llvm/userspace/glue.cpp
+++ divine/llvm/userspace/glue.cpp
@@ -95,4 +95,5 @@ extern "C" unsigned int sleep( unsigned int seconds ) {
     return 0;
 }
 
-extern "C" int usleep(useconds_t usec) { return 0; }
+// should be useconds_t really, but wait for proper unistd for now
+extern "C" int usleep( unsigned int usec ) { return 0; }
