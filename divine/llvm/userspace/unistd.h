0bbfbbcc2801fbf6a7df6d658692b8bc78912bb5
Petr Rockai <me@mornfall.net>
2015-03-01 16:16:44 +0000

    LLVM: typedef useconds_t to unsigned int for now.

--- divine/llvm/userspace/unistd.h
+++ divine/llvm/userspace/unistd.h
@@ -49,6 +49,8 @@ extern "C" {
 
 #define FS_NOINLINE __attribute__((noinline))
 
+typedef unsigned int useconds_t;
+
 FS_NOINLINE int close( int fd );
 FS_NOINLINE ssize_t read( int fd, void *buf, size_t count );
 FS_NOINLINE ssize_t write( int fd, const void *buf, size_t count );
