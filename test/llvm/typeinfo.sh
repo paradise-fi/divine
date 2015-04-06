933f8ba63d5e769f5bf0e3f95c0389e1f0713aa0
Petr Rockai <me@mornfall.net>
2015-04-06 09:59:44 +0000

    test: Add a few vtable/typeinfo checks for LLVM.

---
+++ test/llvm/typeinfo.sh
@@ -0,0 +1,49 @@
+. lib
+
+llvm_verify_cpp valid <<EOF
+#include <cassert>
+#include <new>
+
+int global = 0;
+
+struct x { virtual ~x() {} };
+struct y : x { virtual ~y() { global = 1; } };
+
+int main() {
+    char mem[ 32 ];
+    x *a = new (mem) y();
+    y *b = dynamic_cast< y * >( a );
+    delete a;
+    assert( global == 1 );
+    return 0;
+}
+EOF
+
+llvm_verify_cpp valid <<EOF
+#include <typeinfo>
+#include <cassert>
+struct x { virtual ~x() {} };
+
+int main() {
+    auto ti = &typeid( x );
+    void **vtable = *(void***)ti;
+    void **ti2 = vtable - 1;
+    void **tiX = vtable - 2;
+    assert( vtable[-1] );
+    return 0;
+}
+EOF
+
+llvm_verify_cpp valid <<EOF
+#include "../private_typeinfo.h"
+
+using namespace __cxxabiv1;
+
+struct x { virtual ~x() {} };
+
+int main() {
+    auto ti = &typeid( x );
+    auto shim = dynamic_cast< const __shim_type_info * >( ti );
+    return 0;
+}
+EOF
