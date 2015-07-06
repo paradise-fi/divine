8ed6d7885d53ee28a6bc1116d25d1f02b1ee58bb
Petr Rockai <me@mornfall.net>
2015-07-06 06:48:14 +0000

    lart: Simplify makeUniqueMDNode a bit.

--- lart/support/id.cpp
+++ lart/support/id.cpp
@@ -5,11 +5,10 @@ namespace lart {
 typedef llvm::ArrayRef< llvm::Value * > ValsRef;
 
 llvm::MDNode *makeUniqueMDNode( llvm::Module &m ) {
-    auto md_tmp = llvm::MDNode::getTemporary( m.getContext(), ValsRef() );
     llvm::Value **v = new llvm::Value *[ 1 ];
-    v[0] = md_tmp;
+    v[0] = nullptr;
     auto md = llvm::MDNode::get( m.getContext(), ValsRef( v, 1 ) );
-    md_tmp->replaceAllUsesWith( md );
+    md->replaceOperandWith( 0, md );
     return md;
 }
 
