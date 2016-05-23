7d53639f3e90be1141c9a21fb38c896d6765d1b6
Petr Rockai <me@mornfall.net>
2016-05-23 07:42:02 +0000

    clang: Include libc++abi in -stdlib=libc++ builds.

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -452,6 +452,7 @@ void ToolChain::AddCXXStdlibLibArgs(const ArgList &Args,
   switch (Type) {
   case ToolChain::CST_Libcxx:
     CmdArgs.push_back("-lc++");
+    CmdArgs.push_back("-lc++abi");
     break;
 
   case ToolChain::CST_Libstdcxx:
