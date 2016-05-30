d795035473c030f4b8642f04b6816edaf3c8eaa6
Petr Rockai <me@mornfall.net>
2016-05-30 00:21:42 +0000

    runtime: Remove obsolete code from entry.cpp and add __sys_env.

--- runtime/divine/entry.cpp
+++ runtime/divine/entry.cpp
@@ -1,25 +1,6 @@
 #include <divine.h>
-#include <cstdlib>
 
 extern "C" {
-    int main(...);
-    struct global_ctor { int prio; void (*fn)(); void *data; };
-
-    void _divine_start( int ctorcount, void *ctors, int mainproto )
-    {
-        int r;
-        global_ctor *c = reinterpret_cast< global_ctor * >( ctors );
-        for ( int i = 0; i < ctorcount; ++i ) {
-            c->fn();
-            c ++;
-        }
-        switch (mainproto) {
-            case 0: r = main(); break;
-            case 1: r = main( 0 ); break;
-            case 2: r = main( 0, 0 ); break;
-            case 3: r = main( 0, 0, 0 ); break;
-            default: __vm_fault( _VM_F_Control, "don't know how to run main()" );
-        }
-        exit( r );
-    }
+    extern const _VM_Env __sys_env[];
+    const _VM_Env *__sys_env_get() { return __sys_env; }
 }
