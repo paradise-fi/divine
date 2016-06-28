bbdc68d19440c370dac8a856742792808ec8ca29
Vladimir Still <xstill@fi.muni.cz>
2016-06-28 17:23:39 +0000

    lart: Fill in type and native runtime frame size in function metadata.

--- lart/divine/functionmeta.cpp
+++ lart/divine/functionmeta.cpp
@@ -6,6 +6,7 @@ DIVINE_RELAX_WARNINGS
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/DataLayout.h>
 #include <llvm/Transforms/Utils/BasicBlockUtils.h>
 DIVINE_UNRELAX_WARNINGS
 
@@ -19,16 +20,40 @@ DIVINE_UNRELAX_WARNINGS
 #include <lart/support/util.h>
 #include <lart/support/metadata.h>
 
+// used to calculate frame size & encode function types
+#include <runtime/divine.h>
+#include <runtime/native/vm.h>
+#include <runtime/divine/metadata.h>
+
 namespace lart {
 namespace divine {
+
+char encLLVMBasicType( llvm::Type *t ) {
+    if ( t->isVoidTy() )
+        return metadata::TypeSig::encodeOne< void >;
+    if ( t->isPointerTy() )
+        return metadata::TypeSig::encodeOne< void * >;
+    if ( auto *it = llvm::dyn_cast< llvm::IntegerType >( t ) )
+        return metadata::TypeSig::encodeIntTy( it->getBitWidth() );
+    return metadata::TypeSig::unknownType;
+}
+
+std::string encLLVMFunType( llvm::FunctionType *ft ) {
+    std::string enc;
+    enc.push_back( encLLVMBasicType( ft->getReturnType() ) );
+    for ( auto t : ft->params() )
+        enc.push_back( encLLVMBasicType( t ) );
+    return enc;
+}
 
 struct FunctionMeta {
     template< typename Index >
     explicit FunctionMeta( llvm::Function &fn, Index &index ) :
         entryPoint( &fn ),
-        frameSize( 0 ), // filed by loader
+        frameSize( sizeof( _VM_Frame ) + argsSize( fn, index ) + sizeof( nativeRuntime::FrameEx ) ), // re-filed by the loader
         argCount( fn.arg_size() ),
         isVariadic( fn.isVarArg() ),
+        type( encLLVMFunType( fn.getFunctionType() ) ),
         insts( query::query( fn ).flatten().map( [&]( llvm::Instruction &i ) {
                 // Instructio info contains opcode and suboperation (for ARMW and *CMP)
                 return llvm::ConstantStruct::get( index._instMetaT, {
@@ -46,10 +71,19 @@ struct FunctionMeta {
         return 0;
     }
 
+    template< typename Index >
+    static int argsSize( llvm::Function &fn, Index &index ) {
+        auto tys = fn.getFunctionType()->params();
+        return std::accumulate( tys.begin(), tys.end(), 0, [&]( int accum, llvm::Type *t ) {
+                return accum + index._dl->getTypeStoreSize( t );
+            } );
+    }
+
     llvm::Function *entryPoint;
     int frameSize;
     int argCount;
     bool isVariadic;
+    std::string type;
     std::vector< llvm::Constant * > insts;
 };
 
@@ -64,16 +98,18 @@ struct IndexFunctions : lart::Pass {
         if ( !tagModuleWithMetadata( mod, "lart.divine.index.functions" ) )
             return llvm::PreservedAnalyses::all();
 
+        _dl = std::make_unique< llvm::DataLayout >( &mod );
+
         llvm::GlobalVariable *mdRoot = mod.getGlobalVariable( "__md_functions" );
         ASSERT( mdRoot && "The bitcode must define __md_functions" );
 
         _funcMetaT = llvm::cast< llvm::StructType >( llvm::cast< llvm::ArrayType >(
                             mdRoot->getType()->getElementType() )->getElementType() );
         ASSERT( _funcMetaT && "The bitcode must define _MD_Function" );
-        ASSERT( _funcMetaT->getNumElements() == 7 && "Incompatible _MD_Function" );
+        ASSERT( _funcMetaT->getNumElements() == 8 && "Incompatible _MD_Function" );
 
         _instMetaT = llvm::cast< llvm::StructType >( llvm::cast< llvm::PointerType >(
-                            _funcMetaT->getElementType( 6 ) )->getElementType() );
+                            _funcMetaT->getElementType( 7 ) )->getElementType() );
         ASSERT( _instMetaT && "The bitcode must define _MD_InstInfo" );
         ASSERT( _instMetaT->getNumElements() == 2 && "Incompatible _MD_InstInfo" );
 
@@ -101,13 +137,11 @@ struct IndexFunctions : lart::Pass {
                                 llvm::ConstantArray::get( instArrayT, m.second.insts ),
                                 "lart.divine.index.table." + funNameStr );
 
-            auto *funNameA = llvm::ConstantDataArray::get( mod.getContext(),
-                                llvm::ArrayRef< uint8_t >(
-                                    reinterpret_cast< const uint8_t * >( funNameCStr.data() ),
-                                    funNameCStr.size() ) );
-            auto *funName = new llvm::GlobalVariable( mod, funNameA->getType(), true,
-                                llvm::GlobalValue::ExternalLinkage,
-                                funNameA, "lart.divine.index.name." + funNameStr );
+            auto *funName = util::getStringGlobal( funNameCStr, mod );
+            funName->setName( "lart.divine.index.name." + funNameStr );
+            auto type = m.second.type;
+            type.push_back( 0 );
+            auto *funType = util::getStringGlobal( type, mod );
 
             metatable.push_back( llvm::ConstantStruct::get( _funcMetaT, {
                     llvm::ConstantExpr::getPointerCast( funName, _funcMetaT->getElementType( 0 ) ),
@@ -116,7 +150,9 @@ struct IndexFunctions : lart::Pass {
                     mkint( _funcMetaT, 2, m.second.frameSize ),
                     mkint( _funcMetaT, 3, m.second.argCount ),
                     mkint( _funcMetaT, 4, m.second.isVariadic ),
-                    mkint( _funcMetaT, 5, m.second.insts.size() ),
+                    llvm::ConstantExpr::getPointerCast( funType,
+                                            _funcMetaT->getElementType( 5 ) ),
+                    mkint( _funcMetaT, 6, m.second.insts.size() ),
                     llvm::ConstantExpr::getPointerCast( instTable,
                             llvm::PointerType::getUnqual(  _instMetaT ) )
                 } ) );
@@ -166,6 +202,7 @@ struct IndexFunctions : lart::Pass {
     std::map< llvm::StringRef, FunctionMeta > _meta;
     llvm::StructType *_funcMetaT;
     llvm::StructType *_instMetaT;
+    std::unique_ptr< llvm::DataLayout > _dl;
 };
 
 PassMeta functionMetaPass() {
