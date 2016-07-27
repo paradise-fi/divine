4b25438e32eb6a8326c50505563cb0ec04deedd2
me <me@mornfall.net>
2016-07-27 12:18:52 +0000

    VM: Fix formatting of value::Int< 8 >'s defbits mask.

--- divine/vm/value.hpp
+++ divine/vm/value.hpp
@@ -142,7 +142,7 @@ struct Int : Base
     friend std::ostream & operator<<( std::ostream &o, Int v )
     {
         std::stringstream def;
-        def << std::hex << std::setw( width / 4 ) << std::setfill( '0' )  << v._m;
+        def << std::hex << std::setw( width / 4 ) << std::setfill( '0' ) << +v._m;
         return o << "[int " << brick::string::fmt( v.cooked() ) << " " << def.str() << "]";
     }
 };
