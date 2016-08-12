b15b87ad260e96772aa6da75febca404f755f3ff
Petr Rockai <me@mornfall.net>
2016-08-12 06:57:21 +0000

    VM: Implement choose() in explore::Context.

--- divine/vm/explore.hpp
+++ divine/vm/explore.hpp
@@ -44,14 +44,22 @@ struct State
 struct Context : vm::Context< MutableHeap >
 {
     std::vector< std::string > _trace;
-    std::stack< State > _stack;
+    std::vector< std::pair< int, int > > _stack;
+    int _level;
 
-    using vm::Context< MutableHeap >::Context;
+    Context( Program &p ) : vm::Context< MutableHeap >( p ), _level( 0 ) {}
 
     template< typename I >
-    int choose( int, I, I )
+    int choose( int count, I, I )
     {
-        NOT_IMPLEMENTED();
+        ASSERT_LEQ( _level, int( _stack.size() ) );
+        _level ++;
+        if ( _level < int( _stack.size() ) )
+            return _stack[ _level - 1 ].first;
+        if ( _level == int( _stack.size() ) )
+            return ++ _stack[ _level - 1 ].first;
+        _stack.emplace_back( 0, count );
+        return 0;
     }
 
     void doublefault()
@@ -65,7 +73,13 @@ struct Context : vm::Context< MutableHeap >
         _trace.push_back( s );
     }
 
-    bool finished() { return true; }
+    bool finished()
+    {
+        _level = 0;
+        while ( !_stack.empty() && _stack.back().first + 1 == _stack.back().second )
+            _stack.pop_back();
+        return _stack.empty();
+    }
 };
 
 }
@@ -105,9 +119,9 @@ struct Explore
     void edges( explore::State from, Y yield )
     {
         Eval eval( program(), _ctx );
-        auto root = clone( *from.heap, _ctx.heap(), from.root );
 
         do {
+            auto root = clone( *from.heap, _ctx.heap(), from.root );
             _ctx.enter( _ctx.sched(), nullPointer(),
                         Eval::IntV( eval.heap().size( root ) ), PointerV( root ) );
             _ctx.mask( true );
@@ -140,6 +154,8 @@ struct Explore
 
 namespace t_vm {
 
+using namespace std::literals;
+
 struct TestExplore
 {
     TEST(instance)
@@ -148,32 +164,36 @@ struct TestExplore
         vm::Explore ex( bc );
     }
 
-    static constexpr const char * const lin5 =
-        "void *__vm_make_object( int );"
-        "void *__vm_set_sched( void *(*)( int, void * ) );"
-        "void *__sys_sched( int sz, void *s ) { int *r = s; *r = *r - 1; return *r > 0 ? s : 0; }"
-        "void *__sys_init( void *e ) { __vm_set_sched( __sys_sched ); "
-        "int *r = __vm_make_object( 4 ); *r = 5; return r; }";
+    auto prog( std::string p )
+    {
+        return c2bc(
+            "void *__vm_make_object( int );"s +
+            "void *__vm_set_sched( void *(*)( int, void * ) );"s +
+            "int __vm_choose( int, ... );"s +
+            "void *__sys_sched( int, void * );"s + p );
+    }
 
-    static constexpr const char * const cir5 =
-        "void *__vm_make_object( int );"
-        "void *__vm_set_sched( void *(*)( int, void * ) );"
-        "void *__sys_sched( int sz, void *s ) { int *r = s; *r = ( *r + 1 ) % 5; return s; }"
-        "void *__sys_init( void *e ) { __vm_set_sched( __sys_sched ); "
-        "int *r = __vm_make_object( 4 ); *r = 0; return r; }";
+    auto prog_int( std::string first, std::string next )
+    {
+        return prog(
+            "void *__sys_sched( int sz, void *s ) { int *r = s; *r = "s
+            + next + "; return *r < 0 ? 0 : s; }"s +
+            "void *__sys_init( void *e ) {"s +
+            "    __vm_set_sched( __sys_sched ); "s +
+            "    int *r = __vm_make_object( 4 ); *r = "s + first + "; return r; }"s );
+    }
 
     TEST(simple)
     {
-        auto bc = c2bc( lin5 );
+        auto bc = prog_int( "4", "*r - 1" );
         vm::Explore ex( bc );
         bool found = false;
         ex.initials( [&]( auto ) { found = true; } );
         ASSERT( found );
     }
 
-    void _search( const char *prog, int sc, int ec )
+    void _search( std::shared_ptr< vm::BitCode > bc, int sc, int ec )
     {
-        auto bc = c2bc( prog );
         vm::Explore ex( bc );
         int edgecount = 0, statecount = 0;
         ss::search( ss::Order::PseudoBFS, ex, 1, ss::passive_listen(
@@ -185,8 +205,16 @@ struct TestExplore
 
     TEST(search)
     {
-        _search( lin5, 5, 4 );
-        _search( cir5, 5, 5 );
+        _search( prog_int( "4", "*r - 1" ), 5, 4 );
+        _search( prog_int( "0", "( *r + 1 ) % 5" ), 5, 5 );
+    }
+
+    TEST(branching)
+    {
+        _search( prog_int( "4", "*r - __vm_choose( 2 )" ), 5, 9 );
+        _search( prog_int( "4", "*r - 1 - __vm_choose( 2 )" ), 5, 7 );
+        _search( prog_int( "0", "( *r + __vm_choose( 2 ) ) % 5" ), 5, 10 );
+        _search( prog_int( "0", "( *r + 1 + __vm_choose( 2 ) ) % 5" ), 5, 10 );
     }
 };
 
