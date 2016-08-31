4bc0f5d2615e27275ebae95ac2932bda4b9db034
Jan Mrázek <email@honzamrazek.cz>
2016-08-31 11:44:36 +0000

    DiOS: Refactor Scheduler.

--- runtime/dios/scheduling.cpp
+++ runtime/dios/scheduling.cpp
@@ -46,3 +46,155 @@ void get_thread_id( __dios::Context& ctx, void *retval, va_list vl ) {
 }
 
 } // namespace __sc
+
+namespace __dios {
+
+Thread::Thread( FunPtr fun, FunPtr cleanup ) noexcept
+    : _frame( static_cast< _VM_Frame * >( __vm_obj_make( fun->frame_size ) ) ),
+      _cleanup_handler( cleanup ),
+      _state( State::RUNNING )
+{
+    _frame->pc = fun->entry_point;
+    _frame->parent = nullptr;
+}
+
+Thread::Thread( Thread&& o ) noexcept
+    : _frame( o._frame ), _cleanup_handler( o._cleanup_handler ),
+      _state( o._state )
+{
+    o._frame = 0;
+    o._state = State::ZOMBIE;
+}
+
+Thread& Thread::operator=( Thread&& o ) noexcept {
+    std::swap( _frame, o._frame );
+    std::swap( _cleanup_handler, o._cleanup_handler );
+    std::swap( _state, o._state );
+    return *this;
+}
+
+void Thread::update_state() noexcept {
+    if ( !_frame )
+        _state = State::ZOMBIE;
+}
+
+void Thread::stop( int reason ) noexcept {
+    if ( !active() ) {
+        __vm_fault( static_cast< _VM_Fault >( _DiOS_F_Threading ) );
+        return;
+    }
+
+    clear();
+    auto* frame = reinterpret_cast< CleanupFrame * >( _frame );
+    frame->pc = _cleanup_handler->entry_point;
+    frame->parent = nullptr;
+    frame->reason = reason;
+    _state = State::CLEANING_UP;
+}
+
+void Thread::hard_stop() noexcept {
+    _state = State::ZOMBIE;
+    clear();
+}
+
+void Thread::clear() noexcept {
+    while ( _frame ) {
+        _VM_Frame *f = _frame->parent;
+        __vm_obj_free( _frame );
+        _frame = f;
+    }
+}
+
+Thread *Scheduler::choose_thread() noexcept {
+    get_active_thread()->update_state();
+    int idx = __vm_choose( _cf->thread_count );
+    _cf->active_thread = idx;
+    Thread &thread = get_threads()[ idx ];
+    if ( !thread.zombie() )
+        return &thread;
+    return nullptr;
+}
+
+Thread *Scheduler::choose_live_thread() noexcept {
+    get_active_thread()->update_state();
+    int count = 0;
+    for ( int i = 0; i != _cf->thread_count; i++ ) {
+        if ( get_threads()[ i ].active() )
+            count++;
+    }
+
+    if ( count == 0 )
+        return nullptr;
+
+    struct PI { int pid, tid, choice; };
+    PI *pi = reinterpret_cast< PI * >( __vm_obj_make( count * sizeof( PI ) ) );
+    PI *pi_it = pi;
+    count = 0;
+    for ( int i = 0; i != _cf->thread_count; i++ ) {
+        if ( get_threads()[ i ].active() ) {
+            pi_it->pid = 1;
+            pi_it->tid = i;
+            pi_it->choice = count++;
+            ++pi_it;
+        }
+    }
+
+    __vm_trace( _VM_T_SchedChoice, pi );
+    int sel = __vm_choose( count );
+    auto thr = get_threads();
+    while ( sel != 0 ) {
+        if ( thr->active() )
+            --sel;
+        ++thr;
+    }
+
+    _cf->active_thread = thr - get_threads();
+    __dios_assert_v( thr->_frame, "Frame is invalid" );
+    return thr;
+}
+
+void Scheduler::start_main_thread( FunPtr main, int argc, char** argv, char** envp ) noexcept {
+    __dios_assert_v( main, "Invalid main function!" );
+
+    _DiOS_FunPtr dios_main = __dios_get_fun_ptr( "__dios_main" );
+    __dios_assert_v( dios_main, "Invalid DiOS main function" );
+
+    new ( &( _cf->main_thread ) ) Thread( dios_main );
+    _cf->active_thread = 0;
+    _cf->thread_count = 1;
+
+    DiosMainFrame *frame = reinterpret_cast< DiosMainFrame * >( _cf->main_thread._frame );
+    frame->l = main->arg_count;
+
+    frame->argc = argc;
+    frame->argv = argv;
+    frame->envp = envp;
+}
+
+ThreadId Scheduler::start_thread( FunPtr routine, void *arg, FunPtr cleanup ) noexcept {
+    __dios_assert( routine );
+
+    __vm_obj_resize( _cf, __vm_obj_size( _cf ) + sizeof( Thread ) );
+
+    Thread &t = get_threads()[ _cf->thread_count++ ];
+    new ( &t ) Thread( routine, cleanup );
+    ThreadRoutineFrame *frame = reinterpret_cast< ThreadRoutineFrame * >( t._frame );
+    frame->arg = arg;
+
+    return _cf->thread_count - 1;
+}
+
+void Scheduler::kill_thread( ThreadId t_id, int reason ) noexcept {
+    __dios_assert( t_id );
+    __dios_assert( int( t_id ) < _cf->thread_count );
+    get_threads()[ t_id ].stop( reason );
+}
+
+void Scheduler::terminate() noexcept {
+    for ( int i = 0; i != _cf->thread_count; i++ ) {
+        if ( get_threads()[ i ].active() )
+            get_threads()[ i ].hard_stop();
+    }
+}
+
+} // namespace __dios
--- runtime/dios/scheduling.hpp
+++ runtime/dios/scheduling.hpp
@@ -44,74 +44,23 @@ struct Thread {
     FunPtr _cleanup_handler;
     State _state;
 
-    Thread( FunPtr fun, FunPtr cleanup = nullptr )
-        : _frame( static_cast< _VM_Frame * >( __vm_obj_make( fun->frame_size ) ) ),
-          _cleanup_handler( cleanup ),
-          _state( State::RUNNING )
-    {
-        _frame->pc = fun->entry_point;
-        _frame->parent = nullptr;
-        __dios_trace_f( "Thread constuctor: %p, %p", _frame, _frame->pc );
-    }
+    Thread( FunPtr fun, FunPtr cleanup = nullptr ) noexcept;
+    Thread( const Thread& o ) noexcept = delete;
+    Thread& operator=( const Thread& o ) noexcept = delete;
+    Thread( Thread&& o ) noexcept;
+    Thread& operator=( Thread&& o ) noexcept;
+    ~Thread() noexcept { clear(); }
 
-    Thread( const Thread& o ) = delete;
-    Thread& operator=( const Thread& o ) = delete;
+    bool active() const noexcept { return _state == State::RUNNING; }
+    bool cleaning_up() const noexcept { return _state == State::CLEANING_UP; }
+    bool zombie() const noexcept { return _state == State::ZOMBIE; }
 
-    Thread( Thread&& o ) :
-        _frame( o._frame ), _cleanup_handler( o._cleanup_handler ),
-        _state( o._state )
-    {
-        o._frame = 0;
-        o._state = State::ZOMBIE;
-    }
-
-    Thread& operator=( Thread&& o ) {
-        std::swap( _frame, o._frame );
-        std::swap( _cleanup_handler, o._cleanup_handler );
-        std::swap( _state, o._state );
-        return *this;
-    }
-
-    ~Thread() {
-        clear();
-    }
-
-    bool active() const { return _state == State::RUNNING; }
-    bool cleaning_up() const { return _state == State::CLEANING_UP; }
-    bool zombie() const { return _state == State::ZOMBIE; }
-
-    void update_state() {
-        if ( !_frame )
-            _state = State::ZOMBIE;
-    }
-
-    void stop( int reason ) {
-        if ( !active() ) {
-            __vm_fault( static_cast< _VM_Fault >( _DiOS_F_Threading ) );
-            return;
-        }
-
-        clear();
-        auto* frame = reinterpret_cast< CleanupFrame * >( _frame );
-        frame->pc = _cleanup_handler->entry_point;
-        frame->parent = nullptr;
-        frame->reason = reason;
-        _state = State::CLEANING_UP;
-    }
-
-    void hard_stop() {
-        _state = State::ZOMBIE;
-        clear();
-    }
+    void update_state() noexcept;
+    void stop( int reason ) noexcept;
+    void hard_stop() noexcept;
 
 private:
-    void clear() {
-        while ( _frame ) {
-            _VM_Frame *f = _frame->parent;
-            __vm_obj_free( _frame );
-            _frame = f;
-        }
-    }
+    void clear() noexcept;
 };
 
 struct ControlFlow {
@@ -123,109 +72,16 @@ struct ControlFlow {
 struct Scheduler {
     Scheduler() : _cf( static_cast< ControlFlow * >( __vm_obj_make( sizeof( ControlFlow ) ) ) ) { }
 
-    Thread* get_threads() const noexcept {
-        return &( _cf->main_thread );
-    }
+    Thread *get_threads() const noexcept { return &( _cf->main_thread ); }
+    Thread *get_active_thread() noexcept { return &get_threads()[ _cf->active_thread ]; }
+    Thread *choose_thread() noexcept;
+    Thread *choose_live_thread() noexcept;
 
-    void *get_cf() const noexcept {
-        return _cf;
-    }
+    void start_main_thread( FunPtr main, int argc, char** argv, char** envp ) noexcept;
+    ThreadId start_thread( FunPtr routine, void *arg, FunPtr cleanup ) noexcept;
 
-    Thread *get_active_thread() {
-        return &get_threads()[ _cf->active_thread ];
-    }
-
-    Thread *choose_thread() {
-        get_active_thread()->update_state();
-        int idx = __vm_choose( _cf->thread_count );
-        _cf->active_thread = idx;
-        Thread &thread = get_threads()[ idx ];
-        if ( !thread.zombie() )
-            return &thread;
-        return nullptr;
-    }
-
-    Thread *choose_live_thread() {
-        get_active_thread()->update_state();
-        int count = 0;
-        for ( int i = 0; i != _cf->thread_count; i++ ) {
-            if ( get_threads()[ i ].active() )
-                count++;
-        }
-
-        if ( count == 0 )
-            return nullptr;
-
-        struct PI { int pid, tid, choice; };
-        PI *pi = reinterpret_cast< PI * >( __vm_obj_make( count * sizeof( PI ) ) );
-        PI *pi_it = pi;
-        count = 0;
-        for ( int i = 0; i != _cf->thread_count; i++ ) {
-            if ( get_threads()[ i ].active() ) {
-                pi_it->pid = 1;
-                pi_it->tid = i;
-                pi_it->choice = count++;
-                ++pi_it;
-            }
-        }
-
-        __vm_trace( _VM_T_SchedChoice, pi );
-        int sel = __vm_choose( count );
-        auto thr = get_threads();
-        while ( sel != 0 ) {
-            if ( thr->active() )
-                --sel;
-            ++thr;
-        }
-
-        _cf->active_thread = thr - get_threads();
-        __dios_assert_v( thr->_frame, "Frame is invalid" );
-        return thr;
-    }
-
-    void start_main_thread( FunPtr main, int argc, char** argv, char** envp ) noexcept {
-        __dios_assert_v( main, "Invalid main function!" );
-
-        _DiOS_FunPtr dios_main = __dios_get_fun_ptr( "__dios_main" );
-        __dios_assert_v( dios_main, "Invalid DiOS main function" );
-
-        new ( &( _cf->main_thread ) ) Thread( dios_main );
-        _cf->active_thread = 0;
-        _cf->thread_count = 1;
-
-        DiosMainFrame *frame = reinterpret_cast< DiosMainFrame * >( _cf->main_thread._frame );
-        frame->l = main->arg_count;
-
-        frame->argc = argc;
-        frame->argv = argv;
-        frame->envp = envp;
-    }
-
-    ThreadId start_thread( FunPtr routine, void *arg, FunPtr cleanup ) {
-        __dios_assert( routine );
-
-        __vm_obj_resize( _cf, __vm_obj_size( _cf ) + sizeof( Thread ) );
-
-        Thread &t = get_threads()[ _cf->thread_count++ ];
-        new ( &t ) Thread( routine, cleanup );
-        ThreadRoutineFrame *frame = reinterpret_cast< ThreadRoutineFrame * >( t._frame );
-        frame->arg = arg;
-
-        return _cf->thread_count - 1;
-    }
-
-    void kill_thread( ThreadId t_id, int reason ) {
-        __dios_assert( t_id );
-        __dios_assert( int( t_id ) < _cf->thread_count );
-        get_threads()[ t_id ].stop( reason );
-    }
-
-    void kill_all() {
-        for ( int i = 0; i != _cf->thread_count; i++ ) {
-            if ( get_threads()[ i ].active() )
-                get_threads()[ i ].hard_stop();
-        }
-    }
+    void kill_thread( ThreadId t_id, int reason ) noexcept;
+    void terminate() noexcept;
 
     ThreadId get_thread_id() {
         return _cf->active_thread;
@@ -239,7 +95,7 @@ void sched() noexcept
 {
     auto ctx = static_cast< Context * >( __vm_control( _VM_CA_Get, _VM_CR_State ) );
     if ( ctx->fault->triggered ) {
-        ctx->scheduler->kill_all();
+        ctx->scheduler->terminate();
         ctx->fault->triggered = false;
         return;
     }
@@ -248,10 +104,12 @@ void sched() noexcept
     if ( ctx->syscall->handle( ctx ) ) {
         // Allow thread to obtain syscall result
         t = ctx->scheduler->get_active_thread();
-    else
+    }
+    else {
         t = THREAD_AWARE_SCHED ?
                 ctx->scheduler->choose_live_thread() :
                 ctx->scheduler->choose_thread();
+    }
 
     if ( t && t->_frame ) {
         __vm_control( _VM_CA_Set, _VM_CR_Frame, t->_frame,
