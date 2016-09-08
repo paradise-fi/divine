607945d115d87a335f673516d53dfe52bd1b12f8
Vladimir Still <xstill@fi.muni.cz>
2016-09-08 15:25:45 +0000

    tools: Rewrite debug paths in runtime-cc to point to VFS.

--- tools/runtime-cc.cpp
+++ tools/runtime-cc.cpp
@@ -1,19 +1,40 @@
 #include <divine/cc/clang.hpp>
+#include <divine/rt/runtime.hpp>
 #include <llvm/Bitcode/ReaderWriter.h>
+#include <lart/divine/debugpaths.hpp>
+#include <brick-string>
+#include <brick-fs>
 #include <iostream>
 
 using namespace divine;
+using namespace brick;
 
 /* usage: runtime-cc srcdir bindir source.c output.bc [flags] */
 int main( int argc, const char **argv )
 {
     try {
         cc::Compiler clang;
-        clang.allowIncludePath( argv[1] );
-        clang.allowIncludePath( argv[2] );
+        std::string srcDir = argv[1], binDir = argv[2];
+        clang.allowIncludePath( srcDir );
+        clang.allowIncludePath( binDir );
         std::vector< std::string > opts;
         std::copy( argv + 5, argv + argc, std::back_inserter( opts ) );
         auto mod = clang.compileModule( argv[3], opts );
+
+        lart::divine::rewriteDebugPaths( *mod, [=]( auto p ) {
+                std::string relpath;
+                if ( string::startsWith( p, srcDir ) )
+                    relpath = p.substr( srcDir.size() );
+                else if ( string::startsWith( p, binDir ) )
+                    relpath = p.substr( binDir.size() );
+                if ( !relpath.empty() ) {
+                    while ( fs::isPathSeparator( relpath[0] ) )
+                        relpath.erase( relpath.begin() );
+                    return fs::joinPath( rt::directory( relpath ), relpath );
+                }
+                return p;
+            } );
+
         std::error_code err;
         llvm::raw_fd_ostream outs( argv[4], err, llvm::sys::fs::F_None );
         llvm::WriteBitcodeToFile( mod.get(), outs );
