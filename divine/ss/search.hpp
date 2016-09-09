792b91a85e04ea9269cd053867c8bfb734a60d59
Petr Rockai <me@mornfall.net>
2016-09-09 11:16:48 +0000

    SS: Fix multi-threaded pseudo-BFS.

--- divine/ss/search.hpp
+++ divine/ss/search.hpp
@@ -20,7 +20,7 @@ namespace shmem = ::brick::shmem;
 template< typename L >
 struct SearchInterface
 {
-    virtual void run( bool ) = 0;
+    virtual void run( int, bool ) = 0;
     virtual ~SearchInterface() {}
 };
 
@@ -36,7 +36,7 @@ struct SearchBase : SearchInterface< L >
 template< typename B, typename L >
 struct DFS : SearchBase< B, L >
 {
-    virtual void run( bool ) override
+    virtual void run( int, bool ) override
     {
         NOT_IMPLEMENTED();
     }
@@ -49,53 +49,68 @@ struct BFS : SearchBase< B, L >
 {
     shmem::SharedQueue< typename B::State > _q;
     shmem::StartDetector _start;
+    shmem::ApproximateCounter _work;
+    std::shared_ptr< std::atomic< bool > > _terminate;
 
-    BFS( const B &b, const L &l ) : SearchBase< B, L >( b, l ) {}
+    BFS( const B &b, const L &l )
+        : SearchBase< B, L >( b, l ),
+          _terminate( new std::atomic< bool >( false ) )
+    {}
 
-    virtual void run( bool initials ) override
+    virtual void run( int peers, bool initials ) override
     {
-        bool terminate = false;
         int count = 0;
 
-        /* TODO the set of initial exploration states should be customisable */
         if ( initials )
+        {
             this->_b.initials( [&]( auto i )
                                {
                                    auto b = this->_l.state( i );
                                    if ( b == L::Process || b == L::AsNeeded )
-                                       _q.push( i );
-                                   if ( count++ % 10 == 0 )
-                                       _q.flush();
+                                       _q.push( i ), ++ _work;
                                } );
+            _q.flush();
+        }
 
-        while ( !terminate && !_q.empty() )
+        _start.waitForAll( peers );
+
+        while ( _work )
         {
+            if ( _q.empty() )
+            {
+                _q.flush();
+                _work.sync();
+                if ( _terminate->load() )
+                    break;
+                else
+                    continue;
+            }
             auto v = _q.pop();
             this->_b.edges(
                 v, [&]( auto x, auto label, bool isnew )
                 {
                     auto a = this->_l.edge( v, x, label );
                     if ( a == L::Terminate )
-                        terminate = true;
+                        _terminate->store( true );
                     if ( a == L::Process || ( a == L::AsNeeded && isnew ) )
                     {
                         auto b = this->_l.state( x );
                         if ( b == L::Terminate )
-                            terminate = true;
+                            _terminate->store( true );
                         if ( b == L::Process || ( b == L::AsNeeded && isnew ) )
-                            _q.push( x );
+                            _q.push( x ), ++ _work;
                     }
                 } );
-            if ( _q.empty() )
-                _q.flush();
+            -- _work;
         }
+        ASSERT( _terminate->load() || _q.empty() );
     }
 };
 
 template< typename B, typename L >
 struct DistributedBFS : BFS< B, L >
 {
-    virtual void run( bool ) override
+    virtual void run( int, bool ) override
     {
         NOT_IMPLEMENTED();
         /* also pull stuff out from networked queues with some probability */
@@ -121,10 +136,10 @@ auto do_search( const B &b, const L &l, int threads )
     S< B, L > search( b, l );
     for ( int i = 0; i < threads; ++i )
         ls.emplace_back(
-            std::async( [&search, i]()
+            std::async( [&search, i, threads]()
                         {
                             S< B, L > s( search );
-                            s.run( i == 0 );
+                            s.run( threads, i == 0 );
                             return s.result();
                         } ) );
 
@@ -146,7 +161,7 @@ auto search( Order o, B b, int threads, L l )
     if ( threads == 1 && o == Order::PseudoBFS )
     {
         BFS< B, L > search( b, l );
-        search.run( true );
+        search.run( 1, true );
         return search.result();
     }
     else if ( /* g.peers().empty() && */ o == Order::PseudoBFS )
