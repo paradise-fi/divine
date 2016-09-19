103af36bc88699f4d3b30d96e83efd8f1f7792d1
Petr Rockai <me@mornfall.net>
2016-09-19 10:09:22 +0000

    test: Convert the remaining pthread-related tests.

---
+++ test/pthread/mutex-missing.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <pthread.h>
+
+int shared = 0;
+
+void *thread( void *x )
+{
+    ++ shared;
+    return 0;
+}
+
+int main()
+{
+    pthread_t tid;
+    pthread_create( &tid, NULL, thread, NULL );
+    ++ shared;
+    pthread_join( tid, NULL );
+    assert( shared == 2 ); /* ERROR */
+    return 0;
+}
