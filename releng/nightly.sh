160a76cd8a410ba4e9acda70134073511f367dff
Petr Rockai <me@mornfall.net>
2016-09-25 15:28:36 +0000

    releng: Add nightly.sh, for pushing patches around & producing reports.

---
+++ releng/nightly.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+email()
+{
+    echo "From: xrockai@fi.muni.cz"
+    echo "To: $address"
+    echo "Subject: DIVINE daily $(date +%Y-%m-%d): $patches"
+    echo
+    cat report.txt
+}
+
+finished()
+{
+    perl -i.orig -e '$rep = `cat report.txt`; while (<>) { s/\@report\@/$rep/; print $_; }' \
+         doc/website/status.md
+    make website
+    mv doc/website/status.md.orig doc/website/status.md
+
+    email | sendmail $address
+    exit $1
+}
+
+changes()
+{
+    echo "# Latest Changes"
+    echo
+    grep -v ^patch changes.txt | \
+        perl -ne 'if ( /^Author: (.*)/ ) { $author = $1; }
+              elsif ( /^Date:   (.*)/ ) { print " * $author, $1  \n"; }
+              elsif ( /^    ([RMA]) \.\/(.*)/ ) { print "       `$1 $2`  \n"; }
+              else { s/^  \* /  /; print "  $_"; }'
+}
+
+cantbuild()
+{
+    make 2>&1 || true # dump the failing stuff into the report
+    echo
+    echo "Build failed. Stopping here."
+}
+
+failed()
+{
+    if test -e $list; then
+        echo "Some tests failed:"
+        echo
+        grep -v passed $list | perl -pe 's,([a-z]+):(.*) (.*),    $3: [$1] $2,'
+        echo
+        echo "Remaining $(grep -c passed $list) tests passed. Stopping here."
+    else
+        echo "Make check failed:"
+        echo
+        make check 2>&1 || true
+    fi
+}
+
+passed()
+{
+    echo "All $(grep -c passed $list) tests passed. The above patches are now part of"
+    echo "<http://divine.fi.muni.cz/current>."
+}
+
+set -ve
+umask 0022
+
+address=$1
+upstream=$2
+downstream=$3
+
+# anything new?
+darcs pull --dry -a --match 'not name XXX' --no-deps -s --no-set-default $upstream 2>&1 | \
+    egrep -v '^Would|^Making|^No remote' | tee changes.txt
+if ! grep -q ^patch changes.txt; then exit 0; fi
+
+darcs pull --quiet -a --match 'not name XXX' --no-deps
+
+patchcount=$(grep -c ^patch changes.txt)
+if test $patchcount -eq 1; then patches="1 new patch"; else patches="$patchcount new patches"; fi
+
+objdir="$(make show var=OBJ)/debug"
+list="$objdir/test/results/list"
+
+changes > report.txt
+
+if ! make; then # does it build at all?
+    cantbuild >> report.txt
+    finished 1
+fi
+
+(echo "# Test results"; echo) >> report.txt
+
+rm -f $list
+if ! make check || grep -q -v passed $list; then
+    failed >> report.txt
+    finished 1
+fi
+
+# everything looks good, push to current
+passed >> report.txt
+darcs push --quiet -a --no-set-default $downstream
+finished 0
