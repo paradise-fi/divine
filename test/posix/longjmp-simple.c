4802a73869fc18a40962c0ae2db62d9e3bfda943
Vladimir Still <xstill@fi.muni.cz>
2016-09-25 13:41:37 +0000

    test: Add setjmp and longjmp tests.

---
+++ test/posix/longjmp-simple.c
@@ -0,0 +1,21 @@
+#include <setjmp.h>
+#include <assert.h>
+#include <stdbool.h>
+
+volatile bool jumped = false;
+
+int main() {
+    jmp_buf jb;
+    int r = setjmp( jb );
+    if ( jumped )
+        assert( r == 42 );
+    else
+        assert( r == 0 );
+
+    if ( jumped )
+        return 0;
+
+    jumped = true;
+    longjmp( jb, 42 );
+}
+
---
+++ test/posix/setjmp.c
@@ -0,0 +1,8 @@
+#include <setjmp.h>
+#include <assert.h>
+
+int main() {
+    jmp_buf jb;
+    int r = setjmp( jb );
+    assert( r == 0 );
+}
