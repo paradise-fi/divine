65e64933be7d6ccd6f12b211f09d87182bca3bbd
Vladimir Still <xstill@fi.muni.cz>
2016-10-04 06:43:28 +0000

    test: Add tests for pthread once, including one running in global constructors.

---
+++ test/pthread/once-init.c
@@ -0,0 +1,20 @@
+#include <pthread.h>
+#include <assert.h>
+
+int x = 0;
+pthread_once_t once = PTHREAD_ONCE_INIT;
+
+void callee( void ) {
+    ++x;
+}
+
+void init( void ) {
+    pthread_once( &once, callee );
+}
+
+__attribute__((constructor)) void foo( void ) { init(); }
+__attribute__((constructor)) void bar( void ) { init(); }
+
+int main() {
+    assert( x == 1 );
+}
