ca443b342a2522c1b64a0f745bad4eb914a61b48
Vladimir Still <xstill@fi.muni.cz>
2016-10-14 16:53:26 +0000

    test: Add more exception related tests.

---
+++ test/cpp/exception-uncaught-exit.cpp
@@ -0,0 +1,19 @@
+#include <cassert>
+#include <cstdlib>
+#include <exception>
+
+void foo() {
+    throw 4;
+}
+
+int main() {
+    std::set_terminate( [] {
+            std::exit( 0 );
+        } );
+    try {
+        foo();
+    } catch ( long ) {
+        assert( 0 );
+    }
+    assert( 0 );
+}
---
+++ test/cpp/nounwind.cpp
@@ -0,0 +1,11 @@
+/* ERRSPEC: _Unwind_RaiseException */
+
+__attribute__((noinline)) void bar() { throw 0; }
+
+__attribute__((nothrow, noinline)) void foo() { bar(); }
+
+int main() {
+    try {
+        foo();
+    } catch ( ... ) { }
+}
