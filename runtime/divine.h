0db6512bf30d7f08a2db892f61d39c7944173daa
Petr Rockai <me@mornfall.net>
2016-11-12 15:40:33 +0000

    runtime: Make the argument to __vm_obj_size 'const void *'.

--- runtime/divine.h
+++ runtime/divine.h
@@ -222,7 +222,7 @@ void __vm_trace( enum _VM_Trace type, ... ) NOTHROW NATIVE_VISIBLE;
 void *__vm_obj_make( int size ) NOTHROW NATIVE_VISIBLE;
 void  __vm_obj_resize( void *ptr, int size ) NOTHROW NATIVE_VISIBLE;
 void  __vm_obj_free( void *ptr ) NOTHROW NATIVE_VISIBLE;
-int   __vm_obj_size( void * ) NOTHROW NATIVE_VISIBLE;
+int   __vm_obj_size( const void * ) NOTHROW NATIVE_VISIBLE;
 
 CPP_END
 
