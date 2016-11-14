b97682cb6de521661129cdae80a4d6294f09b494
Petr Rockai <me@mornfall.net>
2016-11-14 18:24:54 +0000

    test: Add a test for the 'trace' command in sim.

---
+++ test/sim/trace.sh
@@ -0,0 +1,19 @@
+. lib/testcase
+
+sim $TESTS/c/malloc.c <<EOF
++ ^# executing __boot at
+> trace 0 0 1
+- FAULT
++ ^traced states: #1 #2
++ ^# executing malloc at
+> step --count 3
++ ^# executing - at -
+> trace 0 0 0
++ ^traced states: #1 #2
++ ^# executing malloc at
+> step --count 5
++ ^T: FAULT: null pointer dereference
++ ^# executing __dios::Fault::handler
+> trace 0 0 0 0
++ \[loop closed, unused choices: 0\]
+EOF
