bbaaa6bf054788b9628e9c7f0663913ac569ead8
Jan Mrázek <email@honzamrazek.cz>
2016-11-16 17:22:40 +0000

    test: Add test for DiOS simfail configration API.

---
+++ test/dios/api/fault-config-simfail-a.2.c
@@ -0,0 +1,19 @@
+#include <dios.h>
+#include <assert.h>
+#include <stdlib.h>
+
+extern unsigned char const *_DiOS_fault_cfg;
+
+int main() {
+    int ret = __dios_configure_fault( _DiOS_SF_Malloc, _DiOS_FC_NoFail );
+    assert( ret >= 0 );
+    int *a = malloc( sizeof( int ) );
+    assert( a );
+    free( a );
+    ret = __dios_configure_fault( _DiOS_SF_Malloc, _DiOS_FC_SimFail );
+    assert( ret >= 0 );
+    int *b = malloc( sizeof( int ) );
+    assert( b ); /* ERROR */
+    free( b );
+}
+
---
+++ test/dios/api/fault-config-simfail-b.1.c
@@ -0,0 +1,24 @@
+#include <dios.h>
+#include <assert.h>
+#include <stdlib.h>
+
+int main() {
+    int orig = __dios_get_fault_config( _DiOS_SF_Malloc );
+    int ret = __dios_configure_fault( _DiOS_SF_Malloc, _DiOS_FC_Ignore );
+    assert( ret == _DiOS_FC_EInvalidCfg );
+    assert( orig == __dios_get_fault_config( _DiOS_SF_Malloc ) );
+    ret = __dios_configure_fault( _DiOS_SF_Malloc, _DiOS_FC_Abort );
+    assert( ret == _DiOS_FC_EInvalidCfg );
+    assert( orig == __dios_get_fault_config( _DiOS_SF_Malloc ) );
+    ret = __dios_configure_fault( _DiOS_SF_Malloc, _DiOS_FC_Report );
+    assert( ret == _DiOS_FC_EInvalidCfg );
+    assert( orig == __dios_get_fault_config( _DiOS_SF_Malloc ) );
+    ret = __dios_configure_fault( _DiOS_SF_Last, _DiOS_FC_SimFail );
+    assert( ret == _DiOS_FC_EInvalidFault );
+    assert( orig == __dios_get_fault_config( _DiOS_SF_Malloc ) );
+
+    ret = __dios_configure_fault( _DiOS_SF_Malloc, _DiOS_FC_SimFail );
+    int *b = malloc( sizeof( int ) );
+    assert( b ); /* ERROR */
+    free( b );
+}
