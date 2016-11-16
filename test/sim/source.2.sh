f920c9b94e271b4cc46d6340fc762d821931a32a
Vladimir Still <xstill@fi.muni.cz>
2016-11-16 14:31:29 +0000

    test: Add test for sim source command.

---
+++ test/sim/source.2.sh
@@ -0,0 +1,22 @@
+. lib/testcase
+
+cat > file.cpp <<EOF
+int main() {
+    struct X {
+        ~X() { }
+    } _;
+}
+EOF
+
+sim file.cpp <<EOF
++ ^# executing __boot at
+> start
+- ^# executing
++ ^# executing main at
+> source
++ ^>>\s*1\s*int main\(\) \{
++ ^\s*2\s*    struct X \{
++ ^\s*3\s*        ~X\(\) \{ \}
++ ^\s*4\s*    \} _;
++ ^\s*5\s*\}
+EOF
