09da749176373d7f52c786a0d846a7d652b3cd27
Petr Rockai <me@mornfall.net>
2016-11-21 08:19:01 +0000

    test: Update sim/trace.2.sh to match new behaviour.

--- test/sim/trace.2.sh
+++ test/sim/trace.2.sh
@@ -15,5 +15,25 @@ sim $TESTS/c/malloc.1.c <<EOF
 + ^T: FAULT: null pointer dereference
 + ^# executing __dios::Fault::handler
 > trace 0 0 0 0
-+ \[loop closed, unused choices: 0\]
++ traced states: #1 #2 #4
++ unused choices: 0
+EOF
+
+tee loop.c <<EOF
+#include <divine.h>
+
+void __sched() {}
+int main() {}
+
+void __boot()
+{
+    __vm_control( _VM_CA_Set, _VM_CR_State, __vm_obj_make( 4 ) );
+    __vm_control( _VM_CA_Set, _VM_CR_Scheduler, __sched );
+}
+EOF
+
+sim loop.c <<EOF
+> trace 0 0
++ traced states: #1 #1 \[loop closed\]
++ unused choices: 0 0
 EOF
