387e1fb4968e064e38ba23b992e5a695c5de9e94
Katarina Kejstova <k.kejstova@gmail.com>
2016-11-30 16:41:13 +0000

    test: Add tests for VFS initialization from a snapshot.

---
+++ test/posix/fs-dirent-capture2.2.sh
@@ -0,0 +1,30 @@
+. lib/testcase
+
+mkdir -p capture
+
+cat > capture/fs-dirent-capture.c <<EOF
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int main() {
+
+    int fd = open( "emptyDir", O_RDONLY );
+    assert( fd >= 0 );
+    assert( close( fd ) == 0 );
+
+    fd = open( "emptyDir", O_WRONLY );
+    assert( fd == -1 );
+    assert( errno == EISDIR );
+    return 0;
+}
+EOF
+
+mkdir capture/dir
+mkdir capture/dir/emptyDir
+
+divine verify --threads 1 --max-backtrace-depth 65536 --capture capture/dir:follow:/ capture/fs-dirent-capture.c
---
+++ test/posix/fs-open-capture1.2.sh
@@ -0,0 +1,29 @@
+. lib/testcase
+
+mkdir -p capture
+
+cat > capture/fs-open-capture.c <<EOF
+#include <unistd.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main() {
+
+    int fd = open( "dir", O_RDONLY );
+    assert( fd >= 0 );
+    assert( close( fd ) == 0 );
+
+    fd = open( "dir", O_WRONLY );
+    assert( fd == -1 );
+    assert( errno == EISDIR );
+    return 0;
+}
+EOF
+
+mkdir capture/link
+mkdir capture/link/dir
+touch capture/link/file
+ln capture/link/file capture/link/hardlinkFile
+
+divine verify  --threads 1 --max-backtrace-depth 65536 --capture capture/link:follow:/ capture/fs-open-capture.c
