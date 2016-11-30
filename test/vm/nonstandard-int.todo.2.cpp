a0420470c1e0ec5ed552101ca31cbd2947e19cd5
Jan Mrázek <email@honzamrazek.cz>
2016-11-30 14:12:20 +0000

    test: Add test for non-standard integer widths.

---
+++ test/vm/nonstandard-int.todo.2.cpp
@@ -0,0 +1,21 @@
+// VERIFY_OPTS: -C,-Oz
+#include <divine.h>
+
+// Constructor of this struct produces in Clang 3.7 with Oz 48bit integer
+struct Evil {
+    bool a, b, c, d, e, f;
+    Evil() :
+        a( false ), b( false ), c( false ), d( false ), e( false ), f( false ) {}
+};
+
+Evil evilFunc() {
+    Evil evil;
+    if ( __vm_choose( 2 ) )
+        evil.a = true;
+    return evil;
+}
+
+int main() {
+    Evil evil = evilFunc();
+    return 0;
+}
