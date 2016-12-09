3239fc222f5ceb2dc5554d51b5a6855694e79764
Vladimir Still <xstill@fi.muni.cz>
2016-12-09 17:54:27 +0000

    test: Add tests for initialization of pthread barrier/cond/mutex/rwlock.

---
+++ test/pthread/barrier-init.2.c
@@ -0,0 +1,16 @@
+#include <pthread.h>
+#include <assert.h>
+#include <errno.h>
+
+int main() {
+    pthread_barrier_t barrier;
+    int r = pthread_barrier_init( &barrier, NULL, 2 );
+    assert( r == 0 );
+    r = pthread_barrier_init( &barrier, NULL, 2 );
+    assert( r == EBUSY );
+    r = pthread_barrier_destroy( &barrier );
+    assert( r == 0 );
+    r = pthread_barrier_init( &barrier, NULL, 2 );
+    assert( r == 0 );
+}
+
