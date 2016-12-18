d0eb1636c1c75d090cf64342faa16af90ebe29d9
Vladimir Still <xstill@fi.muni.cz>
2016-12-18 19:44:54 +0000

    test: Harden --dont-link/-c tests to check the produced file contains symbols.

--- test/cc/dont-link-a.2.sh
+++ test/cc/dont-link-a.2.sh
@@ -1,6 +1,16 @@
 . lib/testcase
 
-touch a.c b.c
+cat > a.c <<EOF
+void dont_link_test_foo( void ) { }
+EOF
+cat > b.c <<EOF
+void dont_link_test_bar( void ) { }
+EOF
+
 divine cc -c a.c b.c
-test -f a.bc
-test -f b.bc
+llvm-nm a.bc | grep dont_link_test_foo
+llvm-nm b.bc | grep dont_link_test_bar
+
+if llvm-nm a.bc | grep dont_link_test_bar || llvm-nm b.bc | grep dont_link_test_foo; then
+    false
+fi
