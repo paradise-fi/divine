338538ffdfa101c281e1c76da86bdce55be457b6
Petr Rockai <me@mornfall.net>
2016-12-19 15:30:57 +0000

    UI: Convert 'draw' to use mc::draw() and to run 'dot' automatically.

--- divine/ui/cli.hpp
+++ divine/ui/cli.hpp
@@ -130,10 +130,11 @@ struct Sim : WithBC
 struct Draw : WithBC
 {
     int _number = 0;
-    int _distance = 0;
+    int _distance = 32;
     enum { All, None, Trace } _labels = None;
     bool _bfs = false, _raw = false;
-    std::string _render = std::string( "dot -Tx11" );
+    std::string _render = "dot -Tx11"s;
+    std::string _output = "-"s;
 
     void run();
 };
@@ -370,6 +371,7 @@ struct CLI : Interface
         auto drawopts = cmd::make_option_set< Draw >( v )
             .option( "[--distance {int}|-d {int}]", &Draw::_distance, "node distance"s )
             .option( "[--raw]", &Draw::_raw, "show hex dumps of heap objects"s )
+            .option( "[--render {string}]", &Draw::_render, "command to execute on the dot output"s )
             .option( "[--labels {label}]", &Draw::_labels, "label all, none or only trace"s )
             .option( "[--bfs-layout]", &Draw::_bfs, "draw in bfs layout (levels)"s );
 
--- divine/ui/draw.cpp
+++ divine/ui/draw.cpp
@@ -16,133 +16,25 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <divine/vm/explore.hpp>
-#include <divine/vm/bitcode.hpp>
-#include <divine/vm/debug.hpp>
-#include <divine/ss/search.hpp>
+#include <divine/mc/draw.hpp>
 #include <divine/ui/cli.hpp>
 
 namespace divine {
 namespace ui {
 
-std::string escape( std::string s )
-{
-    std::string buf;
-    buf.resize( s.length() * 2 );
-    int i = 0, j = 0;
-    while ( i < int( s.length() ) ) {
-        char c = s[ i ++ ];
-        if ( c == '\\' || c == '\n' || c == '"' )
-            buf[ j++ ] = '\\';
-        if ( c == '\n' )
-            buf[ j++ ] = 'l';
-        else
-            buf[ j++ ] = c;
-    }
-    return std::string( buf, 0, j );
-}
-
-using DNMap = std::map< vm::DNKey, int >;
-
-template< typename DN >
-int dump( bool raw, DN dn, DNMap &dumped, int &seq, std::string prefix )
-{
-    if ( dn._address.type() != vm::PointerType::Heap )
-        return 0;
-    if ( dumped.find( dn.sortkey() ) != dumped.end() )
-        return dumped[ dn.sortkey() ];
-    int hid = ++seq;
-    dumped.emplace( dn.sortkey(), hid );
-
-    auto related =
-        [&]( std::string k, auto rel )
-        {
-            if ( int t = dump( raw, rel, dumped, seq, prefix ) )
-                std::cout << prefix << hid << " -> "  << prefix << t
-                          << " [ label=\"" << k << "\" ]" << std::endl;
-        };
-
-    std::function< void( std::string, DN ) > component =
-        [&]( std::string ck, auto comp )
-        {
-            comp.related( [&]( std::string rk, auto rel )
-                          { related( ck + "." + rk, rel ); }, false );
-            comp.components( [&]( std::string sk, auto scomp )
-                             { component( ck + "." + sk, scomp ); } );
-        };
-
-    std::stringstream str;
-    dn.format( str, 0, false, 0 );
-    std::cout << prefix << hid << " [ shape=rectangle label=\"" << escape( str.str() )
-              << "\" ]" << std::endl;
-
-    dn.related( related, false );
-    dn.components( component );
-
-    return hid;
-}
+namespace proc = brick::proc;
 
 void Draw::run()
 {
-    vm::Explore ex( bitcode() );
-    ex.start();
-
-    vm::DebugContext< vm::Program, vm::CowHeap > dbg( bitcode()->program() );
-    vm::setup::boot( dbg );
-    vm::Eval< vm::Program, decltype( dbg ), vm::value::Void > dbg_eval( dbg.program(), dbg );
-    dbg_eval.run();
-
-    int edgecount = 0, statecount = 0;
-
-    brick::mem::SlavePool< typename vm::CowHeap::SnapPool > ext( ex.pool() );
-
-    int seq = 0;
-
-    std::cout << "digraph { node [ fontname = Courier ]\n";
-
-    ex.initials( [&]( auto st )
-                 {
-                     ext.materialise( st.snap, sizeof( int ));
-                     int *id = ext.machinePointer< int >( st.snap );
-                     *id = ++seq;
-                 } );
-
-    ss::search(
-        ss::Order::PseudoBFS, ex, 1, ss::passive_listen(
-            [&]( auto f, auto t, auto trace, bool isnew )
-            {
-                if ( isnew )
-                    ext.materialise( t.snap, sizeof( int ) );
-                int *f_id = ext.machinePointer< int >( f.snap ),
-                    *t_id = ext.machinePointer< int >( t.snap );
-                if ( isnew )
-                    *t_id = ++seq;
-                std::string lbl;
-                for ( auto l : trace.first )
-                    lbl += l + "\n";
-                std::cout << *f_id << " -> " << *t_id
-                          << " [ label = \"" << escape( lbl ) << "\"]"
-                          << std::endl;
-                ++edgecount;
-            },
-            [&]( auto st )
-            {
-                int *id = ext.machinePointer< int >( st.snap );
-                vm::DebugNode< vm::Program, vm::CowHeap > dn( ex._ctx, st.snap );
-                dn.address( vm::DNKind::Object, ex._ctx.get( _VM_CR_State ).pointer );
-                dn.type( dbg._state_type );
-                dn.di_type( dbg._state_di_type );
-                DNMap _dumped;
-                int hseq = 0;
-                std::cerr << "# new state" << std::endl;
-                std::cout << *id << " [ style=filled fillcolor="
-                          << ( st.error ? "red" : "gray" ) << " ]" << std::endl;
-                std::cout << *id << " -> " << *id << ".1 [ label=root ]" << std::endl;
-                dump( _raw, dn, _dumped, hseq, brick::string::fmt( *id ) + "." );
-                ++statecount;
-            } ) );
-    std::cout << "}";
-    std::cerr << "found " << statecount << " states and " << edgecount << " edges" << std::endl;
+    auto dot = mc::draw( bitcode(), _distance, false );
+    auto out = proc::shellSpawnAndWait( proc::StdinString( dot ) | proc::CaptureStdout, _render );
+    if ( _output == "-" )
+        std::cout << out.out();
+    else
+    {
+        std::ofstream of( _output );
+        of << out.out();
+    }
 }
 
 }
