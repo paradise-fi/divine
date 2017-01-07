5dd80b9d81af3f0a766c28091e511f5f26bb15ab
Petr Rockai <me@mornfall.net>
2017-01-07 21:10:14 +0000

    test: Add a (failing) test for absolute include paths in 'divine cc'.

---
+++ test/cc/abs-include.2.todo.sh
@@ -0,0 +1,13 @@
+. lib/testcase
+
+mkdir 1
+touch 1/foo.h
+cat > test.c <<EOF
+#include "foo.h"
+int main() {}
+EOF
+
+touch 1/foo.h
+divine cc test.c -I1
+divine cc test.c -I$PWD/1
+divine run test.c -C,-I$PWD/1
