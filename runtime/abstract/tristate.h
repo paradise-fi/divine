df9b8c71c143ce890888ce90e272f8522fe9fc26
Vladimir Still <xstill@fi.muni.cz>
2017-01-10 13:17:15 +0000

    abstract: Fix compilation of tristate, add constructor.

--- runtime/abstract/tristate.h
+++ runtime/abstract/tristate.h
@@ -15,25 +15,28 @@ namespace abstract {
 
 struct Tristate {
 	enum Domain { False = 0, True = 1, Unknown = 2 };
+
+    Tristate( Domain val ) : value( val ) { }
+
 	Domain value;
 };
 
-Tristate * __abstract_tristate_construct( Tristate::Domain value ) {
+static Tristate * __abstract_tristate_construct( Tristate::Domain value ) {
     auto obj = allocate< Tristate >();
     obj->value = value;
     return obj;
 }
 
-Tristate * __abstract_tristate_construct() {
+static Tristate * __abstract_tristate_construct() {
     return __abstract_tristate_construct( Tristate::Domain::Unknown );
 }
 
-Tristate * __abstract_tristate_construct( bool b ) {
+static Tristate * __abstract_tristate_construct( bool b ) {
     auto value =  b ? Tristate::Domain::True : Tristate::Domain::False;
     return __abstract_tristate_construct( value );
 }
 
-Tristate * __abstract_tristate_negate( Tristate * t ) {
+static Tristate * __abstract_tristate_negate( Tristate * t ) {
     if ( t->value == Tristate::Domain::True )
         t->value = Tristate::Domain::False;
     else if ( t->value == Tristate::Domain::False )
@@ -42,7 +45,7 @@ Tristate * __abstract_tristate_negate( Tristate * t ) {
     return t;
 }
 
-Tristate * __abstract_tristate_and( Tristate * a, Tristate * b ) {
+static Tristate * __abstract_tristate_and( Tristate * a, Tristate * b ) {
     if ( a->value == Tristate::Domain::Unknown )
         return a;
     if ( b->value == Tristate::Domain::Unknown )
@@ -54,7 +57,7 @@ Tristate * __abstract_tristate_and( Tristate * a, Tristate * b ) {
     return a;
 }
 
-Tristate * __abstract_tristate_or( Tristate * a, Tristate * b ) {
+static Tristate * __abstract_tristate_or( Tristate * a, Tristate * b ) {
     if ( a->value == Tristate::Domain::True )
         return a;
     if ( b->value == Tristate::Domain::True )
@@ -67,15 +70,15 @@ Tristate * __abstract_tristate_or( Tristate * a, Tristate * b ) {
 }
 
 extern "C" {
-    Tristate * __abstract_tristate_create() _ROOT {
+    static Tristate * __abstract_tristate_create() _ROOT {
         return __abstract_tristate_construct();
     }
 
-    Tristate * __abstract_tristate_lift( bool b ) _ROOT {
+    static Tristate * __abstract_tristate_lift( bool b ) _ROOT {
         return __abstract_tristate_construct( b );
     }
 
-    bool __abstract_tristate_lower( Tristate * tristate ) _ROOT {
+    static bool __abstract_tristate_lower( Tristate * tristate ) _ROOT {
         if ( tristate->value == Tristate::Domain::Unknown ) {
 #ifdef __divine__
             return __vm_choose( 2 );
