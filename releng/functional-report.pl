2ca9d22e1fc56823ac6ad70f14dba2dd3efbb430
Petr Rockai <me@mornfall.net>
2017-01-19 15:40:12 +0000

    releng: Clean up the output of functional-report a little.

--- releng/functional-report.pl
+++ releng/functional-report.pl
@@ -77,19 +77,17 @@ sub doIndex {
     my $summary = "";
 
     for my $k ( sort keys %rescount ) {
-        $summary .= "<span class=\"test_$k\">$rescount{ $k } $k</span>, ";
+        $summary .= "<span class=\"test-$k\">$rescount{ $k } $k</span>, ";
     }
     $summary =~ s/, $//;
 
     print $index <<EOF
-## DIVINE Functional Test Results
-
 **summary**: $summary \\
 **DIVINE**: $divine \\
 **build type**: $buildtype \\
 **build date**: $builddate
 
-<table class="test_results test_$name">
+<div class="test-$name"><table class="test test-$name">
 EOF
 ;
 
@@ -102,10 +100,11 @@ EOF
     }
     $printer->( $index );
 
-    print $index "</table>\n";
+    print $index "</table></div>\n";
     close( $index );
 
-    system( "pandoc", "--to=html", "-s", "--smart", "--template", catfile( $webdir, "template.html" ), catfile( $out, "$name.md" ), "-o", catfile( $out, "$name.html" ) );
+    system( "pandoc", "--to=html", "-s", "--smart", "--template", catfile( $webdir, "template.html" ),
+            catfile( $out, "$name.md" ), "-o", catfile( $out, "$name.html" ) );
 
     $/ = undef;
     open( my $ii, "<", catfile( $out, "$name.html" ) );
@@ -144,7 +143,7 @@ doIndex( "full", sub {
 
                 my $res = $results{ $id };
                 $res = "none" unless $res;
-                print $index "<td class=\"test_result test_$res long_test\">[$res](./$report)</td>";
+                print $index "<td class=\"test test-$res test-long\">[$res](./$report)</td>";
                 system( "cp", catfile( $dir, $origreport ), catfile( $out, $report ) );
             }
             print $index "</tr>\n";
@@ -177,13 +176,13 @@ doIndex( "compact", sub {
                 my $r = $res;
                 $r = $codemap{ $res } if exists $codemap{ $res };
                 $agreg{ $aid }->{ $f } = "" unless exists $agreg{ $aid }->{ $f };
-                $agreg{ $aid }->{ $f } .= "<a class=\"test_result test_$res\" title=\"$t\" href=\"./$report\">$r</a>&#8203;";
+                $agreg{ $aid }->{ $f } .= "<a class=\"test test-$res\" title=\"$t\" href=\"./$report\">$r</a>&#8203;";
             }
         }
         for my $t ( sort( keys %agreg ) ) {
-            print $compact "<tr><td class=\"test\">$t</td>";
+            print $compact "<tr><td class=\"test-name\"><div>$t</div></td>";
             for my $f ( @flavours ) {
-                print $compact ( "<td class=\"test_result compact_tests\"><div>" . $agreg{ $t }->{ $f } . "</div></td>" );
+                print $compact ( "<td class=\"test test-compact\"><div>" . $agreg{ $t }->{ $f } . "</div></td>" );
             }
             print $compact "</tr>\n";
         }
