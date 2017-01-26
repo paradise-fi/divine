4cebf6c21c89d7c9e86037cdfd8da3fa882176f0
Vladimir Still <xstill@fi.muni.cz>
2017-01-26 18:00:51 +0000

    test: Enable sym-cast-g, add another cast test.

---
+++ test/abstract/2.sym-cast-h.cpp
@@ -0,0 +1,24 @@
+/* VERIFY_OPTS: --symbolic */
+
+#include <cassert>
+#include <limits>
+#define __sym __attribute__((__annotate__("lart.abstract.symbolic")))
+
+int nondet() {
+    __sym int x;
+    return x;
+}
+
+int main() {
+    int x;
+    x = nondet();
+    if ( x < 0 )
+        return 0;
+    short &y = *reinterpret_cast< short * >( &x );
+    y = 0;
+    ++y;
+    assert( y == 1 );
+    assert( x <= 0x7fff0001 );
+    assert( x == 0 ); /* ERROR */
+}
+
