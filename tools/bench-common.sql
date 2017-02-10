6535a94dbe0489d601114dae89716c3b159b2cd0
Petr Rockai <me@mornfall.net>
2017-02-10 09:57:18 +0000

    tools: Make the divbench database init SQL compatible with Postgres.

--- tools/bench-common.sql
+++ tools/bench-common.sql
@@ -1,32 +1,35 @@
+-- Tested with PostgreSQL and SQLite3.
+
 -- hardware specs
-create table cpu( id   integer primary key not null,
+create table cpu( id   serial primary key not null,
                   model varchar unique not null );
 
-create table pool( id   integer primary key not null,
+create table pool( id   serial primary key not null,
                    name varchar unique not null );
 
-create table machine(id    integer primary key not null,
+create table machine(id    serial primary key not null,
                      cpu   integer references cpu(id) not null,
                      cores integer not null,
                      mem   integer not null,
                      unique ( cpu, cores, mem ) );
 
 -- model checker inputs
-create table source( id   integer primary key not null,
-                     text blob    unique not null );
+create table source( id   serial  primary key not null,
+                     text text    unique not null );
 
-create table model( id       integer primary key not null,
+create table model( id       serial primary key not null,
                     name     varchar not null,
+                    variant  varchar,
                     revision integer not null,
                     script   integer references source( id ) not null,
-                    unique( name, revision ) );
+                    unique( name, variant, revision ) );
 
 create table model_srcs( model    integer references model( id ) not null,
                          source   integer references source( id ) not null,
                          filename varchar not null,
                          unique ( model, filename ) );
 
-create table tag( id integer primary key not null,
+create table tag( id serial primary key not null,
                   name varchar unique not null );
 
 create table model_tags( model integer references model( id ) not null,
@@ -34,7 +37,7 @@ create table model_tags( model integer references model( id ) not null,
                          unique ( model, tag ) );
 
 -- model checker versions
-create table build( id          integer primary key not null,
+create table build( id          serial primary key not null,
                     version     varchar  not null,
                     source_sha  char(40) not null,
                     runtime_sha char(40) not null,
@@ -42,12 +45,12 @@ create table build( id          integer primary key not null,
                     unique( version, source_sha, runtime_sha, build_type ) );
 
 -- ties the machine and the model checker version together
-create table instance( id      integer primary key not null,
+create table instance( id      serial primary key not null,
                        build   integer references build( id ) not null,
                        machine integer references machine( id ) not null,
                        unique( build, machine ) );
 
-create table execution( id       integer primary key not null,
+create table execution( id       serial primary key not null,
                         instance integer references instance( id ) not null,
                         started  timestamp default current_timestamp not null,
                         time_lart   integer, -- milliseconds
@@ -59,16 +62,16 @@ create table execution( id       integer primary key not null,
                         states      integer,
                         result   char(1) default 'U' not null ); -- V = valid, E = error, B = boot error, U = unknown
 
-create table pool_log( id integer primary key,
+create table pool_log( id serial primary key,
                        seq integer not null,
                        stamp timestamp default current_timestamp not null,
                        execution integer references execution( id ) not null,
-                       pool integer references pool_id( id ) not null,
+                       pool integer references pool( id ) not null,
                        items integer not null,
                        used integer not null,
                        held integer not null );
 
-create table search_log( id integer primary key,
+create table search_log( id serial primary key,
                          seq integer not null,
                          stamp timestamp default current_timestamp not null,
                          execution integer references execution( id ) not null,
@@ -76,13 +79,13 @@ create table search_log( id integer primary key,
                          queued integer not null );
 
 -- benchmarking job: model + instance → execution
-create table job( id        integer primary key not null,
+create table job( id        serial primary key not null,
                   model     integer references model( id ) not null,
                   instance  integer references instance( id ) not null,
                   execution integer references execution( id ),
                   status    char(1) not null ); -- P = pending, R = running, D = done
 
 -- attach notes to a particular build
-create table build_notes( id integer primary key,
+create table build_notes( id serial primary key,
                           build integer references build( id ),
                           note varchar );
---
+++ tools/bench-initdb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+dir=$(dirname $0)
+
+if test "$1" = "--postgres"; then
+    cat $dir/bench-postgres.sql $dir/bench-common.sql
+elif test "$1" = "--sqlite"; then
+    cat $dir/bench-common.sql | sed -e s,serial,integer,
+else
+    echo "HINT: run me with --postgres or --sqlite" >&2
+    echo "like this: bench-initdb.sh --sqlite | sqlite3 ./test.sqlite" >&2
+    echo "or: bench-initdb.sh --postgres | psql -h dbhost" >&2
+fi
