caeff499396bd9a512f9d8f9b479cd49aad9d21f
Jan Mrázek <email@honzamrazek.cz>
2017-04-07 19:25:24 +0000

    DiOS: Make Syscall compatible with components.

--- runtime/dios/core/syscall.cpp
+++ runtime/dios/core/syscall.cpp
@@ -4,44 +4,10 @@
 #include <dios/core/fault.hpp>
 #include <dios/core/scheduling.hpp>
 #include "fault.hpp"
+#include <signal.h>
 #include <cerrno>
-
-namespace __sc {
-// Mapping of syscodes to implementations
-#define SYSCALL(n,...) extern void n ( __dios::Context& ctx, int *err, void *retval, va_list vl );
-    #include <sys/syscall.def>
-} // namespace __sc
-
-
-namespace __sc_passthru {
-// Mapping of syscodes to implementations
-#define SYSCALL(n,...) extern void n ( __dios::Context& ctx, int *err, void *retval, va_list vl );
-    #include <sys/syscall.def>
-} // namespace __sc_passthru
 
 namespace __dios {
 
-const SC_Handler _DiOS_SysCalls_Virt[ SYS_MAXSYSCALL ] =
-{
-    #define SYSCALL(n,...)  [ SYS_ ## n ] = __sc::n,
-        #include <sys/syscall.def>
-    #undef SYSCALL
-};
-
-const SC_Handler _DiOS_SysCalls_Passthru[ SYS_MAXSYSCALL ] =
-{
-    #define SYSCALL(n,...)  [ SYS_ ## n ] = __sc_passthru::n,
-        #include <sys/syscall.def>
-    #undef SYSCALL
-};
-
-const SC_Handler *_DiOS_SysCalls = _DiOS_SysCalls_Virt;
-
-const SchedCommand _DiOS_SysCallsSched[ SYS_MAXSYSCALL ] =
-{
-    #define SYSCALL(n, sched,...) [ SYS_ ## n ] = sched,
-        #include <sys/syscall.def>
-    #undef SYSCALL
-};
 
 } // namespace _dios
--- runtime/dios/core/syscall.hpp
+++ runtime/dios/core/syscall.hpp
@@ -7,7 +7,16 @@
 #include <new>
 #include <dios.h>
 #include <errno.h>
+#include <dios/kernel.hpp>
 #include <sys/syscall.h>
+
+// Syscall argument types
+#include <sys/syscall.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/monitor.h>
+#include <signal.h>
 
 namespace __dios {
 
@@ -17,47 +26,64 @@ enum _VM_SC {
 };
 #undef SYSCALL
 
-enum class SchedCommand : uint8_t { RESCHEDULE, CONTINUE };
+template < typename Context >
+struct Syscall {
+    using ScHandler = SchedCommand (*)( Context& c, int *, void *, va_list );
 
-using SC_Handler = void (*)( Context& ctx, int *err, void* retval, va_list vl );
-
-// Mapping of syscodes to implementations
-extern const SC_Handler * _DiOS_SysCalls;
-extern const SC_Handler _DiOS_SysCalls_Virt[ SYS_MAXSYSCALL ];
-extern const SC_Handler _DiOS_SysCalls_Passthru[ SYS_MAXSYSCALL ];
-
-// True if corresponding syscall requires thread rescheduling
-extern const SchedCommand _DiOS_SysCallsSched[ SYS_MAXSYSCALL ];
-
-struct Syscall : _DiOS_Syscall
-{
-    Syscall() noexcept { _syscode = SYS_NONE; }
-
-    SchedCommand handle( Context *ctx ) noexcept
-    {
-        if ( _syscode != SYS_NONE ) {
-            ( *( _DiOS_SysCalls[ _syscode ] ) )( *ctx, _err ,_ret , _args );
-            if ( *_err == EAGAIN2 )
-            {
-                _syscode = SYS_NONE;
-                return SchedCommand::RESCHEDULE;;
-            }
-            auto cmd = _DiOS_SysCallsSched[ _syscode ];
-            _syscode = SYS_NONE;
-            // Either CONTINUE or RESCHEDULE
+    static SchedCommand handle( Context& c, _DiOS_Syscall& s ) noexcept {
+        if ( s._syscode != SYS_NONE ) {
+            auto cmd = ( *( table[ s._syscode ] ) )( c, s._err, s._ret, s._args );
+            s._syscode = SYS_NONE;
+            if ( *s._err == EAGAIN2 )
+                return SchedCommand::RESCHEDULE;
             return cmd;
         }
         return SchedCommand::RESCHEDULE;
     }
+
+    static void kernelHandle( void *ctx, _DiOS_SC syscode, void *ret, va_list vl ) noexcept {
+        Context& c = *static_cast< Context* >( ctx );
+        int err;
+        ( *( table[ syscode ] ) )( c, &err, ret, vl );
+    }
+
+    static ScHandler table[ SYS_MAXSYSCALL ];
+
+    #include <dios/macro/syscall_common>
+    #include <dios/macro/no_memory_tags>
+    #define SYSCALL( name, schedule, ret, arg ) \
+        static SchedCommand name ## Wrappper( Context& ctx, int *err, void *retVal, va_list vl) { \
+            UNPACK arg \
+            IF(IS_VOID(ret))( \
+                ctx. name ( err ARG_NAMES arg ); \
+            ) \
+            IF(NOT(IS_VOID(ret))) ( \
+                auto *r = static_cast< ret * >( retVal ); \
+                *r = ctx. name ( err ARG_NAMES arg ); \
+            ) \
+            va_end( vl ); \
+            return SchedCommand:: schedule; \
+        }
+    #define SYSCALLSEP( ... ) EVAL( SYSCALL( __VA_ARGS__ ) )
+
+        #include <sys/syscall.def>
+
+    #undef SYSCALLSEP
+    #undef SYSCALL
+    #include <dios/macro/no_memory_tags.cleanup>
+    #include <dios/macro/syscall_common.cleanup>
+};
+
+template < typename Context >
+typename Syscall< Context >::ScHandler Syscall< Context >::table[ SYS_MAXSYSCALL ] = {
+    #define SYSCALL( name, ... ) [ SYS_ ## name ] = Syscall:: name ## Wrappper,
+    #define SYSCALLSEP( ... ) EVAL( SYSCALL( __VA_ARGS__ ) )
+        #include <sys/syscall.def>
+    #undef SYSCALL
+    #undef SYSCALLSEP
 };
 
 } // namespace __dios
-
-namespace __sc {
-
-    void uname( __dios::Context& c, int *err, void* retval, va_list vl );
-
-} // namespace __sc
 
 
 #endif // __DIOS_SYSCALL_H__
