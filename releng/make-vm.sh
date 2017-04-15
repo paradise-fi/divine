928f9a94a10b28f52e64b7960907424f2d20a2b4
Vladimir Still <xstill@fi.muni.cz>
2017-04-15 06:59:09 +0000

    releng: Make building of VM images more robust.

--- releng/make-vm.sh
+++ releng/make-vm.sh
@@ -1,18 +1,30 @@
-resources="-m 12288 -smp 8"
+real_mem=`cat /proc/meminfo | grep MemTotal | sed 's/^[^0-9]*\([0-9]*\).*$/\1/'`
+real_cpus=`cat /proc/cpuinfo  | grep processor | wc -l`
+
+mem=$(($real_mem / 1024 / 2))
+test $mem -gt 12288 && mem=12288
+test $mem -lt 4096 && mem=4096
+cpus=$(($real_cpus / 2))
+test $cpus -gt 8 && cpus=8
+test $cpus -lt 1 && cpus=1
+
+resources="-m $mem -smp $cpus"
 dist=xenial
 img=xenial-server-cloudimg-amd64-disk1.img
 
-test -e $img || wget https://cloud-images.ubuntu.com/$dist/current/$img
-qemu-img create -o backing_file=$img -f qcow2 divine-$dist.qcow2 20G
-qemu-img create -f qcow2 divine-build.qcow2 20G
-mkdir -p cloud-config
+if [[ "$1" != "--pack-only" ]]; then
 
-cat > cloud-config/meta-data <<EOF
+    test -e $img || wget https://cloud-images.ubuntu.com/$dist/current/$img
+    qemu-img create -o backing_file=$img -f qcow2 divine-$dist.qcow2 20G
+    qemu-img create -f qcow2 divine-build.qcow2 20G
+    mkdir -p cloud-config
+
+    cat > cloud-config/meta-data <<EOF
 instance-id: iid-divine-xenial
 local-hostname: divine
 EOF
 
-cat > cloud-config/user-data <<EOF
+    cat > cloud-config/user-data <<EOF
 #cloud-config
 users:
  - name: divine
@@ -21,6 +33,7 @@ users:
    home: /home/divine
    sudo: ALL=(ALL) NOPASSWD:ALL
 runcmd:
+ - apt-get update
  - apt-get install -y make
  - mkfs.ext4 /dev/sdb
  - mount /dev/sdb /mnt
@@ -31,21 +44,23 @@ runcmd:
  - poweroff
 EOF
 
-genisoimage -V cidata -D -J -joliet-long -o config.iso cloud-config
-rm -rf cloud-config
+    genisoimage -V cidata -D -J -joliet-long -o config.iso cloud-config
+    rm -rf cloud-config
 
-cdrom="-drive file=config.iso,media=cdrom"
+    cdrom="-drive file=config.iso,media=cdrom"
 
-vboxadditions=/usr/lib/virtualbox/additions/VBoxGuestAdditions.iso
-if test -f $vboxadditions; then
-    cdrom="$cdrom -drive file=$vboxadditions,media=cdrom"
+    vboxadditions=/usr/lib/virtualbox/additions/VBoxGuestAdditions.iso
+    if test -f $vboxadditions; then
+        cdrom="$cdrom -drive file=$vboxadditions,media=cdrom"
+    fi
+
+    qemu-system-x86_64 -machine accel=kvm -display none -serial stdio \
+                       -hda divine-$dist.qcow2 -hdb divine-build.qcow2 \
+                       $cdrom $resources
+
+    rm -f config.iso
+
 fi
-
-qemu-system-x86_64 -machine accel=kvm -display none -serial stdio \
-                   -hda divine-$dist.qcow2 -hdb divine-build.qcow2 \
-                   $cdrom $resources
-
-rm -f config.iso
 
 if [[ "$1" != "--skip-vbox" ]]; then
     version=$(curl -s https://divine.fi.muni.cz/install.sh | grep version= | head -n1 | sed 's/version=//')
@@ -55,18 +70,24 @@ if [[ "$1" != "--skip-vbox" ]]; then
     vdi=$base.vdi
     vm=$base
     qemu-img convert -O vdi divine-xenial.qcow2 $vdi
-    VBoxManage createvm --name $vm --ostype Ubuntu_64 --register
+    rm -rf _vbox
+    mkdir _vbox
+    VBoxManage unregistervm $vm --delete >& /dev/null
+    VBoxManage createvm --name $vm --ostype Ubuntu_64 --basefolder $PWD/_vbox --register
     VBoxManage storagectl $vm --name "SATA Controller" --add sata --controller IntelAHCI
     VBoxManage storageattach $vm --storagectl "SATA Controller" --port 0 \
                                  --device 0 --type hdd --medium $vdi
     VBoxManage modifyvm $vm --memory 4096 --cpus 2 --nic1 nat \
                             --uart1 0x3F8 4 --uartmode1 disconnected
-    rm $ova
+    rm -f $ova
     VBoxManage export $vm -o $ova --vsys 0 \
                           --product "DIVINE $version" \
                           --producturl "https://divine.fi.muni.cz" \
                           --vendor "ParaDiSe Lab, FI MU, CZ" \
                           --vendorurl "https://paradise.fi.muni.cz"
-    xz -v -9 $vdi
-    VBoxManage unregistervm $vm
+    rm -f $vdi.xz
+    xz -vk -9 $vdi
+    VBoxManage unregistervm $vm --delete
+    rm -rf _vbox
+    chmod go=r $ova
 fi
