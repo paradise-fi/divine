3711570413d56555c0214d33b733fb17ea6a1d3c
Vladimir Still <xstill@fi.muni.cz>
2017-05-05 12:42:40 +0000

    test: Add a very simple test for sim show.

---
+++ test/sim/2.show.sh
@@ -0,0 +1,23 @@
+. lib/testcase
+
+cat > file.cpp <<EOF
+int main() {
+    int x = 42; // alloca should be removed
+    int y = 0; // alloca should not be removed
+    if ( x >= 0 )
+        y = 1;
+}
+EOF
+
+sim file.cpp <<EOF
++ ^# executing __boot at
+> start
++ ^# executing main at
+> show .x
++ ^attributes
++ type:\s*int
++ value:\s*[i32 42 d]
+> show .y
++ ^attributes
++ type:\s*int
+EOF
