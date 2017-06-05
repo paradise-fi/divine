2af3d429e019ab0a92b83a21982e9edb11e5d7f6
Zuzana Baranova <xbaranov@fi.muni.cz>
2017-06-05 07:39:25 +0000

    test: Add tests for processes.

---
+++ test/dios/2.process-globals.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <assert.h>
+
+int x = 1;
+
+int main()
+{
+    pid_t pid = fork();
+
+    if ( pid != 0 )
+        assert( x == 1 );
+    else
+    {
+        x = 2;
+        assert( x == 2 );
+    }
+}
