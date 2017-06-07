366ef133735b6d37825e115af4c791dc82d2300e
Petr Rockai <me@mornfall.net>
2017-06-07 12:28:27 +0000

    libc: Fix the implementation of fork (sysfork no longer has a C wrapper).

--- runtime/libc/functions/unistd/fork.c
+++ runtime/libc/functions/unistd/fork.c
@@ -1,9 +1,10 @@
+#include <sys/syscall.h>
 #include <unistd.h>
 
 pid_t fork( void )
 {
-    pid_t old_pid = getpid();
-    pid_t child_pid = sysfork();
+    pid_t old_pid = getpid(), child_pid;
+    __dios_syscall( SYS_sysfork, &child_pid );
     pid_t new_pid = getpid();
     return ( old_pid == new_pid ) ? child_pid : 0;
 }
