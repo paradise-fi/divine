15283b041a9c37aaafe960f1a5c0154a3541eee4
Zuzana Baranova <xbaranov@fi.muni.cz>
2017-07-02 09:13:23 +0000

    test: Add tests for get- and set- of {sid,pgid}.

---
+++ test/dios/2.setpgid.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+
+int main() {
+    assert( getpgid( 0 ) == 1 );
+    assert( setpgid( 0, -1 ) == -1 );
+    assert( errno == EINVAL );
+
+    assert( setpgid( 0, 2 ) == -1 );
+    assert( errno == EPERM );
+
+    pid_t pid = fork();
+
+    if ( pid == 0 )
+    {
+        assert( setpgid( 2, 3 ) == -1 );
+        assert( getpgid( 2 ) == 1 );
+        assert( errno == EPERM );
+
+        pid_t pid = fork();
+
+        while( pid != 0 ){} // ensure child #3 is created
+
+        if ( pid != 0 ) {
+            assert( setpgid( 2, 0 ) == 0 );
+            assert( getpgid( 2 ) == 2 );
+
+            assert( setpgid( 3, 2 ) == 0 );
+            assert( getpgid( 3 ) == 2 );
+        }
+    }
+}
