9bc151aa2b6589e23269ccaf800e9019a69fb738
Vladimir Still <xstill@fi.muni.cz>
2017-08-10 16:07:43 +0000

    lart: Fix error reporting, add macro for warnings.

--- lart/support/error.h
+++ lart/support/error.h
@@ -5,6 +5,7 @@
 #include <brick-types>
 DIVINE_RELAX_WARNINGS
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/ADT/StringRef.h>
 DIVINE_UNRELAX_WARNINGS
 
 namespace lart {
@@ -13,39 +14,57 @@ struct UnexpectedValue : std::runtime_error {
     using std::runtime_error::runtime_error;
 };
 
+template< typename... >
+using VoidT = void;
+
+namespace detail {
+    template< typename V >
+    static auto dump( llvm::raw_string_ostream &str, V &v, brick::types::Preferred )
+        -> decltype( v.print( str ) )
+    { v.print( str ); str << "\n"; }
+
+    template< typename V >
+    static auto dump( llvm::raw_string_ostream &str, V *v, brick::types::Preferred )
+        -> decltype( v->print( str ) )
+    {
+        if ( v != nullptr ) {
+            v->print( str );
+            str << "\n";
+        } else {
+            str << "nullptr\n";
+        }
+    }
+
+    static void dump( llvm::raw_string_ostream &str, llvm::StringRef v, brick::types::NotPreferred )
+    { str << "  " << v << "\n"; }
+
+    template< typename Range,
+              typename = std::enable_if_t< !std::is_convertible< Range, llvm::StringRef >::value > >
+    static auto dump( llvm::raw_string_ostream &str, Range &vs, brick::types::NotPreferred )
+        -> VoidT< decltype( vs.begin() ), decltype( vs.end() ) >
+    {
+        for ( auto v : vs )
+            dump( str, v, brick::types::Preferred() );
+    }
+
+    template< typename... Vals >
+    static std::string mkmsg( std::string msg, Vals &&... toDump ) {
+        std::string buffer;
+        llvm::raw_string_ostream str( buffer );
+        str << msg;
+        str << "\nrelevant information:\n";
+        ( dump( str, std::forward< Vals >( toDump ), brick::types::Preferred() ), ... );
+        return str.str();
+    }
+}
+
 struct UnexpectedLlvmIr : UnexpectedValue {
     using UnexpectedValue::UnexpectedValue;
 
     template< typename... Vals >
     UnexpectedLlvmIr( std::string msg, Vals &&... toDump ) :
-        UnexpectedValue( megeMsg( std::move( msg ), std::forward< Vals >( toDump )... ) )
+        UnexpectedValue( detail::mkmsg( std::move( msg ), std::forward< Vals >( toDump )... ) )
     { }
-
-  private:
-    template< typename V >
-    static auto dump( llvm::raw_string_ostream &str, V &v, brick::types::Preferred )
-        -> decltype( v.print( str ) )
-    { v.print( str ); }
-
-    template< typename V >
-    static auto dump( llvm::raw_string_ostream &str, V *v, brick::types::Preferred )
-        -> decltype( v->print( str ) )
-    { v->print( str ); }
-
-
-    template< typename V >
-    static void dump( llvm::raw_string_ostream &, V &&, brick::types::NotPreferred )
-    { }
-
-    template< typename... Vals >
-    static std::string megeMsg( std::string msg, Vals &&... toDump ) {
-        std::string buffer;
-        llvm::raw_string_ostream str( buffer );
-        str << msg;
-        str << "\nrelevant values:\n";
-        ( dump( str, std::forward< Vals >( toDump ), brick::types::Preferred() ), ... );
-        return str.str();
-    }
 };
 
 #define ENSURE_LLVM( X, ... ) \
@@ -53,6 +72,12 @@ struct UnexpectedLlvmIr : UnexpectedValue {
         throw ::lart::UnexpectedLlvmIr( \
                 std::string( "Failed ENSURE: " #X " at " __FILE__ ":" ) + std::to_string( __LINE__ ), \
                 ##__VA_ARGS__ ); \
+    } else ((void)0)
+
+#define WARN_LLVM( X, ... ) \
+    if ( !( X ) ) { \
+        std::cerr << ::lart::detail::mkmsg( std::string( "LART WARNING: " #X " at " __FILE__ ":" ) \
+                      + std::to_string( __LINE__ ),  ##__VA_ARGS__ ); \
     } else ((void)0)
 
 } // namespace llvm
