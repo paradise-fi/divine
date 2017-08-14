70c6ade00194995cfd34d160eb3ca9627e9f0691
Zuzana Baranova <xbaranov@fi.muni.cz>
2017-08-14 13:00:03 +0000

    test: Add tests for waitpid() which is essentially wait4().

---
+++ test/libc/2.wait-signal.c
@@ -0,0 +1,26 @@
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <signal.h>
+
+int main( int argc, char *argv[] )
+{
+    pid_t cpid;
+    int wstatus;
+
+    cpid = fork();
+
+    if ( cpid == 0 ) // child
+        while( 1 );
+    else
+    {
+        kill( cpid, SIGINT );
+        pid_t ret = waitpid( cpid, &wstatus, 0 );
+
+        assert( WIFSIGNALED( wstatus ) );
+        assert( WTERMSIG( wstatus ) == SIGINT );
+        assert( !WIFEXITED( wstatus ) );
+        assert( ret == cpid );
+    }
+}
