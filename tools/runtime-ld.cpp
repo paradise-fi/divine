eb9dc7b3fc506012b10c4054e23d26be289bd096
Vladimir Still <xstill@fi.muni.cz>
2017-08-17 14:20:54 +0000

    tools: Collect link essentials in runtime-ld.

--- tools/runtime-ld.cpp
+++ tools/runtime-ld.cpp
@@ -8,11 +8,22 @@ int main( int argc, const char **argv )
     llvm::LLVMContext ctx;
     std::vector< std::unique_ptr< llvm::Module > > modules;
 
+    modules.emplace_back( std::make_unique< llvm::Module >( "_link_essentials.ll", ctx ) );
+    llvm::Module &essentials = *modules.back();
+
     for ( int i = 2; i < argc; ++i )
     {
         auto input = std::move( llvm::MemoryBuffer::getFile( argv[i] ).get() );
         modules.emplace_back( std::move(
                     llvm::parseBitcodeFile( input->getMemBufferRef(), ctx ).get() ) );
+        llvm::Module &m = *modules.back();
+        essentials.setDataLayout( m.getDataLayoutStr() );
+        brick::llvm::enumerateFunctionsForAnno( "divine.link.always", m,
+            [&]( llvm::Function *f ) {
+                essentials.getOrInsertFunction( f->getName(),
+                                                f->getFunctionType(),
+                                                f->getAttributes() );
+            } );
     }
 
     brick::llvm::writeArchive( modules.begin(), modules.end(), argv[1] );
