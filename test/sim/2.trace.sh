40c53b53dedc1b1607cb1fd6efc52d0e5649b9dd
Petr Rockai <me@mornfall.net>
2017-08-18 23:31:45 +0000

    test: Adapt to a change in the 'trace' command in sim.

--- test/sim/2.trace.sh
+++ test/sim/2.trace.sh
@@ -2,20 +2,20 @@
 
 sim $TESTS/c/1.malloc.c <<EOF
 + ^# executing __boot at
-> trace 0 0 1
+> trace --choices 0 0 1
 - FAULT
 + ^traced states: #1 #2
 + ^# executing malloc at
 > stepa --count 3
 + ^# executing - at -
-> trace 0 0 0
+> trace --choices 0 0 0
 + ^traced states: #1 #2
 + ^# executing malloc at
 > step --out
 > step --count 2
 + ^T: FAULT: null pointer dereference
 + ^# executing .*::handler
-> trace 0 0 0 0
+> trace --choices 0 0 0 0
 + traced states: #1 #2 #5
 + unused choices: 0
 EOF
@@ -34,7 +34,7 @@ void __boot()
 EOF
 
 sim loop.c <<EOF
-> trace 0 0
+> trace --choices 0 0
 + traced states: #1 #1 \[loop closed\]
 + unused choices: 0 0
 EOF
