54602bf36c6e10221ef5f770650559a6e06b071d
Petr Rockai <me@mornfall.net>
2017-08-19 08:10:13 +0000

    releng: Ignore skipped tests in testhook-vim.

--- releng/testhook-vim.sh
+++ releng/testhook-vim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p $SRCDIR/_failed_tests
 rm -f $SRCDIR/_failed_tests/*
-grep -v passed results/list | while read f g; do
+egrep -v 'passed|skipped' results/list | while read f g; do
     cp results/$(echo $f | sed -e s,/,_,g).txt $SRCDIR/_failed_tests/
 done
-grep -qv passed results/list && vim $SRCDIR/_failed_tests/
+egrep -qv 'passed|skipped' results/list && vim $SRCDIR/_failed_tests/
