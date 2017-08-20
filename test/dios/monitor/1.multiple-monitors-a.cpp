df8b9687486c9d18f5654c3f471a150d39e96e70
Petr Rockai <me@mornfall.net>
2017-08-20 20:23:43 +0000

    test: Add explicit interrupts to monitor tests.

--- test/dios/monitor/1.multiple-monitors-a.cpp
+++ test/dios/monitor/1.multiple-monitors-a.cpp
@@ -1,22 +1,26 @@
 #include <sys/monitor.h>
+#include <sys/vmutil.h>
 #include <dios/kernel.hpp>
 
 volatile int glob;
 
 bool isZero() { return glob == 0; }
 
-struct DummyMon : public __dios::Monitor {
+struct DummyMon : public __dios::Monitor
+{
     void step() { }
 };
 
-struct GlobMon : public __dios::Monitor {
+struct GlobMon : public __dios::Monitor
+{
     void step()  {
         if ( !isZero() )
             __vm_control( _VM_CA_Bit, _VM_CR_Flags, _VM_CF_Error, _VM_CF_Error ); /* ERROR */
     }
 };
 
-int main() {
+int main()
+{
     for ( int i = 0; i != 2; i++ )
         glob = glob ? 0 : 1;
 
@@ -24,5 +28,8 @@ int main() {
     __dios::register_monitor( __dios::new_object< GlobMon >() );
 
     for ( int i = 0; i != 2; i++ )
+    {
         glob = glob ? 0 : 1;
+        __vmutil_interrupt();
+    }
 }
--- test/dios/monitor/1.simple-monitor-a.cpp
+++ test/dios/monitor/1.simple-monitor-a.cpp
@@ -1,23 +1,29 @@
 #include <sys/monitor.h>
+#include <sys/vmutil.h>
 #include <dios/kernel.hpp>
 
 volatile int glob;
 
 bool isZero() { return glob == 0; }
 
-struct GlobMon : public __dios::Monitor {
+struct GlobMon : public __dios::Monitor
+{
     void step() {
         if ( !isZero() )
             __vm_control( _VM_CA_Bit, _VM_CR_Flags, _VM_CF_Error, _VM_CF_Error ); /* ERROR */
     }
 };
 
-int main() {
+int main()
+{
     for ( int i = 0; i != 2; i++ )
         glob = glob ? 0 : 1;
 
     __dios::register_monitor( __dios::new_object< GlobMon >() );
 
     for ( int i = 0; i != 2; i++ )
+    {
         glob = glob ? 0 : 1;
+        __vmutil_interrupt();
+    }
 }
