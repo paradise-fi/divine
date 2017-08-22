9a620238210010c09c9a2ebc3f59d0743e0d09c1
Zuzana Baranova <xbaranov@fi.muni.cz>
2017-08-22 14:23:59 +0000

    test: Add tests ensuring file descriptors and umask are per-process.

---
+++ test/posix/2.fs-per-process-fds.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int main() {
+    int fd = open( "file", O_CREAT | O_WRONLY, 0666 );
+
+    pid_t ret = fork();
+
+    if ( ret == 0 )
+        assert( write( fd, "text\n",5 ) == 5 );
+    else
+        close( fd );
+
+    return 0;
+}
