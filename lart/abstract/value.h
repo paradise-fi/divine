9ca7e093ea8e7ede6301183e03157469825d263a
Henrich Lauko <xlauko@mail.muni.cz>
2017-08-24 11:08:58 +0000

    lart: Simplify AbstractValue wrapper.

--- lart/abstract/value.h
+++ lart/abstract/value.h
@@ -6,49 +6,51 @@ DIVINE_RELAX_WARNINGS
 DIVINE_UNRELAX_WARNINGS
 
 #include <lart/abstract/domains/domains.h>
-#include <lart/abstract/types/common.h>
-#include <lart/abstract/types/composed.h>
-#include <lart/abstract/types/scalar.h>
-#include <lart/abstract/types/utils.h>
+#include <lart/abstract/util.h>
+#include <lart/abstract/types.h>
 
 namespace lart {
 namespace abstract {
 
 struct AbstractValue {
-
-    AbstractValue( llvm::Value * value, DomainPtr dom )
-        : _value( value ),
-          _type( liftType( value->getType(), dom ) )
-    {}
-
-    AbstractValue( llvm::Value * value, const DomainMap & dmap )
-        : AbstractValue( value, Domain::make( value->getType(), dmap ) )
-    {}
-
-    AbstractValue( llvm::Value * value, DomainValue dom )
-        : AbstractValue( value, Domain::make( dom ) )
-    {}
+    AbstractValue( llvm::Value * v, Domain d ) : value( v ), domain( d ) { }
 
     AbstractValue( const AbstractValue & ) = default;
     AbstractValue( AbstractValue && ) = default;
-    AbstractValue &operator=( const AbstractValue & other ) = default;
-    AbstractValue &operator=( AbstractValue && other ) = default;
+    AbstractValue &operator=( const AbstractValue & ) = default;
+    AbstractValue &operator=( AbstractValue && ) = default;
+
+    bool isAbstract() const { return domain != Domain::LLVM; }
 
     template< typename T >
-    decltype(auto) value() const { return llvm::cast< T >( _value ); }
+    decltype(auto) get() const { return llvm::cast< T >( value ); }
 
-    llvm::Value * value() const { return _value; }
-    DomainPtr domain() const  { return _type->domain(); }
-    const AbstractTypePtr type() const { return _type; }
+    template< typename T >
+    decltype(auto) safeGet() const { return llvm::dyn_cast< T >( value ); }
 
-private:
-    llvm::Value * _value;
-    AbstractTypePtr _type;
+    template< typename T >
+    bool isa() const { return llvm::isa< T >( value ); }
+
+    llvm::Function * getFunction() const {
+        if ( auto i = safeGet< llvm::Instruction >() )
+            return i->getParent()->getParent();
+        else
+            return safeGet< llvm::Argument >()->getParent();
+    }
+
+    template< typename TMap >
+    llvm::Type * type( TMap & tmap ) const {
+        return liftType( value->getType(), domain, tmap );
+    }
+
+    llvm::Value * value;
+    Domain domain;
 };
 
-inline bool operator==( const AbstractValue & l, const AbstractValue & r ) {
-    // TODO type
-    return l.value() == r.value();
+using AbstractValues = std::vector< AbstractValue >;
+
+inline bool operator==( const AbstractValue & a, const AbstractValue & b ) {
+    return std::tie( a.domain, a.value ) == std::tie( b.domain, b.value );
 }
 
 } // namespace abstract
@@ -57,9 +59,8 @@ inline bool operator==( const AbstractValue & l, const AbstractValue & r ) {
 namespace std {
     template <>
     struct hash< lart::abstract::AbstractValue > {
-        size_t operator()( const lart::abstract::AbstractValue& n ) const {
-            // TODO type
-            return hash_value( n.value() );
+        size_t operator()( const lart::abstract::AbstractValue & n ) const {
+            return static_cast< std::size_t >( n.domain ) ^ hash_value( n.value );
         }
     };
 } // namespace std
