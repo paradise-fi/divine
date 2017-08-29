b02edc7adebc6c8b31dd3d06876c5178158f3113
Henrich Lauko <xlauko@mail.muni.cz>
2017-08-29 12:57:04 +0000

    lart: Implement FieldTrie.

--- lart/abstract/util.h
+++ lart/abstract/util.h
@@ -8,6 +8,8 @@ DIVINE_RELAX_WARNINGS
 DIVINE_UNRELAX_WARNINGS
 
 #include <iostream>
+#include <variant>
+#include <optional>
 
 #include <brick-data>
 #include <brick-types>
@@ -137,19 +139,84 @@ private:
 
 template< typename T >
 struct FieldTrie {
+    using key_type = size_t;
+    using Path = std::vector< key_type >;
 
+    void insert( const Path & keys, T val ) {
+        std::get< Internal >( *createPath( keys ) ).setValue( val );
+    }
+
+    std::optional< T > search( const Path & keys ) {
+        auto level = root.get();
+        for ( auto k : keys ) {
+            if ( !level )
+                return {};
+            auto & i = std::get< Internal >( *level );
+            if ( !i.children.count( k ) )
+                return {};
+            level = i.children.at( k ).get();
+        }
+        return std::get< Internal >( *level ).value();
+    }
+
+private:
     struct TrieNode;
     using TrieNodePtr = std::unique_ptr< TrieNode >;
 
     struct Leaf {
+        Leaf( T v ) : value( v ) {}
         T value;
     };
 
     struct Internal {
-        ArrayMap< size_t, TrieNodePtr > children;
+        ArrayMap< key_type, TrieNodePtr > children;
+
+        bool isLeaf() const {
+            return children.size() == 1 && children.count( 0 ) &&
+                   std::holds_alternative< Leaf >( *children.at( 0 ) );
+        }
+
+        void setValue( T val ) {
+            if ( children.empty() )
+                children[ 0 ] = TrieNode::make_leaf( val );
+            assert( isLeaf() && value() == val );
+        }
+
+        T & value() const {
+            assert( isLeaf() );
+            return std::get< Leaf >( *children.at( 0 ) ).value;
+        }
+
+        TrieNode * getOrInsertChild( key_type key ) {
+            if ( !children.count( key ) )
+                children[ key ] = TrieNode::make_internal();
+            return children[ key ].get();
+        }
     };
 
-    struct TrieNode : Union< Internal, Leaf > { };
+    using Storage = std::variant< Leaf, Internal >;
+    struct TrieNode : Storage {
+        using Storage::Storage;
+
+        static TrieNodePtr make_internal() {
+            return std::make_unique< TrieNode >( Internal{} );
+        }
+
+        static TrieNodePtr make_leaf( T val ) {
+            return std::make_unique< TrieNode >( Leaf{ val } );
+        }
+    };
+
+    TrieNode * createPath( const Path & keys ) {
+        if ( !root )
+            root = TrieNode::make_internal();
+        auto level = root.get();
+        for ( auto k : keys ) {
+            auto & i = std::get< Internal >( *level );
+            level = i.getOrInsertChild( k );
+        }
+        return level;
+    }
 
     TrieNodePtr root;
 };
