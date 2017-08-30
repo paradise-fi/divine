407559f45324ca64c4df1d7f2bb13f9580fdbe2f
Petr Rockai <me@mornfall.net>
2017-08-30 20:11:00 +0000

    test: Add a simple test for bitfield display in sim.

---
+++ test/sim/3.bitfield.sh
@@ -0,0 +1,33 @@
+. lib/testcase
+
+cat > test.c <<EOF
+struct BF
+{
+    int a:3;
+    int b:7;
+    int c:30;
+    char d:1;
+};
+
+int main() {
+    struct BF x = { .a = 2, .b = -5, .c = 8, .d = 1 };
+    return 0;
+}
+EOF
+
+sim test.c <<EOF
+> start
+> step --over
+> source
+> step
+> step --out
+> show .x
++ a:
++ value:.*[i16 2 d]
++ b:
++ value:.*[i16 123 d]
++ c:
++ value:.*[i32 8 d]
++ d:
++ value:.*[i8 1 d]
+EOF
