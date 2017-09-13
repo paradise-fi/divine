9d672e589668ac5b68bca01ffbb410454f1011dc
Henrich Lauko <xlauko@mail.muni.cz>
2017-09-13 13:41:36 +0000

    test: Add test on symbolic value insertion into binary tree.

---
+++ test/abstract/2.sym-struct-64-e.cpp
@@ -0,0 +1,66 @@
+/* VERIFY_OPTS: --symbolic */
+
+#include <cstdint>
+#include <limits>
+#include <array>
+#include <cassert>
+#define __sym __attribute__((__annotate__("lart.abstract.sym")))
+
+struct BinTree {
+    using value_type = int64_t;
+
+    struct Node {
+        Node( value_type v = 0 ) : val( v ){}
+
+        value_type val;
+        Node *left = nullptr, *right = nullptr;
+    };
+
+    void insert( Node *node ) {
+        root = insert_impl( root, node );
+    }
+
+    Node * insert_impl( Node *node, Node *in ) {
+        assert( in );
+        if ( !node )
+            return in;
+        if ( in->val < node->val )
+            node->left = insert_impl( node->left, in );
+        else
+            node->right = insert_impl( node->right, in );
+        return node;
+    }
+
+    bool is_correct_impl( Node *node, value_type min, value_type max ) {
+        if ( !node ) return true;
+        if ( node->val < min || node->val > max )
+            return false;
+        return is_correct_impl( node->left, min, node->val - 1 ) &&
+               is_correct_impl( node->right, node->val, max );
+    }
+
+    bool is_correct() {
+        return is_correct_impl( root,
+            std::numeric_limits< value_type >::min(),
+            std::numeric_limits< value_type >::max() );
+    }
+
+    Node * root = nullptr;
+};
+
+BinTree::Node make_abstract_node() {
+    __sym int x;
+    return { x };
+}
+
+int main() {
+    BinTree bt;
+
+    std::array< BinTree::Node, 100 > ns;
+    std::generate( ns.begin(), ns.end(), [] { return make_abstract_node(); } );
+
+    for ( auto & n : ns )
+        bt.insert( &n );
+
+    assert( bt.is_correct() );
+}
