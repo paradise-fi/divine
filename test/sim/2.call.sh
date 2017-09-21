2b6367c80c06aab128a540f297d4e74412e1310f
Petr Rockai <me@mornfall.net>
2017-09-21 20:51:16 +0000

    test: Add a test for the 'call' command in sim.

---
+++ test/sim/2.call.sh
@@ -0,0 +1,37 @@
+. lib/testcase
+
+cat > file.c <<EOF
+#include <sys/divm.h>
+
+void print_hello()
+{
+    __vm_trace( _VM_T_Text, "hello world" );
+}
+
+void die()
+{
+    static int *bad;
+    *bad = 0;
+}
+
+void has_args( int i ) {}
+
+int main() {}
+EOF
+
+sim file.c <<EOF
++ ^# executing __boot at
+> call print_hello
++ hello world
+> call die
++ FAULT: null pointer dereference
++ ^ERROR: encountered an error while running 'die'
+> start
++ executing main()
+> call print_hello
++ hello world
+> call has_args
++ ERROR: the function must not take any arguments
+> call not_defined
++ ERROR: the function 'not_defined' is not defined
+EOF
