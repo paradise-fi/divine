ca23328e439f4a53ebfb466051dfac634f9c672d
Vladimir Still <xstill@fi.muni.cz>
2017-09-23 09:35:23 +0000

    test: Add test for sim's register command.

---
+++ test/sim/3.register.sh
@@ -0,0 +1,27 @@
+. lib/testcase
+
+cat > file.c <<EOF
+int main( int argc, char **argv ) {
+    argv[ argc + 1 ] = 0;
+}
+EOF
+
+sim file.c <<EOF
++ ^# executing __boot at
+> start
+- ^# executing
++ ^# executing main at
+> register
++ ^\s*Constants
++ ^\s*Globals
++ ^\s*Frame
++ ^\s*PC
+> stepa
++ ^# executing \{Fault\}
+> register
++ ^\s*Flags:.*Mask
+> register
++ ^\s*Flags:.*Error
+> register
++ ^\s*Flags:.*KernelMode
+EOF
