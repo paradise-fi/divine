1af355b97af1566c63452e4a7bb480f4398aee57
Petr Rockai <me@mornfall.net>
2017-10-17 17:08:10 +0000

    test: Make sure the check in 2.stdio-trace.sh works with any CE.

--- test/dios/2.stdio-trace.sh
+++ test/dios/2.stdio-trace.sh
@@ -3,8 +3,8 @@
 divine verify -std=c++11 $TESTS/demo/1.thread.cpp | tee report.txt
 cat > expected <<EOF
 +   \[0\] starting thread
-+   \[1\] thread started
 +   \[0\] incrementing
++   \[1\] thread done
 EOF
 
 ordgrep expected < report.txt
