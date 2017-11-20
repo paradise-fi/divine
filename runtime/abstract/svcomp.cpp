7639c798fb6cb079a698a853292db8b997eb8f4c
Henrich Lauko <xlauko@fi.muni.cz>
2017-11-20 12:54:40 +0000

    runtime: Add definitions of svcomp abstract calls.

---
+++ runtime/abstract/svcomp.cpp
@@ -0,0 +1,16 @@
+#include <abstract/domains.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wuninitialized"
+
+extern "C" {
+
+int __VERIFIER_nondet_int() { _SYM int i; return i; }
+int __VERIFIER_nondet_uint() { _SYM unsigned int i; return i; }
+int __VERIFIER_nondet_char() { _SYM char i; return i; }
+int __VERIFIER_nondet_ulong() { _SYM unsigned long i; return i; }
+int __VERIFIER_nondet_long() { _SYM long i; return i; }
+
+}
+
+#pragma clang diagnostic pop
