21edf419d7ff28c0941fb9f1c30eeb0a08b882b1
Henrich Lauko <xlauko@mail.muni.cz>
2017-11-21 19:41:40 +0000

    runtime: Update svcomp definitions for abstraction.

--- runtime/abstract/svcomp.cpp
+++ runtime/abstract/svcomp.cpp
@@ -1,15 +1,40 @@
 #include <abstract/domains.h>
+#include <sys/fault.h>
+#include <stdint.h>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wuninitialized"
 
+#define _SVC_NONDET(t,n) t __VERIFIER_nondet_ ## n () { _SYM t x; return x; }
+
+
 extern "C" {
 
-int __VERIFIER_nondet_int() { _SYM int i; return i; }
-int __VERIFIER_nondet_uint() { _SYM unsigned int i; return i; }
-int __VERIFIER_nondet_char() { _SYM char i; return i; }
-int __VERIFIER_nondet_ulong() { _SYM unsigned long i; return i; }
-int __VERIFIER_nondet_long() { _SYM long i; return i; }
+_SVC_NONDET(char, char)
+_SVC_NONDET(int, int)
+_SVC_NONDET(short, short)
+_SVC_NONDET(long, long)
+_SVC_NONDET(unsigned int, uint)
+_SVC_NONDET(unsigned long, ulong)
+_SVC_NONDET(unsigned, unsigned)
+
+void __VERIFIER_error() {
+	__dios_fault( _DiOS_F_Assert, "verifier error called" );
+}
+
+void __VERIFIER_assume( int expression ) {
+	if ( !expression )
+		__vm_control( _VM_CA_Bit, _VM_CR_Flags, _VM_CF_Interrupted | _VM_CF_Cancel | _VM_CF_Mask,
+												_VM_CF_Interrupted | _VM_CF_Cancel );
+}
+
+void __VERIFIER_atomic_begin() {
+	__vm_control( _VM_CA_Bit, _VM_CR_Flags, _VM_CF_Mask, _VM_CF_Mask );
+}
+
+void __VERIFIER_atomic_end() {
+	__vm_control( _VM_CA_Bit, _VM_CR_Flags, _VM_CF_Mask, _VM_CF_None );
+}
 
 }
 
