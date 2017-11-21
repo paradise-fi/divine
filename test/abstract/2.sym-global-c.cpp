044dacec95bc324d623d360e3e910fc40791c4e0
Henrich Lauko <xlauko@mail.muni.cz>
2017-11-21 14:03:05 +0000

    test: Add test on abstraction of global structures.

---
+++ test/abstract/2.sym-global-c.cpp
@@ -0,0 +1,16 @@
+/* VERIFY_OPTS: --symbolic */
+#include <abstract/domains.h>
+#include <cassert>
+#include <cstdint>
+
+struct Widget {
+    int64_t value;
+};
+
+Widget w;
+
+int main() {
+    _SYM int val;
+    w.value = val % 10;
+    assert( w.value < 10 );
+}
