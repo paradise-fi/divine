6e96128b389c8f04f6926bc0a890d8817155983f
Vladimir Still <xstill@fi.muni.cz>
2017-11-29 13:35:13 +0000

    test: Add a test for setting invalid frame pointer.

---
+++ test/vm/2.frame-invalid.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <sys/divm.h>
+#include <dios.h>
+
+void f()
+{
+    struct _VM_Frame *frame = __vm_control( _VM_CA_Get, _VM_CR_Frame );
+    __vm_obj_free( frame->parent );
+    __vm_control( _VM_CA_Set, _VM_CR_Frame, frame->parent ); /* ERROR */
+    assert( 0 );
+}
+
+int main()
+{
+    f();
+}
---
+++ test/vm/2.frame.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <sys/divm.h>
+#include <dios.h>
+
+void f()
+{
+    struct _VM_Frame *frame = __vm_control( _VM_CA_Get, _VM_CR_Frame );
+    __vm_control( _VM_CA_Set, _VM_CR_Frame, frame->parent );
+    assert( 0 );
+}
+
+int main()
+{
+    f();
+}
