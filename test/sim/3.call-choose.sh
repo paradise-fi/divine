10a84805cf0498c10c3aa9d3bed0654305b254c2
Petr Rockai <me@mornfall.net>
2017-12-17 11:53:54 +0000

    test: Check that __vm_choose in sim's 'call' behaves as advertised (i.e. dies).

---
+++ test/sim/3.call-choose.sh
@@ -0,0 +1,19 @@
+. lib/testcase
+
+cat > file.cpp <<EOF
+#include <dios/kernel.hpp>
+
+extern "C" void use_choice()
+{
+    __vm_choose( 2 );
+}
+
+int main() {}
+EOF
+
+sim -std=c++14 -C,-I$SRC_ROOT/bricks file.cpp <<EOF
++ ^# executing __boot at
+> start
+> call use_choice
++ fault in debug mode
+EOF
