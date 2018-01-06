2d68e3379300a579eb6f6b2c49124104301010c4
Petr Rockai <me@mornfall.net>
2018-01-06 13:22:41 +0000

    VM: Refactor the xg::AddressMap interface and semantics, add local pointers.

--- divine/vm/xg-code.hpp
+++ divine/vm/xg-code.hpp
@@ -44,8 +44,21 @@ namespace divine::vm::xg
 struct AddressMap
 {
     std::map< llvm::BasicBlock *, CodePointer > _code, _terminator;
-    std::map< llvm::Constant *, GlobalPointer > _const;
-    std::map< llvm::GlobalVariable *, GlobalPointer > _mutable;
+    std::map< llvm::GlobalVariable *, GlobalPointer > _global;
+    GlobalPointer _next_global = GlobalPointer( 0, 0 );
+
+    GlobalPointer next_global()
+    {
+        _next_global.object( _next_global.object() + 1 );
+        return _next_global;
+    }
+
+    bool is_codeptr( llvm::Value *val )
+    {
+        return ( llvm::isa< llvm::Function >( val ) ||
+                 llvm::isa< llvm::BlockAddress >( val ) ||
+                 llvm::isa< llvm::BasicBlock >( val ) );
+    }
 
     CodePointer code( llvm::BasicBlock *bb )
     {
@@ -58,42 +71,23 @@ struct AddressMap
         return addr( &*f->begin() );
     }
 
-    CodePointer terminator( llvm::BasicBlock *bb )
+    CodePointer code( llvm::Instruction *i )
     {
-        ASSERT( _terminator.count( bb ) );
-        return _terminator[ bb ];
+        auto bb = code( i->getParent() );
+        int seqno = bb.instruction();
+
+        for ( auto &j : *i->getParent() )
+        {
+            ++ seqno;
+            if ( &j == i )
+                return CodePointer( bb.function(), seqno );
+        }
+
+        UNREACHABLE( "instruction not found" );
     }
 
-    GlobalPointer constant( llvm::Constant *v )
+    CodePointer code( llvm::Value *v )
     {
-        if ( auto GA = llvm::dyn_cast< llvm::GlobalAlias >( v ) )
-            return constant( GA->getBaseObject() );
-        ASSERT( _const.count( v ) );
-        return _const[ v ];
-    }
-
-    GlobalPointer mut( llvm::GlobalVariable *v )
-    {
-        ASSERT( _mutable.count( v ) );
-        return _mutable[ v ];
-    }
-
-    template< typename F >
-    void each_const( F )
-    {
-        NOT_IMPLEMENTED();
-    }
-
-    template< typename F >
-    void each_mutable( F )
-    {
-        NOT_IMPLEMENTED();
-    }
-
-    GenericPointer addr( llvm::Value *v )
-    {
-        if ( auto GA = llvm::dyn_cast< llvm::GlobalAlias >( v ) )
-            return addr( GA->getBaseObject() );
         if ( auto F = llvm::dyn_cast< llvm::Function >( v ) )
             return code( F );
         if ( auto B = llvm::dyn_cast< llvm::BasicBlock >( v ) )
@@ -101,10 +95,73 @@ struct AddressMap
         if ( auto B = llvm::dyn_cast< llvm::BlockAddress >( v ) )
             return code( B->getBasicBlock() );
 
-        if ( auto C = llvm::dyn_cast< llvm::Constant >( v ) )
-            return constant( C );
+        UNREACHABLE( "value passed to AddressMap::code() is not a code pointer" );
+    }
 
-        UNREACHABLE( "impossible value type" );
+    CodePointer terminator( llvm::BasicBlock *bb )
+    {
+        ASSERT( _terminator.count( bb ) );
+        return _terminator[ bb ];
+    }
+
+    GlobalPointer global( llvm::GlobalVariable *v )
+    {
+        ASSERT( _global.count( v ) );
+        return _global[ v ];
+    }
+
+    LocalPointer local( llvm::Instruction *i )
+    {
+        return LocalPointer( code( i ).instruction(), 0 );
+    }
+
+    LocalPointer local( llvm::Argument *a )
+    {
+        auto f = a->getParent();
+        int seqno = 0;
+        for ( auto &b : f->args() )
+            if ( &b == a )
+                return LocalPointer( seqno, 0 );
+            else
+                ++ seqno;
+        UNREACHABLE( "argument not found" );
+    }
+
+    /*
+     * Obtain an address corresponding to an llvm::Value. The input llvm::Value
+     * must be either a code pointer, a global variable or a register / local
+     * SSA variable. See also has_addr().
+     */
+    GenericPointer addr( llvm::Value *v )
+    {
+        if ( auto GA = llvm::dyn_cast< llvm::GlobalAlias >( v ) )
+            return addr( GA->getBaseObject() );
+
+        if ( is_codeptr( v ) )
+            return code( v );
+
+        if ( auto I = llvm::dyn_cast< llvm::Instruction >( v ) )
+            return local( I );
+        if ( auto A = llvm::dyn_cast< llvm::Argument >( v ) )
+            return local( A );
+        if ( auto G = llvm::dyn_cast< llvm::GlobalVariable >( v ) )
+            return global( G );
+
+        v->dump();
+        UNREACHABLE( "impossible value in slot_addr()" );
+    }
+
+    bool has_addr( llvm::Value *v )
+    {
+        if ( is_codeptr( v ) )
+            return true;
+        return has_slot( v );
+    }
+
+    bool has_slot( llvm::Value *v )
+    {
+        return llvm::isa< llvm::Instruction >( v ) || llvm::isa< llvm::Argument >( v ) ||
+               llvm::isa< llvm::GlobalVariable >( v );
     }
 
     void add( llvm::BasicBlock *b, CodePointer p )
@@ -114,21 +171,31 @@ struct AddressMap
 
     void build( llvm::Module *m )
     {
+        ASSERT( _code.empty() );
+
+        for ( auto var = m->global_begin(); var != m->global_end(); ++ var )
+            _global[ &*var ] = next_global();
+
         CodePointer pc( 0, 0 );
         for ( auto &f : *m )
         {
             if ( f.isDeclaration() )
                 continue;
             pc.function( pc.function() + 1 );
-            pc.instruction( 0 );
+            pc.instruction( brick::bitlevel::align( f.arg_size() + f.isVarArg(), 4 ) );
+
+            if ( !pc.function() )
+                throw std::logic_error( "Capacity exceeded: too many functions." );
 
             for ( auto &b : f )
             {
                 _code[ &b ] = pc;
+
                 for ( auto &i : b )
-                    pc.instruction( pc.instruction() + 1 ), void(i);
+                    static_cast< void >( i ), pc = pc + 1;
+
                 _terminator[ &b ] = pc;
-                pc.instruction( pc.instruction() + 1 );
+                pc = pc + 1;
             }
         }
     }
