2e4abfea30e2932065e604daa7fa8f6059633dea
Petr Rockai <me@mornfall.net>
2018-01-24 09:22:51 +0000

    test: Import upstream libcxx testcases for std::{forward_,}list.

---
+++ test/libcxx/forwardlist/default_recursive.pass.cpp
@@ -0,0 +1,27 @@
+/* TAGS: c++ */
+/* VERIFY_OPTS: -o nofail:malloc */
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <forward_list>
+
+// class forward_list
+
+// forward_list();
+
+#include <forward_list>
+
+struct X
+{
+    std::forward_list<X> q;
+};
+
+int main()
+{
+}
