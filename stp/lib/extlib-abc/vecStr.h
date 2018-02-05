762aa504f4a8aa8f481a7cf7c2aa59da3655ab74
Petr Rockai <me@mornfall.net>
2018-02-05 16:45:38 +0000

    stp: Fix a few compiler warnings in extlib-abc.

--- stp/lib/extlib-abc/vecStr.h
+++ stp/lib/extlib-abc/vecStr.h
@@ -435,7 +435,8 @@ static inline int Vec_StrBase10Log( unsigned Num )
     int Res;
     if ( Num == 0 ) return 0;
     if ( Num == 1 ) return 1;
-	for ( Res = 0, Num--;  Num;  Num /= 10,  Res++ );
+	for ( Res = 0, Num--;  Num;  Num /= 10,  Res++ )
+            ;
     return Res;
 } /* end of Extra_Base2Log */
 
