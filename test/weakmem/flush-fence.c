4ce844141107f318201f743cf9cc7bbce570046c
Vladimir Still <xstill@fi.muni.cz>
2018-02-09 19:33:17 +0000

    test: Add a test showing a bug in store buffer overflow and fences in weakmem.

---
+++ test/weakmem/flush-fence.c
@@ -0,0 +1,49 @@
+/* TAGS: min c tso ext divine-internal */
+/* VERIFY_OPTS: --relaxed-memory tso:2 */
+
+/* this is a regression test demonstrating error in an early version of lazy
+ * x86-TSO in DIVINE */
+
+#include <pthread.h>
+#include <sys/divm.h>
+#include <sys/lart.h>
+
+#define REACH( X ) assert( !(X) )
+
+volatile int x, y, z;
+volatile int bypass_sig;
+
+void *t1( void *_ ) {
+    x = 1;
+    y = 1;
+    __vm_control( _VM_CA_Bit, _VM_CR_Flags, _LART_CF_RelaxedMemRuntime, _LART_CF_RelaxedMemRuntime );
+    bypass_sig = 1;
+    while ( bypass_sig != 2 ) { }
+    __vm_control( _VM_CA_Bit, _VM_CR_Flags, _LART_CF_RelaxedMemRuntime, _VM_CF_None );
+    __sync_synchronize();
+    return NULL;
+}
+
+
+int main() {
+    pthread_t t1t;
+    pthread_create( &t1t, NULL, &t1, NULL );
+
+    /* wait for the other thread to put everything to SB */
+    __vm_control( _VM_CA_Bit, _VM_CR_Flags, _LART_CF_RelaxedMemRuntime, _LART_CF_RelaxedMemRuntime );
+    while ( bypass_sig != 1 ) { }
+    __vm_control( _VM_CA_Bit, _VM_CR_Flags, _LART_CF_RelaxedMemRuntime, _VM_CF_None );
+
+    x = 2;
+    y = 3;
+    __sync_synchronize();
+
+    /* let the other thread flush its SB */
+    __vm_control( _VM_CA_Bit, _VM_CR_Flags, _LART_CF_RelaxedMemRuntime, _LART_CF_RelaxedMemRuntime );
+    bypass_sig = 2;
+    __vm_control( _VM_CA_Bit, _VM_CR_Flags, _LART_CF_RelaxedMemRuntime, _VM_CF_None );
+    pthread_join( t1t, NULL );
+
+    REACH( y == 3 && x == 2 ); /* ERROR */
+}
+
