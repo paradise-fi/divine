1ecb302c1f2d4b9b72eff29b17a8242989e52835
Petr Rockai <me@mornfall.net>
2018-02-09 11:12:13 +0000

    libc: Raise an error if a syscall is attempted in kernel mode.

--- runtime/libc/functions/unistd/syscall.c
+++ runtime/libc/functions/unistd/syscall.c
@@ -21,6 +21,13 @@ void __dios_syscall( int syscode, void* ret, ... )
     uintptr_t fl = ( uintptr_t )
         __vm_control( _VM_CA_Get, _VM_CR_Flags,
                       _VM_CA_Bit, _VM_CR_Flags, _VM_CF_Mask, _VM_CF_Mask );
+
+    if ( fl & _VM_CF_KernelMode )
+    {
+        __vm_control( _VM_CA_Bit, _VM_CR_Flags, _VM_CF_Error, _VM_CF_Error );
+        return;
+    }
+
     va_list vl;
     va_start( vl, ret );
     __dios_trap( syscode, ret,  &vl );
