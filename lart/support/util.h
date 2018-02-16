a0cb89db73e9c8b66894e0068bf292e4f177e082
Vladimir Still <xstill@fi.muni.cz>
2018-02-16 16:14:55 +0000

    lart: Fix inlineIntoCallers.

--- lart/support/util.h
+++ lart/support/util.h
@@ -915,7 +915,7 @@ inline void inlineIntoCallers( llvm::Function *fn ) {
     std::vector< llvm::Value * > users{ fn->user_begin(), fn->user_end() };
     for ( auto *u : users ) {
         llvm::CallSite cs{ u };
-        if ( !u )
+        if ( !cs )
             continue;
         llvm::InlineFunctionInfo ifi;
         llvm::InlineFunction( cs, ifi );
