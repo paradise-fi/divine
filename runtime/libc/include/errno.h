f6437f152ab51f2dc21def7355b458a9042d02c6
Petr Rockai <me@mornfall.net>
2018-03-02 06:06:04 +0000

    libc: Clean up conflicting definitions of errno.

--- runtime/libc/include/errno.h
+++ runtime/libc/include/errno.h
@@ -10,18 +10,9 @@
 #include "_PDCLIB_aux.h"
 #include <_PDCLIB_errno.h>
 
-#ifdef __divine__
-
-#include <dios.h>
-#define errno (*__dios_get_errno())
-
-#else
-
 _PDCLIB_EXTERN_C
 extern int *__errno_location (void) __attribute__ ((__const__));
 #define errno (*__errno_location ())
 _PDCLIB_EXTERN_END
-
-#endif
 
 #endif
