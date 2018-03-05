595979fb7503cc76a92ca93f587d701284261016
Petr Rockai <me@mornfall.net>
2018-03-05 22:41:27 +0000

    libc: Remove redundant syscall wrappers from sys/fault.c.

--- runtime/libc/functions/sys/fault.c
+++ runtime/libc/functions/sys/fault.c
@@ -1,21 +1,7 @@
-#include <sys/syscall.h>
 #include <sys/fault.h>
 #include <sys/trace.h>
+#include <sys/stack.h>
 #include <sys/divm.h>
-
-int __dios_configure_fault( int fault, int cfg )
-{
-    int ret;
-    __dios_syscall( SYS_configure_fault, &ret, fault, cfg );
-    return ret;
-}
-
-int __dios_get_fault_config( int fault )
-{
-    int ret;
-    __dios_syscall( SYS_get_fault_config, &ret, fault );
-    return ret;
-}
 
 void __dios_fault( int f, const char *msg )
 {
