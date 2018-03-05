c4ab2b5c39ba6bb0159a9ecc9c0e6111c2d70223
Petr Rockai <me@mornfall.net>
2018-03-05 21:17:23 +0000

    libc: Add prototypes for autogenerated, DiOS-specific syscall wrappers.

--- runtime/libc/include/sys/syscall.h
+++ runtime/libc/include/sys/syscall.h
@@ -4,6 +4,11 @@
 #include <_PDCLIB_aux.h>
 #include <stdarg.h>
 
+/* argument types for dios syscalls */
+#include <sys/types.h>
+#include <sys/task.h>
+#include <sys/monitor.h>
+
 typedef enum
 {
     SYS_NONE = 0,
@@ -30,6 +35,14 @@ _PDCLIB_EXTERN_C
 
 void __dios_trap( int syscode, void* ret, va_list *args ) _PDCLIB_nothrow;
 void __dios_syscall( int syscode, void* ret, ... ) _PDCLIB_nothrow;
+
+#define SYSCALL(...)
+#define SYSCALL_DIOS( name, sched, ret, args ) ret __dios_ ## name args __nothrow;
+#include <dios/macro/no_memory_tags>
+#include <sys/syscall.def>
+#include <dios/macro/no_memory_tags.cleanup>
+#undef SYSCALL
+#undef SYSCALL_DIOS
 
 _PDCLIB_EXTERN_END
 /* #endif */
