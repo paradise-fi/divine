04a6135d3966ce113acf33a4e7c9b8d1def037a8
Petr Rockai <me@mornfall.net>
2018-03-06 20:37:53 +0000

    VM: Fix fault handler accidentally clearing _VM_CF_Error when in continue mode.

--- runtime/dios/core/fault.hpp
+++ runtime/dios/core/fault.hpp
@@ -200,7 +200,7 @@ struct Fault: public Next {
 
         // Continue if we get the control back
         cont_frame->pc = cont_pc;
-        old |= uint64_t( __vm_ctl_get( _VM_CR_Flags ) ) & _DiOS_CF_Fault;
+        old |= uint64_t( __vm_ctl_get( _VM_CR_Flags ) ) & ( _DiOS_CF_Fault | _VM_CF_Error );
         __vm_ctl_set( _VM_CR_Flags, reinterpret_cast< void * >( old ) );
         __vm_ctl_set( _VM_CR_Frame, cont_frame );
         __builtin_unreachable();
