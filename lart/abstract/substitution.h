dfcbdba4148b01c278f44ab83b483d2ac1523232
Henrich Lauko <xlauko@mail.muni.cz>
2018-03-15 12:18:56 +0000

    lart: Clean up the substitution pass.

--- lart/abstract/substitution.h
+++ lart/abstract/substitution.h
@@ -1,36 +1,15 @@
-// -*- C++ -*- (c) 2016 Henrich Lauko <xlauko@mail.muni.cz>
+// -*- C++ -*- (c) 2018 Henrich Lauko <xlauko@mail.muni.cz>
 #pragma once
 
-#include <lart/abstract/domains/domains.h>
-#include <lart/abstract/domains/zero.h>
-#include <lart/abstract/domains/sym.h>
-
-#include <lart/abstract/substituter.h>
-#include <lart/abstract/data.h>
+DIVINE_RELAX_WARNINGS
+#include <llvm/IR/Module.h>
+DIVINE_UNRELAX_WARNINGS
 
 namespace lart {
 namespace abstract {
 
-template< typename TMap, typename Fns >
-static auto make_sbuilder( TMap & tmap, Fns & fns, llvm::Module & m ) {
-    auto s = Substituter< TMap, Fns >( tmap, fns );
-    // TODO remove isPresent from domain implementeation
-    s.addAbstraction( std::make_shared< TristateDomain >( tmap ) );
-    if ( Zero::isPresent( m ) )
-        s.addAbstraction( std::make_shared< Zero >( m, tmap ) );
-    if ( Symbolic::isPresent( m ) )
-        s.addAbstraction( std::make_shared< Symbolic >( m, tmap ) );
-    return s;
-}
-
 struct Substitution {
-
-    Substitution( PassData & data ) : data( data ) {}
-
     void run( llvm::Module & );
-
-    PassData & data;
-    ReMap< llvm::Function * > fns;
 };
 
 
