ed83986c49b2029e1b08cca108fa8f0a5fc8670b
Henrich Lauko <xlauko@mail.muni.cz>
2018-03-26 12:37:47 +0000

    rst: Update the 'sym' and 'tristate' domains.

--- dios/rst/sym.cpp
+++ dios/rst/sym.cpp
@@ -1,4 +1,5 @@
 #include <rst/sym.h>
+#include <rst/domains.h>
 #include <rst/common.h>
 #include <dios.h>
 #include <cstdarg>
@@ -34,15 +35,20 @@ extern "C" void __sym_formula_dump()
     }
 }
 
-template< typename T >
-T __sym_val_impl() {
-    return reinterpret_cast< T >( __sym_lift( sizeof( T ), 0 ) );
+extern "C" uint64_t __vm_taint_i64();
+
+template< typename T, typename = std::enable_if_t< sizeof( T ) == sizeof( T* ) > >
+_SYM T __sym_val_impl() {
+    auto val = reinterpret_cast< T >( __sym_lift( sizeof( T ), 0 ) );
+    return val + __vm_taint_i64();
 }
 
+extern "C" {
 // uint8_t __sym_val_i8() { return __sym_val_impl< uint8_t >(); }
 // uint16_t __sym_val_i16() { return __sym_val_impl< uint16_t >(); }
 // uint32_t __sym_val_i32() { return __sym_val_impl< uint32_t >(); }
-extern "C" uint64_t __sym_val_i64() { return __sym_val_impl< uint64_t >(); }
+uint64_t __sym_val_i64() { return __sym_val_impl< uint64_t >(); }
+}
 
 Formula *__sym_lift( int bitwidth, int argc, ... ) {
     if ( bitwidth > 64 )
@@ -58,14 +64,7 @@ Formula *__sym_lift( int bitwidth, int argc, ... ) {
     return mark( __newf< Constant >( Type{ Type::Int, bitwidth }, va_arg( args, int64_t ) ) );
 }
 
-Formula *__abstract_sym_lift( int64_t val, int bitwidth )
-{
-    if ( bitwidth > 64 )
-        _UNREACHABLE_F( "Integer too long: %d bits", bitwidth );
-    return mark( __newf< Constant >( Type{ Type::Int, bitwidth }, val ) );
-}
-
-#define BINARY( suff, op ) Formula *__abstract_sym_ ## suff( Formula *a, Formula *b ) \
+#define BINARY( suff, op ) Formula *__sym_ ## suff( Formula *a, Formula *b ) \
 {                                                                                     \
     return mark( __newf< Binary >( Op::op, a->type(), weaken( a ), weaken( b ) ) );   \
 }
@@ -84,7 +83,7 @@ BINARY( shl, Shl );
 BINARY( lshr, LShr );
 BINARY( ashr, AShr );
 
-#define CAST( suff, op ) Formula *__abstract_sym_ ## suff( Formula *a, int bitwidth ) \
+#define CAST( suff, op ) Formula *__sym_ ## suff( Formula *a, int bitwidth ) \
 {                                                                                     \
     Type t = a->type();                                                               \
     t.bitwidth( bitwidth );                                                           \
@@ -96,7 +95,7 @@ CAST( zext, ZExt );
 CAST( sext, SExt );
 CAST( bitcast, BitCast );
 
-#define ICMP( suff, op ) Formula *__abstract_sym_icmp_ ## suff( Formula *a, Formula *b ) { \
+#define ICMP( suff, op ) Formula *__sym_icmp_ ## suff( Formula *a, Formula *b ) { \
     Type i1( Type::Int, 1 ); \
     return mark( __newf< Binary >( Op::op, i1, weaken( a ), weaken( b ) ) ); \
 }
@@ -112,13 +111,13 @@ ICMP( sge, SGE );
 ICMP( sle, SLE );
 ICMP( slt, SLT );
 
-Tristate *__abstract_sym_bool_to_tristate( Formula * )
+Tristate __sym_bool_to_tristate( Formula * )
 {
     // TODO: pattern matching for trivial cases of True/False
-    return __new< Tristate >( Tristate::Unknown );
+    return Tristate::Value::Unknown;
 }
 
-Formula *__abstract_sym_assume( Formula *value, Formula *constraint, bool assume )
+Formula *__sym_assume( Formula *value, Formula *constraint, bool assume )
 {
     Formula *wconstraint = weaken( constraint );
     if ( !assume )
--- dios/rst/tristate.cpp
+++ dios/rst/tristate.cpp
@@ -10,15 +10,19 @@
 using namespace abstract;
 
 extern "C" {
-    bool __abstract_tristate_lower( Tristate * tristate ) _ROOT {
-        if ( tristate->value == Tristate::Domain::Unknown ) {
+    bool __tristate_lower( Tristate tristate ) _ROOT {
+        if ( tristate.value == Tristate::Value::Unknown ) {
     #ifdef __divine__
             return __vm_choose( 2 );
     #else
             return std::rand() % 2;
     #endif
         }
-        return tristate->value;
+        return tristate.value;
+    }
+
+    Tristate __tristate_lift( bool v ) _ROOT {
+        return v ? Tristate::Value::True : Tristate::Value::False;
     }
 }
 
