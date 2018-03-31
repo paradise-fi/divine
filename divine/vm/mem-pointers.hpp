3cd00c1861b5a2f3aaf9ae2d4e76bb3720a32f7e
Petr Rockai <me@mornfall.net>
2018-03-31 18:55:15 +0000

    VM: Add support for truncated pointer values to shadow write().

--- divine/vm/mem-pointers.hpp
+++ divine/vm/mem-pointers.hpp
@@ -202,6 +202,22 @@ struct PointerLayer : public NextLayer
             //exp[ 1 ].pointer_type = value.cooked().type();
             exp[ 1 ].pointer_type = 0;
         }
+        else if ( sz <= 4 && value.pointer() && value.as_pointer().object() )
+        {
+            PointerException exc;
+            if ( exp[ 0 ].pointer_exception )
+                exc = _ptr_exceptions->at( l.object, l.offset );
+            else
+                exc.invalidate();
+            for ( int i = 0; i < sz; ++i )
+            {
+                exc.objid[ i ] = value.as_pointer().object();
+                exc.type( i, uint8_t( value.as_pointer().type() ) );
+                exc.index( i, 0 ); /* TODO */
+            }
+            exp[ 0 ].pointer_exception = true;
+            _ptr_exceptions->set( l.object, l.offset, exc );
+        }
         else
         {
             for ( int w = 0; w < words; ++w )
