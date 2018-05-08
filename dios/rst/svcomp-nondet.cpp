ff94b13a9e98360dab81f1d729ae3ca52ca7ce6d
Henrich Lauko <xlauko@mail.muni.cz>
2018-05-08 12:32:24 +0000

    rst: Let svcomp nondet functions create a tainted value.

--- dios/rst/svcomp-nondet.cpp
+++ dios/rst/svcomp-nondet.cpp
@@ -6,18 +6,20 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wuninitialized"
 
-#define _SVC_NONDET(t,n) t __VERIFIER_nondet_ ## n () noexcept { _SYM t x; return x; }
+#define _SVC_NONDET(t,n,bw) t __VERIFIER_nondet_ ## n() { \
+    return __sym_val_i ## bw();                           \
+}
 
 extern "C" {
 
-    _SVC_NONDET(bool, bool)
-    _SVC_NONDET(char, char)
-    _SVC_NONDET(int, int)
-    _SVC_NONDET(short, short)
-    _SVC_NONDET(long, long)
-    _SVC_NONDET(unsigned int, uint)
-    _SVC_NONDET(unsigned long, ulong)
-    _SVC_NONDET(unsigned, unsigned)
+    _SVC_NONDET(bool, bool, 8)
+    _SVC_NONDET(char, char, 8)
+    _SVC_NONDET(int, int, 32)
+    _SVC_NONDET(short, short, 16)
+    _SVC_NONDET(long, long, 64)
+    _SVC_NONDET(unsigned int, uint, 32)
+    _SVC_NONDET(unsigned long, ulong, 64)
+    _SVC_NONDET(unsigned, unsigned, 32)
 
 }
 
