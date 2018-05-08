a979c79dff082cd6cdcd539bbde3517cd224c1bf
Henrich Lauko <xlauko@mail.muni.cz>
2018-05-08 13:38:01 +0000

    test: Add a todo tag to abstraction tests which use memcpy.

--- test/abstract/sym-struct-64-b.cpp
+++ test/abstract/sym-struct-64-b.cpp
@@ -1,4 +1,4 @@
-/* TAGS: sym min c++ */
+/* TAGS: sym todo min c++ */
 /* VERIFY_OPTS: --symbolic */
 #include <rst/domains.h>
 #include <cstdint>
@@ -12,8 +12,7 @@ struct S {
 
 int main() {
     S a, b;
-    int y = __sym_val_i32();
-    a.y = y;
-    b = a;
+    a.y = __sym_val_i32();
+    b = a; // TODO peeking from mem copied value
     assert( a.x == b.x && a.y == b.y );
 }
