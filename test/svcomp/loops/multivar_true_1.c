8007291f655cd7776caf5eb9bc58d5d1698af1ce
Petr Rockai <me@mornfall.net>
2018-05-09 21:00:55 +0000

    test: Temporarily disable a test that fails due to a lart regression.

--- test/svcomp/loops/multivar_true_1.c
+++ test/svcomp/loops/multivar_true_1.c
@@ -1,6 +1,7 @@
-/* TAGS: c sym */
+/* TAGS: c sym todo */
 /* VERIFY_OPTS: --symbolic --sequential -o nofail:malloc */
 /* CC_OPTS: -O2 */ // we cheat and let clang figure this one out
+
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 extern void __VERIFIER_assert(int);
 extern unsigned int __VERIFIER_nondet_uint(void);
