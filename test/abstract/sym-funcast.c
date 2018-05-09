7dbb9f38349ec1f15d65763231b2f25cdb4a07c5
Petr Rockai <me@mornfall.net>
2018-05-09 20:12:01 +0000

    test: Add a (todo) test for domain propagation through function casts.

---
+++ test/abstract/sym-funcast.c
@@ -0,0 +1,10 @@
+/* TAGS: sym todo c */
+/* VERIFY_OPTS: --symbolic */
+
+#include <assert.h>
+
+int __VERIFIER_nondet_int(); /* note the absence of void in the arg list */
+int main()
+{
+    assert( __VERIFIER_nondet_int() != 3 ); /* ERROR */
+}
