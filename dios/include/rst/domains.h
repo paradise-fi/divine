40aef6c357e3b42b44d1f491379090ed986657e8
Henrich Lauko <xlauko@mail.muni.cz>
2018-05-13 14:49:25 +0000

    rst: Make domains.h C-friendly.

--- dios/include/rst/domains.h
+++ dios/include/rst/domains.h
@@ -4,10 +4,14 @@
 #define _SYM __attribute__((__annotate__("lart.abstract.sym")))
 #define _ZERO __attribute__((__annotate__("lart.abstract.zero")))
 
-extern "C"  {
+#ifdef __cplusplus
+extern "C" {
+#endif
     uint64_t __sym_val_i64();
     uint32_t __sym_val_i32();
     uint16_t __sym_val_i16();
     uint8_t __sym_val_i8();
+#ifdef __cplusplus
 }
+#endif
 
--- dios/include/rst/lart.h
+++ dios/include/rst/lart.h
@@ -1,8 +1,11 @@
 #pragma once
+#include <stdint.h>
 
-#include <cstdint>
-
+#ifdef __cplusplus
 extern "C" {
+#endif
     void __lart_stash( uintptr_t );
     uintptr_t __lart_unstash();
+#ifdef __cplusplus
 }
+#endif
