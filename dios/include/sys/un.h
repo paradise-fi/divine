25669a49f1cef46bf8fb33f4b13b668543ac4ab6
Zuzana Baranova <xbaranov@fi.muni.cz>
2018-05-17 14:34:27 +0000

    libc: Reflect the removal of sockaddr.h.

--- dios/include/sys/un.h
+++ dios/include/sys/un.h
@@ -1,13 +1,13 @@
 #ifndef _SYS_UN_H
 #define _SYS_UN_H  1
 
-#include <bits/sockaddr.h>
+#include <sys/types.h>
 
 #define UNIX_PATH_MAX  107
 
 /* Structure describing the address of an AF_LOCAL (aka AF_UNIX) socket.  */
 struct sockaddr_un  {
-    __SOCKADDR_COMMON (sun_);
+    __sa_family_t sun_family;
     char sun_path[UNIX_PATH_MAX];    /* Path name.  */
 };
 
