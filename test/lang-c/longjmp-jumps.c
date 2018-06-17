9b8013e7ff7f8686503a01eda127f3dd68177b0e
Vladimir Still <xstill@fi.muni.cz>
2018-06-17 08:15:44 +0000

    test: Add a test which checks that longjmp actually jumps.

---
+++ test/lang-c/longjmp-jumps.c
@@ -0,0 +1,21 @@
+/* TAGS: min c */
+#include <setjmp.h>
+#include <assert.h>
+#include <stdbool.h>
+
+volatile bool jumped = false;
+
+int main() {
+    jmp_buf jb;
+    int r = setjmp( jb );
+    if ( jumped )
+        assert( r == 42 );
+    else
+        assert( r == 0 );
+
+    assert( !jumped ); /* ERROR */
+
+    jumped = true;
+    longjmp( jb, 42 );
+}
+
