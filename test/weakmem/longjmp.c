541c2772689e8f23efcd12ddc81a471781bad829
Vladimir Still <xstill@fi.muni.cz>
2018-06-19 11:11:48 +0000

    test: Add a test of interaction between weakmem and longjmp.

---
+++ test/weakmem/longjmp.c
@@ -0,0 +1,23 @@
+/* TAGS: min c tso */
+/* VERIFY_OPTS: --relaxed-memory tso */
+#include <setjmp.h>
+#include <assert.h>
+#include <stdbool.h>
+
+volatile bool jumped = false;
+
+int main() {
+    jmp_buf jb;
+    int r = setjmp( jb );
+    if ( jumped )
+        assert( r == 42 );
+    else
+        assert( r == 0 );
+
+    if ( jumped )
+        return 0;
+
+    jumped = true;
+    longjmp( jb, 42 );
+}
+
