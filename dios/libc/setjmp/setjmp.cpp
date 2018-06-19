66e65fd4062e94b3534950749252a09a81d539f4
Vladimir Still <xstill@fi.muni.cz>
2018-06-19 11:14:51 +0000

    DiOS: Fix longjmp under weakmem.

--- dios/libc/setjmp/setjmp.cpp
+++ dios/libc/setjmp/setjmp.cpp
@@ -18,13 +18,19 @@ int setjmp( jmp_buf env )
     return 0;
 }
 
+/* writting to frame must bypass weakmem */
+__weakmem_direct __attribute__((__noinline__)) static void _longjmp_setretval( int *addr, int val )
+{
+    *addr = val;
+}
+
 void longjmp( jmp_buf env, int val )
 {
     auto retreg = __md_get_register_info( env->__jumpFrame, env->__jumpPC, env->__jumpFunctionMeta );
     assert( retreg.width == sizeof( int ) );
     if ( val == 0 )
         val = 1;
-    *reinterpret_cast< int * >( retreg.start ) = val;
+    _longjmp_setretval( reinterpret_cast< int * >( retreg.start ), val );
     __dios_unwind( nullptr, nullptr, env->__jumpFrame );
     __dios_jump_and_kill_frame( env->__jumpFrame, _VM_CodePointer( uintptr_t( env->__jumpPC ) ), -1 );
 }
