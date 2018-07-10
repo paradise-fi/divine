442c320bbab1422f2403dd934630ea26f0c0892e
Zuzana Baranova <xbaranov@fi.muni.cz>
2018-07-10 16:24:55 +0000

    CC: Pass the correct debug flag to compiler with -g present.

--- divine/cc/options.cpp
+++ divine/cc/options.cpp
@@ -71,6 +71,8 @@ ParsedOpts parseOpts( std::vector< std::string > rawCCOpts )
             po.toObjectOnly = true;
         else if ( *it == "-E" )
             po.preprocessOnly = true;
+        else if ( *it == "-g" )
+            po.opts.emplace_back( "-debug-info-kind=standalone" );
         else
             po.opts.emplace_back( *it );
     }
