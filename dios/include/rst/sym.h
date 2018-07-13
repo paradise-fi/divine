1e3eab7c7b6938ff6dfab1e0065e62701c413529
Henrich Lauko <xlauko@mail.muni.cz>
2018-07-13 18:03:24 +0000

    rst: Let domain.def generate specification structure.

--- dios/include/rst/sym.h
+++ dios/include/rst/sym.h
@@ -1,14 +1,15 @@
 #pragma once
 #include <rst/formula.h>
 
-
 typedef lart::sym::Formula __sym_formula;
 
 #define DOMAIN_NAME sym
+#define DOMAIN_KIND scalar
 #define DOMAIN_TYPE __sym_formula*
     #include <rst/domain.def>
 #undef DOMAIN_NAME
 #undef DOMAIN_TYPE
+#undef DOMAIN_KIND
 
 extern "C" {
 void __sym_formula_dump();
