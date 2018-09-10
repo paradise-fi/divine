0ec02dfbe4f3c235793f200baac33b88ae6c633b
Henrich Lauko <xlauko@mail.muni.cz>
2018-09-10 13:10:30 +0000

    rst: Create interface to 'star' domain.

--- dios/include/rst/domains.h
+++ dios/include/rst/domains.h
@@ -2,6 +2,7 @@
 #include <rst/lart.h>
 
 #define _SYM __attribute__((__annotate__("lart.abstract.return.sym")))
+#define _STAR __attribute__((__annotate__("lart.abstract.return.star")))
 #define _ZERO __attribute__((__annotate__("lart.abstract.return.zero")))
 
 #ifdef __cplusplus
@@ -11,6 +12,11 @@ extern "C" {
     uint32_t __sym_val_i32();
     uint16_t __sym_val_i16();
     uint8_t __sym_val_i8();
+
+    uint64_t __star_val_i64();
+    uint32_t __star_val_i32();
+    uint16_t __star_val_i16();
+    uint8_t __star_val_i8();
 #ifdef __cplusplus
 }
 #endif
