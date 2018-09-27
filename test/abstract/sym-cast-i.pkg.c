65c283897c5d9dc7ce8c59c12028bba77b5073f0
Henrich Lauko <xlauko@mail.muni.cz>
2018-09-27 19:31:45 +0000

    test: Add abstraction test using inttoptr instruction.

---
+++ test/abstract/sym-cast-i.pkg.c
@@ -0,0 +1,21 @@
+/* TAGS: sym c todo */
+/* VERIFY_OPTS: --symbolic */
+/* CC_OPTS: */
+
+// V: v.O0 CC_OPT: -O0 TAGS: min
+// V: v.O1 CC_OPT: -O1
+// V: v.O2 CC_OPT: -O2
+// V: v.Os CC_OPT: -Os
+#include <rst/domains.h>
+#include <assert.h>
+
+unsigned long __VERIFIER_nondet_ulong( void );
+void *foo() { /* creates inttoptr instuction */
+    return __VERIFIER_nondet_ulong();
+}
+
+
+int main() {
+    _Bool b = foo();
+    assert( !b ); /* ERROR */
+}
