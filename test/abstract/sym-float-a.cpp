5144bfb12aa4bf1d15b883c365c5f4ceb7d5640f
Henrich Lauko <xlauko@mail.muni.cz>
2018-10-11 15:51:09 +0000

    test: Add tests covering basic floating point abstraction.

---
+++ test/abstract/sym-float-a.cpp
@@ -0,0 +1,19 @@
+/* TAGS: sym c++ float */
+/* VERIFY_OPTS: --symbolic */
+/* CC_OPTS: */
+
+// V: v.O0 CC_OPT: -O0 TAGS: min
+// V: v.O1 CC_OPT: -O1
+// V: v.O2 CC_OPT: -O2
+// V: v.Os CC_OPT: -Os
+#include <rst/domains.h>
+
+#include <cstdint>
+#include <cassert>
+
+int main() {
+    float x = __sym_val_float32();
+    float y = 0;
+    if ( x != 0 )
+        assert( x != y );
+}
