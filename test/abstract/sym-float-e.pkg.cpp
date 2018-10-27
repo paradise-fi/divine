0b6b4b4f7bc312460bb35010bdcfa9652d93f19e
Henrich Lauko <xlauko@mail.muni.cz>
2018-10-27 07:29:35 +0000

    test: Create packages from float tests.

---
+++ test/abstract/sym-float-e.pkg.cpp
@@ -0,0 +1,21 @@
+/* TAGS: sym c++ float */
+/* VERIFY_OPTS: --symbolic -o nofail:malloc */
+/* CC_OPTS: */
+
+// V: v.O0 CC_OPT: -O0 TAGS: min
+// V: v.O1 CC_OPT: -O1
+// V: v.O2 CC_OPT: -O2
+// V: v.Os CC_OPT: -Os
+#include <rst/domains.h>
+
+#include <cstdlib>
+#include <cstdint>
+#include <cassert>
+
+int main() {
+    float * a = static_cast< float * >( malloc( sizeof( float ) ) );
+    float y = __sym_val_float32();
+    *a = y;
+    assert( *a == y );
+    free( a );
+}
