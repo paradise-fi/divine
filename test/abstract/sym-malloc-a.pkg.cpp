5b38507611ca230aa08893e8f9fa1325b43e6ead
Henrich Lauko <xlauko@mail.muni.cz>
2018-10-27 07:26:48 +0000

    test: Ignore fail of malloc in sym-malloc-a test.

--- test/abstract/sym-malloc-a.pkg.cpp
+++ test/abstract/sym-malloc-a.pkg.cpp
@@ -1,5 +1,11 @@
 /* TAGS: sym min c++ */
-/* VERIFY_OPTS: --symbolic */
+/* VERIFY_OPTS: --symbolic -o nofail:malloc */
+/* CC_OPTS: */
+
+// V: v.O0 CC_OPT: -O0 TAGS: min
+// V: v.O1 CC_OPT: -O1
+// V: v.O2 CC_OPT: -O2
+// V: v.Os CC_OPT: -Os
 #include <rst/domains.h>
 
 #include <cstdlib>
