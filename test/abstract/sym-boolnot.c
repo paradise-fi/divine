94c0ad0a84a6dc7097bdc27cf0c978dd130f93c7
Vladimir Still <xstill@fi.muni.cz>
2018-11-03 11:40:08 +0000

    test: Add a test which demonstrates symbolic bool xor.

---
+++ test/abstract/sym-boolnot.c
@@ -0,0 +1,14 @@
+/* TAGS: c sym min */
+/* VERIFY_OPTS: --symbolic */
+
+#include <stdbool.h>
+#include <assert.h>
+
+bool __VERIFIER_nondet_bool();
+
+int main() {
+    bool a = __VERIFIER_nondet_bool();
+    bool b = !a;
+
+    assert( a == b ); /* ERROR */
+}
