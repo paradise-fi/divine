9ce97e44c1f725d9be0d9d3d3c34692f58addef1
Petr Rockai <me@mornfall.net>
2018-11-12 15:50:39 +0000

    lart: Allow insertion of leak checks at various points in the program.

--- lart/divine/leak-check.cpp
+++ lart/divine/leak-check.cpp
@@ -9,17 +9,29 @@ DIVINE_UNRELAX_WARNINGS
 
 #include <lart/support/pass.h>
 #include <lart/support/meta.h>
+#include <lart/support/cleanup.h>
 #include <divine/vm/divm.h>
 
 namespace lart::divine {
 
-struct LeakCheck {
+struct LeakCheck
+{
+    bool _exit = false, _return = false, _suspend = false;
 
-    static PassMeta meta() {
-        return passMeta< LeakCheck >( "LeakCheck", "" );
+    LeakCheck( std::string opt )
+    {
+        if ( opt.find( "exit" ) != std::string::npos )
+            _exit = true;
+        if ( opt.find( "state" ) != std::string::npos )
+            _suspend = true;
+        if ( opt.find( "return" ) != std::string::npos )
+            _return = true;
+        if ( !_exit && !_return && !_suspend )
+            throw std::logic_error( "constructed a useless LeakCheck instance" );
     }
 
-    void run( llvm::Module &m ) {
+    void run( llvm::Module &m )
+    {
         auto *exit = m.getFunction( "_Exit" );
         auto *suspend = m.getFunction( "__dios_suspend" );
         auto *trace = m.getFunction( "__vm_trace" );
@@ -27,14 +39,36 @@ struct LeakCheck {
         for ( auto *fn : { exit, suspend } ) {
             if ( !fn || fn->empty() )
                 continue;
+            if ( !_exit && fn == exit )
+                continue;
+            if ( !_suspend && fn == suspend )
+                continue;
             llvm::IRBuilder<> irb( &*fn->begin(), fn->begin()->getFirstInsertionPt() );
             irb.CreateCall( trace, { irb.getInt32( _VM_T_LeakCheck ) } );
+        }
+
+        auto ehInfo = cleanup::EhInfo::cpp( m );
+
+        if ( !_return )
+            return;
+
+        for ( auto &fn : m )
+        {
+            if ( fn.empty() || &fn == suspend || &fn == exit )
+                continue;
+            cleanup::makeExceptionsVisible( ehInfo, fn, []( const auto & ) { return true; } );
+            cleanup::atExits( fn, [&]( llvm::Instruction *i )
+                    {
+                        llvm::IRBuilder<> irb( i );
+                        irb.CreateCall( trace, { irb.getInt32( _VM_T_LeakCheck ) } );
+                    } );
         }
     }
 };
 
-PassMeta leakCheck() {
-    return compositePassMeta< LeakCheck >( "leak-check", "Enable memory leak checking" );
+PassMeta leakCheck()
+{
+    return passMetaO< LeakCheck >( "leak-check", "Enable memory leak checking" );
 }
 
 } // namespace lart::divine
