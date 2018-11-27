072ecc9a53f9cc2aceaddbae6f3b362af34ba7ac
Petr Rockai <me@mornfall.net>
2018-11-27 08:47:57 +0000

    sim: Enable autosuspend when performing a 'call' command.

--- divine/sim/dispatch.cpp
+++ divine/sim/dispatch.cpp
@@ -198,7 +198,7 @@ void CLI::go( command::Call c )
 
     Context ctx( _ctx );
     vm::Eval< Context > eval( ctx );
-    ctx.flags_set( 0, _VM_CF_KernelMode | _VM_CF_DebugMode );
+    ctx.flags_set( 0, _VM_CF_KernelMode | _VM_CF_DebugMode | _VM_CF_AutoSuspend );
     ctx.enter( pc, PointerV() );
     eval.run();
     for ( auto t : ctx._trace )
