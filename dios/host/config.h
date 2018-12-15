3b67e7088640ffc7d75a03a4b27516aa8c91cee7
Petr Rockai <me@mornfall.net>
2018-12-15 19:37:51 +0000

    dios: Add a config.h for the host/ bits.

---
+++ dios/host/config.h
@@ -0,0 +1,9 @@
+#define _GL_ATTRIBUTE_PURE
+#define freadahead __freadahead
+#define freadptr __freadptr
+#define freading __freading
+#define freadptr __freadptr
+#define fseterr __fseterr
+#define LSEEK_PIPE_BROKEN 0
+#define HAVE___FREADPTRINC 0 /* FIXME this should be 1 on musl */
+#define _LARGEFILE_SOURCE
