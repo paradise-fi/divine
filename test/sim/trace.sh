4c6b8a9b39bf82acf680e3d0565dd9087e89729d
Petr Rockai <me@mornfall.net>
2018-12-16 22:17:24 +0000

    test: Roll back sim/trace.sh to its state from before LLVM 5.

--- test/sim/trace.sh
+++ test/sim/trace.sh
@@ -4,20 +4,20 @@
 sim $TESTS/lang-c/malloc.c <<EOF
 + ^# executing __boot at
 > setup --debug libc
-> trace --choices 0 0 0 0 1
+> trace --choices 0 1
 - FAULT
 + ^traced states: #1
 + ^# executing malloc at
 > stepa --count 3
 + ^# executing - at -
-> trace --choices 0 0 0 0 0
+> trace --choices 0 0
 + ^traced states: #1
 + ^# executing malloc at
 > step --out
 > step --count 2
 + ^T: FAULT: null pointer dereference
 + ^# executing.*FaultBase::handler
-> trace --choices 0 0 0 0 0 0
-+ traced states: #1 #2 #3 #4 #6
+> trace --choices 0 0 0
++ traced states: #1 #3
 + unused choices: 0/0
 EOF
