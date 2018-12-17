7a72a354b93a5215f5607b620f8480d4b2394c9d
Petr Rockai <me@mornfall.net>
2018-12-17 11:29:24 +0000

    libc: Fix/simplify fork() -- inactive tasks are no longer cloned by sys_fork().

--- dios/libc/unistd/fork.cpp
+++ dios/libc/unistd/fork.cpp
@@ -10,20 +10,9 @@ pid_t fork( void ) noexcept
     __run_atfork_handlers( 0 );
     __dios::InterruptMask mask;
 
-    auto *tasks = __dios_this_process_tasks();
     __dios_sysfork( &child_pid );
     pid_t new_pid = getpid();
     bool is_parent = old_pid == new_pid;
-
-    if ( !is_parent )
-    {
-        auto current_task = __dios_this_task();
-        int cnt = __vm_obj_size( tasks ) / sizeof( __dios_task );
-        for ( int i = 0; i < cnt; ++i )
-            if ( tasks[ i ] != current_task )
-                 __vm_obj_free( tasks[ i ] );
-    }
-    __vm_obj_free( tasks );
 
     mask.without( [=]{ __run_atfork_handlers( is_parent? 1 : 2 ); } );
     return is_parent ? child_pid : 0;
