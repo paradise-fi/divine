84a6503358f8e83279242bbd1a42151a4e51a157
Henrich Lauko <xlauko@mail.muni.cz>
2018-12-18 10:00:51 +0000

    rst: Fix abstract prototypes.

--- dios/include/rst/domains.h
+++ dios/include/rst/domains.h
@@ -9,18 +9,18 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    uint64_t __sym_val_i64();
-    uint32_t __sym_val_i32();
-    uint16_t __sym_val_i16();
-    uint8_t __sym_val_i8();
+    uint64_t __sym_val_i64( void );
+    uint32_t __sym_val_i32( void );
+    uint16_t __sym_val_i16( void );
+    uint8_t __sym_val_i8( void );
 
-    float __sym_val_float32();
-    double __sym_val_float64();
+    float __sym_val_float32( void );
+    double __sym_val_float64( void );
 
-    uint64_t __star_val_i64();
-    uint32_t __star_val_i32();
-    uint16_t __star_val_i16();
-    uint8_t __star_val_i8();
+    uint64_t __star_val_i64( void );
+    uint32_t __star_val_i32( void );
+    uint16_t __star_val_i16( void );
+    uint8_t __star_val_i8( void );
 
     char * __mstring_val( const char * buff, unsigned buff_len );
 #ifdef __cplusplus
