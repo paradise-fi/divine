419670e46a95190ddd7ea1d3b09381ef46200afa
Henrich Lauko <xlauko@mail.muni.cz>
2019-01-10 13:00:28 +0000

    rst: Rework stashing to maintain a list of stashed values.

--- dios/rst/lart.cpp
+++ dios/rst/lart.cpp
@@ -5,15 +5,31 @@
 
 #include <string.h>
 
+struct Stash {
+    Stash * next;
+    uintptr_t val;
+};
+
+static inline Stash * __stash_top() {
+    return reinterpret_cast< Stash * >( __dios_this_task()->__rst_stash );
+}
+
 extern "C"
 {
     uintptr_t __lart_unstash()
     {
-        return __dios_this_task()->__rst_stash;
+        auto stash = __stash_top();
+        __dios_this_task()->__rst_stash = reinterpret_cast< uintptr_t >( stash->next );
+        uintptr_t ret = stash->val;
+        __vm_obj_free( stash );
+        return ret;
     }
 
     void __lart_stash( uintptr_t val )
     {
-        __dios_this_task()->__rst_stash = val;
+        auto *stash = static_cast< Stash * >( __vm_obj_make( sizeof( Stash ) ) );
+        stash->next = __stash_top();
+        stash->val = val;
+        __dios_this_task()->__rst_stash = reinterpret_cast< uintptr_t >( stash );
     }
 }
