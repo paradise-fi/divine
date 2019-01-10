023e63a63d3a92511d17915d406c41fec0e14662
Henrich Lauko <xlauko@mail.muni.cz>
2019-01-10 13:13:31 +0000

    rst: Implement formula cleanup functions.

--- dios/include/rst/formula.h
+++ dios/include/rst/formula.h
@@ -3,7 +3,15 @@
 
 #if __cplusplus >= 201103L
 
+#include <util/array.hpp>
+
 namespace lart::sym {
+
+__dios::Array< Formula * > __orphan_formulae( _VM_Frame * frame ) noexcept;
+
+void __formula_cleanup( Formula * formula ) noexcept;
+
+void __cleanup_orphan_formulae( _VM_Frame * frame ) noexcept;
 
 } // namespace lart::sym
 
--- dios/rst/formula.cpp
+++ dios/rst/formula.cpp
@@ -2,7 +2,81 @@
 
 #if __cplusplus >= 201103L
 
+#include <rst/common.h>
+#include <sys/metadata.h>
+#include <sys/bitcode.h>
+
 namespace lart::sym {
+
+Formula * __peek_formula( void * addr ) {
+    struct { uint32_t off = 0, obj; } ptr;
+    ptr.obj = __vm_peek( addr, _VM_ML_User );
+
+    Formula *formula;
+    memcpy( &formula, &ptr, sizeof( Formula* ) );
+    return formula;
+}
+
+__invisible __dios::Array< Formula * > __orphan_formulae( _VM_Frame * frame ) noexcept {
+    auto *meta = __md_get_pc_meta( frame->pc );
+    auto *inst = meta->inst_table;
+    auto base = reinterpret_cast< uint8_t * >( frame );
+
+    __dios::Array< Formula * > orphans;
+    for ( int i = 0; i < meta->inst_table_size; ++i, ++inst ) {
+        if ( inst->val_width == 8 ) {
+            auto addr = *reinterpret_cast< void ** >( base + inst->val_offset );
+            auto old_flag = __vm_ctl_flag( 0, _DiOS_CF_IgnoreFault );
+
+            if ( inst->opcode == OpCode::Alloca )  {
+                // TODO fix peek of undefined value
+                if ( auto * formula = __peek_formula( addr ) ) {
+                    formula->refcount_decrement();
+                }
+            }
+
+            if ( __dios_pointer_get_type( addr ) == _VM_PT_Marked ) {
+                Formula * formula = static_cast< Formula * >( abstract::weaken( addr ) );
+                if ( formula->refcount() == 0 ) {
+                    orphans.push_back( formula );
+                }
+            }
+
+            if ( ( old_flag & _DiOS_CF_IgnoreFault ) == 0 )
+                __vm_ctl_flag( _DiOS_CF_IgnoreFault, 0 );
+        }
+    }
+
+    std::sort(orphans.begin(), orphans.end());
+    auto end = std::unique(orphans.begin(), orphans.end());
+    orphans.erase( end, orphans.end() );
+
+    return orphans;
+}
+
+__invisible void __formula_cleanup( Formula * formula ) noexcept {
+    auto clean_child = [] (auto * child) {
+        child->refcount_decrement();
+        if ( child->refcount() == 0 )
+            __formula_cleanup( child );
+    };
+
+    if ( isUnary( formula->op() ) ) {
+        clean_child( formula->unary.child );
+    }
+    else if ( isBinary( formula->op() ) ) {
+        clean_child( formula->binary.left );
+        clean_child( formula->binary.right );
+    }
+
+    __dios_safe_free( formula );
+}
+
+__invisible void __cleanup_orphan_formulae( _VM_Frame * frame ) noexcept {
+    for ( auto * ptr : __orphan_formulae( frame ) ) {
+        __formula_cleanup( ptr );
+    }
+}
 
 } // namespace lart::sym
 
