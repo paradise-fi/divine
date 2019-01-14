e99eccf8015821949987cebb02e41ba821636aa1
Henrich Lauko <xlauko@mail.muni.cz>
2019-01-14 09:39:44 +0000

    rst: Decrement refcount of formula when it is poked out.

--- dios/rst/sym.cpp
+++ dios/rst/sym.cpp
@@ -207,6 +207,12 @@ __invisible Formula *__sym_assume( Formula *value, Formula *constraint, bool ass
 }
 
 void __sym_freeze( Formula *formula, void *addr ) {
+    if ( abstract::tainted( *static_cast< char * >( addr ) ) ) {
+        if ( auto * old = peek_object< Formula >( addr ) ) {
+            old->refcount_decrement();
+        }
+    }
+
     formula->refcount_increment();
     poke_object< Formula >( formula, addr );
 }
