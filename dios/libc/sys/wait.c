1a75e650913816f82835ff2c2b2776bce89e6046
Zuzana Baranova <xbaranov@fi.muni.cz>
2019-01-15 23:32:29 +0000

    libc: Use the __libc_ version of wait4() in sys/wait.c.

--- dios/libc/sys/wait.c
+++ dios/libc/sys/wait.c
@@ -1,4 +1,5 @@
 #include <sys/wait.h>
+#include <sys/syswrap.h>
 
 pid_t wait(int *wstatus)
 {
@@ -7,10 +8,10 @@ pid_t wait(int *wstatus)
 
 pid_t waitpid( pid_t pid, int *wstatus, int options )
 {
-    return wait4( pid, wstatus, options, 0 );
+    return __libc_wait4( pid, wstatus, options, 0 );
 }
 
 pid_t wait3( int *wstatus, int options, struct rusage *rusage )
 {
-    return wait4( -1, wstatus, options, rusage );
+    return __libc_wait4( -1, wstatus, options, rusage );
 }
