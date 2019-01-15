676f61fb3d1e99170612197be501badc4d169b05
Zuzana Baranova <xbaranov@fi.muni.cz>
2019-01-15 23:35:54 +0000

    libc: Use the __libc_ version of getpid() in fork.cpp.

--- dios/libc/unistd/fork.cpp
+++ dios/libc/unistd/fork.cpp
@@ -3,15 +3,16 @@
 #include <sys/divm.h>
 #include <sys/interrupt.h>
 #include <dios.h>
+#include <sys/syswrap.h>
 
 pid_t fork( void ) noexcept
 {
-    pid_t old_pid = getpid(), child_pid;
+    pid_t old_pid = __libc_getpid(), child_pid;
     __run_atfork_handlers( 0 );
     __dios::InterruptMask mask;
 
     __dios_sysfork( &child_pid );
-    pid_t new_pid = getpid();
+    pid_t new_pid = __libc_getpid();
     bool is_parent = old_pid == new_pid;
 
     mask.without( [=]{ __run_atfork_handlers( is_parent? 1 : 2 ); } );
