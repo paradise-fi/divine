ee8993c425877dc25980d146067886712f2b5089
Petr Rockai <me@mornfall.net>
2019-01-22 10:33:33 +0000

    releng: Refuse to make a new release if nothing has changed.

--- releng/make.sh
+++ releng/make.sh
@@ -9,7 +9,14 @@ echo $patchlevel > releng/patchlevel
 version=$(cat releng/version).$patchlevel
 bash ./releng/update-version-sha.sh sha1sum . /dev/null 0 > releng/checksum
 
-darcs rec -a --author "Release Bot <divine@fi.muni.cz>" \
-          -m "releng: Update for $version" releng
-darcs tag -m $version
-darcs dist -d divine-${version}
+if darcs wh releng/checksum > /dev/null; then
+    darcs rec -a --author "Release Bot <divine@fi.muni.cz>" \
+              -m "releng: Update for $version" releng
+    darcs tag -m $version
+    darcs dist -d divine-${version}
+elif test $? = 1; then
+    echo "no changes, not making a release" 1>&2
+    exit 1
+else
+    exit 2
+fi
