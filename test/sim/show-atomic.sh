1618045278ea03fac1b909e45d95f8693e25efec
Vladimir Still <xstill@fi.muni.cz>
2019-01-29 11:21:33 +0000

    test: Add a test for showing atomic types in sim.

---
+++ test/sim/show-atomic.sh
@@ -0,0 +1,21 @@
+# TAGS: min
+. lib/testcase
+
+cat > file.cpp <<EOF
+#include <atomic>
+
+int main() {
+    std::atomic< int > x{ 42 };
+}
+EOF
+
+sim file.cpp <<EOF
++ ^# executing __boot at
+> start
+> step --over --count 2 --quiet
++ ^# executing main at
+> show .x
++ ^attributes
++ type:\s*_Atomic int
++ value:\s*[i32 42 d]
+EOF
