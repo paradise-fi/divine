9ac68ad6dbd814db86c23b40f3c41f85f9bc6cbc
Henrich Lauko <xlauko@mail.muni.cz>
2019-01-31 22:07:32 +0000

    lart: Use placeholder builder in duplication pass.

--- lart/abstract/duplicator.cpp
+++ lart/abstract/duplicator.cpp
@@ -4,71 +4,26 @@
 DIVINE_RELAX_WARNINGS
 #include <llvm/IR/Module.h>
 #include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/IR/Constants.h>
 DIVINE_UNRELAX_WARNINGS
 
 #include <lart/support/util.h>
 #include <lart/abstract/util.h>
-
-
-using namespace llvm;
-
-using lart::util::get_module;
-using lart::util::get_or_insert_function;
+#include <lart/abstract/placeholder.h>
 
 namespace lart::abstract
 {
 
-Function* placeholder( Module *m, Type *in, Type *out ) {
-    auto fty = llvm::FunctionType::get( out, { in }, false );
-    std::string name = "lart.placeholder.";
-    if ( auto s = dyn_cast< StructType >( out ) )
-        name += s->getName().str();
-    else
-        name += llvm_name( out );
-    return get_or_insert_function( m, fty, name );
-}
-
-// TODO duplicate freeze and thaws
-
 void Duplicator::run( llvm::Module &m ) {
     auto abstract = query::query( abstract_metadata( m ) )
         .map( [] ( auto mdv ) { return mdv.value(); } )
-        .map( query::llvmdyncast< Instruction > )
+        .map( query::llvmdyncast< llvm::Instruction > )
         .filter( query::notnull )
         .filter( is_duplicable )
         .freeze();
 
+    AbstractPlaceholderBuilder apb( m.getContext() );
     for ( const auto &inst : abstract )
-        process( inst );
+        apb.construct( inst );
 }
 
-void Duplicator::process( llvm::Instruction *i ) {
-    auto m = get_module( i );
-
-    auto dom = ValueMetadata( i ).domain();
-    auto type = is_base_type_in_domain( m, i, dom )
-              ? abstract_type( i->getType(), dom ) : i->getType();
-
-    auto place = isa< PHINode >( i ) ? i->getParent()->getFirstNonPHI() : i;
-
-    IRBuilder<> irb( place );
-    auto fn = placeholder( m, i->getType(), type );
-    auto ph = irb.CreateCall( fn, { i } );
-    add_abstract_metadata( ph, dom );
-
-    if ( !isa< PHINode >( i ) ) {
-        ph->removeFromParent();
-        ph->insertAfter( place );
-    }
-
-    make_duals( i, ph );
-
-    if ( !is_base_type_in_domain( m, i, dom ) ) {
-        i->replaceAllUsesWith( ph );
-        ph->setOperand( 0, i );
-    }
-}
-
-}
+} // namespace lart::abstract
