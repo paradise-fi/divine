f45ce94624783d5d4fad4cebaf6ad5c65bf6769e
Petr Rockai <me@mornfall.net>
2019-02-02 15:57:31 +0000

    test: Remove 'todo' from vm/nonstandard-int.cpp as it works now.

--- test/vm/nonstandard-int.cpp
+++ test/vm/nonstandard-int.cpp
@@ -1,22 +1,26 @@
-/* TAGS: todo min c++ */
+/* TAGS: min c++ */
 // CC_OPTS: -Oz
 #include <sys/divm.h>
 
-// Constructor of this struct produces in Clang 3.7 with Oz 48bit integer
-struct Evil {
+// Constructor of this struct produces a 48b integer in Clang 3.7-6.0 (with -Oz)
+
+struct Evil
+{
     bool a, b, c, d, e, f;
     Evil() :
         a( false ), b( false ), c( false ), d( false ), e( false ), f( false ) {}
 };
 
-Evil evilFunc() {
+Evil evilFunc()
+{
     Evil evil;
     if ( __vm_choose( 2 ) )
         evil.a = true;
     return evil;
 }
 
-int main() {
+int main()
+{
     Evil evil = evilFunc();
     return 0;
 }
