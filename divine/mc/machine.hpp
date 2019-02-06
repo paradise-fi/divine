b65327a1094b76e2ddd286088268f8ca6bd79cbc
Petr Rockai <me@mornfall.net>
2019-02-06 09:18:24 +0000

    MC: Add a first version of Weaver-compatible state-space exploration machines.

---
+++ divine/mc/machine.hpp
@@ -0,0 +1,315 @@
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+
+/*
+ * (c) 2016-2019 Petr Ročkai <code@fixp.eu>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#pragma once
+
+#include <divine/mc/bitcode.hpp>
+#include <divine/mc/context.hpp>
+#include <divine/mc/hasher.hpp>
+#include <divine/mc/search.hpp>
+#include <divine/smt/solver.hpp>
+
+#include <divine/vm/value.hpp>
+#include <divine/vm/memory.tpp>
+#include <divine/vm/setup.hpp>
+#include <divine/vm/eval.tpp> // FIXME
+
+#include <set>
+#include <memory>
+
+namespace divine::mc
+{
+
+    using namespace std::literals;
+    namespace hashset = brick::hashset;
+    using BC = std::shared_ptr< BitCode >;
+
+    struct State
+    {
+        Snapshot snap;
+        explicit State( Snapshot s = Snapshot() ) : snap( s ) {}
+        bool operator==( const State& o ) const { return snap.intptr() == o.snap.intptr(); }
+    };
+
+    struct Label
+    {
+        bool accepting:1;
+        bool error:1;
+    };
+
+    struct TracingLabel : Label, brick::types::Ord
+    {
+        std::vector< std::string > trace;
+        std::vector< vm::Choice > choices;
+        std::vector< vm::Interrupt > interrupts;
+        auto as_tuple() const
+        {
+            /* skip the text trace for comparison purposes */
+            return std::make_tuple( choices, interrupts, accepting, error );
+        }
+    };
+
+    struct MContext : vm::Context< vm::Program, vm::CowHeap >
+    {
+        int _choice_take, _choice_count;
+        vm::GenericPointer _assume;
+
+        template< typename I >
+        int choose( int count, I, I )
+        {
+            if ( _choice_count )
+                ASSERT_LT( _choice_take, _choice_count );
+            _choice_count = count;
+            return _choice_take;
+        }
+
+        using vm::Context< vm::Program, vm::CowHeap >::Context;
+    };
+
+    using StateTQ = GraphTQ< State, Label >;
+
+    struct Task
+    {
+        using GraphTs = StateTQ::Tasks;
+        using Boot = GraphTs::Start;
+        using Schedule = GraphTs::Expand;
+
+        struct Compute
+        {
+            Snapshot snap, origin;
+            vm::State state;
+            Compute( Snapshot snap, Snapshot orig, vm::State state )
+                : snap( snap ), origin( orig ), state( state )
+            {}
+        };
+
+        struct Choose : Compute
+        {
+            int choice, total;
+            Choose( Snapshot snap, Snapshot orig, vm::State state, int c, int t )
+                : Compute( snap, orig, state ), choice( c ), total( t )
+            {}
+        };
+
+        using TQ = StateTQ::Extend< Task, Choose >;
+        using Weaver = mc::Weaver< TQ >;
+    };
+
+    using TQ = Task::TQ;
+}
+
+namespace divine::mc::machine
+{
+    template< typename Solver >
+    struct Tree : TQ::Skel
+    {
+        using Hasher = mc::Hasher< Solver >;
+        using PointerV = vm::value::Pointer;
+        using Context = MContext;
+        using Eval = vm::Eval< Context >;
+        using Env = std::vector< std::string >;
+        using Pool = vm::CowHeap::Pool;
+
+        auto &program() { return _bc->program(); }
+        auto &debug() { return _bc->debug(); }
+        auto &heap() { return context().heap(); }
+        auto &pool() { return _pool; }
+        Context &context() { return _ctx; }
+
+        Solver _solver;
+        BC _bc;
+        Context _ctx;
+        Pool _pool;
+        std::shared_ptr< std::atomic< int64_t > > _total_instructions;
+
+        void update_instructions()
+        {
+            *_total_instructions += context().instruction_count();
+            context().instruction_count( 0 );
+        }
+
+        Tree( BC bc )
+            : Tree( bc, Context( bc->program() ) )
+        {}
+
+        Tree( BC bc, const Context &ctx )
+            : _bc( bc ), _ctx( ctx ), _total_instructions( new std::atomic< int64_t >( 0 ) )
+        {}
+
+        ~Tree() { update_instructions(); }
+
+        void choose( TQ &tq, Snapshot origin )
+        {
+            auto snap = context().snapshot( _pool );
+            auto state = context()._state;
+
+            Eval eval( context() );
+            context()._choice_take = context()._choice_count = 0;
+            eval.refresh();
+            eval.dispatch();
+
+            /* queue up all choices other than 0 */
+            for ( int i = 1; i < context()._choice_count; ++i )
+                tq.add< Task::Choose >( snap, origin, state, i, context()._choice_count );
+
+            /* proceed with choice = 0 */
+            compute( tq, origin );
+        }
+
+        virtual std::pair< State, bool > store()
+        {
+            return { State( context().snapshot( _pool ) ), true };
+        }
+
+        void edge( TQ &tq, Snapshot origin )
+        {
+            Label lbl;
+            lbl.accepting = context().flags_any( _VM_CF_Accepting );
+            lbl.error = context().flags_any( _VM_CF_Error );
+            auto [ state, isnew ] = store();
+            tq.add< StateTQ::Skel::Edge >( State( origin ), state, lbl, isnew );
+        }
+
+        bool feasible()
+        {
+            if ( context().flags_any( _VM_CF_Cancel ) )
+                return false;
+            if ( context()._assume.null() )
+                return true;
+
+            // TODO validate the path condition
+            return _solver.feasible( context().heap(), context()._assume );
+        }
+
+        void compute( TQ &tq, Snapshot origin, bool cont = false )
+        {
+            Eval eval( context() );
+            bool choice = eval.run_seq( cont );
+
+            if ( !feasible() )
+                return;
+
+            if ( choice )
+                choose( tq, origin );
+            else
+                edge( tq, origin );
+        }
+
+        virtual void boot( TQ &tq )
+        {
+            Eval eval( context() );
+            vm::setup::boot( context() );
+            eval.run();
+        }
+
+        using TQ::Skel::run;
+
+        void run( TQ &tq, Task::Boot )
+        {
+            boot( tq );
+
+            if ( vm::setup::postboot_check( context() ) )
+                tq.add< Task::Schedule >( store().first );
+        }
+
+        void run( TQ &tq, Task::Schedule e )
+        {
+            context().load( _pool, e.from.snap );
+            vm::setup::scheduler( context() );
+            compute( tq, e.from.snap, false );
+        }
+
+        void run( TQ &tq, Task::Choose c )
+        {
+            context().load( _pool, c.snap );
+            context()._state = c.state;
+            context()._choice_take = c.choice;
+            context()._choice_count = c.total;
+            context().flush_ptr2i();
+            context().load_pc();
+            compute( tq, c.origin, true );
+        }
+
+        auto make_hasher()
+        {
+            Hasher h;
+            h.setup( _pool, context().heap(), this->_solver );
+            h._root = context().state_ptr();
+            ASSERT( !h._root.null() );
+            return h;
+        }
+
+        bool equal( Snapshot a, Snapshot b )
+        {
+            return make_hasher().equal( a, b );
+        }
+    };
+
+    template< typename Solver >
+    struct Graph : Tree< Solver >
+    {
+        using Hasher = mc::Hasher< Solver >;
+        using HT = hashset::Concurrent< Snapshot, Hasher >;
+        using Tree< Solver >::context;
+
+        Graph( BC bc ) : Tree< Solver >( bc )
+        {
+            this->hasher().setup( this->_pool, context().heap(), this->_solver );
+        }
+
+        struct Ext
+        {
+            HT states;
+            bool overwrite = false;
+
+            Ext() : Ext( HT( Hasher(), 1024 ) ) {}
+            Ext( HT states ) : states( states ) {}
+        } _ext;
+
+        auto &hasher() { return _ext.states.hasher; }
+        void enable_overwrite() { _ext.overwrite = true; }
+        bool equal( Snapshot a, Snapshot b ) { return hasher().equal( a, b ); }
+
+        virtual std::pair< State, bool > store() override
+        {
+            auto snap = context().snapshot( this->_pool );
+            auto r = _ext.states.insert( snap, _ext.overwrite );
+            if ( *r != snap )
+            {
+                ASSERT( !_ext.overwrite );
+                this->pool().free( snap ), context().load( this->_pool, *r );
+            }
+            else
+                context().flush_ptr2i();
+            return { State( *r ), r.isnew() };
+        }
+
+        virtual void boot( TQ &tq ) override
+        {
+            Tree< Solver >::boot( tq );
+            hasher()._root = context().state_ptr();
+        }
+
+    };
+}
+
+namespace divine::mc
+{
+    using TMachine = machine::Tree< smt::NoSolver >;
+    using GMachine = machine::Graph< smt::NoSolver >;
+}
---
+++ divine/mc/t-machine.hpp
@@ -0,0 +1,174 @@
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+
+/*
+ * (c) 2016-2018 Petr Ročkai <code@fixp.eu>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <divine/mc/machine.hpp>
+
+namespace divine::t_mc
+{
+
+    using namespace std::literals;
+
+    namespace
+    {
+
+        auto prog( std::string p )
+        {
+            return t_vm::c2bc(
+                "void *__vm_obj_make( int, int );"s +
+                "void *__vm_ctl_get( int );"s +
+                "void __vm_ctl_set( int, void * );"s +
+                "long __vm_ctl_flag( long, long );"s +
+                "void __vm_trace( int, const char * );"s +
+                "int __vm_choose( int, ... );"s +
+                "void __boot( void * );"s + p );
+        }
+
+        auto prog_int( std::string first, std::string next )
+        {
+            std::stringstream p;
+            p << "void __sched() {" << std::endl
+              << "    int *r = __vm_ctl_get( " << _VM_CR_State
+              << " ); __vm_trace( 0, \"foo\" );" << std::endl
+              << "    *r = " << next << ";" << std::endl
+              << "    if ( *r < 0 ) __vm_ctl_flag( 0, 0b10000 );" << std::endl
+              << "    __vm_ctl_set( " << _VM_CR_Frame << ", 0 );" << std::endl
+              << "}" << std::endl
+              << "void __boot( void *environ ) {"
+              << "    __vm_ctl_set( " << _VM_CR_Scheduler << ", __sched );"
+              << "    void *e = __vm_obj_make( sizeof( int ), " << _VM_PT_Heap << " );"
+              << "    __vm_ctl_set( " << _VM_CR_State << ", e );"
+              << "    int *r = e; *r = " << first << ";"
+              << "    __vm_ctl_set( " << _VM_CR_Frame << ", 0 ); }" << std::endl;
+            return prog( p.str() );
+        }
+
+    }
+
+    struct Machine
+    {
+        TEST( instance )
+        {
+            auto bc = prog( "void __boot( void *e ) { __vm_ctl_flag( 0, 0b10000 ); }" );
+            mc::TMachine m( bc );
+        }
+
+        using Search = mc::Search< mc::TQ >;
+        using Expand = mc::TQ::Tasks::Expand;
+        using Edge = mc::TQ::Tasks::Edge;
+
+        template< typename M >
+        auto weave( M &machine )
+        {
+            return mc::Task::Weaver().extend_ref( machine );
+        }
+
+        TEST( simple )
+        {
+            bool found = false;
+            auto state = [&]( Expand ) { found = true; };
+            auto machine = mc::TMachine( prog_int( "4", "*r - 1" ) );
+            weave( machine ).observe( state ).start();
+            ASSERT( found );
+        }
+
+        TEST( hasher )
+        {
+            bool ran = false;
+            auto machine = mc::GMachine( prog_int( "4", "*r - 1" ) );
+            auto check = [&]( Expand s )
+            {
+                ASSERT( machine.hasher()._pool->size( s.from.snap ) );
+                ran = true;
+            };
+
+            weave( machine ).observe( check ).start();
+            ASSERT( ran );
+        }
+
+        TEST( start_twice )
+        {
+            auto machine = mc::TMachine( prog_int( "4", "*r - 1" ) );
+            mc::State i1, i2;
+
+            weave( machine ).observe( [&]( Expand e ) { i1 = e.from; } ).start();
+            weave( machine ).observe( [&]( Expand e ) { i2 = e.from; } ).start();
+
+            ASSERT( machine.equal( i1.snap, i2.snap ) );
+        }
+
+        TEST( copy )
+        {
+            mc::State i1, i2;
+
+            auto m1 = mc::TMachine( prog_int( "4", "*r - 1" ) ), m2 = m1;
+            weave( m1 ).observe( [&]( Expand e ) { i1 = e.from; } ).start();
+            weave( m2 ).observe( [&]( Expand e ) { i2 = e.from; } ).start();
+
+            ASSERT( m1.equal( i1.snap, i2.snap ) );
+            ASSERT( m2.equal( i1.snap, i2.snap ) );
+        }
+
+        TEST( unfold )
+        {
+            std::vector< mc::Snapshot > snaps;
+            auto machine = mc::TMachine( prog_int( "4", "*r - 1" ) );
+
+            auto state = [&]( Expand e )
+            {
+                snaps.push_back( e.from.snap );
+                if ( snaps.size() == 2 )
+                    ASSERT( !machine.equal( snaps[ 0 ], snaps[ 1 ] ) );
+            };
+
+            weave( machine ).extend( Search() ).observe( state ).start();
+            ASSERT_LEQ( 2, snaps.size() );
+        }
+
+        template< typename M >
+        void _search( std::shared_ptr< mc::BitCode > bc, int sc, int ec )
+        {
+            M machine( bc );
+            int edgecount = 0, statecount = 0;
+            auto edge = [&]( Edge ) { ++edgecount; };
+            auto state = [&]( Expand ) { ++statecount; };
+
+            weave( machine ).extend( Search() ).observe( edge, state ).start();
+
+            ASSERT_EQ( statecount, sc );
+            ASSERT_EQ( edgecount, ec );
+        }
+
+        TEST( search )
+        {
+            _search< mc::TMachine >( prog_int( "4", "*r - 1" ), 5, 4 );
+            _search< mc::GMachine >( prog_int( "4", "*r - 1" ), 5, 4 );
+            _search< mc::GMachine >( prog_int( "0", "( *r + 1 ) % 5" ), 5, 5 );
+        }
+
+        TEST( branching )
+        {
+            _search< mc::GMachine >( prog_int( "4", "*r - __vm_choose( 2 )" ), 5, 9 );
+            _search< mc::GMachine >( prog_int( "2", "*r - 1 - __vm_choose( 2 )" ), 3, 3 );
+            _search< mc::TMachine >( prog_int( "4", "*r - 1 - __vm_choose( 2 )" ), 12, 11 );
+            _search< mc::GMachine >( prog_int( "4", "*r - 1 - __vm_choose( 2 )" ), 5, 7 );
+            _search< mc::GMachine >( prog_int( "0", "( *r + __vm_choose( 2 ) ) % 5" ), 5, 10 );
+            _search< mc::GMachine >( prog_int( "0", "( *r + 1 + __vm_choose( 2 ) ) % 5" ), 5, 10 );
+        }
+    };
+
+}
