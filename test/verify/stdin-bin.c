4100dca60853b9958f8609bc240f8e2a963203a4
Petr Rockai <me@mornfall.net>
2019-02-07 16:07:31 +0000

    test: Add a couple of testcases for --stdin with binary data.

---
+++ test/verify/stdin-bin.c
@@ -0,0 +1,13 @@
+/* TAGS: min */
+/* VERIFY_OPTS: --stdin `dirname $1`/stdin.bin -o nofail:malloc */
+#include <assert.h>
+#include <unistd.h>
+
+int main()
+{
+    char buf[4];
+    char want[] = { 0xde, 0xad, 0xbe, 0xef };
+    assert( read( 0, &buf, 4 ) == 4 );
+    assert( !memcmp( buf, want, 4 ) );
+    assert( read( 0, &buf, 4 ) == 0 );
+}
