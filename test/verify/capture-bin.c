0a87615fd6626ab63a6812dd7da5d1748d0960e5
Petr Rockai <me@mornfall.net>
2019-02-07 21:11:44 +0000

    test: Make sure that it is possible to actually read captured files.

---
+++ test/verify/capture-bin.c
@@ -0,0 +1,15 @@
+/* TAGS: min */
+/* VERIFY_OPTS: --capture `dirname $1`/stdin.bin:nofollow:/file -o nofail:malloc */
+#include <assert.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main()
+{
+    int fd = open( "/file", O_RDONLY );
+    char buf[4];
+    char want[] = { 0xde, 0xad, 0xbe, 0xef };
+    assert( read( fd, &buf, 4 ) == 4 );
+    assert( !memcmp( buf, want, 4 ) );
+    assert( read( fd, &buf, 4 ) == 0 );
+}
