8b3ecf5539aacd8de25381368a35896054d45e53
Petr Rockai <me@mornfall.net>
2019-02-07 21:04:56 +0000

    test: Check that we can configure, build and load gzip into divine.

---
+++ test/divcc/gzip-O2.sh
@@ -0,0 +1,11 @@
+# TAGS: ext todo
+. lib/testcase
+
+unxz --version || skip
+unxz < $SRC_ROOT/test/divcc/gzip-1.8.tar.xz | tar x
+cd gzip-1.8
+./configure CC=divcc CFLAGS=-O2
+make
+echo hello world | ./gzip - > hello.gz
+divine check --stdin hello.gz ./gzip -f -d -
+grep 'error found: no' gzip.report
