db3aaa101eeebcee31aade509a785cb90a6a2ecc
Petr Rockai <me@mornfall.net>
2019-02-10 17:02:34 +0000

    libc: Use the new _VM_T_Fault mechanism for tracing errors in __dios_fault.

--- dios/libc/sys/fault.c
+++ dios/libc/sys/fault.c
@@ -8,7 +8,7 @@ uint8_t __dios_simfail_flags;
 void __dios_fault( int f, const char *msg )
 {
     if( msg )
-        __dios_trace_f( "FAULT: %s", msg );
+        __vm_trace( _VM_T_Fault, msg );
     __vm_fault_t fh = ( __vm_fault_t ) __vm_ctl_get( _VM_CR_FaultHandler );
     struct _VM_Frame *frame = __dios_this_frame();
     ( *fh )( f, frame->parent, frame->parent->pc );
