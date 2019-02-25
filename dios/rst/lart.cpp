436af16ed620154eb93d67e1f4d12a4188d57924
Henrich Lauko <xlauko@mail.muni.cz>
2019-02-25 12:05:34 +0000

    rst: Get rid of a stash linked-list.

--- dios/rst/lart.cpp
+++ dios/rst/lart.cpp
@@ -5,31 +5,15 @@
 
 #include <string.h>
 
-struct Stash {
-    Stash * next;
-    uintptr_t val;
-};
-
-static inline Stash * __stash_top() {
-    return reinterpret_cast< Stash * >( __dios_this_task()->__rst_stash );
-}
-
 extern "C"
 {
-    uintptr_t __lart_unstash()
+    __invisible uintptr_t __lart_unstash()
     {
-        auto stash = __stash_top();
-        __dios_this_task()->__rst_stash = reinterpret_cast< uintptr_t >( stash->next );
-        uintptr_t ret = stash->val;
-        __vm_obj_free( stash );
-        return ret;
+        return __dios_this_task()->__rst_stash;
     }
 
-    void __lart_stash( uintptr_t val )
+    __invisible void __lart_stash( uintptr_t val )
     {
-        auto *stash = static_cast< Stash * >( __vm_obj_make( sizeof( Stash ), _VM_PT_Heap ) );
-        stash->next = __stash_top();
-        stash->val = val;
-        __dios_this_task()->__rst_stash = reinterpret_cast< uintptr_t >( stash );
+        __dios_this_task()->__rst_stash = val;
     }
 }
