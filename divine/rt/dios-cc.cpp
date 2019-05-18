5b92c77be682234f79107b008e06f513f9148870
Petr Rockai <me@mornfall.net>
2019-05-18 20:20:04 +0000

    RT: Link in the DiOS platform lib (dios_divm) in DiosCC.

--- divine/rt/dios-cc.cpp
+++ divine/rt/dios-cc.cpp
@@ -15,6 +15,7 @@ void DiosCC::link_dios_config( std::string n )
 {
     auto mod = load_object( "config/" + n );
     link( std::move( mod ) );
+    linkLib( "dios_divm" );
 }
 
 DiosCC::DiosCC( Options opts, std::shared_ptr< llvm::LLVMContext > ctx ) :
