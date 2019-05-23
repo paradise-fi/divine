e66efff7b586629f36d726d17e4eba4492712b30
Petr Rockai <code@fixp.eu>
2019-05-23 10:44:26 +0000

    dios: Document the BaseContext and the syscall mechanism.

--- dios/sys/syscall.hpp
+++ dios/sys/syscall.hpp
@@ -34,11 +34,29 @@ struct Setup : SetupBase
     Setup( const SetupBase &s ) : SetupBase( s ) {}
 };
 
+/* Trap into the kernel and get back out. Each system call gets a separate trap
+ * function, and those are part of the configuration. They are instantiated in
+ * `dios/config/common.hpp`. DiVM enforces that _VM_CF_Kernel mode is only set
+ * by functions annotated with `divine.trap` (available as `__trapfn`, defined
+ * in `dios/include/sys/cdefs.h`). */
+
+/* Each of the trap functions is expected to locally instantiate the Trap
+ * class, which takes care of the technicalities of the system call protocol
+ * (using the RAII idiom). We force the compiler to inline both the ctor and
+ * the dtor, because we rely on the exact stack layout and also on the __trapfn
+ * annotations which allow us to switch into kernel mode. */
+
+/* Other than that, syscalls are normal function calls (and they are performed
+ * using the `call` LLVM instruction). */
+
 struct Trap
 {
     enum RM { CONTINUE, RESCHEDULE, TRAMPOLINE } retmode;
     uint64_t keepflags;
     static const uint64_t kern = _VM_CF_KernelMode | _VM_CF_IgnoreLoop | _VM_CF_IgnoreCrit;
+
+    /* Enter the kernel. Nothing too interesting, but if reschedule might
+     * happen, we need to sync the _frame pointer of the active task now. */
 
     __inline Trap( RM rm ) : retmode( rm )
     {
@@ -48,6 +66,13 @@ struct Trap
             return;
         __dios_sync_this_frame();
     }
+
+    /* Leave the kernel. This is a little more complicated in case the syscall
+     * needs to return into userspace via a trampoline. The syscall handler
+     * creates the trampoline frame on top of the userspace portion of the
+     * stack. We must transfer control to the trampoline in a somewhat
+     * convoluted way, because it's not executing a call instruction and hence
+     * we cannot `ret` into it. */
 
     __inline ~Trap()
     {
@@ -66,6 +91,11 @@ struct Trap
         }
     }
 };
+
+/* This is the bottom of the configuration stack. We do 3 things here:
+ *  - if a syscall falls through the entire stack and hits the bottom, raise an error
+ *  - if any options are left unprocessed during setup(), raise an error
+ *  - provide the interface for the Upcall component. */
 
 struct BaseContext : KObject
 {
@@ -89,6 +119,7 @@ struct BaseContext : KObject
         __dios_fault( _DiOS_F_Config, "Unused options" );
     }
 
+    /* The interface to the `Upcall` component. */
     virtual void reschedule() = 0;
     virtual Process *make_process( Process * ) = 0;
 
@@ -96,6 +127,9 @@ struct BaseContext : KObject
     Process *findProcess( pid_t ) { return nullptr; }
 
     void getHelp( ArrayMap< std::string_view, HelpOption >& ) {}
+
+    /* Instantiate a method for each system call known to us. Implementations
+     * in `syscall.cpp`. */
 
     #include <dios/macro/no_memory_tags>
     #define SYSCALL( name, schedule, ret, arg ) ret name arg;
