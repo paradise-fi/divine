83f80df41a0f6f293e1c2ee85ef2cc2b9bbb7395
Henrich Lauko <xlauko@mail.muni.cz>
2019-06-10 14:04:55 +0000

    rst: Add C interface to tristate domain.

--- dios/rst/tristate.cpp
+++ dios/rst/tristate.cpp
@@ -1,10 +1,19 @@
 #include <rst/tristate.hpp>
 #include <rst/common.h>
 
+using __tristate = __dios::rst::abstract::Tristate;
+
+extern "C" {
+    _LART_INTERFACE
+    bool __lower_tristate( __tristate tr ) { return tr.lower(); }
+}
+
 namespace __dios::rst::abstract {
+
     bool Tristate::lower() const noexcept {
-        return static_cast<bool>( *this );
+        return static_cast< bool >( *this );
     }
-} // namespace abstract
+
+} // namespace __dios::rst::abstract
 
 
