d4503e442016e4fde5c917b4c833ee090452acfe
Zuzana Baranova <xbaranov@fi.muni.cz>
2019-06-23 21:38:06 +0000

    CC: Add the __gnuc_va_list typedef to stdarg.h.

--- divine/cc/stdarg.h
+++ divine/cc/stdarg.h
@@ -22,6 +22,7 @@ extern "C" {
 #endif
 
 typedef __builtin_va_list va_list;
+typedef __builtin_va_list __gnuc_va_list;
 
   // this will be replaced by the va_arg instruction using LART
 #ifdef __cplusplus
