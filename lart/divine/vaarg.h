9c62c0dcc9335dd3eeeca02c6a1840e241923889
Petr Rockai <me@mornfall.net>
2019-06-28 15:24:58 +0000

    lart: Deal with va_arg calls that are split across a conditional branch.

--- lart/divine/vaarg.h
+++ lart/divine/vaarg.h
@@ -29,13 +29,54 @@ struct VaArgInstr
         return passMeta< VaArgInstr >( "VaArgInstr", "" );
     }
 
-    void run( llvm::Module &m ) {
+    template< typename O >
+    void handle_user( llvm::Module &m, llvm::Value *valist, llvm::Value *user, O to_drop )
+    {
+        auto *load = llvm::dyn_cast< llvm::LoadInst >( user );
+
+        if ( auto *bitcast = llvm::dyn_cast< llvm::BitCastInst >( user ) )
+        {
+            if ( !bitcast->hasNUses( 1 ) )
+                UNREACHABLE( "va_arg bitcast has too many uses:", bitcast );
+            load = llvm::dyn_cast< llvm::LoadInst >( *bitcast->user_begin() );
+            *to_drop ++ = bitcast;
+        }
+
+        if ( !load )
+            UNREACHABLE( "could not find the load corresponding to a va_arg use",*user);
+
+        if ( load->getType() == llvm::Type::getX86_FP80Ty( m.getContext() ) )
+        {
+            auto i8ptr = llvm::Type::getInt8Ty( m.getContext() )->getPointerTo();
+            auto i32  = llvm::Type::getInt32Ty( m.getContext() );
+            auto voidT = llvm::Type::getVoidTy( m.getContext() );
+            auto faultfn_type = llvm::FunctionType::get( voidT, { i32, i8ptr }, false );
+            auto faultfn = m.getOrInsertFunction( "__dios_fault", faultfn_type );
+
+            llvm::IRBuilder irb( load );
+            auto mkstr = [&]( auto s )
+            {
+                return llvm::ConstantDataArray::getString( m.getContext(), s );
+            };
+            auto str = mkstr( "va_arg for long double is not implemented" );
+            auto var_ = m.getOrInsertGlobal( "lart.vaarg.fp80.na", str->getType() );
+            auto var = llvm::cast< llvm::GlobalVariable >( var_ );
+            auto msg = llvm::ConstantExpr::getPointerCast( var, i8ptr );
+            auto id = llvm::ConstantInt::get( i32, _VM_F_NotImplemented );
+
+            var->setInitializer( str );
+            var->setConstant( true );
+            irb.CreateCall( faultfn, { id, msg } );
+            load->replaceAllUsesWith( llvm::UndefValue::get( load->getType() ) );
+            load->eraseFromParent();
+        }
+        else
+            llvm::ReplaceInstWithInst( load, new llvm::VAArgInst( valist, load->getType() ) );
+    }
+
+    void run( llvm::Module &m )
+    {
         auto *vaargfn = m.getFunction( "__lart_llvm_va_arg" );
-        auto i8ptr = llvm::Type::getInt8Ty( m.getContext() )->getPointerTo();
-        auto i32  = llvm::Type::getInt32Ty( m.getContext() );
-        auto voidT = llvm::Type::getVoidTy( m.getContext() );
-        auto faultfn_type = llvm::FunctionType::get( voidT, { i32, i8ptr }, false );
-        auto faultfn = m.getOrInsertFunction( "__dios_fault", faultfn_type );
         if ( !vaargfn )
             return;
         if ( vaargfn->hasAddressTaken() )
@@ -49,54 +90,23 @@ struct VaArgInstr
                             .map( query::llvmcast< llvm::CallInst > )
                             .freeze() /* avoid messing with iterators when we modify BBs */ )
         {
-            if ( call->hasNUses( 0 ) ) {
+            if ( call->hasNUses( 0 ) )
+            {
                 call->eraseFromParent();
                 continue;
             }
 
-            if ( call->hasNUsesOrMore( 2 ) )
-                UNREACHABLE( "call to __lart_llvm_va_arg must have at most one use:", call );
-
-            brick::data::SmallVector< llvm::Instruction * > toDrop{ call };
             auto *valist = call->getArgOperand( 0 );
-            auto *user = *call->user_begin();
-            auto *load = llvm::dyn_cast< llvm::LoadInst >( user );
-            if ( auto *bitcast = llvm::dyn_cast< llvm::BitCastInst >( user ) ) {
-                if ( !bitcast->hasNUses( 1 ) )
-                    UNREACHABLE( "va_arg bitcast has too many uses:", bitcast );
-                load = llvm::dyn_cast< llvm::LoadInst >( *bitcast->user_begin() );
-                toDrop.push_back( bitcast );
-            }
-            if ( !load )
-                UNREACHABLE( "could not find load corresponding to va_arg call", call );
+            brick::data::SmallVector< llvm::Instruction * > to_drop{ call };
 
-            for ( auto x : toDrop )
+            for ( auto user : call->users() )
+                handle_user( m, valist, user, std::back_inserter( to_drop ) );
+
+            for ( auto x : to_drop )
             {
                 x->replaceAllUsesWith( llvm::UndefValue::get( x->getType() ) );
                 x->eraseFromParent();
             }
-
-            if ( load->getType() == llvm::Type::getX86_FP80Ty( m.getContext() ) )
-            {
-                llvm::IRBuilder irb( load );
-                auto mkstr = [&]( auto s )
-                {
-                    return llvm::ConstantDataArray::getString( m.getContext(), s );
-                };
-                auto str = mkstr( "va_arg for long double is not implemented" );
-                auto var_ = m.getOrInsertGlobal( "lart.vaarg.fp80.na", str->getType() );
-                auto var = llvm::cast< llvm::GlobalVariable >( var_ );
-                auto msg = llvm::ConstantExpr::getPointerCast( var, i8ptr );
-                auto id = llvm::ConstantInt::get( i32, _VM_F_NotImplemented );
-
-                var->setInitializer( str );
-                var->setConstant( true );
-                irb.CreateCall( faultfn, { id, msg } );
-                load->replaceAllUsesWith( llvm::UndefValue::get( load->getType() ) );
-                load->eraseFromParent();
-            }
-            else
-                llvm::ReplaceInstWithInst( load, new llvm::VAArgInst( valist, load->getType() ) );
         }
 
         vaargfn->eraseFromParent();
