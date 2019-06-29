9f9cae8b9bb593a58d413d269908fab43c7b0fc3
Zuzana Baranova <xbaranov@fi.muni.cz>
2019-06-29 11:06:21 +0000

    test: Check for both lld and ld error messages with nonexistent library.

--- test/dioscc/nonexistent-lib.sh
+++ test/dioscc/nonexistent-lib.sh
@@ -8,7 +8,8 @@ EOF
 
 not dioscc prog.c -lxxx 2>&1 | tee file.txt
 
-grep "unable to find library -lxxx" file.txt
-grep "lld failed, not linked" file.txt
+# lld | ld  error message
+egrep "unable to find library -lxxx|cannot find -lxxx" file.txt
+egrep "lld failed, not linked|failed to link, ld exited with exitcode" file.txt
 
 test ! -s a.out
