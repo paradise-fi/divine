3a25b38793d20a760b67f9ed1f448f808a92b5b7
Petr Rockai <code@fixp.eu>
2019-07-10 07:14:50 +0000

    dios: Fix a typo in a comment.

--- dios/sys/options.hpp
+++ dios/sys/options.hpp
@@ -34,7 +34,7 @@
  *  - arg.[0-9]+: the argv[] to be passed to main() of pid 1
  *  - env.[0-9]+: the envp[] (and __environ) passed to pid 1
  *  - sys.[0-9]+: the options intended for the kernel (-o xxx in DIVINE)
- *  - vfs.*: the filesystem snapshot (details in `dios/vfs/base.cp`)
+ *  - vfs.*: the filesystem snapshot (details in `dios/vfs/base.cpp`)
  *  - `divine.bcname`: argv[0] (TODO rename this key or merge into arg.*) */
 
 namespace __dios
