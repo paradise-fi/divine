5cee472b73ec6759f4e116b49be1208631c95407
Henrich Lauko <xlauko@mail.muni.cz>
2019-07-19 04:11:20 +0000

    lart: Erase lart helper intrinsics after successful abstraction.

--- lart/abstract/cleanup.cpp
+++ lart/abstract/cleanup.cpp
@@ -2,6 +2,7 @@
 #include <lart/abstract/cleanup.h>
 
 #include <lart/abstract/domain.h>
+#include <lart/abstract/stash.h> // unpacked_argument
 
 #include <lart/support/cleanup.h>
 #include <lart/support/query.h>
@@ -17,6 +18,23 @@ void Cleanup::run( llvm::Module &m ) {
     for ( auto i : insts ) {
         i->dropUnknownNonDebugMetadata();
     }
+
+    // erase argument matching intrinsics
+    auto argintrs = unpacked_arguments( &m );
+    auto intrs = query::query( argintrs )
+        .map( query::llvmdyncast< llvm::CallInst > )
+        .map( [] ( auto call ) {
+            return call->getCalledFunction();
+        } )
+        .freeze();
+
+    std::set< llvm::Function * > unique( intrs.begin(), intrs.end() );
+
+    for ( auto i : argintrs )
+        i->eraseFromParent();
+
+    for ( auto u : unique )
+        u->eraseFromParent();
 }
 
 } // namespace abstract
