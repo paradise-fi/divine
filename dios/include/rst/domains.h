e5d0ba95de8685d3d29a441e65c5cbeb7056ce99
Henrich Lauko <xlauko@mail.muni.cz>
2019-08-08 13:23:06 +0000

    rst: Add missing constructors of abstract values.

--- dios/include/rst/domains.h
+++ dios/include/rst/domains.h
@@ -9,6 +9,16 @@ extern "C" {
     uint32_t __sym_val_i32( void );
     uint16_t __sym_val_i16( void );
     uint8_t __sym_val_i8( void );
+
+    float __sym_val_float32( void );
+    double __sym_val_float64( void );
+
+    uint64_t __star_val_i64( void );
+    uint32_t __star_val_i32( void );
+    uint16_t __star_val_i16( void );
+    uint8_t __star_val_i8( void );
+
+    char * __mstring_val( char * buff, unsigned buff_len );
 #ifdef __cplusplus
 }
 #endif
