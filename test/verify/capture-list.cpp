4802285a6eda34ffac99f67d68b419218502b3e9
Petr Rockai <me@mornfall.net>
2019-08-21 22:20:19 +0000

    test: Check that user-level traversal of a captured filesystem works.

---
+++ test/verify/capture-list.cpp
@@ -0,0 +1,45 @@
+/* TAGS: posix c++ */
+/* VERIFY_OPTS: --capture $(dirname $1)/subdir:follow:/ -o nofail:malloc */
+#include <dirent.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <set>
+#include <cassert>
+
+using namespace std::literals;
+
+std::set< std::string > paths;
+
+void list( std::string entry, std::string name, std::string indent )
+{
+    assert( paths.count( entry ) == 0 );
+    paths.insert( entry );
+
+    DIR *d = opendir( entry.c_str() );
+    if ( d )
+    {
+        struct dirent *dir;
+        indent += " ";
+        while ( (dir = readdir( d )) != nullptr )
+        {
+            if ( dir->d_name == "."sv || dir->d_name == ".."sv )
+                continue;
+            list( ( entry == "/" ? "/" : entry + "/" ) + dir->d_name, dir->d_name, indent );
+        }
+        closedir( d );
+    }
+}
+
+int main()
+{
+    char from[ 256 ];
+    getcwd( from, sizeof( from ) );
+    list( from, from, "" );
+    assert( paths.size() == 4 );
+    assert( paths.count( "/" ) );
+    assert( paths.count( "/foo" ) );
+    assert( paths.count( "/bar" ) );
+    assert( paths.count( "/bar/baz" ) );
+}
