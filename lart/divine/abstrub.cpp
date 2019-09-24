271eed50a8397e4b143fdde227723dd48fffd190
Adam Matoušek <adamat@mail.muni.cz>
2019-09-24 07:11:55 +0000

    lart: Make abstract stubbing pass configurable.

--- lart/divine/abstrub.cpp
+++ lart/divine/abstrub.cpp
@@ -16,28 +16,46 @@ DIVINE_UNRELAX_WARNINGS
 #include <lart/divine/stubs.h>
 #include <bricks/brick-bitlevel>
 
+#include <sstream>
+
 namespace lart::divine {
 
 struct AbstractStubs
 {
+    std::string _domain;
+
+    AbstractStubs( std::string opt )
+    {
+        if ( ! opt.empty() ) {
+            std::stringstream ss( opt );
+            std::getline( ss, _domain );
+        } else {
+            _domain = "unit";
+        }
+    }
+
     static PassMeta meta()
     {
-        return passMeta< AbstractStubs >( "AbstractStubs", "Stub empty declarations with stars." );
+        return passMetaO< AbstractStubs >(
+                "AbstractStubs",
+                "options: [<domain>]\n\n"
+                "Stub empty declarations with abstract values." );
     }
 
     void run( llvm::Module &m )
     {
         auto &ctx = m.getContext();
         llvm::Function *aValCtor[4];
+        std::string aValStem = "__" + _domain + "_val_i";
 
         int i = 4;
         while ( i --> 0 )
         {
             int width = 8 << i;
-            aValCtor[ i ] = m.getFunction( "__unit_val_i" + std::to_string( width ) );
+            aValCtor[ i ] = m.getFunction( aValStem + std::to_string( width ) );
             if ( !aValCtor[ i ] )
             aValCtor[i] = llvm::cast< llvm::Function >(
-                    m.getOrInsertFunction( "__unit_val_i" + std::to_string( width ),
+                    m.getOrInsertFunction( aValStem + std::to_string( width ),
                     llvm::IntegerType::get( ctx, width ) ) );
             ASSERT( aValCtor[ i ] );
             lart::abstract::meta::set( aValCtor[ i ], lart::abstract::meta::tag::abstract_return );
@@ -47,7 +65,7 @@ struct AbstractStubs
         long count = 0;
         for ( auto &fn : m ) {
             if ( !fn.isDeclaration() || fn.isIntrinsic() || fn.getName().startswith( "__vm_" ) ||
-                    fn.getName().startswith( "__unit_val" ) )
+                    fn.getName().startswith( "__" + _domain ) )
                 continue;
             auto retType = fn.getReturnType();
             if ( !retType->isIntegerTy() )
@@ -63,13 +81,19 @@ struct AbstractStubs
         }
         lart::Driver::assertValid( &m );
         if ( count )
-            std::cerr << "INFO: abstrubbed " << count << " declarations" << std::endl;
+            std::cerr << "INFO: abstrubbed " << count << " declarations using " << _domain << std::endl;
     }
 };
 
 PassMeta abstractStubsPass() {
-    return compositePassMeta< DropEmptyDecls, AbstractStubs >(
-            "abstrubs", "Remove unused function declarations and asterify the rest." );
+    return passMetaC< DropEmptyDecls, AbstractStubs >( "abstrubs",
+            "options: [<domain>]\n\n"
+            "Remove unused function declarations and abstract the rest.",
+
+            []( PassVector &mgr, std::string opt ) {
+                DropEmptyDecls::meta().create( mgr, "" );
+                AbstractStubs::meta().create( mgr, opt );
+            } );
 }
 
 } /* lart::divine */
