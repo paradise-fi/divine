7f6d911799b4e7a32d4eee0b2c60a06bedd334ca
Zuzana Baranova <xbaranov@fi.muni.cz>
2019-10-02 13:39:34 +0000

    tools: Add a trigger and corresponding function to accumulate *_log tables.

--- tools/bench-postgres.sql
+++ tools/bench-postgres.sql
@@ -4,3 +4,48 @@ create function last_insert_rowid()
 begin
     return lastval();
 end; $$ language plpgsql;
+
+
+create or replace function accum_log_tables()
+    returns trigger as
+$$
+begin
+    insert into search_stats
+    select slog.execution, stamp_start, stamp, max_states, queued
+    from
+    (select min(stamp) as stamp_start, execution, max(states) as max_states
+     from search_log group by(execution)) smax
+    join search_log slog
+    on
+      slog.execution = smax.execution
+      and smax.execution not in (select execution from search_stats)
+      and states = max_states
+    join job
+    on job.execution = smax.execution
+    where job.status = 'D';
+
+    insert into pool_stats
+    select plog.execution, stamp_start, stamp, pool, items, max_used, held
+    from
+    (select min(stamp) as stamp_start, execution, pool, max(used) as max_used
+     from pool_log group by(execution,pool)) pmax
+    join pool_log plog
+    on
+      plog.execution = pmax.execution
+      and (pmax.execution, pmax.pool) not in (select execution, pool from pool_stats)
+      and used = max_used
+    join job
+    on job.execution = pmax.execution
+    where job.status = 'D';
+
+    delete from search_log where stamp < NOW() - interval '2 weeks';
+    delete from  pool_log  where stamp < NOW() - interval '2 weeks';
+
+    return new;
+end;
+$$ language plpgsql;
+
+
+create trigger job_done
+after update on job
+execute function accum_log_tables();
