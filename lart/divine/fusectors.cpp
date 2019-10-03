2d18161a8e3691da53b6257e747d0d72a0d2bd2f
xkorenc1 <xkorenc1@fi.muni.cz>
2019-10-03 17:02:16 +0000

    lart: Fix crash when llvm.global_dtors contains bitcast.

--- lart/divine/fusectors.cpp
+++ lart/divine/fusectors.cpp
@@ -9,7 +9,10 @@ DIVINE_RELAX_WARNINGS
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Constants.h"
 DIVINE_UNRELAX_WARNINGS
+
+#include <bricks/brick-assert>
 
 #include <lart/support/pass.h>
 #include <lart/support/meta.h>
@@ -25,9 +28,26 @@ struct CtorDtorEntry
     // We don't need last entry here
     // void *ignored;
 
-  CtorDtorEntry( llvm::Constant *entry ) :
-      prio( entry->getAggregateElement( 0U )->getUniqueInteger().getZExtValue() ),
-      func( llvm::dyn_cast< llvm::Function >( entry->getAggregateElement( 1U ) ) ) {}
+    CtorDtorEntry( llvm::Constant *entry )
+        : prio( entry->getAggregateElement( 0U )->getUniqueInteger().getZExtValue() )
+    {
+
+        auto entry_f = entry->getAggregateElement( 1U );
+        if ( auto casted_f = llvm::dyn_cast< llvm::Function >( entry_f ) )
+            func = casted_f;
+        else
+        {
+            // In case constructor is of diferent type than void (*)() compiler will
+            // generate bitcast
+            auto const_expr = llvm::dyn_cast< llvm::ConstantExpr >( entry_f );
+            if ( const_expr && const_expr->isCast() )
+                func = llvm::dyn_cast< llvm::Function >( const_expr->getOperand( 0U ) );
+        }
+
+        // func must be set to some value since someone will most likely try
+        // to create call to it later
+        ASSERT( func );
+    }
 };
 
 bool compare_ctor( const CtorDtorEntry &lhs, const CtorDtorEntry &rhs )
@@ -41,8 +61,11 @@ bool compare_dtor( const CtorDtorEntry &lhs, const CtorDtorEntry &rhs )
 }
 
 // Get all entries from llvm.global_ctors
-std::vector< CtorDtorEntry > get_ctors( llvm::GlobalVariable *arr )
+std::vector< CtorDtorEntry > get_entries( llvm::GlobalVariable *arr )
 {
+    if (!arr)
+        return {};
+
     std::vector< CtorDtorEntry > result;
     for ( auto i = 0U; auto entry = arr->getInitializer()->getAggregateElement( i ); ++i )
       result.emplace_back(entry);
@@ -66,31 +89,29 @@ void create_explicit_calls(
     llvm::IRBuilder<> irb{ entry };
 
     for ( auto& entry : entries )
+    {
         irb.CreateCall( entry.func );
-
+    }
     irb.CreateRetVoid();
+}
+
+template< typename Comparator >
+void fuse_array( llvm::Module &module, const std::string &arr_name,
+                 Comparator &&compare, const std::string &func_name )
+{
+    auto entry_arr = module.getGlobalVariable( arr_name );
+    auto entries = get_entries( entry_arr );
+
+    std::sort( entries.begin(), entries.end(), compare );
+    create_explicit_calls( module, entries, func_name );
 }
 
 // Replaces iteration over llvm.global_ctors and indirect call to each entry with
 // functions that explicitly call all entries in proper order
-void fuse_ctors( llvm::Module &module )
+void fuse( llvm::Module &module )
 {
-    auto ctor_arr = module.getGlobalVariable( "llvm.global_ctors" );
-
-    auto entries = get_ctors( ctor_arr );
-
-    // This can be done smarter but since array is small it does not really matter
-    std::sort( entries.begin(), entries.end(), compare_ctor ),
-    create_explicit_calls(
-        module,
-        entries,
-        "divine_global_ctors_init" );
-
-    std::sort( entries.begin(), entries.end(), compare_dtor ),
-    create_explicit_calls(
-        module,
-        entries,
-        "divine_global_dtors_fini" );
+    fuse_array( module, "llvm.global_ctors", compare_ctor, "divine_global_ctors");
+    fuse_array( module, "llvm.global_dtors", compare_dtor, "divine_global_dtors");
 }
 
 struct FuseCtors
@@ -98,10 +119,19 @@ struct FuseCtors
 
     void run( llvm::Module &m )
     {
-      fuse_ctors( m );
+        fuse( m );
     }
 
 };
+
+/* Inserts two new functions into the module:
+ * void divine_global_ctors()
+ * void divine_global_dtors()
+ *
+ * These two functions directly call all global constructors (or destructors)
+ * in correct order. This is done in order to get rid of indirect calls that are
+ * typically present in a function implementing invocation of global ctors/dtors.
+ */
 
 PassMeta fuseCtorsPass()
 {
