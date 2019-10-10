aa2e82e607a81767392008bd1d1979b956e8b27e
Henrich Lauko <xlauko@mail.muni.cz>
2019-10-10 07:51:52 +0000

    rst: Generate abstract store lifter templates.

--- dios/rst/lart.cpp
+++ dios/rst/lart.cpp
@@ -44,3 +44,56 @@ extern "C"
         return peek_object< void * >( addr );
     }
 }
+
+/* lifter templates */
+
+using Ptr = void *;
+
+using store_op = void ( Ptr /* val */, Ptr /* ptr */ );
+
+_LART_INLINE
+uint8_t domain( Ptr addr ) noexcept { return *static_cast< uint8_t * >( addr ); }
+
+template< typename Op >
+auto get_operation( uint8_t domain, uint32_t op_index ) noexcept
+{
+    return reinterpret_cast< Op * >( __lart_get_domain_operation( domain, op_index ) );
+}
+
+template< typename T >
+struct Argument { bool tainted; T concrete; void * abstract; };
+
+template< typename Value >
+_LART_INLINE
+void __lart_store_lifter_impl( Argument< Value > value, Argument< Ptr > addr, size_t op )
+{
+    if ( !addr.tainted ) { // therefore value is abstract
+        __lart_freeze( value.abstract, addr.concrete );
+        return;
+    }
+
+    auto store = get_operation< store_op >( domain( addr.abstract ), op );
+    auto val = value.tainted ? value.abstract : Constant::lift( value.concrete );
+
+    store( val, addr.abstract );
+}
+
+#define LART_STORE_LIFTER( name, T ) \
+    __invisible \
+    void __lart_store_lifter_ ##name( bool taint_value, T value, void * abstract_value \
+                                   , bool taint_addr, void * addr, void * abstract_addr \
+                                   , size_t op ) \
+    { \
+        __lart_store_lifter_impl< T >( { taint_value, value, abstract_value } \
+                                     , { taint_addr, addr, abstract_addr } \
+                                     , op ); \
+    }
+
+extern "C" {
+
+    LART_STORE_LIFTER(  i8, uint8_t )
+    LART_STORE_LIFTER( i16, uint16_t )
+    LART_STORE_LIFTER( i32, uint32_t )
+    LART_STORE_LIFTER( i64, uint64_t )
+
+}
