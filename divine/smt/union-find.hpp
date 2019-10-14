23d24d12cc73fd05a928f15e66b9e646afef3b7f
Zuzana Baranova <xbaranov@fi.muni.cz>
2019-10-14 12:25:16 +0000

    smt: Keep a map< T,T > for group membership in union-find (drop Nodes).

--- divine/smt/union-find.hpp
+++ divine/smt/union-find.hpp
@@ -1,76 +1,69 @@
 #include <map>
 #include <iostream>
+#include <optional>
 
 namespace divine::smt
 {
     template < typename T >
     struct union_find
     {
-        struct Node
+        using MaybeT = std::optional< T >;
+
+        std::map< T, T > grouping;
+
+        T make_set( T x )
         {
-            T val;
-            Node* parent;
-
-            Node( T v ) : val( v ), parent( this ){}
-        };
-
-        std::map< T, Node* > nodes; // variables
-
-        Node* make_set( T x )
-        {
-            auto it = nodes.find( x );
-            if ( it != nodes.end() )
+            auto it = grouping.find( x );
+            if ( it != grouping.end() )
                 return it->second;
-            Node *n = new Node( x );
-            nodes.insert( { x, n } );
-            return n;
+            grouping.insert( { x, x } );
+            return x;
         }
 
-        Node* find( Node* x )
+        MaybeT find( T x )
         {
-            if ( !x )
-                return nullptr;
-            if ( x->parent != x )
-                x->parent = find( x->parent );
-
-            return x->parent;
+            auto it = grouping.find( x );
+            if ( it != grouping.end() )
+            {
+                if ( it->second != x )
+                    it->second = *find( it->second );
+                return it->second;
+            }
+            return std::nullopt;
         }
 
-        Node* union_( Node* x, Node* y )
+        MaybeT union_( MaybeT x, MaybeT y )
         {
             if ( !x ) return y;
             if ( !y ) return x;
 
-            auto x_root = find( x );
-            auto y_root = find( y );
+            auto x_root = find( *x );
+            auto y_root = find( *y );
+
+            if ( !x_root || !y_root )
+                throw std::runtime_error( "representant not found in union" );
 
             if ( x_root == y_root )
                 return y_root;
 
-            x_root->parent = y_root;
+            grouping.find( *x_root )->second = *y_root;
             return y_root;
         }
 
-        ~union_find()
-        {
-            for( auto n : nodes )
-                delete n.second;
-        }
-
-        std::set< std::set< T >> get_partitions()
+        std::set< std::set< T > > get_partitions()
         {
             std::map< T, std::set< T > > parts;
-            for( auto var : nodes )
+            for( auto var : grouping )
             {
-                auto it = parts.find( find( var.second )->val );
+                auto it = parts.find( *find( var.second ) );
                 if ( it != parts.end() )
                     it->second.insert( var.first );
                 else
-                    parts.insert( { find( var.second )->val,
+                    parts.insert( { *find( var.second ),
                                     std::set< T >( { var.first } ) } );
             }
 
-            std::set< std::set< T >> partitions;
+            std::set< std::set< T > > partitions;
             for( auto part : parts )
                 partitions.insert( part.second );
             return partitions;
@@ -87,19 +80,18 @@ namespace divine::t_uf
             using namespace divine::smt;
 
             union_find< int > uf;
-            // try with nullptr
-            union_find< int >::Node *even = uf.make_set( 0 ), *odd = uf.make_set( 1 );
-            for( int i : { 2,4,5,7,9,6,1,0,-2,-9 } )
+            int even = uf.make_set( 0 ), odd = uf.make_set( 1 );
+            for( int i : { 2, 4, 5, 7, 9, 6, 1, 0, -2, -9 } )
             {
                 if ( i % 2 == 0 )
                 {
-                    uf.union_( even, uf.make_set( i ));
-                    ASSERT_EQ( uf.find( uf.nodes.find( i )->second )->val % 2, 0 );
+                    uf.union_( even, uf.make_set( i ) );
+                    ASSERT_EQ( *uf.find( uf.grouping.find( i )->second ) % 2, 0 );
                 }
                 else
                 {
-                    uf.union_( odd, uf.make_set( i ));
-                    ASSERT_EQ( abs( uf.find( uf.nodes.find( i )->second )->val % 2 ), 1 );
+                    uf.union_( odd, uf.make_set( i ) );
+                    ASSERT_EQ( abs( *uf.find( uf.grouping.find( i )->second ) % 2 ), 1 );
                 }
             }
             auto parts = uf.get_partitions();
@@ -107,9 +99,9 @@ namespace divine::t_uf
 
             for( auto& part : parts )
                 if( part.find( 0 ) != part.end() )
-                    assert( part == std::set< int >( { 2,4,6,0,-2 } ) );
+                    assert( part == std::set< int >( { 2, 4, 6, 0, -2 } ) );
                 else
-                    assert( part == std::set< int >( { 5,7,9,1,-9 } ) );
+                    assert( part == std::set< int >( { 5, 7, 9, 1, -9 } ) );
         }
     };
 }
