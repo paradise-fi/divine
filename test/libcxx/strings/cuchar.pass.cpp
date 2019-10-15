7d609dcfbbfd4053dc86b0bc3cd5d321b2734288
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2019-10-15 09:47:16 +0000

    test: Port libc++ v9.0 tests for strings.

---
+++ test/libcxx/strings/cuchar.pass.cpp
@@ -0,0 +1,24 @@
+/* TAGS: todo c++ fin */
+/* CC_OPTS: -std=c++2a */
+/* VERIFY_OPTS: -o nofail:malloc */
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// XFAIL: *
+
+// <cuchar>
+
+#include <cuchar>
+
+#include "test_macros.h"
+
+int main(int, char**)
+{
+
+  return 0;
+}
