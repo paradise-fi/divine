0b1ee0b66487ed057d92bf8cd25f93effdd77301
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2019-10-15 09:46:23 +0000

    test: Port libc++ v9.0 tests for algorithms.

---
+++ test/libcxx/algorithms/version.pass.cpp
@@ -0,0 +1,26 @@
+/* TAGS: c++ fin */
+/* CC_OPTS: -std=c++2a */
+/* VERIFY_OPTS: -o nofail:malloc */
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <algorithm>
+
+#include <algorithm>
+
+#include "test_macros.h"
+
+#ifndef _LIBCPP_VERSION
+#error _LIBCPP_VERSION not defined
+#endif
+
+int main(int, char**)
+{
+
+  return 0;
+}
