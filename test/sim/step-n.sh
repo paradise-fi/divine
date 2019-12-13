1a162161ec64d4f298ca14627b6e062eb887ba65
Petr Rockai <me@mornfall.net>
2019-12-13 22:23:47 +0000

    test: Fix one more sim test that misbehaves if regex chars appear in $SRC.

--- test/sim/step-n.sh
+++ test/sim/step-n.sh
@@ -1,28 +1,28 @@
 . lib/testcase
 
-SRC=$TESTS/lang-c/ptr-arith.c
+cp $TESTS/lang-c/ptr-arith.c .
 
-sim $SRC <<EOF
+sim ptr-arith.c <<EOF
 + ^# executing __boot
 > start
 + ^# executing main
 > step --over --count 2
-+ ^# executing main at $SRC:9
++ ^# executing main at ptr-arith.c:9
 EOF
 
-sim $SRC <<EOF
+sim ptr-arith.c <<EOF
 + ^# executing __boot
 > start
 + ^# executing main
 > step --count 2
-+ ^# executing main at $SRC:9
++ ^# executing main at ptr-arith.c:9
 EOF
 
-sim $SRC <<EOF
+sim ptr-arith.c <<EOF
 + ^# executing __boot
 > setup --debug-everything
 > start
 + ^# executing main
 > step --over --count 2
-+ ^# executing main at $SRC:9
++ ^# executing main at ptr-arith.c:9
 EOF
