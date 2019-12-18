d402a59f3d92bdddb0f59a142b24c79c6b70eae9
Vladimir Still <xstill@fi.muni.cz>
2019-12-18 13:41:16 +0000

    test: Add a getline test.

---
+++ test/libc/getline-null.c
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char* line = NULL;
+    size_t len = 0;
+    errno = 0;
+    int r = getline( &line, &len, stdin );
+    assert( r == 0 || r == -1 );
+    assert( r != -1 || (errno == 0 && "EOF") || (errno == ENOMEM) );
+    free( line );
+}
+
