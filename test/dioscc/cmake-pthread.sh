fef308103ddc207c0d7dd298ff7ac11c2a80ea5a
Petr Rockai <me@mornfall.net>
2020-01-11 20:40:34 +0000

    test: Fail more verbosely in cmake-pthread.sh.

--- test/dioscc/cmake-pthread.sh
+++ test/dioscc/cmake-pthread.sh
@@ -5,4 +5,8 @@ cmake_minimum_required( VERSION 3.2 )
 find_package( Threads REQUIRED )
 EOF
 
-cmake -DCMAKE_C_COMPILER=dioscc -DCMAKE_CXX_COMPILER=dioscc
+if ! cmake -DCMAKE_C_COMPILER=dioscc -DCMAKE_CXX_COMPILER=dioscc .; then
+    cat CMakeFiles/CMakeOutput.log
+    cat CMakeFiles/CMakeError.log
+    exit 1
+fi
