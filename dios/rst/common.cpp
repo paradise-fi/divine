5c179030a6329828d5d1f5b43a70152b76c6eafe
Petr Rockai <me@mornfall.net>
2020-01-14 12:14:42 +0000

    rst: Use the updated __vm_poke interface to set up the __tainted value.

--- dios/rst/common.cpp
+++ dios/rst/common.cpp
@@ -5,6 +5,8 @@ uint64_t __tainted = 0;
 // TODO call from abstraction init
 __attribute__((constructor)) void __tainted_init()
 {
-    __vm_poke( &__tainted, _VM_ML_Taints, 0xF );
+    __vm_pointer_t ptr = __vm_pointer_split( &__tainted );
+    __vm_poke( _VM_ML_Taints, ptr.obj, ptr.off, 4, 0xF );
+    __vm_poke( _VM_ML_Taints, ptr.obj, ptr.off + 4, 4, 0xF );
 }
 
