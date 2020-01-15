89920d494cfa3d6921285dec557cda97fb4fc009
Petr Rockai <me@mornfall.net>
2020-01-15 22:35:34 +0000

    rst: Rewrite the unit domain on top of the new tagged_array base class.

--- dios/include/rst/unit.hpp
+++ dios/include/rst/unit.hpp
@@ -1,145 +1,135 @@
-// -*- C++ -*- (c) 2019 Henrich Lauko <xlauko@mail.muni.cz>
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+
+/*
+ * (c) 2020 Petr Ročkai <code@fixp.eu>
+ * (c) 2019 Henrich Lauko <xlauko@mail.muni.cz>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #pragma once
 
 #include <rst/tristate.hpp>
 #include <rst/base.hpp>
 
-namespace __dios::rst::abstract {
+namespace __dios::rst::abstract
+{
+    struct unit;
+    // extern unit common_unit;
 
-    struct unit_t;
-
-    extern unit_t unit_base_id;
-
-    /* unit_t/Star domain contains a single value, everything is abstracted to a Star. */
-    struct unit_t : tagged_abstract_domain_t
+    struct unit : tagged_array<>, domain_mixin< unit >
     {
-        using unit_value_t = unit_t *;
+        __noinline unit() {}
 
-        _LART_INLINE
-        static unit_value_t unit_value() noexcept { return &unit_base_id; }
+        template< typename type > static unit lift( type ) { return {}; }
+        template< typename type > static unit lift_any( Abstracted< type > ) { return {}; }
 
-        #define __op( name, ... ) \
-            _LART_INTERFACE static unit_value_t name(__VA_ARGS__) noexcept { \
-                return unit_value(); \
-            }
+        __lartop static unit lift_any() noexcept { return {}; }
+        __lartop static unit lift_any_ptr() noexcept { return {}; }
 
-        #define __un( name ) __op( name, unit_value_t )
+        __lartop static unit op_thaw( unit, uint8_t ) { return {}; }
+        __lartop static unit op_load( unit, uint8_t ) { return {}; }
+        __lartop static unit op_store( unit, unit, uint8_t ) { return {}; }
 
-        #define __bin( name ) __op( name, unit_value_t, unit_value_t )
+        __lartop static unit op_insertvalue( unit, unit, uint64_t ) { return {}; }
+        __lartop static unit op_extractvalue( unit, uint64_t ) { return {}; }
+        __lartop static unit op_gep( size_t, unit, unit ) { return {}; }
+        __lartop static unit assume( unit, unit, bool ) noexcept { return {}; }
 
-        #define __cast( name ) __op( name, unit_value_t, bitwidth_t )
-
-        _LART_INTERFACE
-        static unit_t lift_any() noexcept { return unit_t(); }
-
-        // TODO lift bool
-        /* abstraction operations */
-        __op( lift_one_i8, uint8_t )
-        __op( lift_one_i16, uint16_t )
-        __op( lift_one_i32, uint32_t )
-        __op( lift_one_i64, uint64_t )
-
-        __op( lift_one_float, float )
-        __op( lift_one_double, double )
-
-        template< typename T >
-        __op( lift_any, Abstracted< T > )
-
-        __op( lift_any_ptr )
-        __op( lift_one_ptr, void * ptr )
-
-        __op( lift_any_aggr, unsigned size )
-        __op( lift_one_aggr, void * addr, unsigned size )
-
-        __op( op_thaw, unit_value_t addr, uint8_t bw )
-
-        __op( op_load, unit_value_t addr, uint8_t bw )
-
-        _LART_INTERFACE
-        static void op_store( unit_value_t val, unit_value_t addri, bitwidth_t bw ) noexcept { }
-
-        __op( op_insertvalue, unit_value_t agg, unit_value_t val, uint64_t off )
-        __op( op_extractvalue, unit_value_t agg, uint64_t off )
-
-        __op( op_gep, size_t bw, unit_value_t addr, unit_value_t off )
-
-        _LART_INTERFACE
-        static tristate_t to_tristate( unit_value_t ) noexcept
-        {
-            return { tristate_t::Unknown };
-        }
-
-        __op( assume, unit_value_t, unit_value_t, bool );
+        __lartop static tristate_t to_tristate( unit ) noexcept { return { tristate_t::Unknown }; }
 
         /* arithmetic operations */
-        __bin( op_add )
-        __bin( op_fadd )
-        __bin( op_sub )
-        __bin( op_fsub )
-        __bin( op_mul )
-        __bin( op_fmul )
-        __bin( op_udiv )
-        __bin( op_sdiv )
-        __bin( op_fdiv )
-        __bin( op_urem )
-        __bin( op_srem )
-        __bin( op_frem )
+        __lartop static unit op_add ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fadd( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_sub ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fsub( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_mul ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fmul( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_udiv( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_sdiv( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fdiv( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_urem( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_srem( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_frem( unit, unit ) noexcept { return {}; }
 
-        /* logic operations */
-        __un( op_fneg )
+        __lartop static unit op_fneg( unit ) noexcept { return {}; }
 
         /* bitwise operations */
-        __bin( op_shl )
-        __bin( op_lshr )
-        __bin( op_ashr )
-        __bin( op_and )
-        __bin( op_or )
-        __bin( op_xor )
+        __lartop static unit op_shl ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_lshr( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_ashr( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_and ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_or  ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_xor ( unit, unit ) noexcept { return {}; }
+
+        using bw = uint8_t;
 
         /* comparison operations */
-        __bin( op_ffalse );
-        __bin( op_foeq );
-        __bin( op_fogt );
-        __bin( op_foge );
-        __bin( op_folt );
-        __bin( op_fole );
-        __bin( op_fone );
-        __bin( op_ford );
-        __bin( op_funo );
-        __bin( op_fueq );
-        __bin( op_fugt );
-        __bin( op_fuge );
-        __bin( op_fult );
-        __bin( op_fule );
-        __bin( op_fune );
-        __bin( op_ftrue );
-        __bin( op_eq );
-        __bin( op_ne );
-        __bin( op_ugt );
-        __bin( op_uge );
-        __bin( op_ult );
-        __bin( op_ule );
-        __bin( op_sgt );
-        __bin( op_sge );
-        __bin( op_slt );
-        __bin( op_sle );
+        __lartop static unit op_foeq( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fogt( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_foge( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_folt( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fole( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fone( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_ford( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_funo( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fueq( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fugt( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fuge( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fult( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fule( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_fune( unit, unit ) noexcept { return {}; }
 
-        /* cast operations */
-        __cast( op_fpext );
-        __cast( op_fptosi );
-        __cast( op_fptoui );
-        __cast( op_fptrunc );
-        __cast( op_inttoptr );
-        __cast( op_ptrtoint );
-        __cast( op_sext );
-        __cast( op_sitofp );
-        __cast( op_trunc );
-        __cast( op_uitofp );
-        __cast( op_zext );
+        __lartop static unit op_eq ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_ne ( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_ugt( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_uge( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_ult( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_ule( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_sgt( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_sge( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_slt( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_sle( unit, unit ) noexcept { return {}; }
 
-        #undef __op
-        #undef __un
-        #undef __bin
+        __lartop static unit op_ffalse( unit, unit ) noexcept { return {}; }
+        __lartop static unit op_ftrue( unit, unit ) noexcept { return {}; }
+
+
+        __lartop static unit op_fpext( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_fptosi( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_fptoui( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_fptrunc( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_inttoptr( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_ptrtoint( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_sext( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_sitofp( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_trunc( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_uitofp( unit, bw ) noexcept { return {}; }
+        __lartop static unit op_zext( unit, bw ) noexcept { return {}; }
+
+        /* TODO remove, only here to satisfy the vtable generator */
+        __lartop static unit lift_one_i1( i1 v )   noexcept { return lift( v ); }
+        __lartop static unit lift_one_i8( i8 v )   noexcept { return lift( v ); }
+        __lartop static unit lift_one_i16( i16 v ) noexcept { return lift( v ); }
+        __lartop static unit lift_one_i32( i32 v ) noexcept { return lift( v ); }
+        __lartop static unit lift_one_i64( i64 v ) noexcept { return lift( v ); }
+        __lartop static unit lift_one_f32( f32 v ) noexcept { return lift( v ); }
+        __lartop static unit lift_one_f64( f64 v ) noexcept { return lift( v ); }
+
+        __lartop static unit lift_one_ptr( void *v ) noexcept
+        {
+            return lift( reinterpret_cast< uintptr_t >( v ) );
+        }
     };
 
 } // namespace __dios::rst::abstract
