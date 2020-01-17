3efdefd2ed1417231db7459735b3d2805d40c63c
Petr Rockai <me@mornfall.net>
2020-01-17 23:36:33 +0000

    rst: Remove C-style constructors for term values (__sym_val_*).

--- dios/rst/term.cpp
+++ dios/rst/term.cpp
@@ -1,42 +1,15 @@
 // -*- C++ -*- (c) 2019 Henrich Lauko <xlauko@mail.muni.cz>
-#include <rst/term.hpp>
+#include <dios/lava/term.hpp>
 #include <util/array.hpp>
 #include <util/map.hpp>
 
 #include <cassert>
 
-namespace __dios::rst::abstract
+namespace __lava
 {
     term_state_t * __term_state;
 
-    template< typename concrete_t >
-    _LART_INLINE concrete_t make_term() noexcept
     {
-        return make_abstract< concrete_t, term_t >();
-    }
-
-    template< typename concrete_t >
-    _LART_INLINE concrete_t make_term( concrete_t c ) noexcept
-    {
-        return make_abstract< concrete_t, term_t >( c );
-    }
-
-    extern "C" {
-        _LART_SCALAR uint8_t __sym_val_i8() { return make_term< uint8_t >(); }
-        _LART_SCALAR uint16_t __sym_val_i16() { return make_term< uint16_t >(); }
-        _LART_SCALAR uint32_t __sym_val_i32() { return make_term< uint32_t >(); }
-        _LART_SCALAR uint64_t __sym_val_i64() { return make_term< uint64_t >(); }
-        _LART_SCALAR float __sym_val_float32() { return make_term< float >(); }
-        _LART_SCALAR double __sym_val_float64() { return make_term< double >(); }
-
-        _LART_SCALAR uint8_t __sym_lift_i8( uint8_t c ) { return make_term< uint8_t >( c ); }
-        _LART_SCALAR uint16_t __sym_lift_i16( uint16_t c ) { return make_term< uint16_t >( c ); }
-        _LART_SCALAR uint32_t __sym_lift_i32( uint32_t c ) { return make_term< uint32_t >( c ); }
-        _LART_SCALAR uint64_t __sym_lift_i64( uint64_t c ) { return make_term< uint64_t >( c ); }
-        _LART_SCALAR float __sym_lift_float32( float c ) { return make_term< float >( c ); }
-        _LART_SCALAR double __sym_lift_float64( double c ) { return make_term< double >( c ); }
-    }
-
     auto update_rpns( smt::token::VarID id )
     {
         auto& dcmp = __term_state->decomp;
