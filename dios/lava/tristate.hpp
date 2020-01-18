a4c17cb616d4551af4152b005efa2557f50fcf42
Petr Rockai <me@mornfall.net>
2020-01-18 00:38:34 +0000

    lava: Update and clean up the tristate domain.

--- dios/lava/tristate.hpp
+++ dios/lava/tristate.hpp
@@ -6,34 +6,30 @@
 #include <cstdlib>
 #endif
 
-namespace __dios::rst::abstract {
+namespace __lava
+{
+    /* The tristate abstract domain has three values: true, false and maybe
+     * (unknown).  It is used for branching, to decide whether to take the then
+     * or else path, or whether to choose non-deterministically (in DIVINE this
+     * means split and take both paths).
+     *
+     * Every other abstract domain needs to define a `to_tristate()` method,
+     * which specifies what happens if a value of that abstract type is
+     * encountered in an if condition. */
 
-    /* The tristate_t abstract domain has three values: True, False and Maybe (Unknown).
-     * It is used for branching, to decide whether to take the then or else path,
-     * or whether to choose non-deterministically (in DIVINE this means split and
-     * take both paths).
-     * Every other abstract domain needs to define a `to_tristate()` method, which specifies
-     * what happens if a value of that abstract type is encountered in an if condition. */
-    struct tristate_t {
-        enum value_t { False = 0, True = 1, Unknown = 2 };
-
-        constexpr tristate_t( value_t val ) noexcept : value( val ) { }
+    struct tristate
+    {
+        enum value_t { no = 0, yes = 1, maybe = 2 } value;
+        constexpr tristate( value_t val = maybe ) noexcept : value( val ) { }
 
         bool lower() const noexcept;
-
-        static tristate_t lift_one( bool v ) noexcept
-        {
-            return v ? tristate_t::value_t::True : tristate_t::value_t::False;
-        }
-
-        static tristate_t lift_any() noexcept
-        {
-            return tristate_t::value_t::Unknown;
-        }
+        static tristate lift( bool v ) noexcept { return v ? yes : no; }
+        static tristate any() noexcept { return maybe; }
 
         inline operator bool() const noexcept
         {
-            if ( value == tristate_t::value_t::Unknown ) {
+            if ( value == maybe )
+            {
                 #ifdef __divine__
                     return __vm_choose( 2 );
                 #else
@@ -42,8 +38,6 @@ namespace __dios::rst::abstract {
             }
             return value;
         }
-
-        value_t value;
     };
 
 } // namespace abstract
