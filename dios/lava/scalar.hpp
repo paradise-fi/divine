624dbcf264a0756f0142b7b9c65c06da74f7590f
Petr Rockai <me@mornfall.net>
2020-01-25 18:08:19 +0000

    lava: Update the header and includes in scalar.hpp.

--- dios/lava/scalar.hpp
+++ dios/lava/scalar.hpp
@@ -1,11 +1,27 @@
-// -*- C++ -*- (c) 2019 Henrich Lauko <xlauko@mail.muni.cz>
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+
+/*
+ * (c) 2019 Henrich Lauko <xlauko@mail.muni.cz>
+ * (c) 2020 Petr Ročkai <code@fixp.eu>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #pragma once
+#include "tristate.hpp"
+#include <sys/trace.h>
 
-#include <rst/tristate.hpp>
-#include <rst/constant.hpp>
-#include <rst/base.hpp>
-
-namespace __dios::rst::abstract
+namespace __lava
 {
     // c++ wrapper for scalar domains
 
