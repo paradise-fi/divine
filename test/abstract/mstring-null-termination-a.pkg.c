277e8270e623dcc51e6fd6d3bf123be10fee33d6
Petr Rockai <me@mornfall.net>
2020-01-31 14:39:16 +0000

    test: Cut mstring-null-termination-a in half (again).

--- test/abstract/mstring-null-termination-a.pkg.c
+++ test/abstract/mstring-null-termination-a.pkg.c
@@ -12,25 +12,25 @@
 
 int main()
 {
-    char buffa[8] = {};
+    char buffa[4] = {};
     char * a = __lamp_lift_arr( buffa, sizeof( buffa ) );
-    char buffb[8] = {};
+    char buffb[4] = {};
     char * b = __lamp_lift_arr( buffb, sizeof( buffb ) );
-    char buffc[15] = {};
+    char buffc[7] = {};
     char * c = __lamp_lift_arr( buffc, sizeof( buffc ) );
 
-    char buff1[8] = "0123456";
+    char buff1[4] = "012";
     char * src1 = __lamp_lift_arr( buff1, sizeof( buff1 ) );
     strcpy( a, src1 );
 
-    char buff2[8] = "0123456";
+    char buff2[4] = "012";
     char * src2 = __lamp_lift_arr( buff2, sizeof( buff2 ) );
     strcpy( b, src2 );
 
     strcpy( c, a );
     strcat( c, b );
 
-    char expected[15] = "01234560123456";
+    char expected[7] = "012012";
     char * exp = __lamp_lift_arr( expected, sizeof( expected ) );
 
     assert( strcmp( c, exp ) == 0 );
