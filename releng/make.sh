cd7d5064f65cddbd477c31878302d1ffb351e9b5
Petr Rockai <me@mornfall.net>
2020-02-01 00:20:52 +0000

    releng: Do not bump the version unless we are actually making a release.

--- releng/make.sh
+++ releng/make.sh
@@ -3,13 +3,13 @@
 set -xe
 test -f divine/ui/version.cpp
 
-patchlevel=$(cat releng/patchlevel)
-patchlevel=$(($patchlevel + 1))
-echo $patchlevel > releng/patchlevel
-version=$(cat releng/version).$patchlevel
 bash ./releng/update-version-sha.sh sha1sum . /dev/null 0 > releng/checksum
 
 if darcs wh releng/checksum > /dev/null; then
+    patchlevel=$(cat releng/patchlevel)
+    patchlevel=$(($patchlevel + 1))
+    echo $patchlevel > releng/patchlevel
+    version=$(cat releng/version).$patchlevel
     darcs rec -a --author "Release Bot <divine@fi.muni.cz>" \
               -m "releng: Update for $version" releng
     darcs tag -m $version
